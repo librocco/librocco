d126283bb0f74dc0d84ea68240ba577d4c288d68
Silvio Tomatis <silviot@gmail.com>
2022-05-09 12:26:22 +0200

    Actually use a local PouchDB synced to a remote CouchDB

--- pkg/web/src/Search.tsx
+++ pkg/web/src/Search.tsx
@@ -3,52 +3,79 @@ import PouchDB from 'pouchdb';
 import { Search as JSSearch } from 'js-search';
 
 
-const pouchDb = new PouchDB('http://admin:admin@localhost:5984/books');
-window.pouchDb = pouchDb;
-
-const indexBooks = async function() {
-  // Loop over all elements in the pouchdb database
-  const docs = await pouchDb.allDocs({include_docs: true});
-  const search = new JSSearch("isbn");
-  search.addIndex('title');
-  search.addIndex('author');
-  search.addIndex('publisher');
-  // Loop 20 times
-  console.log("start")
-  await window.bookIndex.addDocuments(docs.rows.map(row => row.doc));
-  console.log("finish")
-}
-
+const localDB = new PouchDB('books')
+const couchDb = new PouchDB('http://admin:admin@localhost:5984/books');
+localDB.replicate.to(couchDb);
+localDB.replicate.from(couchDb);
+window.couchDb = couchDb;
+window.localDB = localDB;
 window.bookIndex = new JSSearch("isbn");
 window.bookIndex.addIndex('title');
 window.bookIndex.addIndex('author');
 window.bookIndex.addIndex('publisher');
 
+
+const saveIndex = async function(index) {
+  var db;
+  var request = indexedDB.open("MyTestDatabase");
+  request.onerror = event => {
+    console.log("Why didn't you allow my web app to use IndexedDB?!");
+  };
+  request.onsuccess = event => {
+    db = event.target.result;
+  };  
+}
+
+
+const loadIndex = async function() {
+  return {};
+}
+
 const Search: React.FC = () => {
-  useEffect(() => {indexBooks(); return undefined}, []);
-  // Put the windows style newline into the variable winnewline
-  const winnewline = "\r\n";
-  // Put the unix style newline into the variable winnewline
-  const unixnewline = "\n";
-
-
   const [books, setBooks] = React.useState([]);
   const [total, setTotal] = React.useState(0);
+  const [found, setFound] = React.useState(0);
+  const [status, setStatus] = React.useState("Initializing");
+  
+  const indexBooks = async function() {
+    // Loop over all elements in the pouchdb database
+    setStatus("Retrieving documents from Pouchdb");
+    const docs = await localDB.allDocs({include_docs: true, limit: 1000});
+    const search = new JSSearch("isbn");
+    search.addIndex('title');
+    search.addIndex('author');
+    search.addIndex('publisher');
+    // Loop 20 times
+    console.log("start")
+    setStatus(`Creating index for ${docs.rows.length} books`);
+    await window.bookIndex.addDocuments(docs.rows.map(row => row.doc));
+    setTotal(window.bookIndex._documents.length);
+    setStatus(`Ready`);
+    // setStatus(`Ready to save to indexedDb`);
+    // var request = window.indexedDB.open("MyTestDatabase", 3);
+    // request.onsuccess = event => {
+    //   db = event.target.result;
+    // };
+    
+    console.log("finish")
+  }
+  useEffect(() => {indexBooks()}, []);
+  
 
   const manageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const result = bookIndex.search(e.target.value);
+    const result = bookIndex.search(e.target.value).filter(el => el.isbn);
     setBooks(result.slice(0, 10))
-    setTotal(result.length);
+    setFound(result.length);
   }
 
   return (
     <div className="foobar">
-      <h2 className="text-[2.5rem]">Search books</h2>
+      <h2 className="text-[2.5rem]">Search books - {status}</h2>
       <input className="border-2" onChange={(manageChange)} />
-      <h3>{total} books found</h3>
+      <h3>{found} books found ({total} in total)</h3>
       <ul>
         {books.map(book => (
-          <li key={book.isbn}> {book.title} </li>
+          <li key={book.isbn}>{book.isbn} - <b>{book.author}</b> - {book.title} </li>
         ))}
       </ul>
     </div>
