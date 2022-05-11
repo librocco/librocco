0653bbfb391387f732dd93226aada7e7a62be629
Fadwa <fadwa.mahmoud@gmail.com>
2022-05-11 11:31:00 +0200

    Use generator fn for chunk sized batching

--- pkg/scripts/csv-to-couchdb.ts
+++ pkg/scripts/csv-to-couchdb.ts
@@ -1,18 +1,23 @@
 #!/usr/bin/env -S deno run --allow-read --allow-net
 
-import { CouchClient } from "https://denopkg.com/keroxp/deno-couchdb/couch.ts";
 import Denomander from "https://deno.land/x/denomander@0.9.1/mod.ts";
-import { CommonCSVReaderOptions, readCSV } from "https://deno.land/x/csv/mod.ts";
+import { readCSV } from "https://deno.land/x/csv/mod.ts";
 import ProgressBar from "https://deno.land/x/progress@v1.2.4/mod.ts";
 import { BufReader } from "https://deno.land/std/io/buffer.ts";
 import { bulkDocs, createDatabase } from "../couchdb/couchdb.ts"
 
-const howManyLines = async function (fileName: string) {
+/**
+  * Counts lines in a csv file for progress bar calculations
+  * @param {string} filePath - csv file path 
+  * @return {Number} - Number of lines in file
+  */
+
+const howManyLines = async function (filePath: string) {
     let lineCount: number = 0;
     var file;
-    console.log({ fileName })
+    console.log({ filePath })
     try {
-        file = await Deno.open(fileName);
+        file = await Deno.open(filePath);
         const bufReader = new BufReader(file);
         while (await bufReader.readLine()) {
             lineCount++
@@ -24,24 +29,38 @@ const howManyLines = async function (fileName: string) {
 }
 
 //#region progress bar
+/**
+  * Renders progress bar
+  * @param {ProgressBar} progress - progress bar instance 
+  * @param {Number} progress - chunk size for incrementation
+  */
 let completed = 0;
-function downloading(progress: ProgressBar) {
+function downloading(progress: ProgressBar, chunkSize: number) {
     if (completed <= progress.total!) {
-        progress.render(completed++);
+        progress.render(completed += chunkSize);
     }
 }
 //#endregion progress bar
 
 
 //#region csv to couch
-
+/**
+  * Parses CSV file and saves docs into couchdb
+  * @param {string} filePath - csv file path 
+  * @param {string} columns - headers of doc columns 
+  * @param {string} couchdbName - name of couchdb database (default: books)
+  * @param {string} couchdbURL - url of couchdb database (default: http://admin:admin@localhost:5984)
+  * @param {string} columnSeparator - separator used for columns (default: ,)
+  * @param {ProgressBar} progress - progress bar instance
+  * @return {Iterator} - An iterator over the chunks
+  */
 
 async function CSVToCouch({ filePath, columns, couchdbName = "books",
-    couchdbURL = "http://admin:admin@localhost:5984", columnSeparator = ",", individual, progress }: {
+    couchdbURL = "http://admin:admin@localhost:5984", columnSeparator = ",", chunkSize = 1000, progress }: {
         filePath: string,
         columns: string, couchdbName: string,
         couchdbURL: string, columnSeparator: string,
-        individual: string
+        chunkSize: number,
         progress: ProgressBar
 
     }) {
@@ -52,8 +71,23 @@ async function CSVToCouch({ filePath, columns, couchdbName = "books",
     // csv file options
     const options = { columnSeparator: columnSeparator }
 
-    individual ? await CSVToDocs(filePath, columnsArr, couchdbURL, couchdbName, progress, options) :
-        await CSVToBulkDocs(filePath, columnsArr, couchdbURL, couchdbName, progress, options)
+
+    try {
+        await createDatabase(couchdbName, couchdbURL)
+        const f = await Deno.open(filePath);
+        console.log("Saving docs bulk...")
+
+        for await (const chunk of inChunks(readCSV(f, options), chunkSize, columnsArr)) {
+            await bulkDocs(chunk, couchdbName, couchdbURL)
+            downloading(progress, chunkSize);
+
+        }
+        f.close();
+
+    }
+    catch (error) {
+        console.error(error)
+    }
 
 
 }
@@ -63,6 +97,7 @@ async function CSVToCouch({ filePath, columns, couchdbName = "books",
 
 
 //#region program
+
 const program = new Denomander({
     app_name: "CSV to CouchDB",
 });
@@ -74,7 +109,7 @@ program.command("parse", "parses csv file into the db")
     .option("-n --name", "couch database name")
     .option("-s --separator", "column separator of csv file")
     .option("-u -couchdbURL", "url of couchdb, defaults to local couchdb")
-    .option("-i -individual", "insert docs into couchdb one by one, default is bulk")
+    .option("-k -chunkSize", "size of chunk the csv file will be divided into")
     .action(async () => {
         const title = 'parsing csv files:';
         const progress = new ProgressBar({
@@ -86,98 +121,44 @@ program.command("parse", "parses csv file into the db")
         await CSVToCouch({
             filePath: program.filePath, columns: program.columns,
             couchdbName: program.name, couchdbURL: program.couchdbURL,
-            columnSeparator: program.separator, individual: program.individual,
+            columnSeparator: program.separator, chunkSize: program.chunkSize,
             progress: progress,
 
         })
     })
     .parse(Deno.args)
 
-
-
 //#endregion program
 
 
-//#region bulk parse fn
-const CSVToBulkDocs = async (filePath: string, columnsArr: string[], couchdbURL: string, couchdbName: string,
-    progress: ProgressBar, options?: Partial<CommonCSVReaderOptions> | undefined) => {
+//#region chunk generator
 
-    try {
-        await createDatabase(couchdbName, couchdbURL)
-        const f = await Deno.open(filePath);
-        console.log("Saving docs bulk...")
+/**
+  * Iterate over a csv file returning chunks of a given size
+  * parsed into json docs
+  * @param {Iterator} csv file - The iterator to split
+  * @param {Number} chunkSize - The size of the chunks
+  * @return {Iterator} - An iterator over the chunks
+  */
+const inChunks = async function*<T>(sequence: AsyncIterable<AsyncIterable<string>>, chunkSize: number, columnsArr: string[]) {
 
-        let books = [];
-        for await (const row of readCSV(f, options)) {
+    let chunk = [];
+    for await (const element of sequence) {
+        let book: { [key: string]: string } = {};
+        let i = 0;
+        for await (const field of element) {
 
-            let book: { [key: string]: string } = {};
-            let i = 0;
-            for await (const cell of row) {
-
-                const fieldName = columnsArr[i];
-                book[fieldName] = cell;
-                i++;
-            }
-            downloading(progress);
-
-            books.push((book))
-
-            if (books.length === 1000) {
-                await bulkDocs(books, couchdbName, couchdbURL)
-                books = []
-
-            }
+            const fieldName = columnsArr[i];
+            book[fieldName] = field;
+            i++;
         }
-
-        f.close();
-
-    }
-    catch (error) {
-        console.error(error)
-    }
-}
-//#endregion bulk parse fn
-
-//#region individual doc parse fn
-const CSVToDocs = async (filePath: string, columnsArr: string[], couchdbURL: string, couchdbName: string, progress: ProgressBar, options?: Partial<CommonCSVReaderOptions> | undefined) => {
-
-    const couch = new CouchClient(couchdbURL);
-
-    // choose db to use
-    const db = couch.database(couchdbName)
-
-    // check if specified database exists
-    if (!(await couch.databaseExists(couchdbName))) {
-        // create new database
-        await couch.createDatabase(couchdbName);
-        console.log(`Created database ${couchdbName}`)
-    } else {
-        console.log(`Database ${couchdbName} already present`)
-    }
-    try {
-        const f = await Deno.open(filePath);
-
-        console.log("Saving docs one by one...")
-
-        for await (const row of readCSV(f, options)) {
-            let book: { [key: string]: string } = {};
-            let i = 0;
-            for await (const cell of row) {
-
-                const fieldName = columnsArr[i];
-                book[fieldName] = cell;
-                i++;
-            }
-            await db.insert(book)
-            downloading(progress);
-
+        chunk.push(book);
+        if (chunk.length === chunkSize) {
+            yield chunk;
+            chunk = [];
         }
-
-        f.close();
-
     }
-    catch (error) {
-        console.error(error)
-    }
+    yield chunk;
 }
-//#endregion individual doc parse fn
+
+//#endregion chunk generator
