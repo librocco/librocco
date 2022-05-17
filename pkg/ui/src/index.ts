a96ed610302561fcc172fa3ecf43a85125279a35
ikusteu <ivan.kust57@gmail.com>
2022-05-17 10:58:07 +0200

    Add jest config scaffold, add SVG mock for jest tests

--- pkg/ui/src/index.ts
+++ pkg/ui/src/index.ts
@@ -1,9 +1,12 @@
 import Button from "./Button";
 import SideNavButton from "./SideNavButton";
+import SVGTest from "./SVGTest";
 
+// Main CSS is imported here to signal the bundler the usage of Tailwind
+// in order to generate a PostCSS built css file next to the bundle
 import "./main.css";
 
-export { Button, SideNavButton };
+export { Button, SideNavButton, SVGTest };
 
 export * from "./Button";
 export * from "./SideNavButton";
