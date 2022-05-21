59a18dcb7770134e0c395740ee60f6d825c25d02
ikusteu <ivan.kust57@gmail.com>
2022-05-21 22:58:06 +0200

    Scaffold Button component

--- pkg/ui/src/components/Button/Button.tsx
+++ pkg/ui/src/components/Button/Button.tsx
@@ -1,32 +1,68 @@
 import React from "react";
 
-import Icon from "@/assets/TestIcon.svg";
-
-interface ButtonProps {
-  children: string;
-  className?: string;
-  onClick?: (e: React.SyntheticEvent) => void;
+export enum ButtonVariant {
+  Text = "text",
+  Circular = "circular",
 }
 
+export enum ButtonShape {
+  Square = "square",
+  Rounded = "rounded",
+}
+
+export enum ButtonColor {
+  Primary = "primary",
+  White = "white",
+  Secondary = "secondary",
+}
+
+export enum ButtonSize {
+  XS = "xs",
+  SM = "sm",
+  Base = "md",
+  LG = "lg",
+  XL = "xl",
+}
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  shape?: ButtonShape;
+  color?: ButtonColor;
+  /** @TODO This should be `SVGComponent` type alias */
+  StartIcon?: React.FC;
+  /** @TODO This should be `SVGComponent` type alias */
+  EndIcon?: React.FC;
+  as?: keyof JSX.IntrinsicElements;
+}
+
+/**
+ * A `React.FC` returning `null` used as icon fallback
+ */
+const IconFallback: React.FC = () => null;
+
 const Button: React.FC<ButtonProps> = ({
+  variant = ButtonVariant.Text,
+  size = ButtonSize.Base,
+  shape = ButtonShape.Square,
+  color = ButtonColor.Primary,
+  StartIcon = IconFallback,
+  EndIcon = IconFallback,
+  as = "button",
+  className: classes = "",
   children,
-  className = "",
-  onClick = () => {},
+  ...props
 }) => {
-  return (
-    <button
-      {...{ onClick }}
-      className={[
-        "px-3 py-2 bg-[rgba(0,0,255,0.3)] rounded flex items-center",
-        className,
-      ].join(" ")}
-    >
-      <div className="w-4 h-4 mr-2">
-        <Icon />
-      </div>
-      {children}
-    </button>
-  );
+  const className = [
+    "px-3 py-2 bg-[rgba(0,0,255,0.3)] rounded flex items-center",
+    classes,
+  ].join(" ");
+
+  return React.createElement(as, { ...props, className }, [
+    <StartIcon />,
+    children,
+    <EndIcon />,
+  ]);
 };
 
 export default Button;
