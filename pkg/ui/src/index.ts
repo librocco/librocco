bf75f5dca8b34787efe9ca56214c651b771e9a9a
ikusteu <ivan.kust57@gmail.com>
2022-05-23 22:24:29 +0200

    Remove general path alias from ui package, leave only the @assets alias

--- pkg/ui/src/index.ts
+++ pkg/ui/src/index.ts
@@ -1,6 +1,6 @@
-import Button from "@/components/Button";
-import SideNavButton from "@/components/SideNavButton";
-import SVGTest from "@/components/SVGTest";
+import Button from "./components/Button";
+import SideNavButton from "./components/SideNavButton";
+import SVGTest from "./components/SVGTest";
 
 // Main CSS is imported here to signal the bundler the usage of Tailwind
 // in order to generate a PostCSS built css file next to the bundle
