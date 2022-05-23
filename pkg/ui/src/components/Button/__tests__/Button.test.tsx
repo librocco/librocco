a72592e69e4f5e2b57d85effa50e7187952a0b4f
ikusteu <ivan.kust57@gmail.com>
2022-05-23 10:24:08 +0200

    Put the UI Button tests u 'Button/__tests__' folder, fix 'keys' not provided bug while rendering Button

--- pkg/ui/src/components/Button/__tests__/Button.test.tsx
+++ pkg/ui/src/components/Button/__tests__/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import Button from "./Button";
+import Button from "../Button";
 
 describe("An example unit test", () => {
   test("should fire 'onClick' method", () => {
@@ -9,5 +9,10 @@ describe("An example unit test", () => {
     render(<Button onClick={mockOnClick}>Button Text</Button>);
     screen.getByText("Button Text").click();
     expect(mockOnClick).toHaveBeenCalled();
+  });
+
+  test("if no input for 'as' provided, should fall back to button", () => {
+    render(<Button>Button Text</Button>);
+    screen.getByRole("button");
   });
 });
