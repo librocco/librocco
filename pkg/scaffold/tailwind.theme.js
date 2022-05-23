b24b79ea58daaab41a29b434157f8a857c4348f0
ikusteu <ivan.kust57@gmail.com>
2022-05-23 13:51:47 +0200

    Replace repeating JIT values with extended tailwind config

--- pkg/scaffold/tailwind.theme.js
+++ pkg/scaffold/tailwind.theme.js
@@ -8,11 +8,29 @@ const rem = (px) => `${px / 16}rem`;
 module.exports = {
   extend: {
     spacing: {
+      // Provide spacing for large elements
       sm: rem(640),
       md: rem(768),
       lg: rem(1024),
       xl: rem(1280),
       "2xl": rem(1536),
+
+      // Extend additional spacings used more than once
+      // the ones used only once are done with JIT
+      //
+      // NOTE:  For consistency, all spacing extensions should be keyed the same way tailwind does for defaults:
+      //
+      //        spacingKey = numPixels / 4
+      //
+      // This will prevent confusion and duplication
+      0.5: rem(2),
+      1.5: rem(6),
+      1.75: rem(7),
+      2.25: rem(9),
+      2.5: rem(10),
+      3.25: rem(13),
+      4.25: rem(17),
+      6.25: rem(25),
     },
   },
 };
