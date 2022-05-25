753e854f42fd5eeb7defce19c630b84637614ab2
ikusteu <ivan.kust57@gmail.com>
2022-05-25 14:14:43 +0200

    Switch to table driven tests for 'getItemsToRender' of NumberLinks component

--- pkg/ui/src/components/NumberLinks/__tests__/NumberLinks.test.tsx
+++ pkg/ui/src/components/NumberLinks/__tests__/NumberLinks.test.tsx
@@ -1,5 +1,24 @@
 import { getItemsToRender } from "../getItemsToRender";
 
+/**
+ * Runs table tests on the 'getItemsToRender' to test for all possible inputs for
+ * given combination of 'numItems' and 'maxItems'
+ * @param {Object} params The first input contains params constant over the entire run: `numItems` and `maxItems`
+ * @param {number} params.numItems number of items (pages) we're paginating over
+ * @param {number} params.maxItems number of items displayed in `Pagination` component
+ * @param tests an array of test tuples: current item (used as a currently active item in the pagination) and expected return array
+ */
+const runTableTests = (
+  { numItems, maxItems }: { numItems: number; maxItems: number },
+  tests: [{ currentItem: number }, Array<number | null>][]
+) => {
+  tests.forEach(([{ currentItem }, want]) => {
+    test(`Input: numItems = ${numItems}, maxItems = ${maxItems}, currentItem = ${currentItem}, want = ${want}`, () => {
+      expect(getItemsToRender(numItems, maxItems, currentItem)).toEqual(want);
+    });
+  });
+};
+
 describe("NumberLinks", () => {
   describe("Test 'getItemsToRender' error handling", () => {
     test("should throw an error if 'maxItems' less than 5", () => {
@@ -28,73 +47,67 @@ describe("NumberLinks", () => {
       }
       expect(tooLow).toBeTruthy();
     });
+
+    test("if an even number of 'maxItems' provided, should fall back to preceeding odd number", () => {
+      // maxItems = 8   ->   should render 7 items
+      const itemsShown = getItemsToRender(12, 8, 2).length;
+      expect(itemsShown).toEqual(7);
+    });
+
+    test("trivial: should return an empty array if 'numItems === 0'", () => {
+      // maxItems = 8   ->   should render 7 items
+      expect(getItemsToRender(0, 5, 0)).toEqual([]);
+    });
   });
 
-  describe("Test 'getItemsToRender' for standard cases: 'numItems' substantially higher than 'maxItems'", () => {
-    // We'll be using 11-long array and testing it out against 5 and 7 item display
-    const numItems = 11;
-
-    test("should show the left side, '...' and the last item if 'currentItem' lower than the highest possibly shown on the left side", () => {
-      // maxItems = 5
-      //
-      // Used to render: 1, [2], 3, ..., 11   ->   [2] being the 'currentItem' (indexed as 1)
-      const want5 = [0, 1, 2, null, 10];
-      expect(getItemsToRender(numItems, 5, 1)).toEqual(want5);
-
-      // maxItems = 7
-      //
-      // Used to render: 1, 2, 3, [4], 5, ..., 11   ->   [4] being the 'currentItem' (indexed as 3)
-      const want7 = [0, 1, 2, 3, 4, null, 10];
-      expect(getItemsToRender(numItems, 7, 3)).toEqual(want7);
-    });
-
-    test("should show the first item, '...' and the right side if 'currentItem' higher than the lowest possibly shown on the right side", () => {
-      // maxItems = 5
-      //
-      // Used to render: 1, ..., 9, [10], 11   ->   [10] being the 'currentItem' (indexed as 9)
-      const want5 = [0, null, 8, 9, 10];
-      expect(getItemsToRender(numItems, 5, 9)).toEqual(want5);
-
-      // maxItems = 7
-      //
-      // Used to render: 1, ..., 7, [8], 9, 10, 11   ->   [8] being the 'currentItem' (indexed as 7)
-      const want7 = [0, null, 6, 7, 8, 9, 10];
-      expect(getItemsToRender(numItems, 7, 7)).toEqual(want7);
-    });
-
-    test("should show the first item, '...', range around central item, '...' and the last item, if currentItem not within start or end rage", () => {
-      // maxItems = 5
-      //
-      // Used to render: 1, ..., [4], ..., 11   ->   [4] being the 'currentItem' (indexed as 3)
-      const want5 = [0, null, 3, null, 10];
-      expect(getItemsToRender(numItems, 5, 3)).toEqual(want5);
-
-      // maxItems = 7
-      //
-      // Used to render: 1, ..., 4, [5], 6, ..., 11   ->   [6] being the 'currentItem' (indexed as 5)
-      const want7 = [0, null, 3, 4, 5, null, 10];
-      expect(getItemsToRender(numItems, 7, 4)).toEqual(want7);
-    });
-
-    test("should render all of the items if 'numItems <= maxItems'", () => {
-      const numItems = 5;
-      // Want is the same for both cases as 5-length fits the items prefectly and 7 still shows 5 as there aren't any more to show
-      // Used to render: 1, 2, 3, 4, 5   ->   'currentItem' is unimportant here
-      const want = [0, 1, 2, 3, 4];
-      // maxItems = 5
-      expect(getItemsToRender(numItems, 5, 2)).toEqual(want);
-      expect(getItemsToRender(numItems, 7, 2)).toEqual(want);
-    });
-
-    test("edge case: should show left side even if 'currentItem = 2' for 'maxItems = 5'", () => {
-      // Used to render: 1, 2, [3], ..., 11   ->   [3] being the 'currentItem' (indexed as 2)
-      const want = [0, 1, 2, null, 10];
-      expect(getItemsToRender(numItems, 5, 2)).toEqual(want);
-    });
-    test("edge case: should show right side even if 'currentItem = numItems - 3' for 'maxItems = 5'", () => {
-      // Used to render: 1, ..., [9], 10, 11   ->   [9] being the 'currentItem' (indexed as 8)
-      const want = [0, null, 8, 9, 10];
-      expect(getItemsToRender(numItems, 5, 8)).toEqual(want);
-    });
+  describe("Table tests for 'getItemsToRender'", () => {
+    runTableTests({ numItems: 11, maxItems: 5 }, [
+      //  "1"|  2 |  3 | ...| 11
+      [{ currentItem: 0 }, [0, 1, 2, null, 10]],
+      //   1 | "2"|  3 | ...| 11
+      [{ currentItem: 1 }, [0, 1, 2, null, 10]],
+      //   1 |  2 | "3"| ...| 11
+      [{ currentItem: 2 }, [0, 1, 2, null, 10]],
+      //   1 | ...| "4"| ...| 11
+      [{ currentItem: 3 }, [0, null, 3, null, 10]],
+      //   1 | ...| "5"| ...| 11
+      [{ currentItem: 4 }, [0, null, 4, null, 10]],
+      //   1 | ...| "6"| ...| 11
+      [{ currentItem: 5 }, [0, null, 5, null, 10]],
+      //   1 | ...| "7"| ...| 11
+      [{ currentItem: 6 }, [0, null, 6, null, 10]],
+      //   1 | ...| "8"| ...| 11
+      [{ currentItem: 7 }, [0, null, 7, null, 10]],
+      //   1 | ...| "9"| 10 | 11
+      [{ currentItem: 8 }, [0, null, 8, 9, 10]],
+      //   1 | ...|  9 |"10"| 11
+      [{ currentItem: 9 }, [0, null, 8, 9, 10]],
+      //   1 | ...|  9 | 10 |"11"
+      [{ currentItem: 10 }, [0, null, 8, 9, 10]],
+    ]);
+    runTableTests({ numItems: 11, maxItems: 7 }, [
+      //  "1"|  2 |  3 |  4 |  5 | ...| 11
+      [{ currentItem: 0 }, [0, 1, 2, 3, 4, null, 10]],
+      //   1 | "2"|  3 |  4 |  5 | ...| 11
+      [{ currentItem: 1 }, [0, 1, 2, 3, 4, null, 10]],
+      //   1 |  2 | "3"|  4 |  5 | ...| 11
+      [{ currentItem: 2 }, [0, 1, 2, 3, 4, null, 10]],
+      //   1 |  2 |  3 | "4"|  5 | ...| 11
+      [{ currentItem: 3 }, [0, 1, 2, 3, 4, null, 10]],
+      //   1 | ...|  4 | "5"|  6 | ...| 11
+      [{ currentItem: 4 }, [0, null, 3, 4, 5, null, 10]],
+      //   1 | ...|  5 | "6"|  7 | ...| 11
+      [{ currentItem: 5 }, [0, null, 4, 5, 6, null, 10]],
+      //   1 | ...|  6 | "7"|  8 | ...| 11
+      [{ currentItem: 6 }, [0, null, 5, 6, 7, null, 10]],
+      //   1 | ...|  7 | "8"|  9 | 10 | 11
+      [{ currentItem: 7 }, [0, null, 6, 7, 8, 9, 10]],
+      //   1 | ...|  7 |  8 | "9"| 10 | 11
+      [{ currentItem: 8 }, [0, null, 6, 7, 8, 9, 10]],
+      //   1 | ...|  7 |  8 |  9 |"10"| 11
+      [{ currentItem: 9 }, [0, null, 6, 7, 8, 9, 10]],
+      //   1 | ...|  7 |  8 |  9 | 10 |"11"
+      [{ currentItem: 10 }, [0, null, 6, 7, 8, 9, 10]],
+    ]);
   });
 });
