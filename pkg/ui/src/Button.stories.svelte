5cecd03edaa97576b4f912962397b3cc79d22b41
ikusteu <ivan.kust57@gmail.com>
2022-08-08 21:22:17 +0200

    Add storybook to ui package

---
+++ pkg/ui/src/Button.stories.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+
+	import Button from './Button.svelte';
+</script>
+
+<Meta
+	title="Components/Button"
+	argTypes={{
+		label: { control: 'string' }
+	}}
+/>
+
+<Template let:args={{ label }}>
+	<Button {label} />
+</Template>
+
+<Story name="Default" args={{ label: 'Click me' }} />
