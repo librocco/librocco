a6de442a38f19275d6903dcb4ea528c98d63ed9e
ikusteu <ivan.kust57@gmail.com>
2022-08-08 20:10:12 +0200

    Setup tailwind for ui package

--- pkg/ui/src/routes/index.svelte
+++ pkg/ui/src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import '../main.css';
+</script>
+
+<h1 class="text-red-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
