7003de5a948cb894085bc87aea2d8ea2ff0ee6f2
ikusteu <ivan.kust57@gmail.com>
2022-08-11 23:49:57 +0200

    Make the StorybookItem accept a 'class' prop

--- pkg/ui/src/utils/stories/StorybookItem.svelte
+++ pkg/ui/src/utils/stories/StorybookItem.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+	let inputClasses = '';
+	export { inputClasses as class };
 	export let label = '';
+
+	const baseClasses = 'w-full h-24 flex flex-col items-stretch';
+	$: finalClasses = [baseClasses, inputClasses].join(' ');
 </script>
 
-<figure class="w-full h-24 flex flex-col items-stretch">
+<figure class={finalClasses}>
 	<div class="w-full h-full flex justify-center items-center">
 		<slot />
 	</div>
