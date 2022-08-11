ed00d3afc0aa40a65bf11e28c8028d7b0dc192d4
ikusteu <ivan.kust57@gmail.com>
2022-08-11 13:18:53 +0200

    Add index.ts for svg package (to please 'svelte package')

---
+++ pkg/svg/src/index.ts
@@ -0,0 +1,4 @@
+export { default as ChevronRight } from './ChevronRight.svelte';
+export { default as ChevronLeft } from './ChevronLeft.svelte';
+export { default as Mail } from './Mail.svelte';
+export { default as Plus } from './Plus.svelte';
