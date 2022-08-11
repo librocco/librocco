05307b3fadb5c4767a7d6ae12e25657663da8840
ikusteu <ivan.kust57@gmail.com>
2022-08-11 14:05:55 +0200

    Create SideNavButton.svelte component in ui package

---
+++ pkg/ui/src/SideNavButton/SideNavButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { filterPositionClass } from '../utils/styles';
+
+	let inputClasses = '';
+	export { inputClasses as class };
+	export let selected = false;
+	export let as = 'button';
+
+	const baseClasses = ['relative', 'w-14', 'h-14', 'rounded-lg', 'p-4', 'cursor-pointer'];
+
+	$: bgColor = selected ? 'bg-gray-900' : 'bg-none';
+	$: iconColor = selected ? 'text-white' : 'text-gray-400 hover:text-white';
+
+	$: filteredBaseClasses = filterPositionClass(inputClasses, [...baseClasses, bgColor, iconColor]);
+
+	$: finalClasses = [...filteredBaseClasses, inputClasses].join(' ').trim();
+</script>
+
+<svelte:element this={as} class={finalClasses} on:click>
+	<slot />
+</svelte:element>
