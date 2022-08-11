b00b1d5f209c4032857338e6db5e85f24ecdfca7
ikusteu <ivan.kust57@gmail.com>
2022-08-11 12:14:08 +0200

    Create storybook util components (StorybookItem and StorybookGrid) in ui package

---
+++ pkg/ui/src/utils/stories/StorybookItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let label = '';
+</script>
+
+<figure class="w-full h-24 flex flex-col items-stretch">
+	<div class="w-full h-full flex justify-center items-center">
+		<slot />
+	</div>
+
+	{#if label}
+		<figcaption class="w-full text-center text-gray-600">
+			{label}
+		</figcaption>
+	{/if}
+</figure>
