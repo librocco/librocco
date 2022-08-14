10f354e3c060d8460d44ca1399cd6bc71ed16614
Fadwa <fadwa.mahmoud@gmail.com>
2022-08-14 15:54:07 +0200

    Migrate NoteButton to svelte

---
+++ pkg/ui/src/NoteButton/NoteButton.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { ArrowCircleRight, ArrowCircleLeft } from '@librocco/svg';
+
+	import { NoteButtonLabel, NoteButtonType, Warehouse } from './enums';
+
+	import {
+		text,
+		shapeSpacing,
+		iconSizeClasses,
+		iconSpacingClasses,
+		iconColorClass,
+		colorClassesLookup,
+		borderClassesLookup
+	} from './styles';
+
+	export let type: NoteButtonType = NoteButtonType.In;
+	export let label: NoteButtonLabel | Warehouse = Warehouse.Nuovo;
+	export let classes: string = '';
+
+	const arrowIconLookup = {
+		[NoteButtonType.In]: ArrowCircleRight,
+		[NoteButtonType.Out]: ArrowCircleLeft
+	};
+	const Icon = arrowIconLookup[type];
+
+	const color = colorClassesLookup[type];
+	const border = borderClassesLookup[label];
+	const buttonClasses = [
+		classes,
+		'flex',
+		'items-center',
+		border,
+		...text,
+		...shapeSpacing,
+		...color
+	].join(' ');
+	const buttonContentClasses = [
+		'flex',
+		'items-center',
+		...iconSizeClasses,
+		...iconSpacingClasses,
+		iconColorClass
+	].join(' ');
+</script>
+
+<span class={border}>
+	<button class={buttonClasses}>
+		<div class={buttonContentClasses}>
+			<svelte:component this={Icon} />
+		</div>
+		{label}
+	</button>
+</span>
