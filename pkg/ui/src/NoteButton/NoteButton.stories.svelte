99c57cd9d627f4e1a9675a74543121565292f776
Fadwa <fadwa.mahmoud@gmail.com>
2022-08-14 15:54:07 +0200

    Migrate NoteButton to svelte

---
+++ pkg/ui/src/NoteButton/NoteButton.stories.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import { StorybookItem, StorybookGrid } from '../utils/stories';
+	import { NoteButtonLabel, NoteButtonType } from './enums';
+
+	import NoteButton from './NoteButton.svelte';
+</script>
+
+<Meta title="Components: NoteButton" />
+
+<Story name="Note Button">
+	<StorybookGrid>
+		<StorybookItem label={`Type: Note In`}>
+			<NoteButton />
+		</StorybookItem>
+		<StorybookItem label={`Type: Note Out`}>
+			<NoteButton type={NoteButtonType.Out} />
+		</StorybookItem>
+		<StorybookItem label={`Type: Remove Books`}>
+			<NoteButton type={NoteButtonType.Out} label={NoteButtonLabel.RemoveBooks} />
+		</StorybookItem>
+	</StorybookGrid>
+</Story>
