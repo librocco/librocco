7a128e73c355bc5c01d56a0726c0c59d5c2a71dd
ikusteu <ivan.kust57@gmail.com>
2022-08-16 11:14:03 +0200

    Remove the, no longer necessary, 'web' package

--- pkg/web/src/__mocks__/svg.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-/**
- * A mock react component we're using to load (in tests)
- * instead of any `.svg` file
- */
-const MockSVG: React.FC = () => null;
-
-export default MockSVG;
