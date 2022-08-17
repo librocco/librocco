b5d6e156282f4f1b83661baaa420cde3437bfba6
Fadwa <fadwa.mahmoud@gmail.com>
2022-08-17 21:13:02 +0200

    Match pouch and var APIs

--- pkg/web/src/__tests__/dataModel.test.ts
+++ pkg/web/src/__tests__/dataModel.test.ts
@@ -1,18 +1,24 @@
-// import { newNote, updateNote } from "../../db-actions";
-import { newNote, updateNote, database, getNote } from "../../pouchdb-actions";
+// import { newNote, updateNote, getNote, deleteDatabase } from "../../db-actions";
+import {
+  newNote,
+  updateNote,
+  database,
+  getNote,
+  deleteDatabase,
+} from "../../pouchdb-actions";
 
+// import { note, updatedNote, database } from "../__testData__/dataModel";
 import { note, updatedNote } from "../__testData__/dataModel";
 
 describe("Test data model", () => {
   afterAll(async () => {
-    const deleted = await database.destroy();
+    const deleted = await deleteDatabase(database);
     expect(deleted).toHaveProperty("ok");
   });
   test("New Note", async () => {
     const { id, ok } = await newNote(database, note);
     expect(ok).toBeTruthy();
     const { _id } = await getNote(database, id);
-    // console.log({ noteFromDB })
     expect(_id).toEqual(id);
   });
 
