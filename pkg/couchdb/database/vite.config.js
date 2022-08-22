19f1959c864dd71850133a4a13ae4ee47fe88821
Fadwa <fadwa.mahmoud@gmail.com>
2022-08-22 17:16:08 +0200

    More config stuff

--- pkg/couchdb/database/vite.config.js
+++ pkg/couchdb/database/vite.config.js
@@ -1,8 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from "vite";
+import path from "path";
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+  build: {
+    lib: {
+      name: "@librocco/shared",
+      entry: path.join(__dirname, "src", "index.ts"),
+      fileName: (fmt) => (fmt === "es" ? "index.es.js" : "index.js"),
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        exports: "named",
+      },
+    },
+    outDir: "dist",
+  },
+});
