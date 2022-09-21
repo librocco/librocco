79d82d3eba6407da842b4ea519109f40c9a6d9dc
ikusteu <ivan.kust57@gmail.com>
2022-09-21 16:44:17 +0200

    Add functionality to map books to warehouses

--- pkg/datamodel-tests/src/runner/__tests__/runner.test.ts
+++ pkg/datamodel-tests/src/runner/__tests__/runner.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect } from 'vitest';
 
+import { RawBook, RawDBSnap } from '../../types/raw-data';
+
 import { Runner } from '../runner';
 
 import * as testDataLoader from './testDataLoader';
@@ -12,21 +14,49 @@ describe('Datamodel test runner smoke test', async () => {
 	await runner.loadData(testDataLoader);
 
 	// Simple transform functions to test the tranform functionality
-	runner.transformBooks((b) => ({ _id: b.volumeInfo.title }));
+	const idByTitle = (b: RawBook) => {
+		return { _id: b.volumeInfo.title };
+	};
+	const extractIdAndBooks = (s: RawDBSnap) => ({ _id: s.id, books: s.books.map(idByTitle) });
+
+	runner.transformBooks(idByTitle);
 	runner.transformNotes((n) => ({ _id: n.id }));
-	runner.transformSnap((s) => ({ _id: s.id }));
+	runner.transformSnap(extractIdAndBooks);
+	runner.mapWarehouses((b, addToWarehouse) => addToWarehouse(b.warehouse, b));
 
 	runner.test('smoke test', async (books, getNotesAndSnaps) => {
-		const transformedBooks = allBooks.map((b) => ({ _id: b.volumeInfo.title }));
+		const transformedBooks = allBooks.map(idByTitle);
 
 		expect(books).toEqual(transformedBooks);
 
-		const transformedNotes = allNotes.slice(0, 4).map((n) => ({ _id: n.id }));
-		const transformedSnap = { _id: testDataLoader.getSnap(3).id };
+		const rawTestSnap = testDataLoader.getSnap(9);
 
-		const { notes, snap } = getNotesAndSnaps(4);
+		const transformedNotes = allNotes.map((n) => ({ _id: n.id }));
+		const transformedSnap = extractIdAndBooks(rawTestSnap);
+		const scienceWarehouse = {
+			_id: 'science',
+			books: rawTestSnap.books.reduce(
+				(acc, { warehouse, ...b }) => (warehouse === 'science' ? [...acc, idByTitle(b)] : acc),
+				[] as { _id: string }[]
+			)
+		};
+		const jazzWarehouse = {
+			_id: 'jazz',
+			books: rawTestSnap.books.reduce(
+				(acc, { warehouse, ...b }) => (warehouse === 'jazz' ? [...acc, idByTitle(b)] : acc),
+				[] as { _id: string }[]
+			)
+		};
+
+		const {
+			notes,
+			snap,
+			warehouses: { jazz, science }
+		} = getNotesAndSnaps(10);
 
 		expect(notes).toEqual(transformedNotes);
 		expect(snap).toEqual(transformedSnap);
+		expect(jazz).toEqual(jazzWarehouse);
+		expect(science).toEqual(scienceWarehouse);
 	});
 });
