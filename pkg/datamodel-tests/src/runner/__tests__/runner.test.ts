df10d746e54913a33594f00449a0682c2159f21a
ikusteu <ivan.kust57@gmail.com>
2022-09-22 07:46:14 +0200

    Enable running of multiple test setups against the same loaded data

--- pkg/datamodel-tests/src/runner/__tests__/runner.test.ts
+++ pkg/datamodel-tests/src/runner/__tests__/runner.test.ts
@@ -10,53 +10,81 @@ const allBooks = testDataLoader.getBooks();
 const allNotes = testDataLoader.getNotes();
 
 describe('Datamodel test runner smoke test', async () => {
-	const runner = new Runner();
-	await runner.loadData(testDataLoader);
+	// We're instantiating the runner with the data loaded
+	// for efficiency when running multiple tests
+	const runner = new Runner(testDataLoader);
 
-	// Simple transform functions to test the tranform functionality
-	const idByTitle = (b: RawBook) => {
-		return { _id: b.volumeInfo.title };
-	};
-	const extractIdAndBooks = (s: RawDBSnap) => ({ _id: s.id, books: s.books.map(idByTitle) });
-
-	runner.transformBooks(idByTitle);
-	runner.transformNotes((n) => ({ _id: n.id }));
-	runner.transformSnap(extractIdAndBooks);
-	runner.mapWarehouses((b, addToWarehouse) => addToWarehouse(b.warehouse, b));
-
-	runner.test('smoke test', async (books, getNotesAndSnaps) => {
-		const transformedBooks = allBooks.map(idByTitle);
-
-		expect(books).toEqual(transformedBooks);
-
-		const rawTestSnap = testDataLoader.getSnap(9);
-
-		const transformedNotes = allNotes.map((n) => ({ _id: n.id }));
-		const transformedSnap = extractIdAndBooks(rawTestSnap);
-		const scienceWarehouse = {
-			_id: 'science',
-			books: rawTestSnap.books.reduce(
-				(acc, { warehouse, ...b }) => (warehouse === 'science' ? [...acc, idByTitle(b)] : acc),
-				[] as { _id: string }[]
-			)
+	describe('Transform case 1', () => {
+		// Simple transform functions to test the tranform functionality
+		const idByTitle = (b: RawBook) => {
+			return { _id: b.volumeInfo.title };
 		};
-		const jazzWarehouse = {
-			_id: 'jazz',
-			books: rawTestSnap.books.reduce(
-				(acc, { warehouse, ...b }) => (warehouse === 'jazz' ? [...acc, idByTitle(b)] : acc),
-				[] as { _id: string }[]
-			)
-		};
+		const extractIdAndBooks = (s: RawDBSnap) => ({ _id: s.id, books: s.books.map(idByTitle) });
 
-		const {
-			notes,
-			snap,
-			warehouses: { jazz, science }
-		} = getNotesAndSnaps(10);
+		const testCase = runner.newCase({
+			transformBooks: idByTitle,
+			transformNotes: (n) => ({ _id: n.id }),
+			transformSnaps: extractIdAndBooks,
+			mapWarehouses: (b, addToWarehouse) => addToWarehouse(b.warehouse, b)
+		});
 
-		expect(notes).toEqual(transformedNotes);
-		expect(snap).toEqual(transformedSnap);
-		expect(jazz).toEqual(jazzWarehouse);
-		expect(science).toEqual(scienceWarehouse);
+		testCase.test('smoke test 1', async (books, getNotesAndSnaps) => {
+			const transformedBooks = allBooks.map(idByTitle);
+
+			expect(books).toEqual(transformedBooks);
+
+			const rawTestSnap = testDataLoader.getSnap(9);
+
+			const transformedNotes = allNotes.map((n) => ({ _id: n.id }));
+			const transformedSnap = extractIdAndBooks(rawTestSnap);
+			const scienceWarehouse = {
+				_id: 'science',
+				books: rawTestSnap.books.reduce(
+					(acc, { warehouse, ...b }) => (warehouse === 'science' ? [...acc, idByTitle(b)] : acc),
+					[] as { _id: string }[]
+				)
+			};
+			const jazzWarehouse = {
+				_id: 'jazz',
+				books: rawTestSnap.books.reduce(
+					(acc, { warehouse, ...b }) => (warehouse === 'jazz' ? [...acc, idByTitle(b)] : acc),
+					[] as { _id: string }[]
+				)
+			};
+
+			const {
+				notes,
+				snap,
+				warehouses: { jazz, science }
+			} = getNotesAndSnaps(10);
+
+			expect(notes).toEqual(transformedNotes);
+			expect(snap).toEqual(transformedSnap);
+			expect(jazz).toEqual(jazzWarehouse);
+			expect(science).toEqual(scienceWarehouse);
+		});
+	});
+
+	describe('Transformation 2', () => {
+		const getISBN = (b: RawBook) =>
+			Object.values(b.volumeInfo.industryIdentifiers).find(({ type }) => type === 'ISBN_10')
+				?.identifier || 'isbn_unknown';
+
+		// Try a different transformation then the one above
+		const testCase = runner.newCase({
+			transformBooks: (b) => ({ _id: getISBN(b) }),
+			transformNotes: (n) => ({ _id: n.id }),
+			transformSnaps: (s) => ({ _id: s.id }),
+			mapWarehouses: () => {
+				//
+			}
+		});
+
+		testCase.test('Different transformation', async (books) => {
+			// We're testing the books against the same transform function as the
+			// transformation being applied is enough for this case
+			const transformedBooks = allBooks.map((b) => ({ _id: getISBN(b) }));
+			expect(books).toEqual(transformedBooks);
+		});
 	});
 });
