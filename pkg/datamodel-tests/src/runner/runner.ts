df29e9267c981caf5b94ed01e16f2e945153557b
ikusteu <ivan.kust57@gmail.com>
2022-09-23 11:47:12 +0200

    Refactor datamodel test runner to factory instead of a class

--- pkg/datamodel-tests/src/runner/runner.ts
+++ pkg/datamodel-tests/src/runner/runner.ts
@@ -1,32 +1,17 @@
-import { RawBook, RawSnap, RawNote, TestDataLoader, TransformConfig } from '../types';
+import { TestDataLoader, TransformConfig } from '../types';
 
 import { TestSetup } from './testSetup';
 
-export class Runner {
-	private _rawBooks: RawBook[] = [];
-	private _rawNotes: RawNote[] = [];
-	private _rawSnaps: RawSnap[] = [];
+export const newTestRunner = async (loader: TestDataLoader) => {
+	const [books, notes, snaps] = await Promise.all([
+		loader.getBooks(),
+		loader.getNotes(),
+		loader.getSnaps()
+	]);
 
-	async loadData(loader: TestDataLoader) {
-		const [rawBooks, rawNotes, rawSnaps] = await Promise.all([
-			loader.getBooks(),
-			loader.getNotes(),
-			loader.getSnaps()
-		]);
+	const data = { books, notes, snaps };
 
-		this._rawBooks = rawBooks;
-		this._rawNotes = rawNotes;
-		this._rawSnaps = rawSnaps;
-	}
-
-	newSetup(config: TransformConfig = {}): TestSetup {
-		return new TestSetup(
-			{
-				books: this._rawBooks,
-				notes: this._rawNotes,
-				snaps: this._rawSnaps
-			},
-			config
-		);
-	}
-}
+	return {
+		newModel: (config: TransformConfig) => new TestSetup(data, config)
+	};
+};
