a8e10692b2fecb03580b6de9ac6532f63d98912f
Fadwa <fadwa.mahmoud@gmail.com>
2022-09-27 16:36:34 +0200

    Initial version of TextFIeld component

---
+++ pkg/ui/src/TextField/TextField.stories.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Story, Meta } from '@storybook/addon-svelte-csf';
+
+	import TextField from './TextField.svelte';
+</script>
+
+<Meta title="Text Field component" />
+
+<Story name="Default">
+	<TextField label="Default" class="w-1/2" />
+</Story>
+<Story name="Start Adornment">
+	<TextField label="Start Adronment Input" startAdornment="start" class="w-1/2" />
+</Story>
+<Story name="End Adornment">
+	<TextField label="End Adronment Input" endAdornment="end" class="w-1/2" />
+</Story>
