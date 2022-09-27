77be2d4bee688fe378d1f7048c56b1b9f92c6bbb
ikusteu <ivan.kust57@gmail.com>
2022-09-27 15:26:00 +0200

    Enable inline unit tests in database package

--- pkg/database/vite.config.ts
+++ pkg/database/vite.config.ts
@@ -1,19 +1,31 @@
 import { defineConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 import path from 'path';
 
-export default defineConfig({
-	build: {
-		lib: {
-			name: '@librocco/database',
-			entry: path.join(__dirname, 'src', 'index.ts'),
-			fileName: (fmt) => (fmt === 'es' ? 'index.es.js' : 'index.js'),
-			formats: ['es', 'cjs']
-		},
-		rollupOptions: {
-			output: {
-				exports: 'named'
-			}
-		},
-		outDir: 'dist'
-	}
-});
+const config =
+	process.env.NODE_ENV === 'test'
+		? // We're using a special config for tests, to enable
+		  // inlining tests to source files
+		  defineVitestConfig({
+				test: {
+					includeSource: ['src/**/*.ts']
+				}
+		  })
+		: defineConfig({
+				build: {
+					lib: {
+						name: '@librocco/database',
+						entry: path.join(__dirname, 'src', 'index.ts'),
+						fileName: (fmt) => (fmt === 'es' ? 'index.es.js' : 'index.js'),
+						formats: ['es', 'cjs']
+					},
+					rollupOptions: {
+						output: {
+							exports: 'named'
+						}
+					},
+					outDir: 'dist'
+				}
+		  });
+
+export default config;
