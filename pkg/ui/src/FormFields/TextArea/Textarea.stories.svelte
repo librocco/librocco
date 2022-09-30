15d67572908e29ec825e845d701418e30291fda6
delasous <sousa.yak@googlemail.com>
2022-09-30 11:02:29 +0200

    Scaffold textarea component under FormFields folder

---
+++ pkg/ui/src/FormFields/TextArea/Textarea.stories.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Story, Meta } from '@storybook/addon-svelte-csf';
+
+	import Textarea from './Textarea.svelte';
+</script>
+
+<Meta title="Form Fields / Textarea" />
+
+<Story name="Default">
+	<Textarea label="Default" helpText="Insert your input" />
+</Story>
