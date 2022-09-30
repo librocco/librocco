dac35fe7bd54839a9d831c9dd1ca4ba4feacaad8
delasous <sousa.yak@googlemail.com>
2022-09-30 12:23:11 +0200

    Add checkbox component

---
+++ pkg/ui/src/FormFields/Checkbox/Checkbox.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	export let name: string;
+
+	export let label = '';
+	export let helpText = '';
+	export let checked = false;
+	export let disabled = false;
+
+	const labelBaseClasses = ['font-medium', disabled ? 'text-gray-400' : 'text-gray-700'].join(' ');
+	const helpTextColor = disabled ? 'text-gray-300' : 'text-gray-500';
+	const inputBaseClasses = [
+		'focus:ring-teal-500',
+		'h-4',
+		'w-4',
+		'text-gray-800',
+		'border-gray-300',
+		'rounded',
+		'disabled:text-gray-200'
+	].join(' ');
+</script>
+
+<div class="relative flex items-start">
+	<div class="flex items-center h-5">
+		<input
+			type="checkbox"
+			id={name}
+			aria-describedby={`${name}-description`}
+			class={inputBaseClasses}
+			{checked}
+			{disabled}
+		/>
+	</div>
+	<div class="ml-3 text-sm">
+		<label for={name} class={labelBaseClasses}>
+			{label}
+		</label>
+		<p id={`${name}-description`} class={helpTextColor}>
+			{helpText}
+		</p>
+	</div>
+</div>
