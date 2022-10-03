c20f3d7462c8fc04540c864b276f3356e5ffceed
Fadwa <fadwa.mahmoud@gmail.com>
2022-10-03 19:20:34 +0200

    Initial TabNav component

---
+++ pkg/ui/src/TabNav/Tab.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
+
+	const tab = {};
+	const { registerTab, selectTab, selectedTab } = getContext(TABS);
+
+	registerTab(tab);
+
+	export let classes = '';
+
+	const baseClass = '';
+	const button = 'border-solid border-[1px] bg-none m-0 text-gray-500';
+	const selected = 'border-b-solid border-b-2 border-purple-500 text-gray-900';
+
+	$: tabClass = [baseClass, $selectedTab === tab ? selected : button, classes].join(' ');
+</script>
+
+<button class={tabClass} on:click={() => selectTab(tab)}>
+	<slot />
+</button>
---
+++ pkg/ui/src/TabNav/TabNav.stories.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+	import { Tabs, TabList, TabPanel, Tab } from './index';
+</script>
+
+<Meta title="Components: TabNav" />
+
+<Story name="TabNav">
+	<Tabs>
+		<TabList>
+			<Tab classes=" w-[33%] h-10 justify-center">Agglungi con codice</Tab>
+			<Tab classes="w-[33%] h-10 justify-center">Cerca libri da caricare</Tab>
+			<Tab classes="w-[33%] h-10 justify-center">Crea nuovo libro</Tab>
+		</TabList>
+
+		<TabPanel>
+			<h2>Agglungi con codice panel</h2>
+		</TabPanel>
+
+		<TabPanel>
+			<h2>Cerca libri da caricare panel</h2>
+		</TabPanel>
+
+		<TabPanel>
+			<h2>Crea nuovo libro</h2>
+		</TabPanel>
+	</Tabs>
+</Story>
