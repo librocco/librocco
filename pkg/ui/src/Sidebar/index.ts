dba6676e8eb17d91ddded5f71b37f0f757b4e78c
delasous <sousa.yak@googlemail.com>
2022-10-05 17:19:24 +0200

    Update Folder > ItemGroup name; Add rotate to icon on expand

--- pkg/ui/src/Sidebar/index.ts
+++ pkg/ui/src/Sidebar/index.ts
@@ -1,3 +1,3 @@
 export { default as SidebarItem } from './SidebarItem.svelte';
-export { default as SidebarFolder } from './SidebarFolder.svelte';
+export { default as SidebarItemGroup } from './SidebarItemGroup.svelte';
 export { default as SideBar } from './Sidebar.svelte';
