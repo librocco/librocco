f925ff9de9e1143b0bdb0a4af99af9cd9164a854
delasous <sousa.yak@googlemail.com>
2022-10-05 16:33:26 +0200

    Draft Sidebar nav component

--- pkg/ui/src/Sidebar/index.ts
+++ pkg/ui/src/Sidebar/index.ts
@@ -1,2 +1,3 @@
 export { default as SidebarItem } from './SidebarItem.svelte';
 export { default as SidebarFolder } from './SidebarFolder.svelte';
+export { default as SideBar } from './Sidebar.svelte';
