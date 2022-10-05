ca7526e292c79820bf7604c8b16f476e9bb3b378
delasous <sousa.yak@googlemail.com>
2022-10-05 20:49:25 +0200

    Add small screen tabs & tablist

--- pkg/ui/src/TabNav/Tab.svelte
+++ pkg/ui/src/TabNav/Tab.svelte
@@ -2,20 +2,21 @@
 	import { getContext } from 'svelte';
 	import { TABS } from './Tabs.svelte';
 
-	const tab = {};
-	const { registerTab, selectTab, selectedTab } = getContext(TABS);
+	const { registerTab } = getContext(TABS);
 
-	export let name: string;
+	export let tabName: string;
+	export let selected: boolean;
 
+	registerTab(tabName);
+
+	// TODO: Handle this
 	$: href = 'javascript:void(0)'; // `#${name.replace(' ', '-')}`;
-
-	registerTab(tab);
-
-	$: selected = $selectedTab === tab;
 
 	$: linkTextClasses = selected ? 'text-gray-900' : 'text-gray-700 hover:text-gray-700';
 	$: linkClasses = [
 		linkTextClasses,
+		'hidden',
+		'sm:inline',
 		'group',
 		'relative',
 		'min-w-0',
@@ -34,7 +35,9 @@
 	$: spanClasses = [spanColourClasses, 'absolute', 'inset-x-0', 'bottom-0', 'h-0.5'].join(' ');
 </script>
 
-<a {href} on:click={() => selectTab(tab)} class={linkClasses} aria-current="page">
-	<span>{name}</span>
+<option class="sm:hidden" {selected}>{tabName}</option>
+
+<a {href} on:click class={linkClasses} aria-current="page">
+	<span>{tabName}</span>
 	<span aria-hidden="true" class={spanClasses} />
 </a>
--- pkg/ui/src/TabNav/TabList.svelte
+++ pkg/ui/src/TabNav/TabList.svelte
@@ -1,5 +1,55 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import { TABS } from './Tabs.svelte';
+
+	import Tab from './Tab.svelte';
+
+	const { selectTab, selectedTab } = getContext(TABS);
+
+	export let tabs: string[];
+
+	// TODO: label & aria
+
+	const handleSelectOnChange = (e: Event) => {
+		const target = e.target as HTMLSelectElement;
+		selectTab(target.value);
+	};
+
+	const selectClasses = [
+		'block',
+		'w-full',
+		'rounded-md',
+		'border-gray-300',
+		'focus:border-0',
+		'focus:ring-0',
+		'focus:outline-2',
+		'focus:outline-teal-500'
+	].join(' ');
+
+	const navClasses = [
+		'isolate',
+		'flex',
+		'divide-x',
+		'divide-gray-200',
+		'rounded-lg',
+		'shadow'
+	].join(' ');
+</script>
+
+<!-- Small screens = Select > Option (Tab.svelte) -->
+<div class="sm:hidden">
+	<label for="tabs" class="sr-only">Select a tab</label>
+	<select on:change={handleSelectOnChange} id="tabs" name="tabs" class={selectClasses}>
+		{#each tabs as tabName}
+			<Tab {tabName} selected={tabName === $selectedTab} />
+		{/each}
+	</select>
+</div>
+<!-- Med screens+ = Nav > A (Tab.svelte)  -->
 <div class="hidden sm:block">
-	<nav class="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
-		<slot />
+	<nav class={navClasses} aria-label="Tabs">
+		{#each tabs as tabName}
+			<Tab {tabName} selected={tabName === $selectedTab} on:click={() => selectTab(tabName)} />
+		{/each}
 	</nav>
 </div>
