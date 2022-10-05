27f6fcfb01b430024eb93479b916531a2fc2bc08
delasous <sousa.yak@googlemail.com>
2022-10-05 15:23:52 +0200

    Draft Sidebar folder component

--- pkg/ui/src/Sidebar/Sidebar.stories.svelte
+++ pkg/ui/src/Sidebar/Sidebar.stories.svelte
@@ -4,27 +4,41 @@
 	import { Mail } from '@librocco/svg';
 
 	import SidebarItem from './SidebarItem.svelte';
+	import SidebarFolder from './SidebarFolder.svelte';
 
 	const voidLink = 'javascript:void(0)';
+
+	const items = [
+		{
+			name: 'Overview',
+			href: voidLink,
+			current: true
+		},
+		{
+			name: ' Members',
+			href: voidLink,
+			current: false
+		}
+	];
 </script>
 
 <Meta
-	title="Sidebar Navigation Item"
+	title="Sidebar Navigation"
 	argTypes={{
 		class: { control: 'string' },
 		selected: { control: 'boolean' }
 	}}
 />
 
-<Story name="Default">
+<Story name="Item">
 	<div class="w-52 space-y-4">
-		<div>
-			<h1 class="text-md bold block mb-2">Default:</h1>
+		<div class="space-y-2">
+			<h1 class="text-md bold">Default:</h1>
 			<SidebarItem name="Warehouse 1" href={voidLink} />
 			<SidebarItem name="Warehouse 1" href={voidLink} current />
 		</div>
-		<div>
-			<h1 class="text-md bold block mb-2">With Icon:</h1>
+		<div class="space-y-2">
+			<h1 class="text-md bold">With Icon:</h1>
 			<SidebarItem name="Warehouse 1" href={voidLink}>
 				<Mail />
 			</SidebarItem>
@@ -32,10 +46,20 @@
 				<Mail />
 			</SidebarItem>
 		</div>
-		<div>
-			<h1 class="text-md bold block mb-2">Nested:</h1>
+		<div class="space-y-2">
+			<h1 class="text-md bold">Nested:</h1>
 			<SidebarItem name="Warehouse 1" href={voidLink} nested />
 			<SidebarItem name="Warehouse 1" href={voidLink} nested current />
+		</div>
+	</div>
+</Story>
+
+<Story name="Folder">
+	<div class="w-52 space-y-4">
+		<div class="space-y-2">
+			<h1 class="text-md bold">Default:</h1>
+			<SidebarFolder name="Team" {items} expanded={false} index={1} />
+			<SidebarFolder name="Team" {items} expanded index={2} />
 		</div>
 	</div>
 </Story>
