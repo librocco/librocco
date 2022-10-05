aa8da510e379a8bb6f597c3ada1ed141b9fa9ba3
ikusteu <ivan.kust57@gmail.com>
2022-10-05 09:12:19 +0200

    Add a datamodel test testing againsta a larger set of data (pulled from the testdata container)

--- pkg/datamodel-tests/src/tests/tests.ts
+++ pkg/datamodel-tests/src/tests/tests.ts
@@ -109,3 +109,29 @@ export const commitNote: TestFunction = async (db, getNotesAndWarehouses) => {
 	wStock = await warehouse.getStock();
 	expect(wStock).toEqual(stock2.books);
 };
+
+export const test20Notes: TestFunction = async (db, getNotesAndWarehouses) => {
+	const { fullStock, notes } = getNotesAndWarehouses(20);
+
+	const w = db.warehouse();
+
+	const noteUpdates = notes.map(
+		(note) =>
+			new Promise<void>((resolve, reject) => {
+				(note.type === 'inbound' ? w.createInNote() : w.createOutNote())
+					.then((n) =>
+						n.addVolumes(
+							...note.books.map(({ isbn, quantity }) => [isbn, quantity] as VolumeQuantityTuple)
+						)
+					)
+					.then((n) => n.commit())
+					.then(() => resolve())
+					.catch((err) => reject(err));
+			})
+	);
+	await Promise.all(noteUpdates);
+
+	const stock = await w.getStock();
+
+	expect(stock).toEqual(fullStock.books);
+};
