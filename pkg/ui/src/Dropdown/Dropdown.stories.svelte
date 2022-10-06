3f137ee2264a9c592313d622c9861bf46ef6563c
Fadwa <fadwa.mahmoud@gmail.com>
2022-10-06 18:13:45 +0200

    Dropdown Items

---
+++ pkg/ui/src/Dropdown/Dropdown.stories.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import { StorybookItem, StorybookGrid } from '../utils/stories';
+
+	import { ChevronLeft } from '@librocco/svg';
+
+	import DropdownTrigger from './DropdownTrigger.svelte';
+	import DropdownItem from './DropdownItem.svelte';
+	import DropdownItemCompound from './DropdownItemCompound.svelte';
+</script>
+
+<Meta title="Components: Dropdown" />
+
+<Story name="Dropdown Trigger">
+	<StorybookGrid>
+		<StorybookItem>
+			<DropdownTrigger Icon={ChevronLeft} label="Options" />
+		</StorybookItem>
+	</StorybookGrid>
+</Story>
+<Story name="Dropdown Items">
+	<StorybookGrid>
+		<StorybookItem>
+			<div class="absolute left-10">
+				<DropdownItem items={['Edit', 'Archive', 'Move', 'Share', 'Add To Favorites', 'Delete']} />
+			</div>
+		</StorybookItem>
+	</StorybookGrid>
+</Story>
+<Story name="Dropdown Compound Items">
+	<div>
+		<StorybookItem>
+			<div class="absolute left-10">
+				<DropdownItemCompound
+					items={[
+						{
+							label: 'Published',
+							description: 'This job posting can be viewed by anyone who has the link',
+							selected: false
+						},
+						{
+							label: 'Published',
+							description: 'This job posting can be viewed by anyone who has the link',
+							selected: true
+						}
+					]}
+				/>
+			</div>
+		</StorybookItem>
+	</div></Story
+>
