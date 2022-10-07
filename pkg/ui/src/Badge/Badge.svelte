b2915d96e47d86430ca7b8e08ea398b9e4ab4c18
Fadwa <fadwa.mahmoud@gmail.com>
2022-10-07 04:27:53 +0200

    Badge Component

---
+++ pkg/ui/src/Badge/Badge.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { BadgeColor, BadgeSize } from './enums';
+
+	export let size: BadgeSize = BadgeSize.Base;
+
+	export let color: BadgeColor = BadgeColor.Primary;
+
+	/**
+	 * ```yaml
+	 * Primary:
+	 * 	bg-gray-100 text-gray-800
+	 * Error:
+	 * 	bg-red-100 px-2.5 text-red-800
+	 * Warning:
+	 * 	bg-yellow-100 text-yellow-800
+	 * Success:
+	 * bg-green-100 text-green-800
+	 * ```
+	 */
+
+	export const colorClassesLookup: Record<BadgeColor, string> = {
+		[BadgeColor.Primary]: 'bg-gray-100 text-gray-800',
+		[BadgeColor.Error]: 'bg-red-100 px-2.5 text-red-800',
+		[BadgeColor.Warning]: 'bg-yellow-100 text-yellow-800',
+		[BadgeColor.Success]: 'bg-green-100 text-green-800'
+	};
+
+	export const sizeLookup: Record<BadgeSize, string> = {
+		[BadgeSize.Base]: 'px-2.5 py-0.5 text-xs',
+		[BadgeSize.LG]: 'px-3 py-0.5 text-sm'
+	};
+
+	$: colorAndSize = [colorClassesLookup[color], sizeLookup[size]].join(' ');
+</script>
+
+<span class={['inline-flex items-center rounded-full', colorAndSize].join(' ')}>Badge</span>
