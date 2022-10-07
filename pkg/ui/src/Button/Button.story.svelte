cfa577c5d9c852f47335bd5462fbf4e8ab7dd33d
delasous <sousa.yak@googlemail.com>
2022-10-07 13:11:14 +0200

    Test histoire setup

---
+++ pkg/ui/src/Button/Button.story.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type { Hst } from '@histoire/plugin-svelte'
+
+	import { ButtonSize, ButtonShape, ButtonColor, IconPosition } from './enums';
+
+	import Button from './Button.svelte';
+	import { StorybookItem, StorybookGrid } from '../utils/stories';
+
+    export let Hst: Hst
+
+	const buttonText = 'Button Text';
+  </script>
+  
+  <Hst.Story title="Button"> <!-- Typed! -->
+    <h1 class="text-lg font-bold mb-4">Square:</h1>
+    <StorybookGrid class="mb-16">
+		{#each Object.values(ButtonSize) as size}
+			<StorybookItem label={`Size: ${size}`}>
+				<Button {...{ size }}>{buttonText}</Button>
+			</StorybookItem>
+		{/each}
+	</StorybookGrid>
+  </Hst.Story>
