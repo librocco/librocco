66c6501e0c1eab6f168324bed7647c0942aba088
ikusteu <ivan.kust57@gmail.com>
2022-10-07 10:16:35 +0200

    Transform test and implementation version names to more friendly display in datamodel-tests

--- pkg/datamodel-tests/src/main.test.ts
+++ pkg/datamodel-tests/src/main.test.ts
@@ -7,15 +7,17 @@ import * as testDataLoader from '@loaders/couchdb-image-loader';
 import * as implementations from './implementations';
 import * as tests from './tests';
 
+import { processTestName, processVersionName } from './utils/misc';
+
 describe('Datamodel tests', async () => {
 	const runner = await newTestRunner(testDataLoader);
 
-	Object.entries(implementations).forEach(([name, config]) => {
-		describe(name, () => {
+	Object.entries(implementations).forEach(([versionName, config]) => {
+		describe(processVersionName(versionName), () => {
 			const setup = runner.setup(config);
 
 			Object.entries(tests).forEach(([name, testFn]) => {
-				setup.test(name, testFn);
+				setup.test(processTestName(name), testFn);
 			});
 		});
 	});
--- pkg/datamodel-tests/src/runner/__tests__/runner.test.ts
+++ pkg/datamodel-tests/src/runner/__tests__/runner.test.ts
@@ -4,20 +4,22 @@ import { newTestRunner } from '@runner/runner';
 
 import * as testDataLoader from './test-data-loader';
 
-import * as datamodels from '@/implementations';
+import * as implementations from '@/implementations';
 import tests from './tests';
+
+import { processTestName, processVersionName } from '@/utils/misc';
 
 describe('Datamodel tests', async () => {
 	// We're currently using the `testDataLoader` from unit tests,
 	// but will switch it up with loader reading data from fs or an image
 	const runner = await newTestRunner(testDataLoader);
 
-	Object.entries(datamodels).forEach(([name, config]) => {
-		describe(name, () => {
+	Object.entries(implementations).forEach(([versionName, config]) => {
+		describe(processVersionName(versionName), () => {
 			const setup = runner.setup(config);
 
 			Object.entries(tests).forEach(([name, testFn]) => {
-				setup.test(name, testFn);
+				setup.test(processTestName(name), testFn);
 			});
 		});
 	});
