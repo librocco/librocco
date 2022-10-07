ff890a848e25168fb8e461f72bb02a754ec7b21b
ikusteu <ivan.kust57@gmail.com>
2022-10-07 09:57:18 +0200

    Clean up datamodel-tests folder structure

--- pkg/datamodel-tests/vite.config.ts
+++ pkg/datamodel-tests/vite.config.ts
@@ -5,11 +5,10 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': path.join(__dirname, 'src'),
-			'@runner': path.join(__dirname, 'src/runner'),
-			'@tests': path.join(__dirname, 'src/tests'),
-			'@loaders': path.join(__dirname, 'src/data-loaders'),
-			'@unit-tests': path.join(__dirname, 'src/__tests__'),
-			'@unit-test-data': path.join(__dirname, 'src/__testData__')
+			'@runner': path.join(__dirname, 'src', 'runner'),
+			'@tests': path.join(__dirname, 'src', 'tests'),
+			'@loaders': path.join(__dirname, 'src', 'data-loaders'),
+			'@unit-test-data': path.join(__dirname, 'src', 'runner', '__testData__')
 		}
 	},
 	build: {
