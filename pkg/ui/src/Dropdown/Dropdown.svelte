a45d1fec9cb31d66e567debc5170d4cc66008d30
Fadwa <fadwa.mahmoud@gmail.com>
2022-10-10 13:56:45 +0200

    SelectMenu component

---
+++ pkg/ui/src/Dropdown/Dropdown.svelte
@@ -0,0 +1,10 @@
+<div class="relative mt-1">
+	<slot name="trigger" />
+	<!-- <Transition
+        as={Fragment}
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      > -->
+	<slot name="items" />
+</div>
