40c4dc36e3a57783904824bd33428b444f53c06d
delasous <sousa.yak@googlemail.com>
2022-10-10 10:42:52 +0200

    Add Hst Button story

--- pkg/ui/src/Button/Button.story.svelte
+++ pkg/ui/src/Button/Button.story.svelte
@@ -1,23 +1,69 @@
 <script lang="ts">
-    import type { Hst } from '@histoire/plugin-svelte'
+	import type { Hst } from '@histoire/plugin-svelte';
+
+	import { Mail } from 'lucide-svelte';
 
 	import { ButtonSize, ButtonShape, ButtonColor, IconPosition } from './enums';
-
 	import Button from './Button.svelte';
-	import { StorybookItem, StorybookGrid } from '../utils/stories';
 
-    export let Hst: Hst
+	export let Hst: Hst;
 
 	const buttonText = 'Button Text';
-  </script>
-  
-  <Hst.Story title="Button"> <!-- Typed! -->
-    <h1 class="text-lg font-bold mb-4">Square:</h1>
-    <StorybookGrid class="mb-16">
+
+	let color = ButtonColor.Primary;
+</script>
+
+<Hst.Story title="Button" layout={{ type: 'grid' }}>
+	<svelte:fragment slot="controls">
+		<Hst.Select
+			bind:value={color}
+			options={[ButtonColor.Primary, ButtonColor.Secondary, ButtonColor.White]}
+			title="Color"
+		/>
+		<pre>{JSON.stringify({ color }, null, 2)}</pre>
+	</svelte:fragment>
+
+	<Hst.Variant title="Square">
 		{#each Object.values(ButtonSize) as size}
-			<StorybookItem label={`Size: ${size}`}>
-				<Button {...{ size }}>{buttonText}</Button>
-			</StorybookItem>
+			<div class="py-2">
+				<Button {size} {color}>{buttonText}</Button>
+			</div>
 		{/each}
-	</StorybookGrid>
-  </Hst.Story>
+	</Hst.Variant>
+
+	<Hst.Variant title="Rounded">
+		{#each Object.values(ButtonSize) as size}
+			<div class="py-2">
+				<Button shape={ButtonShape.Rounded} {size} {color}>{buttonText}</Button>
+			</div>
+		{/each}
+	</Hst.Variant>
+
+	<Hst.Variant title="Circular">
+		{#each Object.values(ButtonSize) as size}
+			<div class="py-2">
+				<Button shape={ButtonShape.Circular} {size} {color} />
+			</div>
+		{/each}
+	</Hst.Variant>
+
+	<Hst.Variant title="Icon Start">
+		{#each Object.values(ButtonSize) as size}
+			<div class="py-2">
+				<Button {size} Icon={Mail} {color}>
+					{buttonText}
+				</Button>
+			</div>
+		{/each}
+	</Hst.Variant>
+
+	<Hst.Variant title="Icon End">
+		{#each Object.values(ButtonSize) as size}
+			<div class="py-2">
+				<Button {size} Icon={Mail} iconPosition={IconPosition.End} {color}>
+					{buttonText}
+				</Button>
+			</div>
+		{/each}
+	</Hst.Variant>
+</Hst.Story>
