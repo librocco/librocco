d3ee79327b5de3740e3656b1e9334b1bdd9f36cc
delasous <sousa.yak@googlemail.com>
2022-10-11 10:24:51 +0200

    Remove svg pkg

--- pkg/svg/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as ChevronRight } from './ChevronRight.svelte';
-export { default as ChevronLeft } from './ChevronLeft.svelte';
-export { default as ArrowCircleRight } from './ArrowCircleRight.svelte';
-export { default as ArrowCircleLeft } from './ArrowCircleLeft.svelte';
-export { default as Mail } from './Mail.svelte';
-export { default as Plus } from './Plus.svelte';
