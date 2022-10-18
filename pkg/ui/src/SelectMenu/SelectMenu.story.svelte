0e8021039cdc5e8c9941c0d41e361e28d2a2daaf
delasous <sousa.yak@googlemail.com>
2022-10-18 13:57:04 +0200

    Add svelte headless-ui port and rebuild SelectMenu with package headless components

--- pkg/ui/src/SelectMenu/SelectMenu.story.svelte
+++ pkg/ui/src/SelectMenu/SelectMenu.story.svelte
@@ -1,55 +1,24 @@
 <script lang="ts">
 	import type { Hst } from '@histoire/plugin-svelte';
-	import { SelectMenuContext, SelectMenuTrigger, SelectMenuItems, SelectMenu } from './index';
+	import { SelectMenu } from './index';
+
+	import { AlignContainerEdge } from './enums';
 
 	export let Hst: Hst;
+
+	const options = [
+		{
+			title: 'Published',
+			description: 'This job posting can be viewed by anyone who has the link.'
+		},
+		{ title: 'Draft', description: 'This job posting will no longer be publicly accessible.' }
+	];
 </script>
 
-<Hst.Story title="SelectMenu" layout={{ type: 'single' }}>
-	<Hst.Variant title="Select Menu Trigger">
-		<SelectMenuContext initialValue="Different Value">
-			<SelectMenuTrigger />
-		</SelectMenuContext>
-	</Hst.Variant>
-	<Hst.Variant title="Select Menu Item">
-		<div>
-			<SelectMenuContext initialValue="Different Value">
-				<SelectMenuItems
-					items={[
-						{
-							label: 'Published',
-							description: 'This job posting can be viewed by anyone who has the link'
-						},
-						{
-							label: 'Draft',
-							description: 'This job posting can be viewed by anyone who has the link'
-						}
-					]}
-				/>
-			</SelectMenuContext>
-		</div>
-	</Hst.Variant>
-	<Hst.Variant title="Select Menu">
-		<div>
-			<SelectMenuContext initialValue="Custom Label">
-				<SelectMenu>
-					<SelectMenuTrigger slot="trigger" />
-
-					<SelectMenuItems
-						slot="items"
-						items={[
-							{
-								label: 'Published',
-								description: 'This job posting can be viewed by anyone who has the link'
-							},
-							{
-								label: 'Draft',
-								description: 'This job posting can be viewed by anyone who has the link'
-							}
-						]}
-					/>
-				</SelectMenu>
-			</SelectMenuContext>
-		</div>
-	</Hst.Variant>
+<Hst.Story title="SelectMenu" layout={{ type: 'grid', width: 500 }}>
+	<SelectMenu
+		{options}
+		alignItemsEdge={AlignContainerEdge.Left}
+		ariaLabel="Change published status"
+	/>
 </Hst.Story>
--- pkg/ui/src/SelectMenu/index.ts
+++ pkg/ui/src/SelectMenu/index.ts
@@ -1,4 +1,3 @@
-export { default as SelectMenuTrigger } from './SelectMenuTrigger.svelte';
-export { default as SelectMenuItems } from './SelectMenuItems.svelte';
 export { default as SelectMenu } from './SelectMenu.svelte';
-export { default as SelectMenuContext } from './SelectMenuContext.svelte';
+
+export * from './enums';
