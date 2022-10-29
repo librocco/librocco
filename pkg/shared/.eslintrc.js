58373193e71e1aac24f2f0cd91c9b553ce6420c6
ikusteu <ivan.kust57@gmail.com>
2022-10-29 20:00:33 +0200

    Remove the unused '@librocco/shared' package

--- pkg/shared/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-const scaffold = require("../scaffold/.eslintrc.js");
-const { useTSConfig } = require("../scaffold/.eslint.utils.js");
-
-module.exports = useTSConfig(scaffold, [path.join(__dirname, "tsconfig.json")]);
