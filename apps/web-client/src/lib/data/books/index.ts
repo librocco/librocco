eb113efb5bc44f77edca4a6ac72db5d446bd03fa
ikusteu <ivan.kust57@gmail.com>
2022-11-02 13:56:18 +0100

    Add test data for TEMP usage with middle-layer store

---
+++ apps/web-client/src/lib/data/books/index.ts
@@ -0,0 +1,17 @@
+import book1 from './0007149522.json';
+import book2 from './0061983411.json';
+import book3 from './0080524230.json';
+import book4 from './0194349276.json';
+import book5 from './0195071409.json';
+import book6 from './0195399706.json';
+import book7 from './019976915X.json';
+
+export default {
+	'0007149522': book1,
+	'0061983411': book2,
+	'0080524230': book3,
+	'0194349276': book4,
+	'0195071409': book5,
+	'0195399706': book6,
+	'019976915X': book7
+};
