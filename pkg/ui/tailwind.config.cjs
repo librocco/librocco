70a6149d1c5d36d54de0c839a632085e99fe50c1
Fadwa <fadwa.mahmoud@gmail.com>
2022-11-02 18:31:44 +0200

    Render columns individually and fix some styles

--- pkg/ui/tailwind.config.cjs
+++ pkg/ui/tailwind.config.cjs
@@ -7,11 +7,10 @@ module.exports = {
 		require('@tailwindcss/forms'),
 		function ({ addUtilities }) {
 			addUtilities({
-				'.scrollbar-hide::-webkit-scrollbar': {
-					display: 'none'
-				},
-
 				'.scrollbar-hide': {
+					'&::--webkit-scrollbar': {
+						display: 'none'
+					},
 					'-ms-overflow-style': 'none',
 					'scrollbar-width': 'none'
 				}
