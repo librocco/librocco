5f748dd87c22bdb8c2b78d11b4eae7320eba9cef
ikusteu <ivan.kust57@gmail.com>
2022-11-03 15:03:26 +0100

    Add logic for middle-layer stores

--- apps/web-client/src/lib/data/stores.ts
+++ apps/web-client/src/lib/data/stores.ts
@@ -1,12 +1,18 @@
-import { writable } from 'svelte/store';
+import { readable, derived, type Readable } from 'svelte/store';
+import { page } from '$app/stores';
+
+import allBooks from './books';
+import allWarehouse from './warehouses';
+import allInbound from './notes/inbound';
+import allOutbound from './notes/outbound';
 
 interface BookEntry {
 	isbn: string;
 	title: string;
-	authors: string[];
-	publisher: string;
-	year: string;
-	price: number;
+	authors?: string[];
+	publisher?: string;
+	year?: string;
+	price?: number;
 }
 
 interface VolumeQuantity {
@@ -14,23 +20,66 @@ interface VolumeQuantity {
 	quantity: number;
 }
 
-interface BookStore {
+type DisplayRow = BookEntry & { quantity: number };
+
+export interface BookStore {
 	[isbn: string]: BookEntry;
 }
-const bookStore = writable<BookStore>({});
+const bookStore = readable<BookStore>(allBooks);
 
 interface WarehouseStore {
 	[warehouse: string]: {
 		entries: VolumeQuantity[];
-		inNotes: string[];
+		inNotes?: string[];
 	};
 }
-const warehouseStore = writable<WarehouseStore>({});
+const warehouseStore = readable<WarehouseStore>(allWarehouse);
 
 interface NoteStore {
 	[noteId: string]: {
 		entries: VolumeQuantity[];
 	};
 }
-const inNoteStore = writable<NoteStore>({});
-const outNoteStore = writable<NoteStore>({});
+const inNoteStore = readable<NoteStore>(allInbound);
+const outNoteStore = readable<NoteStore>(allOutbound);
+
+export const warehouses = derived(warehouseStore, (ws) => Object.keys(ws));
+export const inNotes = derived(warehouseStore, (ws) =>
+	Object.entries(ws).reduce((acc, [wName, { inNotes }]) => (!inNotes?.length ? acc : { ...acc, [wName]: inNotes }))
+);
+export const outNotes = derived(outNoteStore, (on) => Object.keys(on));
+
+const contentStoreLookup = {
+	warehouse: warehouseStore,
+	inbound: inNoteStore,
+	outbound: outNoteStore
+};
+export const createTableContentStore = (contentType: keyof typeof contentStoreLookup) =>
+	derived<[Readable<NoteStore>, typeof page, Readable<BookStore>], DisplayRow[]>(
+		[contentStoreLookup[contentType], page, bookStore],
+		([content, page, bookStore]) => {
+			let { id } = page.params as { id?: string };
+
+			// If no warehouse 'id' specified, show stock for 'all'
+			if (!id && contentType === 'warehouse') {
+				id = 'all';
+			}
+
+			// No id will happen quite ofter: this means we're on root of the view
+			// with no single note specified.
+			if (!id) {
+				return [];
+			}
+
+			// TODO: we might want to throw 404 here...
+			if (!content[id]) {
+				return [];
+			}
+
+			return content[id].entries.map(({ isbn, quantity }) => ({
+				...bookStore[isbn],
+				isbn,
+				quantity
+			}));
+		}
+	);
