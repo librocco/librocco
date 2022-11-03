8b20c2147cfd22046c08b9d745b1de1bbabeb41d
Fadwa <fadwa.mahmoud@gmail.com>
2022-11-03 12:07:59 +0200

    Header component

---
+++ pkg/ui/src/Header/index.ts
@@ -0,0 +1,3 @@
+export { default as HeaderTabContext } from './HeaderTabContext.svelte';
+export { default as HeaderTab } from './HeaderTab.svelte';
+export { default as Header } from './Header.svelte';
