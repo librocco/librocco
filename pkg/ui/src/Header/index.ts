0df8195a170b0ac2135c7779c7ed742cb059f903
Fadwa <fadwa.mahmoud@gmail.com>
2022-11-05 01:39:55 +0200

    Header nav links

--- pkg/ui/src/Header/index.ts
+++ pkg/ui/src/Header/index.ts
@@ -1,3 +1,1 @@
-export { default as HeaderTabContext } from './HeaderTabContext.svelte';
-export { default as HeaderTab } from './HeaderTab.svelte';
 export { default as Header } from './Header.svelte';
