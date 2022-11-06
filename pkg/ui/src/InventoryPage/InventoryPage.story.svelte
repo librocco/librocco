6e34de8ce7c3ea483047fc8a8500f8b16f44f87e
ikusteu <ivan.kust57@gmail.com>
2022-11-06 18:47:50 +0100

    Create InventoryPage and a story with layout template

---
+++ pkg/ui/src/InventoryPage/InventoryPage.story.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+
+	import { SidebarItem, SideBarNav } from '../Sidebar';
+	import InventoryPage from './InventoryPage.svelte';
+
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="Inventory Page">
+	<Hst.Variant title="Inventory Page">
+		<InventoryPage>
+			<div class="flex h-[168px] w-full items-center justify-center bg-gray-200" slot="header">
+				<h1 class="text-3xl font-semibold tracking-wider text-gray-300">HEADER</h1>
+			</div>
+
+			<div class="flex h-full w-full items-center justify-center bg-red-200" slot="sidebar">
+				<h1 class="text-3xl font-semibold tracking-wider text-red-300">SIDEBAR</h1>
+			</div>
+
+			<div class="flex h-20 w-full items-center justify-center bg-green-200" slot="tableHeader">
+				<h1 class="text-3xl font-semibold tracking-wider text-green-300">TABLE HEADER</h1>
+			</div>
+
+			<div class="flex h-full w-full items-center justify-center bg-violet-200" slot="table">
+				<h1 class="text-3xl font-semibold tracking-wider text-violet-300">TABLE</h1>
+			</div>
+
+			<div class="flex h-14 items-center justify-center bg-blue-200" slot="tableFooter">
+				<h1 class="text-3xl font-semibold tracking-wider text-blue-300">TABLE FOOTER</h1>
+			</div>
+		</InventoryPage>
+	</Hst.Variant>
+</Hst.Story>
---
+++ pkg/ui/src/InventoryPage/InventoryPage.svelte
@@ -0,0 +1,21 @@
+<slot name="header" />
+
+<main class="absolute top-[168px] left-0 right-0 bottom-0 flex divide-x divide-gray-300 border border-gray-300">
+	<section class="w-[280px]">
+		<slot name="sidebar" />
+	</section>
+
+	<section class="flex w-full flex-col">
+		<div class="min-h-[140px] bg-gray-50 p-10 pt-6">
+			<slot name="tableHeader" />
+		</div>
+
+		<div class="h-full">
+			<slot name="table" />
+		</div>
+
+		<div class="mt-auto min-h-[140px] p-10">
+			<slot name="tableFooter" />
+		</div>
+	</section>
+</main>
