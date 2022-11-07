df9418b794028f3b3b24a786c8fe778c896d8331
Fadwa <fadwa.mahmoud@gmail.com>
2022-11-07 09:17:28 +0200

    Initialize props with undefined

--- pkg/ui/src/InventoryTable/types.ts
+++ pkg/ui/src/InventoryTable/types.ts
@@ -1,12 +1,12 @@
 export type TableData = {
-	isbn: number;
-	title: string;
-	author: string;
-	editor: string;
-	quantity: number;
-	price: number;
-	year: number;
-	outOfPrint: boolean;
-	editedBy: string;
-	classification: string;
+	isbn?: number | undefined;
+	title?: string | undefined;
+	author?: string | undefined;
+	editor?: string | undefined;
+	quantity?: number | undefined;
+	price?: number | undefined;
+	year?: number | undefined;
+	outOfPrint?: boolean | undefined;
+	editedBy?: string | undefined;
+	classification?: string | undefined;
 };
