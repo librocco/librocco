7547d90a7fb53d9c2809c13beb84fbfb813cca45
Fadwa <fadwa.mahmoud@gmail.com>
2022-11-09 00:41:52 +0200

    Apply prettier-config-tailwindcss formatting

--- pkg/ui/src/InventoryTable/InventoryTable.svelte
+++ pkg/ui/src/InventoryTable/InventoryTable.svelte
@@ -15,17 +15,14 @@
 
 <div class="px-4 sm:px-6 lg:px-8">
 	<div class="mt-8 flex flex-col">
-		<div class="-my-2 -mx-4 scrollbar-hide overflow-x-auto sm:-mx-6 lg:-mx-8">
+		<div class="-my-2 -mx-4 overflow-x-auto scrollbar-hide sm:-mx-6 lg:-mx-8">
 			<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
 				<div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
 					<table class="min-w-full divide-y divide-gray-300">
 						<thead class="bg-gray-50">
 							<tr>
 								{#each columnTitles as columnTitle (columnTitle)}
-									<th
-										scope="col"
-										class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900"
-									>
+									<th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">
 										{columnTitle}
 									</th>
 								{/each}
