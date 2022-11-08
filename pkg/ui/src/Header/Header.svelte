858f3bd3bf54f0f93c83f44e78fe45e84b2c8699
Fadwa <fadwa.mahmoud@gmail.com>
2022-11-08 19:23:25 +0200

    Fix up styles

--- pkg/ui/src/Header/Header.svelte
+++ pkg/ui/src/Header/Header.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Logo from './Logo.svelte';
+	import { page } from '$app/stores';
+
 	export let title = 'Stock';
+	export let currentUrl = $page.url.pathname;
+
 	const links = [
 		{
 			label: 'Stock',
@@ -15,40 +19,37 @@
 			href: '/inventory/outbound'
 		}
 	];
-	export let currentUrl = 'stock';
 </script>
 
-<div class="bg-gray-900 p-10 pt-5 ">
-	<div class="border-b border-sky-900 pb-5 flex sm:items-center ">
-		<Logo />
+<header class="w-full px-[70px] bg-gray-900">
+	<div class="h-16 border-b border-sky-900 flex items-center ">
+		<div class="mr-4">
+			<Logo />
+		</div>
 
-		<div class="pl-4">
-			<nav class="flex space-x-4">
-				{#each links as { label, href }}
-					<a
-						{href}
-						class={[
-							'px-3 py-2 text-sm rounded-md',
-							currentUrl === label.toLowerCase()
-								? 'bg-teal-500 text-gray-900'
-								: 'text-white hover:text-teal-500'
-						].join(' ')}
-						aria-current={currentUrl === label.toLowerCase() ? 'page' : 'false'}
-					>
-						<span>{label}</span>
-					</a>
-				{/each}
-			</nav>
-		</div>
+		<nav class="flex space-x-4">
+			{#each links as { label, href }}
+				<a
+					{href}
+					class="
+							px-3 py-2 text-sm rounded-md
+							{currentUrl === label.toLowerCase()
+						? 'bg-teal-500 text-gray-900'
+						: 'text-white hover:text-teal-500'}"
+					aria-current={currentUrl === label.toLowerCase() ? 'page' : 'false'}
+				>
+					<span>{label}</span>
+				</a>
+			{/each}
+		</nav>
 	</div>
-	<div class="mt-2 md:flex md:items-center md:justify-between">
-		<div class="min-w-0 flex-1">
-			<h2 class="text-3xl font-bold leading-7 text-white">
-				{title}
-			</h2>
-		</div>
-		<div class="mt-4 pt-4 flex flex-shrink-0 md:mt-0 md:ml-4">
+	<div class="flex h-24 items-center justify-between">
+		<h2 class="text-3xl font-bold leading-7 text-white">
+			{title}
+		</h2>
+
+		<div class="flex">
 			<slot name="actionButton" />
 		</div>
 	</div>
-</div>
+</header>
