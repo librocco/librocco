28dc1cd0867b63e443ad939844714280f80563fd
ikusteu <ivan.kust57@gmail.com>
2022-12-07 23:44:04 +0100

    Improve SelectMenu to fit our use case

--- pkg/ui/src/SelectMenu/SelectMenu.svelte
+++ pkg/ui/src/SelectMenu/SelectMenu.svelte
@@ -8,68 +8,78 @@
 		Transition
 	} from '@rgossiaux/svelte-headlessui';
 	import { Check, ChevronDown } from 'lucide-svelte';
-
-	import { AlignContainerEdge } from './enums';
+	import { createEventDispatcher } from 'svelte';
 
 	interface Option {
-		title: string;
-		description: string;
+		value: string;
+		action?: string;
+		description?: string;
 	}
 
 	export let options: Option[];
-	export let currentOptionIndex: number = 0;
-	export let alignItemsEdge: AlignContainerEdge = AlignContainerEdge.Right;
+	export let align: 'left' | 'right' = 'left';
 
-	let selected: Option;
-	$: selected = options[currentOptionIndex];
+	export let value = options[0].value;
+
+	// We need to handle the change internally because we need to update the value
+	// but we want to dispatch the event to the parent as well
+	const dispatch = createEventDispatcher();
+	const handleChange = (e: CustomEvent) => {
+		value = e.detail;
+		dispatch('change', e.detail.value);
+	};
 </script>
 
-<Listbox {...$$restProps} value={selected} on:change={(e) => (selected = e.detail)} let:open>
-	<div class="relative inline-block text-left">
+<Listbox {...$$restProps} {value} on:change={handleChange} let:open>
+	<div class="relative inline-block select-none text-left">
 		<ListboxLabel class="sr-only" />
-		<div class="inline-flex divide-x divide-teal-600 rounded-md text-sm font-medium text-white shadow-sm">
-			<div class="inline-flex items-center gap-x-2 rounded-l-md bg-teal-500 py-2 px-4">
+		<div
+			class="{$$props.disabled
+				? 'bg-gray-50 ring-[1px] ring-gray-200 divide-gray-200 text-gray-500'
+				: 'divide-teal-600 bg-teal-500 text-white'} inline-flex divide-x  rounded-md text-sm font-medium shadow-sm"
+		>
+			<div class="inline-flex items-center gap-x-2 rounded-l-md  py-2 px-4">
 				<Check aria-hidden="true" />
-				<p>{selected.title}</p>
+				<p class="capitalize">{value}</p>
 			</div>
-			<ListboxButton
-				class="inline-flex items-center rounded-l-none rounded-r-md bg-teal-500 p-2 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 focus:ring-offset-gray-50"
-			>
-				<span class="sr-only" />
-				<ChevronDown aria-hidden="true" />
-			</ListboxButton>
+			{#if !$$props.disabled}
+				<ListboxButton
+					class="{$$props.disabled
+						? ''
+						: 'hover:bg-teal-600'}inline-flex items-center rounded-l-none rounded-r-md p-2 focus:outline-none focus:ring-2"
+				>
+					<span class="sr-only" />
+					<ChevronDown aria-hidden="true" />
+				</ListboxButton>
+			{/if}
 		</div>
 
 		<Transition show={open} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
 			<ListboxOptions
-				class="{alignItemsEdge} focus:outline-none' absolute z-10 mt-2 w-72 origin-top-right cursor-pointer select-none divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5
-			"
+				class="{align === 'left'
+					? 'left-0'
+					: 'right-0'} focus:outline-none' absolute z-10 mt-2 w-72 origin-top-right cursor-pointer divide-y divide-gray-200 overflow-hidden rounded-md shadow-lg ring-1 ring-black ring-opacity-5"
 			>
-				{#each options as option, ix (ix)}
-					<ListboxOption value={option} let:selected let:active>
-						<div
-							class="group p-2.5 hover:bg-teal-500
-								{active ? 'bg-teal-500' : 'bg-white'}"
-						>
+				{#each options as option (option.value)}
+					<ListboxOption value={option.value} let:selected let:active>
+						<div class="group whitespace-normal py-[10px] px-4 hover:bg-teal-500 {active ? 'bg-teal-500' : 'bg-white'}">
 							<div class="flex justify-between">
 								<p
-									class="group-hover:text-white
+									class="capitalize group-hover:text-white
 										{selected ? 'font-semibold' : 'font-normal'}
 										{active ? 'text-white' : 'text-gray-900'}"
 								>
-									{option.title}
+									{option.action || option.value}
 								</p>
 								{#if selected}
-									<Check
-										aria-hidden="true"
-										class="group-hover:text-white
-											{active ? 'text-white' : 'text-teal-500'}"
-									/>
+									<Check aria-hidden="true" class="group-hover:text-white {active ? 'text-white' : 'text-teal-500'}" />
 								{/if}
 							</div>
-							<p class="text-gray-500 group-hover:text-gray-200 group-active:text-gray-200">
-								{option.description}
-							</p>
+							{#if option.description}
+								<p class="{active ? 'text-white' : 'text-gray-500'} group-hover:text-white group-active:text-white">
+									{option.description}
+								</p>
+							{/if}
 						</div>
 					</ListboxOption>
 				{/each}
