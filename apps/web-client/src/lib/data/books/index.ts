376f259fd553e1fcf3a7f559f757051053c39346
ikusteu <ivan.kust57@gmail.com>
2022-12-12 10:36:03 +0100

    Update test data to include more books in jazz warehouse (for pagination)

--- apps/web-client/src/lib/data/books/index.ts
+++ apps/web-client/src/lib/data/books/index.ts
@@ -1,19 +1,51 @@
 import type { BookStore } from '../backend_temp';
 
-import book1 from './0007149522.json';
+import book0 from './0007149522.json';
+import book1 from './0028724305.json';
 import book2 from './0061983411.json';
-import book3 from './0080524230.json';
-import book4 from './0194349276.json';
-import book5 from './0195071409.json';
-import book6 from './0195399706.json';
-import book7 from './019976915X.json';
+import book3 from './0076112950.json';
+import book4 from './0080524230.json';
+import book5 from './014028656X.json';
+import book6 from './0140515216.json';
+import book7 from './0194349276.json';
+import book8 from './0195071409.json';
+import book9 from './0195399706.json';
+import book10 from './019976915X.json';
+import book11 from './0205697518.json';
+import book12 from './022623603X.json';
+import book13 from './0253210682.json';
+import book14 from './0292726368.json';
+import book15 from './0300104200.json';
+import book16 from './0306795280.json';
+import book17 from './0312429479.json';
+import book18 from './0312857438.json';
+import book19 from './0375421491.json';
+import book20 from './0385474849.json';
+import book21 from './0415939658.json';
+import book22 from './0446710059.json';
 
 export default {
-	'0007149522': book1,
+	'0007149522': book0,
+	'0028724305': book1,
 	'0061983411': book2,
-	'0080524230': book3,
-	'0194349276': book4,
-	'0195071409': book5,
-	'0195399706': book6,
-	'019976915X': book7
+	'0076112950': book3,
+	'0080524230': book4,
+	'014028656X': book5,
+	'0140515216': book6,
+	'0194349276': book7,
+	'0195071409': book8,
+	'0195399706': book9,
+	'019976915X': book10,
+	'0205697518': book11,
+	'022623603X': book12,
+	'0253210682': book13,
+	'0292726368': book14,
+	'0300104200': book15,
+	'0306795280': book16,
+	'0312429479': book17,
+	'0312857438': book18,
+	'0375421491': book19,
+	'0385474849': book20,
+	'0415939658': book21,
+	'0446710059': book22
 } as BookStore;
