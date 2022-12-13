d78279ba07b390449935f4ad29e1070fcbbdbdb1
ikusteu <ivan.kust57@gmail.com>
2022-12-13 09:32:48 +0100

    Update Pagination component:
    * Ditch the Link and wrapper logic
    * Change 'links', 'currentItem' to 'numPages', 'value' respectively
    * Replace 'onPaginate' with 'on:change' logic
    * Enable binding to the 'value' variable
    * Create TestBindValue component (for testing)
    * Update Pagination tests accordingly

--- pkg/ui/src/Pagination/__tests__/Pagination.spec.ts
+++ pkg/ui/src/Pagination/__tests__/Pagination.spec.ts
@@ -1,15 +1,14 @@
 /**
  * @vitest-environment jsdom
  */
+import { get } from 'svelte/store';
 import { describe, test, expect, vi, afterEach } from 'vitest';
 import { cleanup, render, screen } from '@testing-library/svelte';
 
 import Pagination from '../Pagination.svelte';
 
-import MockLink from './MockLink.svelte';
-import { mockNavigate } from './mockNavigate';
-
 import { getItemsToRender } from '../utils';
+import TestBindValue, { bindValueStore } from '$lib/__testUtils__/TestBindValue.svelte';
 
 /**
  * Runs table tests on the 'getItemsToRender' to test for all possible inputs for
@@ -21,83 +20,86 @@ import { getItemsToRender } from '../utils';
  */
 const runTableTests = (
 	{ numItems, maxItems }: { numItems: number; maxItems: number },
-	tests: [{ currentItem: number }, Array<number | null>][]
+	tests: [{ currentPage: number }, Array<number | null>][]
 ) => {
-	tests.forEach(([{ currentItem }, want]) => {
-		test(`Input: numItems = ${numItems}, maxItems = ${maxItems}, currentItem = ${currentItem}, want = ${want}`, () => {
-			expect(getItemsToRender(numItems, maxItems, currentItem)).toEqual(want);
+	tests.forEach(([{ currentPage }, want]) => {
+		test(`Input: numItems = ${numItems}, maxItems = ${maxItems}, currentPage = ${currentPage}, want = ${want}`, () => {
+			expect(getItemsToRender(numItems, maxItems, currentPage)).toEqual(want);
 		});
 	});
 };
 
 describe('Pagination', () => {
+	afterEach(() => {
+		cleanup();
+		vi.clearAllMocks();
+		bindValueStore.reset();
+	});
+
 	describe('Component tests', () => {
 		afterEach(() => {
 			cleanup();
 			vi.clearAllMocks();
 		});
 
-		test("should not explode if no inputs for 'onPaginate' nor 'Wrapper' provided", () => {
-			render(Pagination, { currentItem: 0, links: ['link1'] });
+		test("should not explode if no inputs for 'onchange' nor 'Wrapper' provided", () => {
+			render(Pagination, { value: 0, numPages: 1 });
 			screen.getAllByRole('button')[0].click();
 		});
 
 		test('should not render if there are no items to show', () => {
-			render(Pagination, { currentItem: 0, links: [] });
+			render(Pagination, { value: 0, numPages: 0 });
 			expect(screen.queryByRole('button')).toBeFalsy();
 		});
 
-		test("should call to 'onPaginate' function, passing link and index of an item on item button click", () => {
-			const mockOnPaginate = vi.fn();
-			render(Pagination, { currentItem: 1, links: ['link1', 'link2'], onPaginate: mockOnPaginate });
+		test("should call to 'onchange' function, passing link and index of an item on item button click", () => {
+			const mockChange = vi.fn();
+			const { component } = render(Pagination, { value: 1, numPages: 2 });
+			component.$on('change', mockChange);
 			screen.getByText('1').click();
-			expect(mockOnPaginate).toHaveBeenCalledWith('link1', 0);
+			expect(mockChange.mock.calls[0][0].detail).toEqual(0);
 		});
 
-		test("should call to 'onPaginate' function, passing previous/next link and index of 'currentItem' left/right arrow click respectively", () => {
-			const mockOnPaginate = vi.fn();
-			const threeItems = Array(3)
-				.fill(null)
-				.map((_, i) => `link-${i}`);
-			render(Pagination, { links: threeItems, currentItem: 1, onPaginate: mockOnPaginate });
+		test("should enable binding to the 'value' for current page", () => {
+			render(TestBindValue, {
+				props: { props: { value: 1, numPages: 2 }, Component: Pagination }
+			});
+			screen.getByText('1').click();
+			expect(get(bindValueStore)).toEqual(0);
+		});
+
+		test("should call to 'onchange' function, passing previous/next link and index of 'value' left/right arrow click respectively", () => {
+			const mockChange = vi.fn();
+			const { component } = render(Pagination, { numPages: 3, value: 1 });
+			component.$on('change', mockChange);
 			const [prevButton, , , , nextButton] = screen.getAllByRole('button');
 			prevButton.click();
-			expect(mockOnPaginate).toHaveBeenCalledWith('link-0', 0);
+			expect(mockChange.mock.calls[0][0].detail).toEqual(0);
 			nextButton.click();
-			expect(mockOnPaginate).toHaveBeenCalledWith('link-2', 2);
+			expect(mockChange.mock.calls[1][0].detail).toEqual(1);
 		});
 
 		test('should disable prev/next buttons if no prevoius or next item respectively', () => {
-			render(Pagination, { currentItem: 0, links: ['link-1'] });
+			render(Pagination, { value: 0, numPages: 1 });
 			const [prevButton, , nextButton] = screen.getAllByRole('button');
 			expect(prevButton).toHaveProperty('disabled', true);
 			expect(nextButton).toHaveProperty('disabled', true);
 		});
 
-		test('should disable currentItem button for clicking', () => {
-			render(Pagination, { currentItem: 0, links: ['link-1'] });
-			const currentItemButton = screen.getByText('1');
-			expect(currentItemButton).toHaveProperty('disabled', true);
+		test('should disable value button for clicking', () => {
+			render(Pagination, { value: 0, numPages: 1 });
+			const valueButton = screen.getByText('1');
+			expect(valueButton).toHaveProperty('disabled', true);
 		});
 
 		test('elipsis ("...") should not be clickable', () => {
-			const mockOnPaginate = vi.fn();
-			// Generate a large enough number of links so that the elipsis has to be shown
-			const links = Array(12)
-				.fill(null)
-				.map((_, i) => `link-${i}`);
-			render(Pagination, { currentItem: 0, links, onPaginate: mockOnPaginate });
+			const mockChange = vi.fn();
+			// Generate a large enough number of pages so that the elipsis has to be shown
+			const numPages = 12;
+			const { component } = render(Pagination, { value: 0, numPages });
+			component.$on('change', mockChange);
 			screen.getByText('...').click();
-			expect(mockOnPaginate).not.toHaveBeenCalled();
-		});
-
-		test("should wrap buttons in 'Wrapper' component if one provided", () => {
-			const links = ['link-1', 'link-2'];
-			// We're using `MockLink` to test wrapping logic
-			// and asseting calls to `mockNavigate` (consumed by `MockLink`)
-			render(Pagination, { currentItem: 0, links, Wrapper: MockLink });
-			screen.getByText('2').click();
-			expect(mockNavigate).toHaveBeenCalledWith('link-2');
+			expect(mockChange).not.toHaveBeenCalled();
 		});
 	});
 
@@ -112,7 +114,7 @@ describe('Pagination', () => {
 			expect(error).toBeTruthy();
 		});
 
-		test("should throw an error if 'currentItem' out of range", () => {
+		test("should throw an error if 'value' out of range", () => {
 			let tooHigh;
 			try {
 				getItemsToRender(1, 5, 1);
@@ -144,51 +146,51 @@ describe('Pagination', () => {
 	describe("Table tests for 'getItemsToRender'", () => {
 		runTableTests({ numItems: 11, maxItems: 5 }, [
 			//  "1"|  2 |  3 | ...| 11
-			[{ currentItem: 0 }, [0, 1, 2, null, 10]],
+			[{ currentPage: 0 }, [0, 1, 2, null, 10]],
 			//   1 | "2"|  3 | ...| 11
-			[{ currentItem: 1 }, [0, 1, 2, null, 10]],
+			[{ currentPage: 1 }, [0, 1, 2, null, 10]],
 			//   1 |  2 | "3"| ...| 11
-			[{ currentItem: 2 }, [0, 1, 2, null, 10]],
+			[{ currentPage: 2 }, [0, 1, 2, null, 10]],
 			//   1 | ...| "4"| ...| 11
-			[{ currentItem: 3 }, [0, null, 3, null, 10]],
+			[{ currentPage: 3 }, [0, null, 3, null, 10]],
 			//   1 | ...| "5"| ...| 11
-			[{ currentItem: 4 }, [0, null, 4, null, 10]],
+			[{ currentPage: 4 }, [0, null, 4, null, 10]],
 			//   1 | ...| "6"| ...| 11
-			[{ currentItem: 5 }, [0, null, 5, null, 10]],
+			[{ currentPage: 5 }, [0, null, 5, null, 10]],
 			//   1 | ...| "7"| ...| 11
-			[{ currentItem: 6 }, [0, null, 6, null, 10]],
+			[{ currentPage: 6 }, [0, null, 6, null, 10]],
 			//   1 | ...| "8"| ...| 11
-			[{ currentItem: 7 }, [0, null, 7, null, 10]],
+			[{ currentPage: 7 }, [0, null, 7, null, 10]],
 			//   1 | ...| "9"| 10 | 11
-			[{ currentItem: 8 }, [0, null, 8, 9, 10]],
+			[{ currentPage: 8 }, [0, null, 8, 9, 10]],
 			//   1 | ...|  9 |"10"| 11
-			[{ currentItem: 9 }, [0, null, 8, 9, 10]],
+			[{ currentPage: 9 }, [0, null, 8, 9, 10]],
 			//   1 | ...|  9 | 10 |"11"
-			[{ currentItem: 10 }, [0, null, 8, 9, 10]]
+			[{ currentPage: 10 }, [0, null, 8, 9, 10]]
 		]);
 		runTableTests({ numItems: 11, maxItems: 7 }, [
 			//  "1"|  2 |  3 |  4 |  5 | ...| 11
-			[{ currentItem: 0 }, [0, 1, 2, 3, 4, null, 10]],
+			[{ currentPage: 0 }, [0, 1, 2, 3, 4, null, 10]],
 			//   1 | "2"|  3 |  4 |  5 | ...| 11
-			[{ currentItem: 1 }, [0, 1, 2, 3, 4, null, 10]],
+			[{ currentPage: 1 }, [0, 1, 2, 3, 4, null, 10]],
 			//   1 |  2 | "3"|  4 |  5 | ...| 11
-			[{ currentItem: 2 }, [0, 1, 2, 3, 4, null, 10]],
+			[{ currentPage: 2 }, [0, 1, 2, 3, 4, null, 10]],
 			//   1 |  2 |  3 | "4"|  5 | ...| 11
-			[{ currentItem: 3 }, [0, 1, 2, 3, 4, null, 10]],
+			[{ currentPage: 3 }, [0, 1, 2, 3, 4, null, 10]],
 			//   1 | ...|  4 | "5"|  6 | ...| 11
-			[{ currentItem: 4 }, [0, null, 3, 4, 5, null, 10]],
+			[{ currentPage: 4 }, [0, null, 3, 4, 5, null, 10]],
 			//   1 | ...|  5 | "6"|  7 | ...| 11
-			[{ currentItem: 5 }, [0, null, 4, 5, 6, null, 10]],
+			[{ currentPage: 5 }, [0, null, 4, 5, 6, null, 10]],
 			//   1 | ...|  6 | "7"|  8 | ...| 11
-			[{ currentItem: 6 }, [0, null, 5, 6, 7, null, 10]],
+			[{ currentPage: 6 }, [0, null, 5, 6, 7, null, 10]],
 			//   1 | ...|  7 | "8"|  9 | 10 | 11
-			[{ currentItem: 7 }, [0, null, 6, 7, 8, 9, 10]],
+			[{ currentPage: 7 }, [0, null, 6, 7, 8, 9, 10]],
 			//   1 | ...|  7 |  8 | "9"| 10 | 11
-			[{ currentItem: 8 }, [0, null, 6, 7, 8, 9, 10]],
+			[{ currentPage: 8 }, [0, null, 6, 7, 8, 9, 10]],
 			//   1 | ...|  7 |  8 |  9 |"10"| 11
-			[{ currentItem: 9 }, [0, null, 6, 7, 8, 9, 10]],
+			[{ currentPage: 9 }, [0, null, 6, 7, 8, 9, 10]],
 			//   1 | ...|  7 |  8 |  9 | 10 |"11"
-			[{ currentItem: 10 }, [0, null, 6, 7, 8, 9, 10]]
+			[{ currentPage: 10 }, [0, null, 6, 7, 8, 9, 10]]
 		]);
 	});
 });
