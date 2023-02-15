ef0c8d5959f3d448c5182dfe9a4a520c68ada9cf
delasous <sousa.yak@googlemail.com>
2023-02-15 16:14:32 +0100

    Draft simple text field form

---
+++ pkg/ui/src/Forms/BookDetailForm.story.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+
+	import BookDetailForm from './BookDetailForm.svelte';
+
+	export let Hst: Hst;
+
+	const book = {
+		isbn: '',
+		title: '',
+		authors: [],
+		publisher: '',
+		year: '',
+		price: 0
+	};
+</script>
+
+<Hst.Story title="Forms / BookDetailForm" layout={{ type: 'grid', width: 600 }}>
+	<BookDetailForm {book} onSubmit={() => {}} />
+</Hst.Story>
