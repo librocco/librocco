a94ad5d351da6cab460ea7e699f1e05d90ce940c
delasous <sousa.yak@googlemail.com>
2023-02-15 16:14:32 +0100

    Draft simple text field form

---
+++ pkg/ui/src/Forms/BookDetailForm.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { createForm } from 'felte';
+
+	import { TextField } from '../FormFields';
+	import { Button, ButtonColor } from '../Button';
+
+	import type { BookEntry } from './types';
+
+	export let book: BookEntry;
+	export let onSubmit: (values: BookEntry) => void = () => {};
+	export let onCancel = () => {};
+
+	const { form } = createForm({
+		initialValues: book,
+		onSubmit
+	});
+</script>
+
+<form class="flex h-auto flex-col gap-y-6" use:form aria-label="Edit book details">
+	<div class="flex flex-col justify-between gap-6 p-6 lg:flex-row-reverse">
+		<div class="flex grow flex-col flex-wrap gap-y-4 lg:flex-row">
+			<div class="basis-full">
+				<TextField name="isbn" label="ISBN" />
+			</div>
+			<div class="basis-full">
+				<TextField name="title" label="Title" />
+			</div>
+		</div>
+	</div>
+	<div class="flex justify-end gap-x-2 p-4">
+		<Button color={ButtonColor.Secondary} on:click={() => onCancel}>Cancel</Button>
+		<Button>Save</Button>
+	</div>
+</form>
