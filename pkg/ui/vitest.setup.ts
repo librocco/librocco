642d5079d8190ef54f33d781e20d6cf059243a93
delasous <sousa.yak@googlemail.com>
2023-02-16 10:38:25 +0100

    Extend testing setup

---
+++ pkg/ui/vitest.setup.ts
@@ -0,0 +1,15 @@
+import { expect } from "vitest";
+import matchers, { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+
+// Required to appease TS
+// Merges standard matchers declaration with js-dom extensions
+declare global {
+	namespace Vi {
+		interface JestAssertion<T = any>
+			extends jest.Matchers<void, T>,
+				TestingLibraryMatchers<T, void> {}
+	}
+}
+
+// Add custom jest matchers
+expect.extend(matchers);
