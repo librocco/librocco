31ddcde0dc9b67ccbea8c52d33a7356f8719781a
ikusteu <ivankust57@gmail.com>
2023-02-16 09:20:15 +0100

    Create a 'newTestDB' function, returning db interface for testing

---
+++ apps/web-client/src/lib/__testUtils__/db.ts
@@ -0,0 +1,13 @@
+import PouchDB from 'pouchdb';
+import MemoryAdapter from 'pouchdb-adapter-memory';
+import { v4 as uuid } from 'uuid';
+import { newDatabaseInterface } from '@librocco/db';
+
+// Enable running of the tests against in-memory PouchDB
+PouchDB.plugin(MemoryAdapter);
+
+/** Creates a new db interface using pouchdb with unique id and memory adapter */
+export const newTestDB = () => {
+	const db = new PouchDB(uuid(), { adapter: 'memory' });
+	return newDatabaseInterface(db);
+};
