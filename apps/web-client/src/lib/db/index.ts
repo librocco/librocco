d2b917a65967c024e78d5985498146fd760bff88
ikusteu <ivankust57@gmail.com>
2023-02-24 12:25:08 +0100

    Update load functions:
    * Update root layout load with 'db.init()' logic
    * Update intervals layout load accordingly
    * Fix small bugs in interval layout load

--- apps/web-client/src/lib/db/index.ts
+++ apps/web-client/src/lib/db/index.ts
@@ -1,12 +1,7 @@
 import pouchdb from 'pouchdb';
 
-import { newDatabaseInterface, designDocuments } from '@librocco/db';
+import { newDatabaseInterface } from '@librocco/db';
 
 // @TODO: This should probably be an env variable
-const pouch = new pouchdb('http://admin:admin@127.0.0.1:5000/dev');
+const pouch = new pouchdb('dev');
 export const db = newDatabaseInterface(pouch);
-
-/**
- * A helper method used to initialise the db. This should be ran once on app start.
- */
-export const initDB = () => Promise.all(designDocuments.map((doc) => db.updateDesignDoc(doc)));
