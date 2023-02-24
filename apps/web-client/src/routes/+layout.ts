e486213a5fa34eee1f13f2bdbab4fddb24bf2dd0
ikusteu <ivankust57@gmail.com>
2023-02-24 12:25:08 +0100

    Update load functions:
    * Update root layout load with 'db.init()' logic
    * Update intervals layout load accordingly
    * Fix small bugs in interval layout load

--- apps/web-client/src/routes/+layout.ts
+++ apps/web-client/src/routes/+layout.ts
@@ -1,19 +1,25 @@
 import { redirect } from '@sveltejs/kit';
 
-import { initDB } from '$lib/db';
+import { db } from '$lib/db';
 
 import type { LayoutLoad } from './$types';
+import { browser } from '$app/environment';
 
 // Paths which are valid (shouldn't return 404, but don't have any content and should get redirected to the default route "/inventory/stock/all")
 const redirectPaths = ['', '/', '/inventory', '/inventory/', '/inventory/stock', '/inventory/stock/'];
 
-export const load: LayoutLoad = async ({ url }) => {
-	await initDB();
-
-	const { pathname } = url;
-
-	if (redirectPaths.includes(pathname)) {
+export const load: LayoutLoad = async ({ route }) => {
+	if (redirectPaths.includes(route.id)) {
 		throw redirect(307, '/inventory/stock/0-all');
 	}
+
+	// If in browser, we init the db, otherwise this is a prerender, for which we're only building basic html skeleton
+	if (browser) {
+		return {
+			db: await db.init({}, { name: '[DB_INIT]', debug: false })
+		};
+	}
+
+	return {};
 };
 export const prerender = true;
