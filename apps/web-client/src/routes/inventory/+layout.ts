9ecd645badb36cf28ec2cd029d6956fe654158ee
ikusteu <ivankust57@gmail.com>
2023-02-24 12:25:08 +0100

    Update load functions:
    * Update root layout load with 'db.init()' logic
    * Update intervals layout load accordingly
    * Fix small bugs in interval layout load

--- apps/web-client/src/routes/inventory/+layout.ts
+++ apps/web-client/src/routes/inventory/+layout.ts
@@ -3,8 +3,6 @@ import { browser } from '$app/environment';
 
 import type { LayoutLoad } from './$types';
 
-import { db } from '$lib/db';
-
 const redirects = {
 	outbound: '/inventory/outbound',
 	inbound: '/inventory/inbound',
@@ -13,21 +11,21 @@ const redirects = {
 
 export const load: LayoutLoad = async ({ route, params, parent }) => {
 	// await db init in ../layout.ts
-	// @TODO: parent should probably return the db interface itself, so no need for import (after that unit of work is in)
-	await parent();
+	const { db } = await parent();
 
-	// This should re-run on change to path, as far as I understand: https://kit.svelte.dev/docs/load#invalidation
-	const docId = params?.id;
-
-	const [, view, location] = route.id.split('/'); // e.g [ '', 'inventory', 'stock', '[...id]' ]
-
-	// DB is initialised only in the browser, so we're prerendering only the html skeleton (without note/warehouse data)
-	if (!browser) {
+	// If db is not returned (we're not in the browser environment, no need for additional loading)
+	if (!db) {
 		return {};
 	}
 
+	// This should re-run on change to path, as far as I understand: https://kit.svelte.dev/docs/load#invalidation
+	const docId = params?.id;
+	console.log('docId', docId);
+
+	const [, , location] = route.id.split('/'); // e.g [ '', 'inventory', 'stock', '[...id]' ]
+
 	// If we're on the stock page (warehouse view), we're only interested in the warehouse
-	if (view !== 'stock' && docId) {
+	if (location === 'stock' && docId) {
 		const warehouse = await db.warehouse(docId).get();
 		if (!warehouse) {
 			throw redirect(307, redirects[location]);
@@ -37,10 +35,13 @@ export const load: LayoutLoad = async ({ route, params, parent }) => {
 	}
 
 	// In note view ('inbound/outbount') we need both the note and the warehouse (and db.findNote returns exactly that)
-	const findNoteRes = await db.findNote(docId);
-	if (!findNoteRes) {
-		throw redirect(307, redirects[location]);
+	if (docId) {
+		const findNoteRes = await db.findNote(docId);
+		if (!findNoteRes) {
+			throw redirect(307, redirects[location]);
+		}
+		return findNoteRes;
 	}
 
-	return findNoteRes;
+	return {};
 };
