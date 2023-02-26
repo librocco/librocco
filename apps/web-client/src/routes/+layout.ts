a4b950583ace114cbd1eb3a9ae34207bfc49fa6c
ikusteu <ivankust57@gmail.com>
2023-02-26 12:09:00 +0100

    Small fix: replace 'route.id' with 'url.pathname' in top level layout load fn

--- apps/web-client/src/routes/+layout.ts
+++ apps/web-client/src/routes/+layout.ts
@@ -8,8 +8,9 @@ import { browser } from '$app/environment';
 // Paths which are valid (shouldn't return 404, but don't have any content and should get redirected to the default route "/inventory/stock/all")
 const redirectPaths = ['', '/', '/inventory', '/inventory/', '/inventory/stock', '/inventory/stock/'];
 
-export const load: LayoutLoad = async ({ route }) => {
-	if (redirectPaths.includes(route.id)) {
+export const load: LayoutLoad = async ({ url }) => {
+	const { pathname } = url;
+	if (redirectPaths.includes(pathname)) {
 		throw redirect(307, '/inventory/stock/0-all');
 	}
 
