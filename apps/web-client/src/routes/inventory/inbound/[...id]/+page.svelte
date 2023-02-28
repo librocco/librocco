a1f98a95d54af571eca3d2a35f629443d3e860d2
ikusteu <ivankust57@gmail.com>
2023-02-28 11:12:37 +0100

    Remove the annoying 'dev' folder written to fs by the dev server:
    * Instead of instantiating and exporting the db (and PouchDB) instance, create a 'createDB' function
    * Instead of exporting the 'db' instance, export 'getDB' function
    * Instantiate the 'db' (and PouchDB) instance only in browser environment (in top level layout load function)
    * Use 'DB_NAME' env variable when instantiating the PouchDB instance (instead of hardcoded value for name)

--- apps/web-client/src/routes/inventory/inbound/[...id]/+page.svelte
+++ apps/web-client/src/routes/inventory/inbound/[...id]/+page.svelte
@@ -21,17 +21,22 @@
 
 	import type { PageData } from './$types';
 
-	import { createNoteStores } from '$lib/stores/inventory';
+	import { getDB } from '$lib/db';
 
-	import { db } from '$lib/db';
+	import { createNoteStores } from '$lib/stores/inventory';
 
 	import { generateUpdatedAtString } from '$lib/utils/time';
 	import { readableFromStream } from '$lib/utils/streams';
 
 	export let data: PageData;
 
+	// Db will be undefined only on server side. If in browser,
+	// it will be defined immediately, but `db.init` is ran asynchronously.
+	// We don't care about 'db.init' here (for nav stream), hence the non-reactive 'const' declaration.
+	const db = getDB();
+
 	const inNoteListCtx = { name: '[IN_NOTE_LIST]', debug: false };
-	const inNoteList = readableFromStream(db.stream(inNoteListCtx).inNoteList, [], inNoteListCtx);
+	const inNoteList = readableFromStream(db?.stream(inNoteListCtx).inNoteList, [], inNoteListCtx);
 
 	$: note = data.note;
 	$: warehouse = data.warehouse;
--- apps/web-client/src/routes/inventory/stock/[...id]/+page.svelte
+++ apps/web-client/src/routes/inventory/stock/[...id]/+page.svelte
@@ -13,16 +13,23 @@
 		TextEditable
 	} from '@librocco/ui';
 
-	import { db } from '$lib/db';
-	import { createWarehouseStores } from '$lib/stores/inventory';
-	import { readableFromStream } from '$lib/utils/streams';
-
 	import type { PageData } from './$types';
+
+	import { getDB } from '$lib/db';
+
+	import { createWarehouseStores } from '$lib/stores/inventory';
+
+	import { readableFromStream } from '$lib/utils/streams';
 
 	export let data: PageData;
 
+	// Db will be undefined only on server side. If in browser,
+	// it will be defined immediately, but `db.init` is ran asynchronously.
+	// We don't care about 'db.init' here (for nav stream), hence the non-reactive 'const' declaration.
+	const db = getDB();
+
 	const wareouseListCtx = { name: '[WAREHOUSE_LIST]', debug: false };
-	const warehouseList = readableFromStream(db.stream(wareouseListCtx).warehouseList, [], wareouseListCtx);
+	const warehouseList = readableFromStream(db?.stream(wareouseListCtx).warehouseList, [], wareouseListCtx);
 
 	$: warehouse = data.warehouse;
 
