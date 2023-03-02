bb3c343f7b7d23dac5cf80bfad1f8a048246201d
Fadwa <fadwa.mahmoud@gmail.com>
2023-03-03 01:42:45 +0200

    New Entity SideNav Button

---
+++ pkg/ui/src/Sidebar/NewEntitySideNavButton.story.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+
+	import SideNavButton from './NewEntitySideNavButton.svelte';
+
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="Sidebar Navigation / New Entity" layout={{ type: 'grid', width: 300 }}>
+	<Hst.Variant title="Inactive">
+		<SideNavButton active={false} />
+	</Hst.Variant>
+	<Hst.Variant title="Active">
+		<SideNavButton />
+	</Hst.Variant>
+	<Hst.Variant title="Warehouse">
+		<SideNavButton label="New Warehouse" />
+	</Hst.Variant>
+</Hst.Story>
