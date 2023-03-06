c7c6b4543c96b54aa3ba13845b47836a5b6aa81e
delasous <sousa.yak@googlemail.com>
2023-03-06 13:21:14 +0100

    Remove InventoryTableRow component & rebuild to new mocks

--- pkg/ui/src/InventoryTable/InventoryTable.story.svelte
+++ pkg/ui/src/InventoryTable/InventoryTable.story.svelte
@@ -1,75 +1,66 @@
 <script lang="ts">
 	import type { Hst } from '@histoire/plugin-svelte';
-	import { InventoryTableRow, InventoryTable } from './index';
+	import InventoryTable from './InventoryTable.svelte';
 	import type { TableData } from './types';
 	export let Hst: Hst;
 
 	const rows: TableData[] = [
 		{
-			isbn: '917289012312',
+			isbn: '917289012390',
 			title: 'Miti Del Nord',
 			author: 'Neil Gaiman',
-			editor: '',
 			quantity: 3,
 			price: 10,
 			year: '2017',
 			outOfPrint: false,
-			editedBy: '',
-			classification: ''
+			editedBy: ''
 		},
 		{
-			isbn: '917289012312',
+			isbn: '917289012381',
 			title: 'Hellenistic history and culture',
 			author: 'Peter Green',
 			quantity: 3,
 			price: 10,
 			year: '2017',
 			outOfPrint: false,
-			editedBy: '',
-			classification: ''
+			editedBy: ''
 		},
 
 		{
-			isbn: '917289012310',
+			isbn: '917289012323',
 			title: 'Studies in Greek culture and Roman policy',
 			author: 'Robert Lamberton',
-			editor: '',
 			quantity: 3,
 			price: 10,
 			year: '2017',
 			outOfPrint: false,
-			editedBy: '',
-			classification: ''
+			editedBy: ''
 		},
 		{
-			isbn: '917289012312',
+			isbn: '917289012371',
 			title: 'Miti Del Nord',
 			author: 'Neil Gaiman',
-			editor: '',
 			quantity: 3,
 			price: 10,
 			year: '2017',
 			outOfPrint: true,
-			classification: ''
+			editedBy: ''
 		},
 		{
-			isbn: '917289012312',
+			isbn: '917289012327',
 			title: 'Hellenistic history and culture',
 			author: 'Peter Green',
-			editor: '',
 			quantity: 3,
 			price: 10,
 			year: '2017',
 			outOfPrint: false,
-			editedBy: '',
-			classification: ''
+			editedBy: ''
 		},
 
 		{
-			isbn: '917289012310',
+			isbn: '917289012188',
 			title: 'Studies in Greek culture and Roman policy',
 			author: 'Robert Lamberton',
-			editor: '',
 			quantity: 3,
 			price: 10,
 			year: '2017',
@@ -81,10 +72,6 @@
 
 <Hst.Story title="Table">
 	<Hst.Variant>
-		<InventoryTable>
-			{#each rows as data}
-				<InventoryTableRow {data} />
-			{/each}
-		</InventoryTable>
+		<InventoryTable {rows} />
 	</Hst.Variant>
 </Hst.Story>
--- pkg/ui/src/InventoryTable/types.ts
+++ pkg/ui/src/InventoryTable/types.ts
@@ -1,12 +1,10 @@
 export type TableData = {
-	isbn?: string | undefined;
-	title?: string | undefined;
-	author?: string | undefined;
-	editor?: string | undefined;
-	quantity?: number | undefined;
-	price?: number | undefined;
-	year?: string | undefined;
-	outOfPrint?: boolean | undefined;
-	editedBy?: string | undefined;
-	classification?: string | undefined;
+	isbn?: string;
+	title?: string;
+	author?: string;
+	quantity?: number;
+	price?: number;
+	year?: string;
+	editedBy?: string;
+	outOfPrint?: boolean;
 };
