7af33bd22de06d6670d5588e77b64ac504a4cad5
delasous <sousa.yak@googlemail.com>
2023-03-06 13:21:14 +0100

    Remove InventoryTableRow component & rebuild to new mocks

--- pkg/ui/src/InventoryTable/InventoryTable.svelte
+++ pkg/ui/src/InventoryTable/InventoryTable.svelte
@@ -1,36 +1,76 @@
 <script lang="ts">
+	import type { TableData } from './types';
+	import { Checkbox } from '../';
+
+	export let rows: TableData[];
+
 	const columnTitles: string[] = [
 		'ISBN',
 		'Title',
 		'Author',
-		'Editor',
 		'Quantity',
 		'Price',
-		'Year',
-		'Out Of Print',
 		'Edited By',
-		'Classification'
+		'Out of Print',
+		'Year'
 	];
 </script>
 
-<div class="overflow-x-auto shadow shadow-gray-300 scrollbar-hide">
-	<div class="inline-block min-w-full align-middle">
-		<div class="overflow-hidden">
-			<table class="min-w-full divide-y divide-gray-300">
-				<thead class="bg-gray-50">
-					<tr>
-						{#each columnTitles as columnTitle (columnTitle)}
-							<th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">
-								{columnTitle}
-							</th>
-						{/each}
-					</tr>
-				</thead>
+<div class="overflow-x-auto">
+	<table class="min-w-full divide-y divide-gray-200 bg-white">
+		<thead>
+			<tr>
+				<th scope="col" class="px-2">
+					<Checkbox name="Select all" />
+				</th>
+				{#each columnTitles as columnTitle (columnTitle)}
+					<th
+						scope="col"
+						class="whitespace-nowrap py-2 px-3  text-left text-sm font-medium uppercase tracking-wide text-gray-500"
+					>
+						{columnTitle}
+					</th>
+				{/each}
+			</tr>
+		</thead>
 
-				<tbody class="divide-y  divide-gray-200 bg-white">
-					<slot />
-				</tbody>
-			</table>
-		</div>
-	</div>
+		<tbody>
+			{#each rows as { isbn, title, author, quantity, price, year, outOfPrint, editedBy } (isbn)}
+				<tr class="whitespace-nowrap text-sm font-light text-gray-500 even:bg-gray-50">
+					<td class="px-2">
+						<span class="inline-block">
+							<Checkbox name={`Select ${title}`} />
+						</span>
+					</td>
+					<th scope="row" class="py-4 px-3 font-medium text-gray-800">
+						{isbn}
+					</th>
+					<td class="py-4 px-3">
+						{title}
+					</td>
+					<td class="py-4 px-3">
+						{author}
+					</td>
+					<td class="py-4 px-3 text-center">
+						{quantity}
+					</td>
+					<td class="py-4 px-3 text-center">
+						{price}
+					</td>
+
+					<td class="py-4 px-3">
+						{editedBy}
+					</td>
+					<td class="py-4 px-3 text-center">
+						<span class="inline-block">
+							<Checkbox name={`${title} is out of print: ${outOfPrint}`} checked={outOfPrint} disabled />
+						</span>
+					</td>
+					<td class="py-4 px-3 text-center">
+						{year}
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
