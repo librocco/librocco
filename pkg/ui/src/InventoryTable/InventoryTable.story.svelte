41561d938d28bfe8035f2798bea4eeda22a77919
delasous <sousa.yak@googlemail.com>
2023-03-09 17:41:19 +0100

    Generalise table action factory data type & add keys to row data

--- pkg/ui/src/InventoryTable/InventoryTable.story.svelte
+++ pkg/ui/src/InventoryTable/InventoryTable.story.svelte
@@ -1,29 +1,16 @@
 <script lang="ts">
 	import type { Hst } from '@histoire/plugin-svelte';
-	import { v4 as uuidv4 } from 'uuid';
 
 	import { Button, ButtonColor } from '../Button';
 
 	import InventoryTable from './InventoryTable.svelte';
 	import { createTable } from '../lib/actions';
 
-	import type { TableData } from './types';
+	import type { InventoryTableData } from './types';
 
 	export let Hst: Hst;
 
-	const newRow = () => ({
-		isbn: uuidv4(),
-		title: 'Miti Del Nord',
-		authors: 'Neil Gaiman',
-		quantity: 3,
-		price: 10,
-		publisher: 'Mondadori',
-		year: '2017',
-		editedBy: '',
-		outOfPrint: false
-	});
-
-	const rows: TableData[] = [
+	const rows: InventoryTableData[] = [
 		{
 			isbn: '917289012390',
 			title: 'Miti Del Nord',
@@ -57,51 +44,17 @@
 			year: '2017',
 			editedBy: '',
 			outOfPrint: false
-		},
-		{
-			isbn: '917289012371',
-			title: 'Miti Del Nord',
-			authors: 'Neil Gaiman',
-			quantity: 3,
-			price: 10,
-			publisher: 'Mondadori',
-			year: '2017',
-			editedBy: '',
-			outOfPrint: true
-		},
-		{
-			isbn: '917289012327',
-			title: 'Hellenistic history and culture',
-			authors: 'Peter Green',
-			quantity: 3,
-			price: 10,
-			publisher: 'Penguin',
-			year: '2017',
-			editedBy: '',
-			outOfPrint: false
-		},
-
-		{
-			isbn: '917289012188',
-			title: 'Studies in Greek culture and Roman policy',
-			authors: 'Robert Lamberton',
-			quantity: 3,
-			price: 10,
-			publisher: 'Penguin',
-			year: '2017',
-			editedBy: '',
-			outOfPrint: true
 		}
 	];
 
-	const table = createTable({ initialRows: rows });
+	const table = createTable({ rows });
 </script>
 
 <Hst.Story title="Table">
 	<Hst.Variant>
 		<div class="flex flex-col gap-y-4">
 			<div class="w-24 p-1">
-				<Button color={ButtonColor.Primary} on:click={() => table.addRows([newRow()])}>Add row</Button>
+				<Button color={ButtonColor.Primary} on:click={() => table.addRows([rows[0]])}>Add row</Button>
 			</div>
 
 			<InventoryTable {table} />
--- pkg/ui/src/lib/actions/table.ts
+++ pkg/ui/src/lib/actions/table.ts
@@ -1,31 +1,40 @@
-import { writable, type Writable } from 'svelte/store';
-
-import type { TableData } from '../../InventoryTable/types';
-
-interface CreateTableOptions {
-	initialRows: TableData[];
-	onRowRemove?: (selected: TableData[]) => void;
-	onRowAdd?: (selected: TableData[]) => void;
-}
+import { v4 as uuidv4 } from 'uuid';
+import { derived, writable } from 'svelte/store';
 
 // eslint-disable-next-line
-export function createTable({ initialRows, onRowRemove = () => {}, onRowAdd = () => {} }: CreateTableOptions) {
-	const data = writable(initialRows);
-	const selected = writable<TableData[]>([]);
+export function createTable<T = Record<string, any>>({
+	rows,
+	onRowRemove = () => ({}),
+	onRowAdd = () => ({})
+}: CreateTableOptions<T>) {
+	const data = writable(setRowKeys(rows));
 
-	let $data: TableData[] = [];
+	type KeyedRows = T & { key: string };
+	let $data: KeyedRows[] = [];
 
-	const addRows = (newRows: TableData[]) => {
-		onRowAdd(newRows);
-		data.update((rows) => [...rows, ...newRows]);
+	const selectedData = writable<KeyedRows[]>([]);
+
+	const state = derived([data, selectedData], ([$data, $selectedData]) => {
+		return {
+			rows: $data,
+			selected: $selectedData
+		};
+	});
+
+	const addRows = (newRows: T[]) => {
+		const keyednNewRows = setRowKeys(newRows);
+		onRowAdd(keyednNewRows);
+		data.update((rows) => [...rows, ...keyednNewRows]);
 	};
-	const removeRows = (selectedRows: TableData[]) => {
-		const selectedIsbns = selectedRows.map(({ isbn }) => isbn);
-		const filteredRows = $data.filter((r) => !selectedIsbns.includes(r.isbn));
+
+	const removeRows = (selectedRows: KeyedRows[]) => {
+		const selectedKeys = selectedRows.map(({ key }) => key);
+		const filteredRows = $data.filter((r) => !selectedKeys.includes(r.key));
 
 		onRowRemove(selectedRows);
+
 		data.set(filteredRows);
-		selected.set([]);
+		selectedData.set([]);
 	};
 
 	// eslint-disable-next-line
@@ -46,11 +55,11 @@ export function createTable({ initialRows, onRowRemove = () => {}, onRowAdd = ()
 			handleSelect
 		}: {
 			on: keyof HTMLElementEventMap;
-			handleSelect: (event: HTMLElementEventMap[typeof on], selected: Writable<TableData[]>) => void;
+			handleSelect: (event: HTMLElementEventMap[typeof on], selected: typeof selectedData) => void;
 		}
 	) => {
 		const onSelect = (event: HTMLElementEventMap[typeof on]) => {
-			handleSelect(event, selected);
+			handleSelect(event, selectedData);
 		};
 
 		node.addEventListener(on, onSelect, true);
@@ -63,11 +72,28 @@ export function createTable({ initialRows, onRowRemove = () => {}, onRowAdd = ()
 	};
 
 	return {
-		data,
-		selected,
+		subscribe: state.subscribe,
 		table,
 		addRows,
 		removeRows,
 		rowSelect
 	};
+}
+
+/**
+ * Table factory params
+ */
+interface CreateTableOptions<T> {
+	rows: T[];
+	onRowRemove?: (selected: T[]) => void;
+	onRowAdd?: (selected: T[]) => void;
+}
+
+/**
+ * Helper to add unique `key` to each row of data
+ * @param rows
+ * @returns
+ */
+function setRowKeys<T>(rows: T[]) {
+	return rows.map((row) => ({ ...row, key: uuidv4() }));
 }
