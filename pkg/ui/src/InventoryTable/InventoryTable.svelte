d4672f5d2e709fddb224a3b03288c14bd3a7fa74
delasous <sousa.yak@googlemail.com>
2023-03-09 11:52:05 +0100

    Add quantity badge styles

--- pkg/ui/src/InventoryTable/InventoryTable.svelte
+++ pkg/ui/src/InventoryTable/InventoryTable.svelte
@@ -162,7 +162,9 @@
 						{authors}
 					</td>
 					<td class="py-4 px-3 text-left">
-						{quantity}
+						<span class="rounded-md bg-gray-100 px-3 py-2">
+							{quantity}
+						</span>
 					</td>
 					<td class="py-4 px-3 text-left">
 						{price}
