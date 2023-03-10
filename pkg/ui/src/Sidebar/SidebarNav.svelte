668a298e365dd261ea4f0922cd106884eab0aa6b
delasous <sousa.yak@googlemail.com>
2023-03-10 16:51:45 +0100

    Update Sidenav styles compsition

--- pkg/ui/src/Sidebar/SidebarNav.svelte
+++ pkg/ui/src/Sidebar/SidebarNav.svelte
@@ -1,3 +1,8 @@
-<nav class="mt-5 flex-1 divide-y-2 bg-white px-2" aria-label="Sidebar">
-	<slot />
+<nav aria-label="Sidebar" class="bg-white px-2">
+	<div class="flex flex-1 flex-col divide-y-2">
+		<slot />
+	</div>
+	<div class="border-t border-dashed border-gray-300">
+		<slot name="actions" />
+	</div>
 </nav>
