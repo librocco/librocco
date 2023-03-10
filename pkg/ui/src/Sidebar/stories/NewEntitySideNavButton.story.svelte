8ed7c394ea41c28c4bf5d67a06440ff0a1fd9363
delasous <sousa.yak@googlemail.com>
2023-03-10 16:51:45 +0100

    Update Sidenav styles compsition

--- pkg/ui/src/Sidebar/stories/NewEntitySideNavButton.story.svelte
+++ pkg/ui/src/Sidebar/stories/NewEntitySideNavButton.story.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import type { Hst } from '@histoire/plugin-svelte';
 
-	import SideNavButton from './NewEntitySideNavButton.svelte';
+	import SideNavButton from '../NewEntitySideNavButton.svelte';
 
 	export let Hst: Hst;
 </script>
 
 <Hst.Story title="Sidebar Navigation / New Entity" layout={{ type: 'grid', width: 300 }}>
 	<Hst.Variant title="Inactive">
-		<SideNavButton active={false} label="New Note" />
+		<SideNavButton label="New Note" />
 	</Hst.Variant>
 	<Hst.Variant title="Active">
 		<SideNavButton label="New Note" />
 	</Hst.Variant>
 	<Hst.Variant title="Warehouse">
-		<SideNavButton />
+		<SideNavButton label="New Note" />
 	</Hst.Variant>
 </Hst.Story>
