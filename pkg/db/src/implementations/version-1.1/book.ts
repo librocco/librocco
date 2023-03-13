39e2ac16d0ca314f57bb4fe90d8a3929fe2a4af6
ikusteu <ivankust57@gmail.com>
2023-03-13 12:48:54 +0100

    A couple of minor fixes:
    * Transform 'unwrapDocs' (pouchdb helper) to return just the docs (without _id and _rev)
    * Tidy up the 'db.books().stream()' in v1 implementation

--- pkg/db/src/implementations/version-1.1/book.ts
+++ pkg/db/src/implementations/version-1.1/book.ts
@@ -1,5 +1,5 @@
 import { BookEntry, BookInterface, DatabaseInterface } from '@/types';
-import { newChangesStream } from '@/utils/pouchdb';
+import { newChangesStream, unwrapDocs } from '@/utils/pouchdb';
 import { debug } from '@librocco/shared';
 
 import { concat, from, map, Observable, switchMap, tap } from 'rxjs';
@@ -38,19 +38,11 @@ class Book implements BookInterface {
 		const rawBooks = await this.#db._pouch.allDocs<BookEntry>({ keys: isbns.map((isbn) => `books/${isbn}`), include_docs: true });
 		// The rows are returned in the same order as the supplied keys array.
 		// The row for a nonexistent document will just contain an "error" property with the value "not_found".
-
-		const bookDocs = rawBooks.rows.map(({ doc }) => {
-			if (!doc) return undefined;
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			const { _id, _rev, ...rest } = doc;
-			return rest;
-		});
-
-		return bookDocs;
+		return unwrapDocs(rawBooks);
 	}
 
-	stream(isbns: string[], ctx: debug.DebugCtx): Observable<BookEntry[]> {
-		return new Observable<BookEntry[]>((subscriber) => {
+	stream(isbns: string[], ctx: debug.DebugCtx) {
+		return new Observable<(BookEntry | undefined)[]>((subscriber) => {
 			const emitter = this.#db._pouch.changes<BookEntry[]>({
 				since: 'now',
 				live: true,
@@ -58,61 +50,39 @@ class Book implements BookInterface {
 				filter: (doc) => isbns.includes(doc._id)
 			});
 
-			const initialPromise = this.#db._pouch
-				.allDocs<(BookEntry | undefined)[]>({ keys: isbns.map((isbn) => `books/${isbn}`), include_docs: true })
-				.then((res) => {
-					debug.log(ctx, 'books_stream:initial_query:result')(res);
-					return res;
-				})
-				// This shouldn't really happen, but as an edge case, we don't want to break the entire app
-				.catch((err) => {
-					debug.log(ctx, 'document_stream:initial_query:error')(err);
-					debug.log(ctx, 'document_stream:initial_query:error:fallback')({ rows: [] });
-					return { rows: [] };
-				});
-			const initialState = from(initialPromise).pipe(
-				map(({ rows }) => {
-					// const { _id, _rev, ...rest } = doc || {}
-					return rows.map(({ doc }) => {
-						if (!doc) return undefined;
-						// eslint-disable-next-line @typescript-eslint/no-unused-vars
-						const { _id, _rev, ...rest } = doc;
-						return rest;
-					});
+			const initialState = from(
+				new Promise<PouchDB.Core.AllDocsResponse<BookEntry>>((resolve) => {
+					this.#db._pouch
+						.allDocs<BookEntry>({ keys: isbns.map((isbn) => `books/${isbn}`), include_docs: true })
+						.then((res) => {
+							debug.log(ctx, 'books_stream:initial_query:result')(res);
+							return resolve(res);
+						})
+						.catch(debug.log(ctx, 'books_stream:initial_query:error'));
 				})
 			);
-			const changeStream = newChangesStream<(BookEntry | undefined)[]>(emitter, ctx).pipe(
-				// The change only triggers a new query (as changes are partial and we require the full view update)
+
+			const changeStream = newChangesStream<BookEntry[]>(emitter, ctx).pipe(
+				// The change only triggers a new query (as changes are partial and we the all docs update)
 				switchMap(() =>
 					from(
 						new Promise<PouchDB.Core.AllDocsResponse<BookEntry>>((resolve) => {
 							this.#db._pouch
 								.allDocs<BookEntry>({ keys: isbns.map((isbn) => `books/${isbn}`), include_docs: true })
 								.then((res) => {
-									debug.log(ctx, 'books_stream:change_query:result')(res);
+									debug.log(ctx, 'books_stream:change_query:res')(res);
 									return resolve(res);
 								})
-								.catch((err) => {
-									debug.log(ctx, 'books_stream:change_query:error')(err);
-								});
+								.catch(debug.log(ctx, 'books_stream:change_query:error'));
 						})
 					)
-				),
-				tap(debug.log(ctx, 'books_stream:change')),
-				map(({ rows }) => {
-					rows.map(({ doc }) => {
-						// eslint-disable-next-line @typescript-eslint/no-unused-vars
-						const { _id, _rev, ...rest } = doc || {};
-						return [rest];
-					});
-				}),
-				tap(debug.log(ctx, 'books_stream:change:transformed'))
+				)
 			);
 
 			concat(initialState, changeStream)
 				.pipe(
 					tap(debug.log(ctx, 'books_stream:result:raw')),
-					map((doc) => doc),
+					map(unwrapDocs),
 					tap(debug.log(ctx, 'books_stream:result:transformed'))
 				)
 				.subscribe((doc) => subscriber.next(doc));
