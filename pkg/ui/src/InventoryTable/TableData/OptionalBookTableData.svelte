a3180f9389cfcc9f5dfde6f6816bc36a3d1cfd5d
delasous <sousa.yak@googlemail.com>
2023-03-16 15:36:00 +0100

    Refactor table row variants as TableData components

---
+++ pkg/ui/src/InventoryTable/TableData/OptionalBookTableData.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Checkbox } from '../../';
+
+	import type { BookOptionalRowData } from '../types';
+
+	export let data: BookOptionalRowData;
+	export let rowIx: number;
+
+	$: ({ publisher, editedBy, outOfPrint } = data);
+</script>
+
+<td class="hidden py-4 px-3 md:table-cell">
+	{publisher}
+</td>
+<td class="hidden py-4 px-3 xl:table-cell">
+	{editedBy}
+</td>
+<td class="hidden py-4 px-3 text-center xl:table-cell">
+	<span class="inline-block">
+		<Checkbox name={`Row ${rowIx} is out of print: ${outOfPrint}`} checked={outOfPrint} disabled />
+	</span>
+</td>
---
+++ pkg/ui/src/InventoryTable/TableData/OutNoteTableData.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { createCombobox } from 'svelte-headlessui';
+	import { ChevronsUpDown } from 'lucide-svelte';
+
+	import type { BookWarehousesRowData } from '../types';
+
+	import { ComboboxMenu } from '../../Menus';
+	import { TextField } from '../../FormFields';
+
+	export let data: BookWarehousesRowData;
+	export let rowIx: number;
+
+	const combobox = createCombobox({ label: `Select ${rowIx} warehouse` });
+
+	$: ({ warehouses } = data);
+</script>
+
+<td class="py-4 px-3">
+	{#if warehouses.length > 1}
+		<TextField
+			name={`Row ${rowIx} warehouse`}
+			inputAction={combobox.input}
+			value={$combobox.selected}
+			placeholder="Select warehouse..."
+		>
+			<span slot="startAdornment" class="rounded-full p-1 {$combobox.selected ? 'bg-teal-400' : 'bg-red-400'}" />
+			<svelte:fragment slot="endAdornment">
+				<button use:combobox.button type="button" class="flex items-center">
+					<ChevronsUpDown class="text-gray-400" />
+				</button>
+			</svelte:fragment>
+		</TextField>
+		<div class="relative">
+			<ComboboxMenu {combobox} options={warehouses} />
+		</div>
+	{:else}
+		<div class="flex items-center rounded-md bg-gray-100 shadow-sm">
+			<span class="ml-3 rounded-full bg-teal-400 p-1" />
+			<input disabled type="text" value={warehouses[0]} class="border-0 bg-gray-100 text-sm text-gray-500" />
+		</div>
+	{/if}
+</td>
