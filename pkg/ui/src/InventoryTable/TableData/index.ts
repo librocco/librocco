c0141de809a3587468894b5c1b85025d3c919797
delasous <sousa.yak@googlemail.com>
2023-03-20 11:08:42 +0100

    Refactor OutNoteTable: Remove TableData components & rebuild as standalone component

--- pkg/ui/src/InventoryTable/TableData/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CoreBookTableData } from './CoreBookTableData.svelte';
-export { default as OptionalBookTableData } from './OptionalBookTableData.svelte';
-export { default as OutNoteTableData } from './OutNoteTableData.svelte';
--- pkg/ui/src/InventoryTable/enums.ts
+++
@@ -1,4 +0,0 @@
-export enum InventoryTableVariant {
-	Default = 'default',
-	OutNote = 'out note'
-}
