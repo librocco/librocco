7a755c1635a53193ae909d16cd5f25113984ae8a
ikusteu <ivankust57@gmail.com>
2023-03-23 20:30:50 +0100

    Create db init states:
    * db.init() returns immediately (after queueing the async processes)
    * create db.stream().initState() stream which streams the stage of db init process
    * update root +layout.svelte to show "initiailsing"/"replicating" (during appropriate stages of db initialisation)
    * update db streams (nav lists) to be kept inside the instance (for the lifetime of the db = the app window)
    * all exposed db streams are multicasted from the internal streams (resulting in much nicer preformance)

--- apps/web-client/src/routes/+layout.svelte
+++ apps/web-client/src/routes/+layout.svelte
@@ -1,13 +1,27 @@
-<script>
+<script lang="ts">
+	import { readableFromStream } from "$lib/utils/streams";
+	import type { DBInitState } from "@librocco/db";
+	import { Header, InventoryPage } from "@librocco/ui";
+
 	// Import main.css in order to generate tailwind classes used in the app
 	import "../main.css";
 
-	// TEMP
-	// import { db } from '$lib/db/temp';
-	// import { onMount } from 'svelte';
-	// onMount(() => {
-	// 	window['db'] = db;
-	// });
+	import type { LayoutData } from "./$types";
+
+	export let data: LayoutData;
+
+	$: db = data.db;
+	$: initState = readableFromStream(db?.stream().initState({}), { state: "void", withReplication: false }, {});
 </script>
 
-<slot />
+<!-- We're returning <slot /> if db doesn't initialise (it's "void") in case of SSR env or if the db is fully initialised -->
+{#if ["ready", "void", "initialising"].includes($initState.state)}
+	<slot />
+{:else}
+	<InventoryPage>
+		<Header slot="header" currentLocation="/" links={[]} />
+		<h1 slot="table" class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl capitalize text-gray-400">
+			{$initState.state}...
+		</h1>
+	</InventoryPage>
+{/if}
