4a637ad6099ed2d217d3d7c68e442ed2e998f79f
delasous <sousa.yak@googlemail.com>
2023-03-23 18:34:58 +0100

    Rebuild table action-store: accept Writable or static value as options; remove local add/remove helpers

--- pkg/ui/src/InventoryTable/table.ts
+++ pkg/ui/src/InventoryTable/table.ts
@@ -1,13 +1,28 @@
 import { v4 as uuidv4 } from 'uuid';
-import { derived, writable, get } from 'svelte/store';
+import { derived, writable, type Writable } from 'svelte/store';
 import type { Action } from 'svelte/action';
 
+/**
+ * Table factory options
+ */
+interface TableOptions<T> {
+	data: T[];
+}
+
+type WritableOrValue<T> = T | Writable<T>;
+
+type Options<T> = WritableOrValue<TableOptions<T>>;
+
 // eslint-disable-next-line
-export function createTable<T = Record<string, any>>({
-	rows,
-	onRowRemove = () => ({}),
-	onRowAdd = () => ({})
-}: CreateTableOptions<T>) {
+export function createTable<T = object[]>(options: Options<T>) {
+	let optionsStore: Writable<TableOptions<T>>;
+
+	if ('subscribe' in options) {
+		optionsStore = options;
+	} else {
+		optionsStore = writable(options);
+	}
+
 	/**
 	 * Type extends generic table data T with unique `key` and `rowIx`
 	 */
@@ -16,8 +31,11 @@ export function createTable<T = Record<string, any>>({
 	/**
 	 * Store of table rows with unique keys & rowIx
 	 */
-	const _data = writable(setRowKeys(rows));
-	const data = derived(_data, ($_data) => $_data.map((row, ix) => ({ ...row, rowIx: ix })));
+	// const _data = writable(setRowKeys(rows));
+	const data = derived(optionsStore, ($options) => {
+		const { data = [] } = $options;
+		return data.map((row) => ({ ...row, key: uuidv4() })).map((row, ix) => ({ ...row, rowIx: ix }));
+	});
 
 	/**
 	 * Store of selected table rows
@@ -35,28 +53,9 @@ export function createTable<T = Record<string, any>>({
 	});
 
 	/**
-	 * Helper adds rows to table data store
-	 * @param newRows
+	 * Helper to reset selected store
 	 */
-	const addRows = (newRows: T[]) => {
-		const keyednNewRows = setRowKeys(newRows);
-		onRowAdd(keyednNewRows);
-		_data.update((rows) => [...rows, ...keyednNewRows]);
-	};
-
-	/**
-	 * Helper removes rows from table data store & resets selected store
-	 * @param selectedRows
-	 */
-	const removeRows = (selectedRows: KeyedRows[]) => {
-		const selectedKeys = selectedRows.map(({ key }) => key);
-		const filteredRows = get(data).filter((r) => !selectedKeys.includes(r.key));
-
-		onRowRemove(selectedRows);
-
-		_data.set(filteredRows);
-		selectedData.set([]);
-	};
+	const resetRowSelection = () => selectedData.set([]);
 
 	/**
 	 * Table action
@@ -120,26 +119,7 @@ export function createTable<T = Record<string, any>>({
 	return {
 		subscribe: state.subscribe,
 		table,
-		addRows,
-		removeRows,
+		resetRowSelection,
 		tableRow
 	};
-}
-
-/**
- * Table factory params
- */
-interface CreateTableOptions<T> {
-	rows: T[];
-	onRowRemove?: (selected: T[]) => void;
-	onRowAdd?: (selected: T[]) => void;
-}
-
-/**
- * Helper to add unique `key` to each row of data
- * @param rows
- * @returns
- */
-function setRowKeys<T>(rows: T[]) {
-	return rows.map((row) => ({ ...row, key: uuidv4() }));
 }
