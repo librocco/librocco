649d399b5269025c33b3feada288869d06ce3bab
delasous <sousa.yak@googlemail.com>
2023-03-24 10:27:38 +0100

    Align VolumeStock + BookEntry types with TableData types

--- pkg/ui/src/InventoryTable/types.ts
+++ pkg/ui/src/InventoryTable/types.ts
@@ -5,15 +5,18 @@ export type InventoryTableData = BookCoreRowData & {
 	outOfPrint?: boolean;
 };
 
+// TODO: Align with DB types =>
+// VolumeStockClient has warehouseName as a string, but it should expect an array where
+// a volume in an Out Note can potentially come out of multiple warehouses
 export type OutNoteTableData = BookCoreRowData & {
-	warehouses: string[];
+	warehouseName: string[] | string;
 };
 
 export type BookCoreRowData = {
 	isbn: string;
 	title: string;
 	price: number;
+	quantity: number;
 	authors?: string;
-	quantity?: number;
 	year?: string;
 };
