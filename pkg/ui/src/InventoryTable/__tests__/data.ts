2b567429f63dd0d7c1c574a896f3e99813272418
delasous <sousa.yak@googlemail.com>
2023-03-24 10:53:27 +0100

    Format files

--- pkg/ui/src/InventoryTable/__tests__/data.ts
+++ pkg/ui/src/InventoryTable/__tests__/data.ts
@@ -1,38 +1,38 @@
-import type { InventoryTableData } from '../types';
+import type { InventoryTableData } from "../types";
 
 export const rows: InventoryTableData[] = [
 	{
-		isbn: '917289012390',
-		title: 'Miti Del Nord',
-		authors: 'Neil Gaiman',
+		isbn: "917289012390",
+		title: "Miti Del Nord",
+		authors: "Neil Gaiman",
 		quantity: 3,
 		price: 10,
-		publisher: 'Mondadori',
-		year: '2017',
-		editedBy: '',
+		publisher: "Mondadori",
+		year: "2017",
+		editedBy: "",
 		outOfPrint: false
 	},
 	{
-		isbn: '917289012381',
-		title: 'Hellenistic history and culture',
-		authors: 'Peter Green',
+		isbn: "917289012381",
+		title: "Hellenistic history and culture",
+		authors: "Peter Green",
 		quantity: 3,
 		price: 10,
-		publisher: 'Penguin',
-		year: '2017',
-		editedBy: '',
+		publisher: "Penguin",
+		year: "2017",
+		editedBy: "",
 		outOfPrint: false
 	},
 
 	{
-		isbn: '917289012323',
-		title: 'Studies in Greek culture and Roman policy',
-		authors: 'Robert Lamberton',
+		isbn: "917289012323",
+		title: "Studies in Greek culture and Roman policy",
+		authors: "Robert Lamberton",
 		quantity: 3,
 		price: 10,
-		publisher: 'Penguin',
-		year: '2017',
-		editedBy: '',
+		publisher: "Penguin",
+		year: "2017",
+		editedBy: "",
 		outOfPrint: false
 	}
 ];
--- pkg/ui/src/InventoryTable/stories/ComboboxTable.story.svelte
+++ pkg/ui/src/InventoryTable/stories/ComboboxTable.story.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import type { Hst } from '@histoire/plugin-svelte';
-	import { createCombobox } from 'svelte-headlessui';
+	import type { Hst } from "@histoire/plugin-svelte";
+	import { createCombobox } from "svelte-headlessui";
 
-	import ComboboxTable from '../ComboboxTable.svelte';
-	import { TextField } from '../..//FormFields';
+	import ComboboxTable from "../ComboboxTable.svelte";
+	import { TextField } from "../..//FormFields";
 
-	import { rows } from '../__tests__/data';
+	import { rows } from "../__tests__/data";
 
 	export let Hst: Hst;
 
-	const combobox = createCombobox({ label: 'Test Table combobox', expanded: true });
+	const combobox = createCombobox({ label: "Test Table combobox", expanded: true });
 
 	const xeroQuantityRow = { ...rows[0], quantity: 0 };
 </script>
@@ -17,12 +17,7 @@
 <Hst.Story title="Tables / Combobox Table">
 	<div class="h-96 overflow-hidden">
 		<div class="relative">
-			<TextField
-				name="search-books"
-				placeholder="Search for books"
-				inputAction={combobox.input}
-				value={$combobox.selected?.title}
-			/>
+			<TextField name="search-books" placeholder="Search for books" inputAction={combobox.input} value={$combobox.selected?.title} />
 			<ComboboxTable rows={[xeroQuantityRow, ...rows]} {combobox} on:select={(e) => console.log(e)} />
 		</div>
 	</div>
