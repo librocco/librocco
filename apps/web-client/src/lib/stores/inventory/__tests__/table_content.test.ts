a63c9f4b0d0f971391cbc2b70901e9875e61c0ab
ikusteu <ivankust57@gmail.com>
2023-03-31 12:33:34 +0200

    Update the client to consume the paginated streams:
    * 'currentPage' store now controls the note/warehouse 'entries()' stream, passing in current page
    * 'createDisplayEntriesStore' now returns both the 'entries' as 'paginationData' (as pagination data is retrieved from the entries stream
    * remove unnecessary store featories
    * update the tests

--- apps/web-client/src/lib/stores/inventory/__tests__/table_content.test.ts
+++ apps/web-client/src/lib/stores/inventory/__tests__/table_content.test.ts
@@ -1,13 +1,14 @@
 import { describe, test, expect } from "vitest";
+import { readable } from "svelte/store";
+
 import { testUtils } from "@librocco/shared";
 
+import type { VolumeQuantity } from "$lib/types/db";
 import type { PaginationData, DisplayRow } from "$lib/types/inventory";
 
-import { createDisplayEntriesStore, createPaginationDataStore } from "../table_content";
-
-import type { VolumeQuantity } from "$lib/types/db";
-import { readable } from "svelte/store";
+import { createDisplayEntriesStore } from "../table_content";
 import { newTestDB } from "$lib/__testUtils__/db";
+
 const { waitFor } = testUtils;
 
 describe("tableContentStore", () => {
@@ -15,8 +16,8 @@ describe("tableContentStore", () => {
 		let displayEntries: VolumeQuantity[] | undefined;
 
 		// Both db and entity are undefined (this will happen at build time, as db is instantiated only in browser)
-		let de$ = createDisplayEntriesStore({}, undefined, undefined, readable(0));
-		de$.subscribe((de) => (displayEntries = de));
+		let tableData = createDisplayEntriesStore({}, undefined, undefined, readable(0));
+		tableData.entries.subscribe((de) => (displayEntries = de));
 		expect(displayEntries).toEqual([]);
 
 		// Reset the 'displayEntries'
@@ -24,14 +25,13 @@ describe("tableContentStore", () => {
 
 		// Should also work if db is defined but entity is undefined (this might happen if the entity id is not specified)
 		const db = await newTestDB();
-		de$ = createDisplayEntriesStore({}, db, undefined, readable(0));
-		de$.subscribe((de) => (displayEntries = de));
+		tableData = createDisplayEntriesStore({}, db, undefined, readable(0));
+		tableData.entries.subscribe((de) => (displayEntries = de));
 		expect(displayEntries).toEqual([]);
 
 		// Same check for pagination
-		const pd$ = createPaginationDataStore({}, undefined, readable(0));
 		let paginationData: PaginationData | undefined;
-		pd$.subscribe((pd) => (paginationData = pd));
+		tableData.paginationData.subscribe((pd) => (paginationData = pd));
 		expect(paginationData).toEqual({
 			numPages: 0,
 			firstItem: 0,
@@ -69,7 +69,7 @@ describe("tableContentStore", () => {
 		};
 
 		// Set up initial state:
-		const note = await db.warehouse("jazz").note("note-1").create();
+		const note = await db.warehouse().note("note-1").create();
 		await Promise.all([
 			// Only the transaction for book1 is present
 			note.addVolumes({ isbn: book1.isbn, quantity: 12, warehouseId: "jazz" }),
@@ -77,13 +77,13 @@ describe("tableContentStore", () => {
 			db.books().upsert([book1, book2])
 		]);
 
-		const de$ = createDisplayEntriesStore({}, db, note, readable(0));
+		const tableData = createDisplayEntriesStore({}, db, note, readable(0));
 		let displayEntries: DisplayRow[];
-		de$.subscribe((de) => (displayEntries = de));
+		tableData.entries.subscribe((de) => (displayEntries = de));
 
 		// Should receive the initial state (only book1 transaction in the note)
 		await waitFor(() =>
-			expect(displayEntries).toEqual([{ ...book1, quantity: 12, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" }])
+			expect(displayEntries).toEqual([{ ...book1, quantity: 12, warehouseId: `v1/jazz`, warehouseName: "not-found" }])
 		);
 
 		// Update the note (add additional transactions)
@@ -93,11 +93,11 @@ describe("tableContentStore", () => {
 		);
 		await waitFor(() => {
 			expect(displayEntries).toEqual([
-				{ ...book1, quantity: 12, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" },
+				{ ...book1, quantity: 12, warehouseId: `v1/jazz`, warehouseName: "not-found" },
 				// Book data for book2 is already available in the db
-				{ ...book2, quantity: 10, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" },
+				{ ...book2, quantity: 10, warehouseId: `v1/jazz`, warehouseName: "not-found" },
 				// Book data for book3 is not available in the db - only the transaction data is shown
-				{ isbn: book3.isbn, quantity: 5, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" }
+				{ isbn: book3.isbn, quantity: 5, warehouseId: `v1/jazz`, warehouseName: "not-found" }
 			]);
 		});
 
@@ -105,10 +105,10 @@ describe("tableContentStore", () => {
 		await db.books().upsert([book3]);
 		await waitFor(() => {
 			expect(displayEntries).toEqual([
-				{ ...book1, quantity: 12, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" },
-				{ ...book2, quantity: 10, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" },
+				{ ...book1, quantity: 12, warehouseId: `v1/jazz`, warehouseName: "not-found" },
+				{ ...book2, quantity: 10, warehouseId: `v1/jazz`, warehouseName: "not-found" },
 				// Full book3 data should be displayed
-				{ ...book3, quantity: 5, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" }
+				{ ...book3, quantity: 5, warehouseId: `v1/jazz`, warehouseName: "not-found" }
 			]);
 		});
 
@@ -122,10 +122,10 @@ describe("tableContentStore", () => {
 					title: "The Age of Wonder (updated)",
 					quantity: 12,
 					warehouseId: `v1/jazz`,
-					warehouseName: "New Warehouse"
+					warehouseName: "not-found"
 				},
-				{ ...book2, quantity: 10, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" },
-				{ ...book3, quantity: 5, warehouseId: `v1/jazz`, warehouseName: "New Warehouse" }
+				{ ...book2, quantity: 10, warehouseId: `v1/jazz`, warehouseName: "not-found" },
+				{ ...book3, quantity: 5, warehouseId: `v1/jazz`, warehouseName: "not-found" }
 			]);
 		});
 	});
