d64095ced07173be90bcab70a9ac2c75ec588a18
Fadwa <fadwa.mahmoud@gmail.com>
2023-04-02 21:53:01 +0200

    Revert db.init changes and get rid of initState

--- apps/web-client/src/routes/+layout.svelte
+++ apps/web-client/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { readableFromStream } from "$lib/utils/streams";
 	import { Header, InventoryPage } from "@librocco/ui";
+	import { DB_NAME, COUCHDB_HOST, COUCHDB_PORT, COUCHDB_USER, COUCHDB_PASSWORD } from "$lib/constants";
 
 	// Import main.css in order to generate tailwind classes used in the app
 	import "../main.css";
@@ -10,17 +10,27 @@
 	export let data: LayoutData;
 
 	$: db = data.db;
-	$: initState = readableFromStream({}, db?.stream().initState({}), { state: "void" });
+	let replicating = db ? true : false;
+
+	db.replicate()
+		.sync(`http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@${COUCHDB_HOST}:${COUCHDB_PORT}/${DB_NAME}`, { name: "[DB_INIT]", debug: false })
+		.then(() => {
+			replicating = false;
+			db.replicate().live(`http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@${COUCHDB_HOST}:${COUCHDB_PORT}/${DB_NAME}`, {
+				name: "[DB_INIT]",
+				debug: false
+			});
+		});
 </script>
 
 <!-- We're returning <slot /> if db doesn't initialise (it's "void") in case of SSR env or if the db is fully initialised -->
-{#if ["ready", "void", "initialising"].includes($initState.state)}
+{#if replicating}
 	<slot />
 {:else}
 	<InventoryPage>
 		<Header slot="header" currentLocation="/" links={[]} />
 		<h1 slot="table" class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl capitalize text-gray-400">
-			{$initState.state}...
+			Replicating...
 		</h1>
 	</InventoryPage>
 {/if}
