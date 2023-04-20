3dd7a69c898eed606fbf538c8f6a58cad7395126
delasous <sousa.yak@googlemail.com>
2023-04-20 15:16:15 +0200

    Fix root layout redirects

--- apps/web-client/src/routes/+layout.ts
+++ apps/web-client/src/routes/+layout.ts
@@ -1,18 +1,20 @@
 import { redirect } from "@sveltejs/kit";
 
-import type { LayoutLoad } from "./$types";
-
 import { browser } from "$app/environment";
+import { base } from "$app/paths";
 
 import { createDB } from "$lib/db";
 
+import type { LayoutLoad } from "./$types";
+
 // Paths which are valid (shouldn't return 404, but don't have any content and should get redirected to the default route "/inventory/stock/all")
-const redirectPaths = ["", "/", "/inventory", "/inventory/", "/inventory/stock", "/inventory/stock/"];
+const redirectPaths = ["", "/", "/inventory", "/inventory/", "/inventory/stock", "/inventory/stock/"].map((path) => `${base}${path}`);
 
 export const load: LayoutLoad = async ({ url }) => {
 	const { pathname } = url;
+
 	if (redirectPaths.includes(pathname)) {
-		throw redirect(307, "/inventory/stock/0-all");
+		throw redirect(307, `${base}/inventory/stock/0-all`);
 	}
 
 	// If in browser, we init the db, otherwise this is a prerender, for which we're only building basic html skeleton
