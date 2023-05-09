a456ccfc1e3b9099cc1909d28ee0b86c01eff93d
Fadwa <fadwa.mahmoud@gmail.com>
2023-05-10 02:28:36 +0300

    Use sveltekit pwa plugin

--- apps/web-client/src/routes/+layout.svelte
+++ apps/web-client/src/routes/+layout.svelte
@@ -3,13 +3,26 @@
 
 	// Import main.css in order to generate tailwind classes used in the app
 	import "../main.css";
-	import { registerSW } from "virtual:pwa-register";
+	import { pwaInfo } from "virtual:pwa-info";
 
 	onMount(async () => {
-		if('serviceWorker' in navigator){
-			registerSW()
+		if (pwaInfo) {
+			const { registerSW } = await import("virtual:pwa-register");
+			registerSW({
+				onRegistered(r) {
+					// uncomment following code if you want check for updates
+					// r && setInterval(() => {
+					//    console.log('Checking for sw update')
+					//    r.update()
+					// }, 20000 /* 20s for testing purposes */)
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log("SW registration error", error);
+				}
+			});
 		}
-	})
+	});
 </script>
 
 <slot />
--- apps/web-client/vite.config.js
+++ apps/web-client/vite.config.js
@@ -1,7 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 import path from "path";
-
 import RollupNodePolyfill from "rollup-plugin-node-polyfills";
 
 const rushDir = path.join(__dirname, "..", "..", "common");
@@ -10,12 +9,25 @@ const rushDir = path.join(__dirname, "..", "..", "common");
 const config = {
 	plugins: [
 		sveltekit(),
-		VitePWA({})
-		// 	{
-		// 	manifest: {
-		// 		icons: [{ src: "/static/android-chrome-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" }]
-		// 	}
-		// }
+		SvelteKitPWA({
+			// outDir: "build",
+			buildBase: "/preview/",
+			kit: {
+				trailingSlash: "always"
+			},
+			// strategies: "generateSW",
+			workbox: {
+				// Only precache these files - html should be excluded
+				globPatterns: ["**/*.{js,css}"],
+
+				// Don't fallback on document based (e.g. `/some-page`) requests
+				// Even though this says `null` by default, I had to set this specifically to `null` to make it work
+				navigateFallback: null
+			}
+			// manifest: {
+			// 	icons: [{ src: "/static/android-chrome-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" }]
+			// }
+		})
 	],
 	resolve: {
 		alias: {
