d273196962605e2f923052adc03f5c8109d59b8e
Fadwa <fadwa.mahmoud@gmail.com>
2023-05-12 23:42:14 +0300

    Manually register a service worker

--- apps/web-client/src/routes/+layout.svelte
+++ apps/web-client/src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+
 	// Import main.css in order to generate tailwind classes used in the app
 	import "../main.css";
+
+	onMount(async () => {
+		console.log( window.location.origin)
+		if ('serviceWorker' in navigator) {
+	   navigator.serviceWorker.register("/my-sw.js", { scope: '/' }).then(()=> {
+		console.log("success")
+	   }).catch((err)=> {
+		console.log("not success", err)
+	   })
+	}
+
+	})
 </script>
 
 <slot />
---
+++ apps/web-client/static/my-sw.js
@@ -0,0 +1,3 @@
+self.onactivate = function (event) {
+	console.log("This is the sw activated");
+};
