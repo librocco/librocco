d4d19263fc82da8236a8623e0cf24695572fdbfa
Fadwa <fadwa.mahmoud@gmail.com>
2023-05-15 11:51:18 +0300

    Basic precaching and manifest params

--- apps/web-client/static/my-sw.js
+++ apps/web-client/static/my-sw.js
@@ -1,3 +1,16 @@
 self.onactivate = function (event) {
 	console.log("This is the sw activated");
 };
+
+self.addEventListener("install", async (event) => {
+	try {
+		const cacheName = "pwa-assets";
+		const cache = await caches.open(cacheName);
+		const urlsToCache = ["/", "index.html", "favicon.ico", "android-chrome-512x512.png"];
+
+		await cache.addAll(urlsToCache);
+		console.log(urlsToCache.length + " files were cached on " + cacheName);
+	} catch (error) {
+		console.log("Error while caching multiple files. " + error.message);
+	}
+});
