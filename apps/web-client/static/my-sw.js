d0548980a001a8906712c283f4ee109c7485853c
Fadwa <fadwa.mahmoud@gmail.com>
2023-05-15 16:52:03 +0300

    Fetch cached resources offline

--- apps/web-client/static/my-sw.js
+++ apps/web-client/static/my-sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", async (event) => {
 	try {
 		const cacheName = "pwa-assets";
 		const cache = await caches.open(cacheName);
-		const urlsToCache = ["/", "index.html", "favicon.ico", "android-chrome-512x512.png"];
+		const urlsToCache = ["/preview", "index.html", "favicon.ico", "android-chrome-512x512.png"];
 
 		await cache.addAll(urlsToCache);
 		console.log(urlsToCache.length + " files were cached on " + cacheName);
@@ -14,3 +14,17 @@ self.addEventListener("install", async (event) => {
 		console.log("Error while caching multiple files. " + error.message);
 	}
 });
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches.match(event.request).then((cachedResponse) => {
+			console.log(event.request);
+			console.log({ cachedResponse });
+			// It can update the cache to serve updated content on the next request
+			return cachedResponse || fetch(event.request).catch((error) => console.log("Failed to fetch resources " + error));
+		})
+	);
+
+	console.log("Cached assets were fetched");
+});
+ 
