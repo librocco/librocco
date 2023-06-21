50c506f0b2402d9d3933c3b2f5584ac3e920055f
ikusteu <ivankust57@gmail.com>
2023-06-21 13:47:00 +0200

    Copy the db version 1.1 to version 1.2 (and use the latter as 'v1' implementation) for funcitonality updates

---
+++ pkg/db/src/implementations/version-1.2/misc.ts
@@ -0,0 +1,4 @@
+export const replicationError = `The above error originated from the db replication process. 
+Please check the remote db's availability. 
+
+Note: This error is not fatal and the app will continue with local-db only.`;
