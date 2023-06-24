f678b128c1b1574aba4cae435b08862c7ab611cf
ikusteu <ivankust57@gmail.com>
2023-06-24 19:53:50 +0200

    Create a 'transform' function (used to compose the generator transformers):
    * Refactor the map, flatMap and filter iterable transformers to a higher order function, returning the iterable transformer
    * Create a 'transform' function, taking in iterable as the first parameter and a "chain" of transformers, producing an iterable with the composition of transformers applied to each element
    * Write a test for the 'transform' function

--- pkg/shared/src/generators.test.ts
+++ pkg/shared/src/generators.test.ts
@@ -1,6 +1,6 @@
 import { describe, test, expect } from "vitest";
 
-import { iterableFromGenerator } from "./generators";
+import { filter, flatMap, iterableFromGenerator, map, transform } from "./generators";
 
 describe("iterableFromGenerator", () => {
 	test("should allow for multiple iterations over the same generator", () => {
@@ -14,5 +14,25 @@ describe("iterableFromGenerator", () => {
 
 		expect([...iterable]).toEqual([1, 2, 3]);
 		expect([...iterable]).toEqual([1, 2, 3]);
+	});
+});
+
+describe("Composition with 'transform'", () => {
+	test("should produce the resulting iterable from the composition of the given transformers", () => {
+		const iterable = [
+			{ step: "1", rows: [1, 2] },
+			{ step: "2", rows: [3, 4] },
+			{ step: "3", rows: [5, 6] }
+		];
+
+		const result = transform(
+			iterable,
+			flatMap(({ rows }) => rows), // Iterable { 1, 2, 3, 4, 5, 6 }
+			map((value) => value + 1), // Iterable { 2, 3, 4, 5, 6, 7 }
+			filter((value) => value % 2 === 0), // Iterable { 2, 4, 6 }
+			map((value) => value.toString()) // Iterable { "2", "4", "6" }
+		);
+
+		expect([...result]).toEqual(["2", "4", "6"]);
 	});
 });
