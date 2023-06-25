898eba42dfcf9468e05068f6a7b1a207dbd497d9
ikusteu <ivankust57@gmail.com>
2023-06-25 10:01:28 +0200

    Refactor the iterable functions logic:
    * Create a 'wrapIter' function - wraps iterable with methods like array methods (same signature) but producing the iterables instead of arrays
    * Remove 'transform' function - composition is achieved using 'wrapIter' and chaining the methods

--- pkg/shared/src/index.ts
+++ pkg/shared/src/index.ts
@@ -1,5 +1,3 @@
 export * as testUtils from "./testUtils";
 export * as debug from "./debugger";
-
-/** @TODO it would be nice to export generators as a separate module (nodenext resolution style) */
-export * as generators from "./generators";
+export * from "./generators";
