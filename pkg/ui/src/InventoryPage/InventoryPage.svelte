8985946386250a5b214bcc859d4de17cff75b80e
ikusteu <ivankust57@gmail.com>
2023-07-06 11:37:50 +0200

    Write e2e tests for warehouse flow:
    * Write the test suite
    * Add utils for easier accessing of elements (locators) in e2e tests (sidebar)
    * Create zip method for iterables

--- pkg/ui/src/InventoryPage/InventoryPage.svelte
+++ pkg/ui/src/InventoryPage/InventoryPage.svelte
@@ -5,12 +5,15 @@
 
 	<main class="flex basis-[95%] items-stretch divide-x divide-gray-300 overflow-y-auto">
 		{#if $$slots.sidebar}
-			<section class="h-full shrink-0 basis-32 overflow-y-auto overflow-x-hidden whitespace-nowrap bg-gray-50 sm:basis-56">
+			<section
+				id="sidebar-section"
+				class="h-full shrink-0 basis-32 overflow-y-auto overflow-x-hidden whitespace-nowrap bg-gray-50 sm:basis-56"
+			>
 				<slot name="sidebar" />
 			</section>
 		{/if}
 
-		<section class="flex h-full w-full flex-col justify-start gap-y-8 overflow-auto py-8">
+		<section id="table-section" class="flex h-full w-full flex-col justify-start gap-y-8 overflow-auto py-8">
 			{#if $$slots.tableHeader}
 				<div class="px-8">
 					<slot name="tableHeader" />
@@ -32,7 +35,7 @@
 	</main>
 
 	{#if $$slots.slideOver}
-		<section class="h-autoshadow-lg fixed right-0">
+		<section id="slideover-section" class="h-autoshadow-lg fixed right-0">
 			<slot name="slideOver" />
 		</section>
 	{/if}
