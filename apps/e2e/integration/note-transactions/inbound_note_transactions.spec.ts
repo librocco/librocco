3a8388f8b7bf465594c954028e701be2625194be
ikusteu <ivan.kust57@gmail.com>
2023-07-25 10:24:31 +0200

    E2E: test each note view's (inbound/outbound) transaction fields explicitly

--- apps/e2e/integration/note-transactions/inbound_note_transactions.spec.ts
+++ apps/e2e/integration/note-transactions/inbound_note_transactions.spec.ts
@@ -6,6 +6,17 @@ import { getDashboard } from "../../helpers";
 
 import { runCommonTransactionTests } from "./shared_tests";
 
+const book1 = {
+	isbn: "1234567890",
+	title: "Book 1",
+	price: 12,
+	year: "2020",
+	authors: "Author and Sons",
+	publisher: "Reed Elsevier",
+	editedBy: "Sons",
+	outOfPrint: true
+};
+
 const createNote = async (page: Page) =>
 	// For all inbound note tests (in this suite) we're using a first warehouse created
 	// ("New Warehouse") to create notes
@@ -31,4 +42,55 @@ test.beforeEach(async ({ page }) => {
 	await createNote(page);
 });
 
-runCommonTransactionTests("inbound");
+test("should display correct transaction fields for the inbound note view", async ({ page }) => {
+	const dashboard = getDashboard(page);
+
+	const content = dashboard.content();
+	const bookForm = dashboard.bookForm();
+
+	// Fill the book form, creating a transaction
+	await content.scanField().create();
+	await bookForm.fillBookData(book1);
+	await bookForm.submit("click");
+
+	// Check the displayed transaction (field by field)
+	const row = content.entries("inbound").row(0);
+	await row.assertField("isbn", book1.isbn);
+	await row.assertField("title", book1.title);
+	await row.assertField("authors", book1.authors);
+	// The default quantity is 1
+	await row.assertField("quantity", 1);
+	await row.assertField("price", book1.price);
+	await row.assertField("year", book1.year);
+	await row.assertField("publisher", book1.publisher);
+	await row.assertField("editedBy", book1.editedBy);
+	// Should show 'Edit' button
+	await row.getByRole("button", { name: "Edit" }).waitFor();
+	await row.assertField("outOfPrint", book1.outOfPrint);
+});
+
+test("should show empty or \"N/A\" fields and not 'null' or 'undefined' (in case no book data is provided)", async ({ page }) => {
+	const dashboard = getDashboard(page);
+
+	const content = dashboard.content();
+
+	// Add a book transaction without book data (only isbn)
+	await content.scanField().add("1234567890");
+
+	// Check the displayed transaction (field by field)
+	const row = content.entries("inbound").row(0);
+	await row.assertField("isbn", book1.isbn);
+	await row.assertField("title", "N/A");
+	await row.assertField("authors", "N/A");
+	// The default quantity is 1
+	await row.assertField("quantity", 1);
+	await row.assertField("price", "N/A" as any);
+	await row.assertField("year", "N/A");
+	await row.assertField("publisher", "");
+	await row.assertField("editedBy", "");
+	// Should show 'Edit' button
+	await row.getByRole("button", { name: "Edit" }).waitFor();
+	await row.assertField("outOfPrint", false);
+});
+
+runCommonTransactionTests("inbound", createNote);
