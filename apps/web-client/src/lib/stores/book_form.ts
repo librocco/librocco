5f93d05cb5fb44fa2ac60fb07116351bdc05bffe
ikusteu <ivankust57@gmail.com>
2023-08-09 21:18:18 +0200

    Remove 'Create' functionality from the client app:
    * Update handlers and data flows to reflect updates to BookDetailForm and ScanInput
    * Greatly simplify the book form store functionality (with simpler BookDetailForm implementation)

--- apps/web-client/src/lib/stores/book_form.ts
+++ apps/web-client/src/lib/stores/book_form.ts
@@ -1,15 +1,10 @@
 import { writable, type Readable } from "svelte/store";
 
-import type { BookEntry, DatabaseInterface } from "@librocco/db";
+import type { BookEntry } from "@librocco/db";
 
 interface SlideoverText {
 	title: string;
 	description: string;
-}
-
-interface BookFormProps {
-	mode: "create" | "edit";
-	book?: Partial<BookEntry>;
 }
 
 type BookFormClosedState = {
@@ -19,30 +14,23 @@ type BookFormClosedState = {
 type BookFormOpenState = {
 	open: true;
 	slideoverText: SlideoverText;
-	bookFormProps: BookFormProps;
+	book?: Partial<BookEntry>;
 };
 
 type BookFormState = BookFormClosedState | BookFormOpenState;
 
-type OpenCreate = (isbn?: string) => Promise<void>;
-type OpenEdit = (book: Partial<BookEntry>) => void;
-interface OpenHandlerLookup {
-	create: OpenCreate;
-	edit: OpenEdit;
-}
-
 interface BookFormStore extends Readable<BookFormState> {
-	open: <M extends "create" | "edit">(mode: M) => OpenHandlerLookup[M];
+	open: (book: Partial<BookEntry>) => void;
 	close: () => void;
 }
 
-export function newBookFormStore(db: DatabaseInterface): BookFormStore {
+export function newBookFormStore(): BookFormStore {
 	const internalStore = writable<BookFormState>({
 		open: false
 	});
 
 	// Open the book form in 'create' mode
-	const openCreate: OpenCreate = async (isbn) => {
+	const open = async (book: Partial<BookEntry>) => {
 		// Open the form
 		internalStore.set({
 			open: true,
@@ -50,40 +38,9 @@ export function newBookFormStore(db: DatabaseInterface): BookFormStore {
 				title: "Create a new book",
 				description: "Use this form to manually add a new book to the note"
 			},
-			bookFormProps: {
-				mode: "create",
-				book: { isbn }
-			}
+			book
 		});
-
-		// Try and fetch book data from the db (if it exists), in the background
-		const [book] = await db.books().get([isbn]);
-		// If book exists, update the form store (the form component will update reactively)
-		if (book) {
-			internalStore.update((store) => ({ ...store, bookFormProps: { ...(store as BookFormOpenState).bookFormProps, book } }));
-		}
 	};
-
-	// Open the book form in 'edit' mode
-	const openEdit: OpenEdit = (book) =>
-		internalStore.set({
-			open: true,
-			slideoverText: {
-				title: "Edit book details",
-				description: "Use this form to manually edit details of an existing book in the note"
-			},
-			bookFormProps: {
-				mode: "edit",
-				book
-			}
-		});
-
-	// A HOF creating the open handler for given ('create' | 'edit') mode
-	const open = <M extends "create" | "edit">(mode: M): OpenHandlerLookup[M] =>
-		({
-			create: openCreate,
-			edit: openEdit
-		}[mode]);
 
 	// Close the form
 	const close = () => internalStore.set({ open: false });
@@ -94,12 +51,3 @@ export function newBookFormStore(db: DatabaseInterface): BookFormStore {
 		close
 	};
 }
-
-const warnCreateNotSupported = () => {
-	console.warn(
-		"[Create functionality not supported]:\n" +
-			"  No note provided to the store initializer, this is a no-op, but it might be a bug.\n" +
-			"  Create mode is not supported in 'stock' view.\n" +
-			"  If this is an 'inbound' or 'outbound' view, check the book store initializer."
-	);
-};
