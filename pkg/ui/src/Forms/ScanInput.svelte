4fed16b83416ee311d3178b7dc55b1f47cf3dc90
ikusteu <ivankust57@gmail.com>
2023-08-09 21:17:34 +0200

    Remove 'Create' button from the ScanInput component

--- pkg/ui/src/Forms/ScanInput.svelte
+++ pkg/ui/src/Forms/ScanInput.svelte
@@ -2,14 +2,13 @@
 	import { Button, ButtonSize } from "$lib/Button";
 	import { TextField, TextFieldSize } from "$lib/FormFields";
 	import { createForm } from "felte";
-	import { Edit, QrCode } from "lucide-svelte";
+	import { QrCode } from "lucide-svelte";
 
 	export let onAdd: (isbn: string) => void | Promise<void> = () => {};
-	export let onCreate: (isbn: string) => void = () => {};
 
 	const initialValues = { isbn: "" };
 
-	const { form, reset } = createForm({
+	const { form } = createForm({
 		initialValues,
 		onSubmit: async ({ isbn }, { reset }) => {
 			await (async () => onAdd(isbn))();
@@ -18,11 +17,6 @@
 	});
 
 	let isbn = "";
-
-	const handleCreate = async () => {
-		await (async () => onCreate(isbn))();
-		reset();
-	};
 </script>
 
 <form id="scan-input-container" use:form>
@@ -32,12 +26,6 @@
 		</svelte:fragment>
 		<div slot="endAdornment" class="flex gap-x-2">
 			<Button type="submit" disabled={isbn === ""}>Add</Button>
-			<Button type="button" on:click={handleCreate} size={ButtonSize.SM}>
-				<svelte:fragment slot="startAdornment">
-					<Edit size={16} />
-				</svelte:fragment>
-				Create
-			</Button>
 		</div>
 	</TextField>
 </form>
