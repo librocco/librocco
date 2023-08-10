66221fe23c68adfc956c61346d82bc0f26f9cd04
delasous <cdelasoul@gmail.com>
2023-08-10 14:42:53 +0200

    Feat: add RemoteDb description list to present data

---
+++ pkg/ui/src/DescriptionLists/RemoteDbData.story.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Hst } from "@histoire/plugin-svelte";
+	import { logEvent } from "histoire/client";
+
+	import RemoteDbData from "./RemoteDbData.svelte";
+
+	export let Hst: Hst;
+
+	const data = {
+		status: "Failed: network error",
+		url: "admin:admin@127.0.0.1:5000/dev",
+		direction: "sync" as const,
+		live: true,
+		retry: true,
+	}
+</script>
+
+<Hst.Story title="Description Lists / RemoteDbData" layout={{ type: "grid", width: 600 }}>
+	<RemoteDbData {data} onEdit={() => logEvent("edit", "Cancel & edit")}/>
+</Hst.Story>
