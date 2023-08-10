03eb0977afcc3062e4459940739f151709156654
delasous <cdelasoul@gmail.com>
2023-08-10 14:44:27 +0200

    Fix: add component exports & touch up spacing on RemoteDbForm

---
+++ pkg/ui/src/DescriptionLists/index.ts
@@ -0,0 +1,1 @@
+export { default as RemoteDbData } from "./RemoteDbData.svelte";
--- pkg/ui/src/Forms/index.ts
+++ pkg/ui/src/Forms/index.ts
@@ -1,3 +1,4 @@
 export { default as BookDetailForm } from "./BookDetailForm.svelte";
+export { default as RemoteDbForm } from "./RemoteDbForm.svelte";
 
 export * from "./types";
