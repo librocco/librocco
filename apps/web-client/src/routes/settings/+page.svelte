059b3dd1abee8ec732ad92a323defc7168b0286f
delasous <cdelasoul@gmail.com>
2023-08-14 17:55:18 +0200

    Rebuild settings page to use new remote settings form & description list

--- apps/web-client/src/routes/settings/+page.svelte
+++ apps/web-client/src/routes/settings/+page.svelte
@@ -1,61 +1,43 @@
 <script lang="ts">
-	import type { EventHandler } from "svelte/elements";
 	import { base } from "$app/paths";
 
-	import { Button, TextField, Header, InventoryPage } from "@librocco/ui";
+	import { Header, InventoryPage, RemoteDbForm, RemoteDbData, type RemoteDbConfig } from "@librocco/ui";
 
 	import { links } from "$lib/data";
-	import { remoteCouchConfigStore } from "$lib/stores/settings";
+	import { remoteDbStore } from "$lib/stores";
 	import { toastSuccess } from "$lib/toasts";
 
-	let errors = {
-		"couch-url": ""
-	};
-
-	const handleSubmit: EventHandler<Event & { readonly submitter: HTMLElement }, HTMLFormElement> = (event) => {
-		const formData = new FormData(event.target as HTMLFormElement);
-
-		const couchUrl = formData.get("couch-url") as string;
-
-		const urlRegex = new RegExp("^(https?://)(.+):(.+)@(.+):(.+)$");
-
-		if (couchUrl && urlRegex.test(couchUrl)) {
-			errors["couch-url"] = "";
-			remoteCouchConfigStore.set({ couchUrl });
-
-			toastSuccess("Remote CouchDB URL updated");
-		} else {
-			errors["couch-url"] =
-				"URL should have format https://<COUCHDB_USER:<COUCHDB_PASSWORD>@<COUCHDB_HOST>:<COUCHDB_PORT>/${DB_NAME}";
-		}
-	};
+	$: ({ replicator } = $remoteDbStore);
 </script>
 
 <InventoryPage>
 	<Header {links} currentLocation={`${base}/settings/`} slot="header" />
 
-	<div slot="table" class="flex justify-start">
-		<div class="flex basis-full flex-col gap-y-4 px-16 lg:basis-2/3">
-			<h1 class="text-xl">Database settings</h1>
+	<div slot="table" class="space-y-12">
+		<div class="flex flex-col sm:flex-row px-16 gap-6">
+			<div>
+				<h2 class="text-base font-semibold leading-7 text-gray-900">Database settings</h2>
+				<p class="mt-1 text-sm leading-6 text-gray-600">
+					Manage a connection to a remote database
+				</p>
+			</div>
+			<div class="w-full max-w-3xl">
+				{#if replicator}
+					<RemoteDbData
+						data={{ ...$replicator.config, status: $replicator.status.state }}
+						onEdit={() => remoteDbStore.destroyHandler(replicator)}
+					/>
+				{:else}
+					<RemoteDbForm 
+						onSubmit={(values) => {
+							remoteDbStore.createHandler(values);
 
-			<form method="POST" on:submit|preventDefault={handleSubmit}>
-				<div class="flex flex-col gap-y-4">
-					<TextField
-						name="couch-url"
-						id="couch-url"
-						label="Remote CouchDB URL"
-						placeholder="<COUCHDB_USER>:<COUCHDB_PASSWORD>@<COUCHDB_HOST>:<COUCHDB_PORT>/"
-						value={$remoteCouchConfigStore?.couchUrl}
-						isValid={errors?.["couch-url"] ? true : false}
-						required
-					>
-						<p slot="helpText">{errors?.["couch-url"] || ""}</p>
-					</TextField>
-					<div class="self-end">
-						<Button type="submit">Save settings</Button>
-					</div>
-				</div>
-			</form>
+							// TODO: centralise toasts. Toast this on status: INIT, along with other status changes
+							toastSuccess("Remote CouchDB URL updated");
+						}}
+					/>
+				{/if}
+			</div>
 		</div>
 	</div>
 </InventoryPage>
