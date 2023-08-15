f8fca946fff81ad5d0bcc8eb6651ee40f289f095
ikusteu <ivankust57@gmail.com>
2023-08-15 23:10:13 +0200

    Scan field autofocus, first pass:
    * Route all keyboard inputs, (unless an input field is the target) to the scan input element (on views where scan input element is present)
    * If another input element is focused, the default behaviour is applied (keyboard input is routed to said element)
    * Potential bug: If other input element is focused (say quantity field on a transaction) and scaner is used, the input will end up in the focused field (rather than scan input field)

--- pkg/ui/src/Forms/ScanInput.svelte
+++ pkg/ui/src/Forms/ScanInput.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { createForm } from "felte";
 	import { QrCode } from "lucide-svelte";
+	import { onDestroy, onMount } from "svelte";
 
 	import { Button } from "$lib/Button";
-	import { TextField, TextFieldSize } from "$lib/FormFields";
 
 	export let onAdd: (isbn: string) => void | Promise<void> = () => {};
 
@@ -16,15 +16,50 @@
 			reset();
 		}
 	});
+
+	let isbn = "";
+
+	let inputElement: HTMLInputElement;
+	const handleScan = (e: KeyboardEvent) => {
+		// If focused on an input element, pass the input to the element
+		if (e.target instanceof HTMLInputElement) return;
+
+		if (/[0-9]/.test(e.key)) {
+			// If numbered input, it passes as isbn input (likely from scan element)
+			isbn += e.key;
+			// Focus the scan element to appropriately handle enter key
+			inputElement?.focus();
+		}
+	};
+	onMount(() => {
+		window.addEventListener("keydown", handleScan);
+	});
+	onDestroy(() => {
+		window.removeEventListener("keydown", handleScan);
+	});
 </script>
 
 <form id="scan-input-container" use:form>
-	<TextField name="isbn" placeholder="Scan to add books..." variant={TextFieldSize.LG}>
-		<svelte:fragment slot="startAdornment">
-			<QrCode />
-		</svelte:fragment>
-		<div slot="endAdornment" class="flex gap-x-2">
-			<Button type="submit" disabled={!$data.isbn}>Add</Button>
+	<div class="my-[2px]">
+		<div class="mx-[2px] flex rounded-md shadow-sm outline outline-1 outline-gray-300 focus-within:outline-2 focus-within:outline-teal-500">
+			<div class="flex items-center bg-white pr-0 pl-3 text-gray-400">
+				<QrCode />
+			</div>
+			<input
+				type="text"
+				id="isbn"
+				placeholder="Scan to add books..."
+				aria-label="isbn"
+				class="block w-full border-0 px-4 py-4 text-sm focus:outline-0 focus:ring-0"
+				name="isbn"
+				bind:value={isbn}
+				bind:this={inputElement}
+			/>
+			<div class="flex items-center bg-white pl-1 pr-3 text-gray-400">
+				<div class="flex gap-x-2">
+					<Button type="submit" disabled={!$data.isbn}>Add</Button>
+				</div>
+			</div>
 		</div>
-	</TextField>
+	</div>
 </form>
