245569b2200b46cba85e151d9b110193567af8dc
fadwa <fadwa.mahmoud@gmail.com>
2023-08-31 10:28:53 +0300

    Test for plugin behaviour

--- plugins/book-data-extension/src/plugin/__tests__/listeners.test.ts
+++ plugins/book-data-extension/src/plugin/__tests__/listeners.test.ts
@@ -7,7 +7,7 @@ function mockAddEventListenerExtension(eventName: string, cb: (event: MessageEve
 		const message = new MessageEvent("message", {
 			source: window,
 			data: {
-				message: "BOOK_FETCHER_REQ:replyFromExtension:"
+				message: "BOOK_FETCHER:PING_RES"
 			}
 		});
 
@@ -20,7 +20,7 @@ function mockAddEventListenerBooks(eventName: string, cb: (event: MessageEvent) 
 		const message = new MessageEvent("message", {
 			source: window,
 			data: {
-				message: "BOOK_FETCHER_RES:bookFetcherPlugin:",
+				message: "BOOK_FETCHER:RES",
 				books: [{ title: "book-123456" }]
 			}
 		});
@@ -33,7 +33,7 @@ function mockAddEventListenerBooksTimeout(eventName: string, cb: (event: Message
 		const message = new MessageEvent("message", {
 			source: window,
 			data: {
-				message: "BOOK_FETCHER_RES:bookFetcherPlugin:",
+				message: "BOOK_FETCHER:RES",
 				books: [{ title: "book-123456" }]
 			}
 		});
@@ -46,28 +46,30 @@ describe("book fetching listeners", () => {
 	test("listenForExtension resolves to true when it receives response to ping", async () => {
 		vi.spyOn(helpers, "addEventListener").mockImplementation(mockAddEventListenerExtension);
 
-		helpers.postMessage(`BOOK_FETCHER_REQ:replyFromExtension:`);
+		helpers.postMessage(`BOOK_FETCHER:PING_REQ`);
 
-		const ww = await listenForExtension("BOOK_FETCHER_REQ:replyFromExtension:", 1000);
+		const extensionAvailable = await listenForExtension("BOOK_FETCHER:PING_RES", 1000);
 
-		expect(ww).toEqual(true);
+		expect(extensionAvailable).toEqual(true);
 	});
+
 	test("listenForBooks resolves to books when it receives response from extension", async () => {
 		vi.spyOn(helpers, "addEventListener").mockImplementation(mockAddEventListenerBooks);
 
-		helpers.postMessage(`BOOK_FETCHER_REQ:bookFetcherPlugin:`);
+		helpers.postMessage(`BOOK_FETCHER:REQ`);
 
-		const ww = await listenForBooks("BOOK_FETCHER_RES:bookFetcherPlugin:", 1000);
+		const books = await listenForBooks("BOOK_FETCHER:RES", 1000);
 
-		expect(ww).toEqual([{ title: "book-123456" }]);
+		expect(books).toEqual([{ title: "book-123456" }]);
 	});
+
 	test("listenForBooks resolves to [] when book data is not received before timeout", async () => {
 		vi.spyOn(helpers, "addEventListener").mockImplementation(mockAddEventListenerBooksTimeout);
 
-		helpers.postMessage(`BOOK_FETCHER_REQ:bookFetcherPlugin:`);
+		helpers.postMessage(`BOOK_FETCHER:REQ`);
 
-		const ww = await listenForBooks("BOOK_FETCHER_RES:bookFetcherPlugin:", 200);
+		const books = await listenForBooks("BOOK_FETCHER:RES", 200);
 
-		expect(ww).toEqual([]);
+		expect(books).toEqual([]);
 	});
 });
