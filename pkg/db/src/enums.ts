838f70842ec3d613b4a40dd7229442ad2ffc0282
ikusteu <ivankust57@gmail.com>
2023-08-31 15:58:15 +0200

    Set up receipts interface:
    * Create the interface, retrieved using `db.receipts()`
    * When instantiating interface, use 'printer-1' as printer id, TODO: make this customisable
    * Add `note.printReceipt` method for quick access (calling `db.receipts().print(note)`)
    * Test the behaviour

--- pkg/db/src/enums.ts
+++ pkg/db/src/enums.ts
@@ -1,4 +1,12 @@
 export enum DocType {
 	Note = "note",
-	Warehouse = "warehouse"
+	Warehouse = "warehouse",
+	PrintJob = "print_job"
+}
+
+export enum PrintJobStatus {
+	Pending = "PENDING",
+	Processing = "PROCESSING",
+	Done = "DONE",
+	Error = "ERROR"
 }
