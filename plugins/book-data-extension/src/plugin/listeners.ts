eaa32fd135e8d73d617de89ff05944dfdffdade0
fadwa <fadwa.mahmoud@gmail.com>
2023-09-02 22:46:33 +0300

    Add more test cases

--- plugins/book-data-extension/src/plugin/listeners.ts
+++ plugins/book-data-extension/src/plugin/listeners.ts
@@ -2,7 +2,10 @@ import { BookEntry } from "@librocco/db";
 import { addEventListener } from "./window-helpers";
 export function listenForExtension(message: string, timeout: number): Promise<boolean> {
 	return new Promise((resolve) => {
-		const promiseTimer = setTimeout(() => resolve(false), timeout);
+		const promiseTimer = setTimeout(() => {
+			removeEventListener("message", handler);
+			resolve(false);
+		}, timeout);
 
 		const handler = (event: MessageEvent) => {
 			if (event.source !== window) return;
@@ -18,7 +21,10 @@ export function listenForExtension(message: string, timeout: number): Promise<bo
 
 export function listenForBook(message: string, timeout: number): Promise<BookEntry | undefined> {
 	return new Promise((resolve) => {
-		const promiseTimer = setTimeout(() => resolve(undefined), timeout);
+		const promiseTimer = setTimeout(() => {
+			removeEventListener("message", handler);
+			resolve(undefined);
+		}, timeout);
 
 		const handler = (event: MessageEvent) => {
 			if (event.source !== window) return;
