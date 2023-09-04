d8c417a699656fbd1006cba2c75a797ea4b607d0
ikusteu <ivankust57@gmail.com>
2023-09-04 11:24:03 +0200

    Create a UI to update warehouse discount:
    * Create the `DiscountInput` element in the `ui` package
    * Add the input to all warehouse pages, except for the default one
    * Connect the dicount input to the db via warehouse discount store

--- pkg/ui/src/Forms/index.ts
+++ pkg/ui/src/Forms/index.ts
@@ -1,5 +1,6 @@
 export { default as BookDetailForm } from "./BookDetailForm.svelte";
 export { default as RemoteDbForm } from "./RemoteDbForm.svelte";
 export { default as ScanInput } from "./ScanInput/ScanInput.svelte";
+export { default as DiscountInput } from "./DiscountInput.svelte";
 
 export * from "./types";
