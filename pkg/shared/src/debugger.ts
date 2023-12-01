e9f4bf1895d3f8553cd9d3ecd2c37d9216696473
ikusteu <ivankust57@gmail.com>
2023-12-01 14:00:06 +0100

    Add time logging functionality to the debug context

--- pkg/shared/src/debugger.ts
+++ pkg/shared/src/debugger.ts
@@ -1,6 +1,11 @@
+import { filter, reduce, wrapIter } from "./generators";
+
 export interface DebugCtx {
 	name?: string;
 	debug?: boolean;
+	logTimes?: boolean;
+	logTimerStart?: (name: string) => void;
+	logTimerEnd?: (name: string) => void;
 }
 
 export const log =
@@ -16,3 +21,64 @@ export const log =
 			console.log(payload);
 		}
 	};
+
+export class DebugCtxWithTimer implements DebugCtx {
+	name?: string;
+	debug?: boolean;
+	logTimes?: boolean;
+
+	timers: Record<string, number>;
+	reports: Map<string, number>;
+
+	constructor(name?: string, { debug = false, logTimes = false } = {}) {
+		this.name = name;
+		this.debug = debug;
+		this.logTimes = logTimes;
+
+		this.timers = {};
+		this.reports = new Map<string, number>();
+	}
+
+	logTimerStart(name: string) {
+		this.timers[name] = Date.now();
+	}
+
+	logTimerEnd(name: string) {
+		const timer = this.timers[name];
+		if (!timer) {
+			console.log(`timer not found: '${name}'`);
+			return;
+		}
+		this.reports.set(name, Date.now() - timer);
+		delete this.timers[name];
+	}
+
+	getTimes(predicate?: (key: string) => boolean) {
+		if (!predicate) {
+			return this.reports;
+		}
+		return new Map(filter(this.reports, ([key]) => predicate(key)));
+	}
+
+	getAvgTimes(filterPredicate?: (key: string) => boolean) {
+		const times = this.getTimes(filterPredicate);
+		const total = reduce(times, (acc, [, cur]) => acc + cur, 0);
+		return total / times.size;
+	}
+
+	clearStats() {
+		this.timers = {};
+		this.reports = new Map<string, number>();
+	}
+}
+
+export const logTimerStart = (ctx: DebugCtx, name: string) => {
+	if (ctx.logTimes && ctx.logTimerStart) {
+		ctx.logTimerStart(name);
+	}
+};
+export const logTimerEnd = (ctx: DebugCtx, name: string) => {
+	if (ctx.logTimes && ctx.logTimerEnd) {
+		ctx.logTimerEnd(name);
+	}
+};
