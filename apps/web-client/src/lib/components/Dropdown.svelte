0c6c4699b6f687be96747ad8f2d01da6bbfb40b5
ikusteu <ivankust57@gmail.com>
2023-12-12 12:08:20 +0100

    Create a Dropdown component (with respect to new mocks), TODO: should be enhanced a bit

---
+++ apps/web-client/src/lib/components/Dropdown.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { MoreVertical } from "lucide-svelte";
+
+	let open = false;
+
+	const toggleOpen = () => (open = !open);
+</script>
+
+<div class="relative">
+	<button on:click={toggleOpen} class="rounded-md border border-gray-300 bg-white py-[9px] pl-[17px] pr-[15px]"
+		><MoreVertical class="border-gray-500" size={20} /></button
+	>
+
+	{#if open}
+		<div
+			class="absolute -bottom-3 right-0 z-50 min-w-[224px] translate-y-full overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+		>
+			<slot />
+		</div>
+	{/if}
+</div>
--- apps/web-client/src/lib/components/index.ts
+++ apps/web-client/src/lib/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Page } from "./Page.svelte";
 
 export { default as PlaceholderBox } from "./PlaceholderBox.svelte";
+export { default as Dropdown } from "./Dropdown.svelte";
 
 export * from "./Breadcrumbs";
