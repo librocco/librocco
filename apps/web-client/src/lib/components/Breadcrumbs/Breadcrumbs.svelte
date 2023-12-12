aff6e71b1f6fc027aaa48df5ab956dae5c89e051
ikusteu <ivankust57@gmail.com>
2023-12-12 12:02:50 +0100

    Create Breadcrumbs component + the 'createBreadcrumbs' util for generation of breadcrumbs per each view (for the new layout)

---
+++ apps/web-client/src/lib/components/Breadcrumbs/Breadcrumbs.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { ChevronRight } from "lucide-svelte";
+
+	import type { Breadcrumb } from "./types";
+
+	export let links: Breadcrumb[];
+</script>
+
+<nav {...$$restProps}>
+	<ul class="flex select-none items-center gap-2 text-sm font-medium leading-5 text-gray-500">
+		{#each links as { label, href }, i}
+			{@const isLast = i === links.length - 1}
+			{@const hasLink = Boolean(href)}
+			<li class="block">
+				{#if hasLink}
+					<a {href} class="underline-offset-2 hover:underline">{label}</a>
+				{:else}
+					<div>{label}</div>
+				{/if}
+			</li>
+			{#if !isLast}
+				<div class="mx-2 block"><ChevronRight size={20} /></div>
+			{/if}
+		{/each}
+	</ul>
+</nav>
