82a36bb50073298c63c97aa4922082052566776a
ikusteu <ivankust57@gmail.com>
2023-12-12 12:05:50 +0100

    Create Placeholder box component (used for empty page placeholders)

---
+++ apps/web-client/src/lib/components/PlaceholderBox.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let className = "";
+	export { className as class };
+
+	export let title = "";
+	export let description = "";
+
+	const iconProps = {
+		size: 36,
+		class: "text-white"
+	};
+</script>
+
+<div class="text-center {className}">
+	{#if $$slots.icon}
+		<div class="mx-auto mb-3 h-14 w-14 rounded-md bg-teal-500 p-2.5 text-center"><slot {iconProps} name="icon" /></div>
+	{/if}
+	<p class="text-xl font-semibold leading-7 text-gray-900">{title}</p>
+	<p class="mb-3 text-base font-normal leading-7 text-gray-600">{description}</p>
+	<slot />
+</div>
--- apps/web-client/src/lib/components/index.ts
+++ apps/web-client/src/lib/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Page } from "./Page.svelte";
 
+export { default as PlaceholderBox } from "./PlaceholderBox.svelte";
+
 export * from "./Breadcrumbs";
