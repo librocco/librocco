e91a3b074f752a0285c94948199b30422196b995
delasous <cdelasoul@gmail.com>
2023-12-14 16:00:33 +0100

    Fix: add trailing slash to proto paths

--- apps/web-client/src/lib/paths.ts
+++ apps/web-client/src/lib/paths.ts
@@ -3,10 +3,10 @@ import { base } from "$app/paths";
 const basepath = `${base}/proto`;
 
 export const PROTO_PATHS = {
-	STOCK: `${basepath}/stock`,
-	WAREHOUSES: `${basepath}/inventory/warehouses`,
-	INVENTORY: `${basepath}/inventory`,
-	INBOUND: `${basepath}/inventory/inbound`,
-	OUTBOUND: `${basepath}/outbound`,
-	SETTINGS: `${basepath}/settings`
+	STOCK: `${basepath}/stock/`,
+	WAREHOUSES: `${basepath}/inventory/warehouses/`,
+	INVENTORY: `${basepath}/inventory/`,
+	INBOUND: `${basepath}/inventory/inbound/`,
+	OUTBOUND: `${basepath}/outbound/`,
+	SETTINGS: `${basepath}/settings/`
 };
