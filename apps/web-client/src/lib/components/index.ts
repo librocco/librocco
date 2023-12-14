b060fabbc8e9ac0c3a85fef694a7fa866793b0d1
delasous <cdelasoul@gmail.com>
2023-12-14 16:58:16 +0100

    Feat: build tooltip wrapper and add to sidebar nav links

--- apps/web-client/src/lib/components/index.ts
+++ apps/web-client/src/lib/components/index.ts
@@ -3,5 +3,7 @@ export { default as Page } from "./Page.svelte";
 export { default as PlaceholderBox } from "./PlaceholderBox.svelte";
 export { default as Dropdown } from "./Dropdown.svelte";
 
+export { default as TooltipWrapper } from "./Melt/TooltipWrapper.svelte";
+
 export * from "./Breadcrumbs";
 export * from "./EntityList";
