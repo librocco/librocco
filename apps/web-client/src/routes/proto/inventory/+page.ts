de56831951841a1d071c35b26231a2e5c4a5147a
ikusteu <ivankust57@gmail.com>
2023-12-14 11:00:16 +0100

    Add functionality to the inbound, outbound, warehouse entity pages:
    * wire in the db
    * add layout/page load functions
    * use existing table components for books list display
    * update styles (overflow, scrolling and such)

--- apps/web-client/src/routes/proto/inventory/+page.ts
+++ apps/web-client/src/routes/proto/inventory/+page.ts
@@ -1,9 +1,9 @@
 import { base } from "$app/paths";
+import { PROTO_PATHS } from "$lib/paths";
 import { redirect, type Load } from "@sveltejs/kit";
 
 export const load: Load = async ({ url }) => {
-	console.log("Bump");
 	if ([`${base}/proto/inventory`, `${base}/proto/inventory/`].includes(url.pathname)) {
-		throw redirect(307, `${base}/proto/inventory/warehouses`);
+		throw redirect(307, PROTO_PATHS.WAREHOUSES);
 	}
 };
