209d75c5aad6ce7ce054803f5bbc45d7681ab393
ikusteu <ivankust57@gmail.com>
2023-12-15 17:48:54 +0100

    Extract button classes and apply to buttons in '/proto' views

--- apps/web-client/tailwind.config.cjs
+++ apps/web-client/tailwind.config.cjs
@@ -1,5 +1,29 @@
 const theme = require("../../pkg/scaffold/tailwind.theme");
-const plugins = require("../../pkg/scaffold/tailwind.plugins");
+const basePlugins = require("../../pkg/scaffold/tailwind.plugins");
+
+const plugins = [
+	...basePlugins,
+	function ({ addComponents }) {
+		addComponents({
+			// Buttons
+			".button": {
+				"@apply inline-flex items-center px-4 py-2 rounded-md gap-2": {}
+			},
+			".button-text": {
+				"@apply text-sm font-medium leading-5": {}
+			},
+			".button-green": {
+				"@apply bg-teal-500 text-green-50 active:bg-teal-400": {}
+			},
+			".button-white": {
+				"@apply bg-white border border-gray-300 text-gray-500 active:bg-gray-50": {}
+			},
+			".button-alert": {
+				"@apply bg-pink-50 text-pink-700 active:bg-pink-100": {}
+			}
+		});
+	}
+];
 
 module.exports = {
 	content: [
