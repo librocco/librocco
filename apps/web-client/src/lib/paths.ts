738bd7f6fe716745fabe099331d7a2678a17e3e4
ikusteu <ivankust57@gmail.com>
2023-12-15 10:49:59 +0100

    Replce PROTO_PATHS lookup with an 'appPath' function - providing more functionality and sanitisation

--- apps/web-client/src/lib/paths.ts
+++ apps/web-client/src/lib/paths.ts
@@ -2,11 +2,25 @@ import { base } from "$app/paths";
 
 const basepath = `${base}/proto`;
 
-export const PROTO_PATHS = {
-	STOCK: `${basepath}/stock/`,
-	WAREHOUSES: `${basepath}/inventory/warehouses/`,
-	INVENTORY: `${basepath}/inventory/`,
-	INBOUND: `${basepath}/inventory/inbound/`,
-	OUTBOUND: `${basepath}/outbound/`,
-	SETTINGS: `${basepath}/settings/`
+const PROTO_PATHS = {
+	stock: `${basepath}/stock/`,
+	warehouses: `${basepath}/inventory/warehouses/`,
+	inventory: `${basepath}/inventory/`,
+	inbound: `${basepath}/inventory/inbound/`,
+	outbound: `${basepath}/outbound/`,
+	settings: `${basepath}/settings/`
 };
+
+/**
+ * We're using this util to construct app paths. This is preferable to using constants as it
+ * allows us to construct paths with dynamic segments, while performing some additional sanitization:
+ * - requires the first segment to be the location key (e.g. "stock", "inventory", etc.)
+ * - joins all of the segments with "/"
+ * - ensures that the path ends with "/" (adds a trailing slash if neecessary)
+ * - ensutres that there are no double slashes in the path
+ * (often a result of joining constants + dynamis segments, e.g. `${base}/stock/${id}` -> base might or might not end with a slash)
+ * @param location - the location key (e.g. "stock", "inventory", etc.)
+ * @param segments - the dynamic segments of the path
+ */
+export const appPath = (location: keyof typeof PROTO_PATHS, ...segments: string[]) =>
+	[PROTO_PATHS[location], ...segments].join("/").concat("/").replaceAll("//", "/");
--- apps/web-client/src/routes/proto/inventory/+page.ts
+++ apps/web-client/src/routes/proto/inventory/+page.ts
@@ -1,9 +1,11 @@
-import { base } from "$app/paths";
-import { PROTO_PATHS } from "$lib/paths";
 import { redirect, type Load } from "@sveltejs/kit";
+
+import { base } from "$app/paths";
+
+import { appPath } from "$lib/paths";
 
 export const load: Load = async ({ url }) => {
 	if ([`${base}/proto/inventory`, `${base}/proto/inventory/`].includes(url.pathname)) {
-		throw redirect(307, PROTO_PATHS.WAREHOUSES);
+		throw redirect(307, appPath("warehouses"));
 	}
 };
