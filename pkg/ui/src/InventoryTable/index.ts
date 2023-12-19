09c7399e9cde1f7fef9ef3ef84f458e481f342b7
delasous <cdelasoul@gmail.com>
2023-12-19 11:55:56 +0100

    Feat: add new stock table to inventory/warehouse page

--- pkg/ui/src/InventoryTable/index.ts
+++ pkg/ui/src/InventoryTable/index.ts
@@ -2,6 +2,9 @@ export { default as InventoryTable } from "./InventoryTable.svelte";
 export { default as OutNoteTable } from "./OutNoteTable.svelte";
 export { default as ComboboxTable } from "./ComboboxTable.svelte";
 
+export { default as NewStockTable } from "./NewStockTable.svelte";
+export { default as NewOutboundTable } from "./NewOutboundTable.svelte";
+
 export { createTable } from "./table";
 
 export type { WarehouseChangeDetail, TransactionUpdateDetail, RemoveTransactionsDetail } from "./types";
