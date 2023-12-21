aa473d8c9cd6f2c50a4ede2906a4a2b85eed9650
delasous <cdelasoul@gmail.com>
2023-12-21 15:19:48 +0100

    Feat: draft Dialog

--- apps/web-client/src/lib/components/index.ts
+++ apps/web-client/src/lib/components/index.ts
@@ -6,4 +6,6 @@ export { default as TooltipWrapper } from "./Melt/TooltipWrapper.svelte";
 export { default as DropdownWrapper } from "./Melt/DropdownWrapper.svelte";
 export { default as PopoverWrapper } from "./Melt/PopoverWrapper.svelte";
 
+export { default as Dialog } from "./Dialog.svelte";
+
 export * from "./Breadcrumbs";
