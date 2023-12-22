e128e6878e66d984662e408dfde13970f5416a81
delasous <cdelasoul@gmail.com>
2023-12-22 11:37:28 +0100

    Feat: finalise ConfirmActionDialog style & structure

--- apps/web-client/src/lib/components/index.ts
+++ apps/web-client/src/lib/components/index.ts
@@ -6,6 +6,6 @@ export { default as TooltipWrapper } from "./Melt/TooltipWrapper.svelte";
 export { default as DropdownWrapper } from "./Melt/DropdownWrapper.svelte";
 export { default as PopoverWrapper } from "./Melt/PopoverWrapper.svelte";
 
-export { default as Dialog } from "./Dialog.svelte";
+export { default as ConfirmActionDialog } from "./ConfirmActionDialog.svelte";
 
 export * from "./Breadcrumbs";
