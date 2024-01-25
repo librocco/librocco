5fd953a3a890e4b532f4c8e190384667331247e7
delasous <cdelasoul@gmail.com>
2024-01-25 11:30:50 +0100

    Chore: upgrade eslint and friends

--- pkg/scaffold/.eslintrc.ui.cjs
+++ pkg/scaffold/.eslintrc.ui.cjs
@@ -2,20 +2,37 @@ const baseConfig = require("./.eslintrc.cjs");
 
 module.exports = {
 	...baseConfig,
-	plugins: [...baseConfig.plugins, "svelte3"],
+	plugins: [...baseConfig.plugins],
 	overrides: [
 		...(baseConfig.overrides || []),
 		{
 			files: ["*.svelte"],
-			processor: "svelte3/svelte3",
-		},
+			parser: "svelte-eslint-parser",
+			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+				// This might not be the smartest move, but I couldn't find a straight answer as to why
+				// we are getting these warnings: open and closed discussions on the topic are very convoluted
+				// It seems to work without issue... but keep an eye on it
+				warnOnUnsupportedTypeScriptVersion: false
+			},
+			rules: {
+				"no-unsafe-optional-chaining": "off",
+				"no-inner-declarations": "off",
+				"no-import-assign": "off",
+				"@typescript-eslint/no-unused-vars": "off",
+				"@typescript-eslint/no-empty-function": "off",
+				"@typescript-eslint/no-inferrable-types": "off",
+				"@typescript-eslint/no-non-null-assertion": "off",
+				"@typescript-eslint/no-non-null-asserted-optional-chain": "off"
+			}
+		}
 	],
 	settings: {
-		...baseConfig.settings,
-		"svelte3/typescript": () => require("typescript"),
+		...baseConfig.settings
 	},
 	env: {
 		...baseConfig.env,
-		browser: true,
-	},
+		browser: true
+	}
 };
