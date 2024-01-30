87cf6c89ce91f3fa23626de3bdb6d64d59e901a2
delasous <cdelasoul@gmail.com>
2024-01-30 12:21:21 +0100

    Feature: create ScannerForm to wrap scan input and handle submit events
    - Removes window 'handleSubmit' defined in scan action => fixes greedy
    enter bug where scan input would try to submit and throw an error on
    empty input when a user presses enter anywhere on the page

---
+++ apps/web-client/src/lib/forms/ScannerForm.svelte
@@ -0,0 +1,35 @@
+<script lang="ts" context="module">
+	import type { SuperForm } from "sveltekit-superforms/client";
+
+	type ScannerForm = SuperForm<ZodValidation<typeof scannerSchema>, unknown>;
+	export type ScannerFormOptions = ScannerForm["options"];
+</script>
+
+<script lang="ts">
+	import type { ZodValidation } from "sveltekit-superforms";
+	import { superForm, superValidateSync } from "sveltekit-superforms/client";
+
+	import { scannerSchema, type ScannerData } from "$lib/forms/schemas";
+
+	import { scan } from "$lib/actions/scan";
+
+	export let data: ScannerData | null;
+	export let options: ScannerFormOptions;
+
+	const _form = superValidateSync(data, scannerSchema);
+	const form = superForm(_form, options);
+
+	const { form: formStore, enhance } = form;
+</script>
+
+<form use:enhance method="POST" id="scan-form" class="h-full w-full p-0.5">
+	<input
+		name="isbn"
+		id="isbn"
+		use:scan
+		bind:value={$formStore.isbn}
+		required
+		placeholder="Scan to add books"
+		class="h-full w-full border-0 focus:outline-none focus:ring-0"
+	/>
+</form>
