47e3f14a4231f526208f7c3b724a58cfee07dbb1
delasous <cdelasoul@gmail.com>
2024-02-02 13:26:04 +0100

    Add story for order dialog

--- apps/web-client/src/lib/components/Dialogs/Dialog.story.svelte
+++ apps/web-client/src/lib/components/Dialogs/Dialog.story.svelte
@@ -11,11 +11,25 @@
 	export let Hst: Hst;
 </script>
 
-<Hst.Story title="Dialogs / Default" layout={{ type: "grid", width: 600 }}>
-	<Hst.Variant title="Active">
+<Hst.Story title="Dialogs" layout={{ type: "grid", width: 600 }}>
+	<Hst.Variant title="Default - Commit">
 		<Dialog {dialog} type="commit">
 			<svelte:fragment slot="title">Commit inbound note-9?</svelte:fragment>
 			<svelte:fragment slot="description">15 books will be added to Warehouse-1</svelte:fragment>
+		</Dialog>
+	</Hst.Variant>
+	<Hst.Variant title="Default - Delete">
+		<Dialog {dialog} type="delete">
+			<svelte:fragment slot="title">Permanently delete warehouse 1?</svelte:fragment>
+			<svelte:fragment slot="description">Once you delete this warehouse, 380 books will be removed from your stock</svelte:fragment>
+		</Dialog>
+	</Hst.Variant>
+	<Hst.Variant title="Order - Commit">
+		<Dialog {dialog} type="commit">
+			<svelte:fragment slot="title">Create a new order for Martin Freeman?</svelte:fragment>
+			<svelte:fragment slot="description"
+				>This includes <span class="font-bold">3 books</span> and totals <span class="font-bold">$66</span>.</svelte:fragment
+			>
 		</Dialog>
 	</Hst.Variant>
 </Hst.Story>
