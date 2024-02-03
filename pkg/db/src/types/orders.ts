8c0c7da092b8ad2432d610f9160d882cb0350376
fadwa <fadwa.mahmoud@gmail.com>
2024-02-03 15:01:35 +0200

    Customer order db interface

--- pkg/db/src/types/orders.ts
+++ pkg/db/src/types/orders.ts
@@ -1,7 +1,43 @@
-import { DatabaseInterface as BaseDatabaseInterface } from "./misc";
+/* eslint-disable @typescript-eslint/ban-types */
+
+import { DatabaseInterface as BaseDatabaseInterface, CouchDocument } from "./misc";
 
 export type OrdersDatabaseInterface = BaseDatabaseInterface;
 
 export interface NewOrdersDatabase {
 	(db: PouchDB.Database): OrdersDatabaseInterface;
 }
+
+/**
+ * A (standardized) customer order interface:
+ * * standard data structure
+ * * standard method interface
+ */
+export type CustomerOrderInterface<A extends Record<string, any> = {}> = CustomerOrderProto<A> & CustomerOrderData<A>;
+
+
+/**
+ * A standardized interface (interface of methods) for a customer order.
+ * Different implementations might vary, but should always extend this interface.
+ */
+export interface CustomerOrderProto<A extends Record<string, any> = {}> {
+	create: () => Promise<CustomerOrderInterface<A>>;
+}
+
+
+/**
+ * Standardized data that should be present in any customer order
+ * (different implementations might differ, but should extend this structure)
+ */
+export type CustomerOrderData<A extends Record<string, any> = {}> = CouchDocument<
+	{
+		orderId: string;
+		updatedAt: string | null;
+		// equivalent of committed 
+		draft: boolean;
+		email: string;
+		deposit: number;
+
+		books: { isbn: string }[]
+	} & A
+>;
