2a545daa48ffecb79aca6b8fb7155677c9ae5866
fadwa <fadwa.mahmoud@gmail.com>
2024-02-07 16:13:49 +0200

    Instantiate multiple customer orders

--- pkg/db/src/implementations/version-1.2/customer-orders.ts
+++ pkg/db/src/implementations/version-1.2/customer-orders.ts
@@ -1,41 +1,21 @@
 import { BehaviorSubject, firstValueFrom, Observable, ReplaySubject, share, Subject } from "rxjs";
 
-import { VolumeStock } from "@librocco/shared";
-
 import { DocType } from "@/enums";
 
 import { VersionedString, OrdersDatabaseInterface, CustomerOrderInterface, CustomerOrderData } from "@/types";
 
 import { versionId } from "./utils";
-import { isEmpty, isVersioned, runAfterCondition, uniqueTimestamp } from "@/utils/misc";
+import { isEmpty, runAfterCondition, uniqueTimestamp } from "@/utils/misc";
 import { newDocumentStream } from "@/utils/pouchdb";
 
-
 class CustomerOrder implements CustomerOrderInterface {
-
-	// {
-	// 	"id": string, // use 'timestampedId' (or 'timestampId', not sure, it's in the 'db' package, under utils I think
-	// 	"draft": boolean,
-	// 	"displayName": string, // "name" -> "displayName" to keep consistent with the rest of the app
-	// 	"email": string,
-	// 	"books": [ 
-	// 	  { isbn: string }, // No status for now we can add that later when dealing with supplier orders
-	// 	],
-	// 	"deposit"?: number // I think the deposit should be calculated on display without persisting and then added when the order is placed
-	// 	}
+	_id: VersionedString;
+	_rev?: string;
+	_deleted?: boolean;
 
 	#db: OrdersDatabaseInterface;
-
 	#initialized = new BehaviorSubject(false);
 	#exists = false;
-
-	draft = true;
-
-	orderId = "";
-	email = "";
-	deposit = 0;
-
-	books = []
 
 	// Update stream receives the latest document (update) stream from the db. It's multicasted using plain RxJS Subject (no repeat or anything).
 	// This stream is used to signal the update has happened (and has been streamed to update the instance). Subscribers needing to be notified when
@@ -46,16 +26,11 @@ class CustomerOrder implements CustomerOrderInterface {
 	// subscribe to this stream.
 	#stream: Observable<CustomerOrderData>;
 
-	// timestramped uid
-	_id: VersionedString;
+	draft = true;
+	email = "";
+	deposit = 0;
+	books = [];
 	docType = DocType.CustomerOrder;
-	_rev?: string;
-	_deleted?: boolean;
-
-
-	entries: VolumeStock[] = [];
-	committed = false;
-	displayName = "";
 	updatedAt: string | null = null;
 
 	constructor(db: OrdersDatabaseInterface, id?: string) {
@@ -64,13 +39,7 @@ class CustomerOrder implements CustomerOrderInterface {
 		// Store the id internally:
 		// - if id is a single segment id, prepend the warehouse id and version the string
 		// - if id is a full id, assign it as is
-		this._id = !id
-			? versionId(`${uniqueTimestamp()}`)
-			: isVersioned(id, "v1") // If id is versioned, it's a full id, assign it as is
-				? id
-				: versionId(`${id}`);
-
-
+		this._id = !id ? versionId(`${uniqueTimestamp()}`) : versionId(`${id}`);
 
 		// Create the internal document stream, which will be used to update the local instance on each change in the db.
 		const updateSubject = new Subject<CustomerOrderData>();
@@ -93,7 +62,37 @@ class CustomerOrder implements CustomerOrderInterface {
 		return this;
 	}
 
+	/**
+	 * Update field is a method for internal usage, used to update the local field, if the value is provided
+	 */
+	private updateField<K extends keyof CustomerOrderData>(field: K, value?: CustomerOrderData[K]) {
+		if (value !== undefined) {
+			this[field] = value as any;
+		}
+		this.#exists = true;
+		return this;
+	}
+	/**
+	 * Update instance is a method for internal usage, used to update the instance with the data (doesn't update the DB)
+	 */
+	private updateInstance(data: Partial<Omit<CustomerOrderData, "_id">>): CustomerOrderInterface {
+		// No-op if the data is empty
+		if (isEmpty(data)) {
+			return this;
+		}
 
+		// Update the data with provided fields
+		this.updateField("_rev", data._rev);
+		this.updateField("books", data.books);
+		this.updateField("deposit", data.deposit);
+		this.updateField("docType", data.docType);
+		this.updateField("updatedAt", data.updatedAt);
+		this.updateField("email", data.email);
+
+		this.#exists = true;
+
+		return this;
+	}
 	/**
 	 * If order doesn't exist in the db, create an order with initial values.
 	 * No-op otherwise.
@@ -116,49 +115,9 @@ class CustomerOrder implements CustomerOrderInterface {
 		}, this.#initialized);
 	}
 
-
-	/**
-	 * Update field is a method for internal usage, used to update the local field, if the value is provided
-	 */
-	private updateField<K extends keyof CustomerOrderData>(field: K, value?: CustomerOrderData[K]) {
-		if (value !== undefined) {
-			this[field] = value as any;
-		}
-		this.#exists = true;
-		return this;
-	}
-	/**
-	 * Update instance is a method for internal usage, used to update the instance with the data (doesn't update the DB)
-	 */
-	private updateInstance(data: Partial<Omit<CustomerOrderData, "_id">>): CustomerOrderInterface {
-		// No-op if the data is empty
-		if (isEmpty(data)) {
-			return this;
-		}
-
-		// Update the data with provided fields
-		this.updateField("_rev", data._rev);
-		this.updateField("orderId", data.orderId);
-		this.updateField("books", data.books);
-		this.updateField("deposit", data.deposit);
-		this.updateField("docType", data.docType);
-		this.updateField("updatedAt", data.updatedAt);
-		this.updateField("email", data.email);
-
-		this.#exists = true;
-
-		return this;
-	}
-
-
 	// get(): Promise<CustomerOrderInterface | undefined> {}
 
-
-
-
 	// async commit(ctx: debug.DebugCtx, options?: { force: boolean }): Promise<CustomerOrderInterface> {}
-
-
 
 	// stream() {}
 }
