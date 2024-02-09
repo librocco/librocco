fe31e09e69fcea3ca2ce8ed7c5247732e4906df5
delasous <cdelasoul@gmail.com>
2024-02-09 11:36:32 +0100

    Feat: adds OrderTables
    - Adds new Tables dir to organise:
    -- reusbable Cells
    -- OrderTables components, styles and variants
    -- stories for Cells and OrderTables

---
+++ apps/web-client/src/lib/components/Tables/OrderTables/BodyHead.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let borderStyle: "yellow" | "gray";
+
+	const className = {
+		gray: "bg-gray-200",
+		yellow: "bg-yellow-200"
+	};
+</script>
+
+<div class="flex h-full items-center gap-x-2">
+	<div class="w-2 self-stretch rounded-md {className[borderStyle]}" />
+	<slot />
+</div>
