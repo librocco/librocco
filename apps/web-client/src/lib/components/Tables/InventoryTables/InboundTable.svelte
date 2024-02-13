10a932a02652037c0371d067a2824525233eb323
delasous <cdelasoul@gmail.com>
2024-02-13 23:38:05 +0100

    Update all InventoryTables to use auto layout

--- apps/web-client/src/lib/components/Tables/InventoryTables/InboundTable.svelte
+++ apps/web-client/src/lib/components/Tables/InventoryTables/InboundTable.svelte
@@ -25,23 +25,23 @@
 	const { editQuantity } = createInboundTableEvents(dispatch);
 </script>
 
-<table id="inventory-table" class="xs:table-fixed table" use:tableAction={{ rowCount }}>
+<table id="inventory-table" class="table" use:tableAction={{ rowCount }}>
 	<thead>
 		<tr>
-			<th scope="col" class="w-[20%] lg:w-[13%] xl:w-[10%]">
+			<th scope="col">
 				<span class="hidden lg:inline">ISBN</span>
 				<span class="inline lg:hidden">Book</span>
 			</th>
 			<th scope="col" class="show-col-lg"> Title </th>
 			<th scope="col" class="show-col-lg"> Authors </th>
-			<th scope="col" class="w-[6%] lg:w-[8%]"> Price </th>
-			<th scope="col" class="w-[6%] lg:w-[8%]"> Quantity </th>
-			<th scope="col" class="show-col-md w-[10%]"> Publisher </th>
-			<th scope="col" class="show-col-md w-[6%]"> Year </th>
-			<th scope="col" class="show-col-xl w-[10%]"> Edited By </th>
-			<th scope="col" class="show-col-xl w-[4%]"> O.P </th>
+			<th scope="col" class="table-cell-fit"> Price </th>
+			<th scope="col" class="table-cell-fit"> Quantity </th>
+			<th scope="col" class="show-col-md"> Publisher </th>
+			<th scope="col" class="show-col-lg table-cell-fit"> Year </th>
+			<th scope="col" class="show-col-xl"> Edited By </th>
+			<th scope="col" class="show-col-xl table-cell-fit"> O.P </th>
 			{#if $$slots["row-actions"]}
-				<th scope="col" class="w-[5%]"> <HeadCol label="Row Actions" srOnly /> </th>
+				<th scope="col" class="table-cell-fit"> <HeadCol label="Row Actions" srOnly /> </th>
 			{/if}
 		</tr>
 	</thead>
@@ -61,36 +61,36 @@
 				warehouseDiscount
 			} = row}
 			<tr use:table.tableRow={{ position: rowIx }}>
-				<th scope="row" data-property="isbn">
+				<th scope="row" data-property="isbn" class="table-cell-max">
 					<BookHeadCell data={{ isbn, title, authors, year }} />
 				</th>
 
-				<td data-property="title" class="show-col-lg">
+				<td data-property="title" class="show-col-lg table-cell-max">
 					{title}
 				</td>
-				<td data-property="authors" class="show-col-lg">
+				<td data-property="authors" class="show-col-lg table-cell-max">
 					{authors}
 				</td>
-				<td data-property="price">
+				<td data-property="price" class="table-cell-fit">
 					<BookPriceCell data={{ price, warehouseDiscount }} />
 				</td>
-				<td data-property="quantity">
+				<td data-property="quantity" class="table-cell-fit">
 					<BookQuantityFormCell {rowIx} {quantity} on:submit={(event) => editQuantity(event, row)} />
 				</td>
-				<td data-property="publisher" class="show-col-md">
+				<td data-property="publisher" class="show-col-md table-cell-max">
 					{publisher}
 				</td>
-				<td data-property="year" class="show-col-md">
+				<td data-property="year" class="show-col-lg table-cell-fit">
 					{year}
 				</td>
-				<td data-property="editedBy" class="show-col-xl">
+				<td data-property="editedBy" class="show-col-xl table-cell-max">
 					{editedBy}
 				</td>
-				<td data-property="outOfPrint" class="show-col-xl">
+				<td data-property="outOfPrint" class="show-col-xl table-cell-fit">
 					<BookOutPrintCell {rowIx} {outOfPrint} />
 				</td>
 				{#if $$slots["row-actions"]}
-					<td>
+					<td class="table-cell-fit">
 						<slot name="row-actions" {row} {rowIx} />
 					</td>
 				{/if}
