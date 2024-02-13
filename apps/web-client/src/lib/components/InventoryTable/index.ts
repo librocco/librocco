6ead7f3b11765e722cdb720584a07c39f15c7889
delasous <cdelasoul@gmail.com>
2024-02-13 17:27:43 +0100

    Feat: Rebuild OutboundTable with ReusableCells

--- apps/web-client/src/lib/components/InventoryTable/index.ts
+++ apps/web-client/src/lib/components/InventoryTable/index.ts
@@ -1,8 +1,6 @@
 export { default as StockTable } from "./StockTable.svelte";
 export { default as OutboundTable } from "./OutboundTable.svelte";
 
-export { default as WarehouseSelect } from "./WarehouseSelect.svelte";
-
 export { createTable } from "./table";
 
-export type { WarehouseChangeDetail, InventoryTableData, OutNoteTableData, BookCoreRowData } from "./types";
+export type { InventoryTableData, OutNoteTableData, BookCoreRowData } from "./types";
--- apps/web-client/src/lib/components/Tables/stories/InventoryTables.story.svelte
+++ apps/web-client/src/lib/components/Tables/stories/InventoryTables.story.svelte
@@ -3,7 +3,7 @@
 	import { writable } from "svelte/store";
 	import { FileEdit } from "lucide-svelte";
 
-	import { StockTable, InboundTable } from "../InventoryTables";
+	import { StockTable, InboundTable, OutboundTable } from "../InventoryTables";
 
 	import { createTable } from "$lib/actions";
 
@@ -42,5 +42,18 @@
 				</button>
 			</div>
 		</InboundTable>
+	</Hst.Variant>
+
+	<Hst.Variant title="Outbound">
+		<OutboundTable table={defaultStockTable} on:edit-warehouse={(e) => console.log(e)}>
+			<div slot="row-actions" let:row let:rowIx>
+				<button on:click={() => console.log(row)} class="rounded p-3 text-gray-500 hover:bg-gray-50 hover:text-gray-900">
+					<span class="sr-only">Edit row {rowIx}</span>
+					<span class="aria-hidden">
+						<FileEdit />
+					</span>
+				</button>
+			</div>
+		</OutboundTable>
 	</Hst.Variant>
 </Hst.Story>
