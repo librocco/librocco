0ff5afd80e9a1bded2abcf032423d52719db555b
delasous <cdelasoul@gmail.com>
2024-02-13 22:27:01 +0100

    Add WarehouseSelect story

---
+++ apps/web-client/src/lib/components/WarehouseSelect/WarehouseSelect.story.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Hst } from "@histoire/plugin-svelte";
+
+	import WarehouseSelect from "./WarehouseSelect.svelte";
+
+	import { rows, outNoteRows } from "$lib/__testData__/rowData";
+
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="Warehouse Select" layout={{ type: "grid", width: 500 }}>
+	<Hst.Variant title="Default">
+		<td>
+			<WarehouseSelect data={outNoteRows[0]} rowIx={0} />
+		</td>
+	</Hst.Variant>
+
+	<Hst.Variant title="Only one option - selected">
+		<td>
+			<WarehouseSelect data={rows[0]} rowIx={1} />
+		</td>
+	</Hst.Variant>
+</Hst.Story>
