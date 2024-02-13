60820efece6171d298015c94668d5b7a1f2caa91
delasous <cdelasoul@gmail.com>
2024-02-13 16:24:13 +0100

    Feat: Rebuild Stock table with reusable cells
    - Fixes discount price rounding
    - Adds discount percentage to price col

---
+++ apps/web-client/src/lib/components/Tables/InventoryTables/BookHeadCell.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import BodyMultiRow from "../Cells/BodyMultiRow.svelte";
+	import type { InventoryTableData } from "../types";
+
+	export let data: Pick<InventoryTableData, "isbn" | "title" | "authors" | "year">;
+
+	$: ({ isbn, title, authors, year } = data);
+</script>
+
+<div class="max-w-sm">
+	<span class="text-gray-800">{isbn}</span>
+	<BodyMultiRow
+		dlClassName="flex flex-col gap-y-0.5 mt-1 font-light text-gray-500 lg:hidden"
+		rows={{
+			name: { data: title },
+			authors: { data: authors },
+			year: { data: year }
+		}}
+	/>
+</div>
