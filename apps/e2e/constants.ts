52205904a6218733e3d9faa3c324a3158977d392
ikusteu <ivankust57@gmail.com>
2024-02-23 18:32:50 +0100

    E2E: Extend assertion timeout to 30000ms

--- apps/e2e/constants.ts
+++ apps/e2e/constants.ts
@@ -1,4 +1,4 @@
 export const baseURL = process.env.CI ? "http://localhost:4173" : "http://localhost:5173";
 
 /** Max timeout for DOM assertions (waitFor, etc. - longer in CI, default in non-CI) */
-export const assertionTimeout = process.env.CI ? 15000 : undefined;
+export const assertionTimeout = process.env.CI ? 30000 : undefined;
