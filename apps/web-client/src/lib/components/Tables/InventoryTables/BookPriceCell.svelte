5c9256cf414c3ead4861d05c0d6d9ca2ce00328f
Lara Šafar Godinić <lara.safar.godinic@gmail.com>
2024-03-18 09:03:32 +0100

    Show price as two decimals

--- apps/web-client/src/lib/components/Tables/InventoryTables/BookPriceCell.svelte
+++ apps/web-client/src/lib/components/Tables/InventoryTables/BookPriceCell.svelte
@@ -9,12 +9,12 @@
 {#if price && warehouseDiscount}
 	<div class="flex flex-col items-start gap-0.5">
 		<span class="sr-only">Discounted price:</span>
-		<span>€{Math.round(price * (100 - warehouseDiscount)) / 100}</span>
+		<span>€{((price * (100 - warehouseDiscount)) / 100).toFixed(2)}</span>
 		<span class="sr-only">Original price:</span>
-		<span class="text-gray-400 line-through">(€{price})</span>
+		<span class="text-gray-400 line-through">(€{price.toFixed(2)})</span>
 		<span class="sr-only">Percentage discount:</span>
 		<span class="text-gray-400">-{warehouseDiscount}%</span>
 	</div>
 {:else}
-	{price ? `€${price}` : "N/A"}
+	{price ? `€${price.toFixed(2)}` : "N/A"}
 {/if}
