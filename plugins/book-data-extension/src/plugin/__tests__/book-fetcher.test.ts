f8140726c7affbd9dc717eefaae23e48464510b3
ikusteu <ivankust57@gmail.com>
2024-04-16 19:06:25 +0200

    book data plugin: refactor to return the same number of results even if some books not found:
    * refactor no-plugin fallback
    * update db tests
    * refactor the book data extension plugin (to support said functionality)
    * refactor the book data extension plugin - for easier testing + update the tests

--- plugins/book-data-extension/src/plugin/__tests__/book-fetcher.test.ts
+++ plugins/book-data-extension/src/plugin/__tests__/book-fetcher.test.ts
@@ -1,87 +1,62 @@
+import { filter, firstValueFrom } from "rxjs";
 import { test, describe, expect, vi } from "vitest";
-import * as listeners from "../listeners";
-import * as helpers from "../window-helpers";
+
+import { BookEntry, BookFetcherPlugin } from "@librocco/db";
+import { testUtils } from "@librocco/shared";
+
+import * as comm from "../comm";
 import { createBookDataExtensionPlugin } from "..";
 
-function mockListenForExtension() {
-	return new Promise<boolean>((resolve) => resolve(true));
-}
-
-function mockAddEventListenerBooks(book: Record<string, string> | undefined, timeout = 0) {
-	return function (eventName: string, cb: (event: MessageEvent) => void) {
-		if (eventName === "message") {
-			const isbn = book?.title.split("-")[1];
-			const message = new MessageEvent("message", {
-				source: window,
-				data: {
-					message: `BOOK_FETCHER:RES:${isbn}`,
-					book
-				}
-			});
-
-			setTimeout(() => cb(message), timeout);
-		}
-	};
-}
+const { waitFor } = testUtils;
+/** A convenience method to wait for the extension to become "available" */
+const waitForAvailable = (plugin: BookFetcherPlugin) =>
+	waitFor(async () => {
+		await firstValueFrom(plugin.isAvailableStream.pipe(filter(Boolean)));
+	});
+const withExtensionAvailable = async (plugin: BookFetcherPlugin) => {
+	await waitForAvailable(plugin);
+	return plugin;
+};
 
 describe("createBookDataExtensionPlugin", () => {
 	test("fetchBookData returns books when called with isbns", async () => {
-		vi.spyOn(listeners, "listenForExtension").mockImplementation(mockListenForExtension);
-		vi.spyOn(helpers, "addEventListener").mockImplementation(mockAddEventListenerBooks({ title: "book-12345" }));
+		vi.spyOn(comm, "ping").mockImplementation(() => Promise.resolve(true));
+		vi.spyOn(comm, "fetchBook").mockImplementation((isbn: string) => Promise.resolve({ title: `book-${isbn}` } as BookEntry));
 
-		const books = await createBookDataExtensionPlugin().fetchBookData(["12345"]);
+		const plugin = await withExtensionAvailable(createBookDataExtensionPlugin());
+		// The isbns are in a scattered lexicographical order to verify that the results will come in the same order as requested
+		const books = await plugin.fetchBookData(["3333", "1111", "2222"]);
 
-		expect(books).toEqual([{ title: "book-12345" }]);
+		expect(books).toEqual([{ title: "book-3333" }, { title: "book-1111" }, { title: "book-2222" }]);
 	});
 
-	test("fetchBookData returns [] when called with isbn that are not found", async () => {
-		vi.spyOn(listeners, "listenForExtension").mockImplementation(mockListenForExtension);
-		vi.spyOn(helpers, "addEventListener").mockImplementation(mockAddEventListenerBooks(undefined));
+	test("fetchBookData returns the same number of results even if book data for some isbns isn't found", async () => {
+		vi.spyOn(comm, "ping").mockImplementation(() => Promise.resolve(true));
+		vi.spyOn(comm, "fetchBook").mockImplementation((isbn: string) =>
+			isbn == "1111"
+				? // Make it so as if "1111" is not found
+				  Promise.resolve(undefined)
+				: Promise.resolve({ title: `book-${isbn}` } as BookEntry)
+		);
 
-		const books = await createBookDataExtensionPlugin().fetchBookData(["12345"]);
+		const plugin = await withExtensionAvailable(createBookDataExtensionPlugin());
+		const books = await plugin.fetchBookData(["3333", "1111", "2222"]);
 
-		expect(books).toEqual([]);
+		expect(books).toEqual([{ title: "book-3333" }, undefined, { title: "book-2222" }]);
 	});
 
-	test("fetchBookData returns [] when book data is not received before timeout", async () => {
-		vi.spyOn(listeners, "listenForExtension").mockImplementation(mockListenForExtension);
-		vi.spyOn(helpers, "addEventListener").mockImplementation(mockAddEventListenerBooks({ title: "book-12345" }, 5000));
+	test("fetchBookData returns array filled with undefined values if extension is not available", async () => {
+		vi.spyOn(comm, "ping").mockImplementation(() => Promise.resolve(false));
+		vi.spyOn(comm, "fetchBook").mockImplementation((isbn: string) =>
+			isbn == "1111"
+				? // Make it so as if "1111" is not found
+				  Promise.resolve(undefined)
+				: Promise.resolve({ title: `book-${isbn}` } as BookEntry)
+		);
 
-		const books = await createBookDataExtensionPlugin().fetchBookData(["12345"]);
+		const plugin = createBookDataExtensionPlugin();
+		const books = await plugin.fetchBookData(["3333", "1111", "2222"]);
 
-		expect(books).toEqual([]);
-	});
-
-	test("fetchBookData returns [] when plugin is not installed", async () => {
-		vi.spyOn(listeners, "listenForExtension").mockImplementation(vi.fn(() => new Promise<boolean>((resolve) => resolve(false))));
-		vi.spyOn(helpers, "addEventListener").mockImplementation(mockAddEventListenerBooks({ title: "book-12345" }));
-
-		const books = await createBookDataExtensionPlugin().fetchBookData(["12345"]);
-
-		expect(books).toEqual([]);
-	});
-
-	test("fetchBookData when called with isbns some of which are not found, returns array with only found books", async () => {
-		vi.spyOn(listeners, "listenForExtension").mockImplementation(mockListenForExtension);
-		vi.spyOn(helpers, "addEventListener")
-			.mockImplementationOnce(mockAddEventListenerBooks({ title: "book-12345" }))
-			.mockImplementationOnce(mockAddEventListenerBooks({ title: "book-56789" }))
-			.mockImplementationOnce(mockAddEventListenerBooks(undefined));
-
-		const books = await createBookDataExtensionPlugin().fetchBookData(["12345", "56789"]);
-
-		expect(books).toEqual([{ title: "book-12345" }, { title: "book-56789" }]);
-	});
-
-	test("fetchBookData when called with isbns some of which timeout, returns array with only returned books", async () => {
-		vi.spyOn(listeners, "listenForExtension").mockImplementation(mockListenForExtension);
-		vi.spyOn(helpers, "addEventListener")
-			.mockImplementationOnce(mockAddEventListenerBooks({ title: "book-56789" }))
-			.mockImplementationOnce(mockAddEventListenerBooks({ title: "book-12345" }))
-			.mockImplementationOnce(mockAddEventListenerBooks({ title: "book-49857" }, 4000));
-
-		const books = await createBookDataExtensionPlugin().fetchBookData(["56789", "12345", "49857"]);
-
-		expect(books).toEqual([{ title: "book-56789" }, { title: "book-12345" }]);
+		expect(books).toEqual([undefined, undefined, undefined]);
 	});
 });
--- plugins/book-data-extension/src/plugin/listeners.ts
+++ plugins/book-data-extension/src/plugin/listeners.ts
@@ -1,46 +1,53 @@
-import { BookEntry } from "@librocco/db";
+/* eslint-disable @typescript-eslint/ban-types */
 import { addEventListener, removeEventListener } from "./window-helpers";
 
-export function listenForExtension(message: string, timeout: number): Promise<boolean> {
+type MessageData<T = {}> = { message: string } & T;
+
+type EmptyResult = { ok: boolean };
+/**
+ * Yes, this is inspired by Rust's 'Result' enum - easier to write a generic function (e.g. 'listenWithTimeout') with
+ * a consistent (generic) return type.
+ */
+type Result<T> = T extends null ? EmptyResult : { ok: true; data: T } | { ok: false };
+
+/**
+ * A helper function used to create the response listener - listening to a message from the extension. The timeout
+ * is applied so as to simulate the req/res behaviour (if it times out, will return a failed result).
+ */
+export function listenWithTimeout(message: string, timeout: number): Promise<Result<null>>;
+export function listenWithTimeout<T, R>(message: string, cb: (data: MessageData<T>) => R, timeout: number): Promise<Result<R>>;
+export function listenWithTimeout<T, R>(..._params: any[]): Promise<Result<R>> | Promise<Result<null>> {
+	const [message, cb, timeout] =
+		_params.length === 3
+			? (_params as [message: string, cb: (data: MessageData<T>) => R, timeout: number])
+			: [_params[0], undefined, _params[1]];
+
 	return new Promise((resolve) => {
 		const promiseTimer = setTimeout(() => {
 			removeEventListener("message", handler);
-			resolve(false);
+			resolve({ ok: false } as Result<R>);
 		}, timeout);
 
 		const handler = (event: MessageEvent) => {
 			if (event.source !== window) return;
-			if (event.data.message && event.data.message === message) {
+			if (event.data?.message === message) {
 				clearTimeout(promiseTimer);
 				removeEventListener("message", handler);
-				resolve(true);
+				if (cb) {
+					return resolve({ ok: true, data: cb(event.data) } as Result<R>);
+				}
+				return resolve({ ok: true } as EmptyResult);
 			}
 		};
 		addEventListener("message", handler);
 	});
 }
 
-export function listenForBook(message: string, timeout: number): Promise<BookEntry | undefined> {
-	return new Promise((resolve) => {
-		const promiseTimer = setTimeout(() => {
-			removeEventListener("message", handler);
-			resolve(undefined);
-		}, timeout);
-
-		const handler = (event: MessageEvent) => {
-			if (event.source !== window) return;
-			const receivedMessage = event.data.message;
-
-			if (event.data && receivedMessage && receivedMessage === message) {
-				clearTimeout(promiseTimer);
-				removeEventListener("message", handler);
-				resolve(event.data.book);
-			}
-		};
-		addEventListener("message", handler);
-	});
-}
-
+/**
+ * Creates a continous listener and performs 'handler' side-effect on each matched message.
+ * Returns a cleanup function (removing the listener). This is useful if handler is used to create
+ * an observable stream from - will deregister the handler when observable is GC'd
+ */
 export function continuousListener<D>(message: string, handler: (data: D) => void) {
 	const _handler = (event: MessageEvent) => {
 		if (event.source !== window) return;
