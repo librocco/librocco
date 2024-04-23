bccc65609692cb60b91352118fc7c790a981049b
Chris De Sousa <cdelasoul@gmail.com>
2024-04-23 16:06:02 +0200

    Chore: rush format

--- apps/web-client/src/lib/forms/SettingsForm.svelte
+++ apps/web-client/src/lib/forms/SettingsForm.svelte
@@ -27,13 +27,13 @@
 	<div class="flex flex-col justify-between gap-6 lg:flex-row-reverse">
 		<div class="flex grow flex-col flex-wrap gap-y-4 lg:flex-row">
 			<div class="basis-full">
-				<Input 
-					id="url" 
-					name="couchUrl" 
-					label="Remote CouchDB URL" 
+				<Input
+					id="url"
+					name="couchUrl"
+					label="Remote CouchDB URL"
 					helpText="Couch DB Url's should be formatted https://user:password@host:post/db_name. 
-						When no url is provided, the app will use local browser storage to persist data." 
-					bind:value={$formStore.couchUrl} 
+						When no url is provided, the app will use local browser storage to persist data."
+					bind:value={$formStore.couchUrl}
 				/>
 			</div>
 
