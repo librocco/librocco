ac0b74423000e511d2138b5aaa0b322570e47064
ikusteu <ivankust57@gmail.com>
2024-05-16 17:59:00 +0200

    fix up historic data section of the db (and some daily summery functionality) + naming:
    	* update 'committed' design document to emit only the doc id (the docs are loaded/streamed by setting 'include_docs: true')
    	* rename db stream 'committedNotesList' -> 'pastTransactions'
    	* create a 'PastTransactions' data structure, supporting various historic transaction use cases
    	* update tests
    	* fix history-related namings in the 'web-client' app
    	* additionally: create '_groupIntoMap' iterator method in shared utils
    	* apply updates to daily summary stores (+ and simplify data flows)

--- apps/web-client/src/lib/stores/inventory/history_entries.ts
+++ apps/web-client/src/lib/stores/inventory/history_entries.ts
@@ -1,23 +1,35 @@
 import { switchMap, combineLatest } from "rxjs";
+import { readable, type Readable } from "svelte/store";
+import type { DateValue } from "@internationalized/date";
 
-import type { CreateHistoryStore, Result } from "$lib/types/inventory";
+import type { debug } from "@librocco/shared";
+import type { InventoryDatabaseInterface } from "@librocco/db";
+
+import type { DailySummaryStore } from "$lib/types/inventory";
 import { observableFromStore, readableFromStream } from "$lib/utils/streams";
 import { mapMergeBookWarehouseData } from "$lib/utils/misc";
 
-export const createHistoryStore: CreateHistoryStore = (ctx, db, committedNotesListStream, warehouseListStream, dateValue) => {
-	const searchResStream = combineLatest([committedNotesListStream, observableFromStore(dateValue)]).pipe(
-		switchMap(([notes, date]) => {
-			const entries = notes.get(date.toString().slice(0, 10)) || [];
+export interface CreateHistoryStore {
+	(ctx: debug.DebugCtx, db: InventoryDatabaseInterface | undefined, dateValue: Readable<DateValue>): Readable<DailySummaryStore>;
+}
 
-			const isbns = entries.map((match) => match.isbn);
+export const createDailySummaryStore: CreateHistoryStore = (ctx, db, dateValue) => {
+	if (!db) {
+		return readable({} as DailySummaryStore);
+	}
 
-			return db
-				?.books()
-				.stream(ctx, isbns)
-				.pipe((bookObs) => mapMergeBookWarehouseData(ctx, entries, warehouseListStream)(bookObs));
+	const pastTransactionsStream = db.stream().pastTransactions(ctx);
+	const warehouseMapStream = db.stream().warehouseMap(ctx);
+
+	const dailySummary = combineLatest([pastTransactionsStream, observableFromStore(dateValue)]).pipe(
+		switchMap(([txnMap, date]) => {
+			const entries = [...(txnMap.get(date.toString().slice(0, 10)) || [])];
+
+			const isbns = entries.map(({ isbn }) => isbn);
+
+			return db.books().stream(ctx, isbns).pipe(mapMergeBookWarehouseData(ctx, entries, warehouseMapStream));
 		})
 	);
-	return {
-		result: readableFromStream(ctx, searchResStream, {} as Result)
-	};
+
+	return readableFromStream(ctx, dailySummary, {} as DailySummaryStore);
 };
