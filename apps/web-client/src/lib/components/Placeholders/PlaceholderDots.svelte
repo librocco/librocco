2f18503c28f720828329417f880fa1440345b39d
Chris De Sousa <cdelasoul@gmail.com>
2024-05-30 16:16:45 +0200

    Enhance: add a "skeleton" loading state for total book count in the warehouse view

---
+++ apps/web-client/src/lib/components/Placeholders/PlaceholderDots.svelte
@@ -0,0 +1,5 @@
+<span class="space-x-[1px] px-[1px]">
+	<span class="inline-flex animate-[loading_1.4s_ease-in-out_infinite] rounded-full">•</span>
+	<span class="inline-flex animate-[loading_1.4s_ease-in-out_0.2s_infinite] rounded-full">•</span>
+	<span class="inline-flex animate-[loading_1.4s_ease-in-out_0.4s_infinite] rounded-full">•</span>
+</span>
