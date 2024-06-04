add38ee58ec6948f2fd92d807e79ed71b0eb6306
ikusteu <ivankust57@gmail.com>
2024-06-04 13:14:01 +0200

    warehouse history: handle responsive display

--- apps/web-client/src/routes/history/warehouse/[warehouse]/[...params]/+page.svelte
+++ apps/web-client/src/routes/history/warehouse/[warehouse]/[...params]/+page.svelte
@@ -105,31 +105,37 @@
 
 <HistoryPage view="history/date">
 	<svelte:fragment slot="heading">
-		<div class="flex w-full justify-between">
-			<h1 class="whitespace-nowrap text-2xl font-bold leading-7 text-gray-900">{$displayName || ""} history</h1>
+		<div class="flex w-full flex-wrap justify-between gap-y-4 xl:flex-nowrap">
+			<h1 class="order-1 whitespace-nowrap text-2xl font-bold leading-7 text-gray-900">{$displayName || ""} history</h1>
 
-			<div class="flex w-full items-center justify-center gap-3">
+			<button on:click={handleExportCsv} class="button button-green order-2 whitespace-nowrap xl:order-3">Export CSV</button>
+
+			<div class="order-3 w-full items-center gap-3 md:flex xl:order-2 xl:justify-center">
 				<p>From:</p>
-				<CalendarPicker onValueChange={onDateValueChange("from")} defaultValue={data.from.dateValue} {isDateDisabled} />
+				<div class="mb-4 inline-block md:mb-0">
+					<CalendarPicker onValueChange={onDateValueChange("from")} defaultValue={data.from.dateValue} {isDateDisabled} />
+				</div>
 				<p>To:</p>
-				<CalendarPicker onValueChange={onDateValueChange("to")} defaultValue={data.to.dateValue} {isDateDisabled} />
+				<div class="mb-4 inline-block md:mb-0">
+					<CalendarPicker onValueChange={onDateValueChange("to")} defaultValue={data.to.dateValue} {isDateDisabled} />
+				</div>
 
-				Filter:
-				<div class="flex items-center divide-x divide-gray-300 overflow-hidden rounded-md border">
-					{#each options as { label, value }}
-						{@const active = value === filter}
-						<button
-							on:click={selectFilter(value)}
-							class="{active ? 'button-green' : 'button-white'} whitespace-nowrap border-none px-3 py-1"
-							class:selected={filter === value}
-						>
-							{label}
-						</button>
-					{/each}
+				<p>Filter:</p>
+				<div class="inline-block">
+					<div class="mt-1 flex items-center divide-x divide-gray-300 overflow-hidden rounded-md border">
+						{#each options as { label, value }}
+							{@const active = value === filter}
+							<button
+								on:click={selectFilter(value)}
+								class="{active ? 'button-green' : 'button-white'} whitespace-nowrap border-none px-3 py-1"
+								class:selected={filter === value}
+							>
+								{label}
+							</button>
+						{/each}
+					</div>
 				</div>
 			</div>
-
-			<button on:click={handleExportCsv} class="button button-green whitespace-nowrap">Export CSV</button>
 		</div>
 	</svelte:fragment>
 
@@ -152,35 +158,37 @@
 				</div>
 				<ul class="grid w-full grid-cols-12 divide-y">
 					{#each $transactions as txn}
-						{@const isbn = txn.isbn}
-						{@const quantity = txn.quantity}
-						{@const noteId = txn.noteId}
-						{@const noteName = txn.noteDisplayName}
-						{@const noteType = txn.noteType}
-						{@const committedAt = txn.date}
+						<li class="entity-list-row col-span-12 grid grid-cols-12 items-center gap-4 whitespace-nowrap text-gray-800">
+							<p class="col-span-12 overflow-hidden font-semibold lg:col-span-2 lg:font-normal">
+								{generateUpdatedAtString(txn.date)}
+							</p>
 
-						<li class="col-span-12 grid grid-cols-12">
-							<div class="entity-list-row col-span-8 grid grid-cols-8 items-center text-gray-800">
-								<p class="col-span-2">
-									{generateUpdatedAtString(committedAt)}
-								</p>
+							<div class="col-span-8 grid items-center gap-x-4 md:grid-cols-1 lg:col-span-7 lg:grid-cols-7 xl:col-span-8 xl:grid-cols-8">
+								<p class="col-span-2 overflow-hidden">{txn.isbn}</p>
 
-								<p class="col-span-2">{isbn}</p>
+								<p class="col-span-3 overflow-hidden">{txn.title || "Unkonwn Title"}</p>
 
-								<a href={appPath("history/notes", noteId)} class="col-span-4 flex items-center">
-									<div class="{noteType === 'outbound' ? 'badge-red' : 'badge-green'} mx-4 flex items-center rounded-sm px-3">
-										{#if noteType === "inbound"}
-											<p><ArrowLeft size={16} /></p>
-											<p>{quantity}</p>
-										{:else}
-											<p>{quantity}</p>
-											<p><ArrowRight size={16} /></p>
-										{/if}
-									</div>
-
-									<p>{noteName}</p>
-								</a>
+								<p class="col-span-2 overflow-hidden xl:col-span-3">{txn.authors || ""}</p>
 							</div>
+
+							<a
+								href={appPath("history/notes", txn.noteId)}
+								class="col-span-4 flex items-center overflow-hidden lg:col-span-3 xl:col-span-2"
+							>
+								{#if txn.noteType === "inbound"}
+									<div class="badge-green mx-4 flex items-center rounded-sm px-3">
+										<p><ArrowLeft size={16} /></p>
+										<p>{txn.quantity}</p>
+									</div>
+								{:else}
+									<div class="badge-red mx-4 flex items-center rounded-sm px-3">
+										<p>{txn.quantity}</p>
+										<p><ArrowRight size={16} /></p>
+									</div>
+								{/if}
+
+								<p>{txn.noteDisplayName}</p>
+							</a>
 						</li>
 					{/each}
 				</ul>
