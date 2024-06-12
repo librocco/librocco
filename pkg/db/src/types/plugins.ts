f6fe7dd678a4c41a110d47611688a39aa292b24e
ikusteu <ivankust57@gmail.com>
2024-06-12 21:11:56 +0200

    book data fetcher: cache results/ongoing fetches:
    * extend the `LibroccoPluginInterface` with the `.reset` method
    * cache results of book fetch inside the plugin controller
    * cache the ongoing fetch (to prevent multiple fetches at the same time)
    * provide a way to overwrite the book fetching cache if explicity specified
    * test the updates

--- pkg/db/src/types/plugins.ts
+++ pkg/db/src/types/plugins.ts
@@ -4,7 +4,15 @@ import { BookEntry } from "./misc";
 
 // #region plugins
 export type LibroccoPlugin<T extends {}> = {
+	/**
+	 * Registers a new implementation of the plugin. It's idempotent in case of the same implementation being registered multiple times (will register only once).
+	 */
 	register: (instance: T) => LibroccoPlugin<T>;
+
+	/**
+	 * Reset the plugin instance - essentially creating a new instance of the plugin.
+	 */
+	reset: () => void;
 } & T;
 
 export type BookFetchResultEntry = Partial<BookEntry> | undefined;
@@ -15,10 +23,18 @@ export interface BookFetchResult {
 	all(): Promise<BookFetchResultEntry[]>;
 }
 
+export interface BookFetchOptions {
+	/**
+	 * Instructs the plugin (controller) to retry the ISBN even if fetch already attempted.
+	 * _Note: this is different than ongoing fetches - the fetch won't be attempted again if it's already in progress._
+	 */
+	retryIfAlreadyAttempted?: boolean;
+}
+
 export interface BookFetcherPlugin {
 	// Name is used to differentiate between different implementations satisfying the same interface
 	__name: string;
-	fetchBookData(isbns: string): BookFetchResult;
+	fetchBookData(isbns: string, opts?: BookFetchOptions): BookFetchResult;
 	isAvailableStream: Observable<boolean>;
 }
 
