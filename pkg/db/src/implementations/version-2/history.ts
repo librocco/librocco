fa0bfca5b1c4fb051f42aa22341df4bad027009e
ikusteu <ivankust57@gmail.com>
2024-08-02 09:49:46 +0200

    incremental implementation of db version 2: finalize the tests:
    * implement history for version 2
    * pass the note transactions test
    * update the note transactions test to order transactions based on 'committedAt' data for respective notes

--- pkg/db/src/implementations/version-2/history.ts
+++ pkg/db/src/implementations/version-2/history.ts
@@ -7,54 +7,61 @@ import { InventoryDatabaseInterface } from "./types";
 
 import { observableFromStore } from "@/helpers";
 
-export const createHistoryInterface = (db: InventoryDatabaseInterface): HistoryInterface => {
-	const newPastTransactionsMap = (
-		rows: Array<
-			VolumeStock<"book"> & {
-				noteId: string;
-				noteType: string;
-				noteDisplayName: string;
-				createdAt: string;
-				updatedAt?: string | null;
-				committedAt?: string | null;
-			}
-		>
-	) => {
-		const internal = rows.map(({ createdAt, updatedAt, committedAt, noteType, ...row }) => ({
-			...row,
-			noteType: noteType as NoteType,
-			// In practice this should always be 'committedAt', but we're doing this to keep TS happy,
-			// and for backwards compatibility
-			date: committedAt ?? updatedAt ?? createdAt
-		}));
-		return {
-			by(key: keyof PastTransaction) {
-				return wrapIter(internal)._groupIntoMap((entry) => [entry[key], entry]);
-			}
-		};
-	};
+class History implements HistoryInterface {
+	#db: InventoryDatabaseInterface;
 
-	return {
-		stream() {
-			return observableFromStore(
-				db.replicated((db) =>
-					db
-						.selectFrom("notes as n")
-						.innerJoin("bookTransactions as t", "n.id", "t.noteId")
-						.where("n.committed", "==", true)
-						.select([
-							"t.isbn",
-							"t.warehouseId",
-							"t.quantity",
-							"n.id as noteId",
-							"n.displayName as noteDisplayName",
-							"n.noteType",
-							"n.createdAt",
-							"n.updatedAt",
-							"n.committedAt"
-						])
-				)
-			).pipe(map(newPastTransactionsMap));
+	constructor(db: InventoryDatabaseInterface) {
+		this.#db = db;
+	}
+
+	stream() {
+		return observableFromStore(
+			this.#db._db.replicated((db) =>
+				db
+					.selectFrom("notes as n")
+					.innerJoin("bookTransactions as t", "n.id", "t.noteId")
+					.where("n.committed", "==", 1)
+					.select([
+						"t.isbn",
+						"t.warehouseId",
+						"t.quantity",
+						"n.id as noteId",
+						"n.displayName as noteDisplayName",
+						"n.noteType",
+						"n.createdAt",
+						"n.updatedAt",
+						"n.committedAt"
+					])
+					.orderBy("n.committedAt", "asc")
+			)
+		).pipe(map(newPastTransactionsMap));
+	}
+}
+
+const newPastTransactionsMap = (
+	rows: Array<
+		VolumeStock<"book"> & {
+			noteId: string;
+			noteType: string;
+			noteDisplayName: string;
+			createdAt: string;
+			updatedAt?: string | null;
+			committedAt?: string | null;
 		}
-	} as HistoryInterface;
+	>
+) => {
+	const internal = rows.map(({ createdAt, updatedAt, committedAt, noteType, ...row }) => ({
+		...row,
+		noteType: noteType as NoteType,
+		// In practice this should always be 'committedAt', but we're doing this to keep TS happy,
+		// and for backwards compatibility
+		date: (committedAt ?? updatedAt ?? createdAt).slice(0, 10)
+	}));
+	return {
+		by(key: keyof PastTransaction) {
+			return wrapIter(internal)._groupIntoMap((entry) => [entry[key] as string, entry]);
+		}
+	};
 };
+
+export const createHistoryInterface = (db: InventoryDatabaseInterface): HistoryInterface => new History(db);
