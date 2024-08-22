11867273cde054ea5bb7abf1802d5e24674e3fe5
ikusteu <ivankust57@gmail.com>
2024-08-22 19:10:15 +0200

    WIP: dummy implementation of reactive SQLite (separate section, will get moved)

--- apps/web-client/src/routes/test/reactive.ts
+++ apps/web-client/src/routes/test/reactive.ts
@@ -1,4 +1,16 @@
-import { Kysely, type SelectQueryBuilder } from "kysely";
+import {
+	AggregateFunctionNode,
+	AliasNode,
+	ColumnNode,
+	Kysely,
+	RawNode,
+	ReferenceNode,
+	SelectAllNode,
+	SelectQueryNode,
+	TableNode,
+	type CompiledQuery
+} from "kysely";
+import { Observable } from "rxjs";
 
 export class SubsMap {
 	#subscriptions = new Map<string, Set<string>>();
@@ -36,60 +48,71 @@ export class SubsMap {
 	}
 }
 
-const wrapQueryBuilder = <T extends SelectQueryBuilder<any, any, any>>(qb: T, tables: string[]): WrappedQueryBuilder<T> => {
-	const _fullJoin = qb.fullJoin.bind(qb);
-	const fullJoin = (...params: Parameters<T["fullJoin"]>) => {
-		const [identifier] = params;
-		const [table] = identifier.toString().split(" ");
-		return wrapQueryBuilder(_fullJoin(...params), [...tables, table]);
+export const createReactive = <K extends Kysely<any>>(db: K, subsMap = new SubsMap()) => {
+	return {
+		notify: subsMap.notify.bind(subsMap) as SubsMap["notify"],
+		replicated: <S extends Selectable<any>>(qb: (db: Kysely<any>) => S, idPrefix = "") => {
+			const q = qb(db);
+			const tables = affectedTables(q.toOperationNode());
+
+			return new Observable<Awaited<ReturnType<S["execute"]>>>((s) => {
+				s.next(undefined);
+				const executeQuery = () => q.execute().then(s.next.bind(s));
+				executeQuery();
+				return subsMap.subscribe(tables, executeQuery, idPrefix);
+			});
+		}
 	};
-
-	const _leftJoin = qb.leftJoin.bind(qb);
-	const leftJoin = (...params: Parameters<T["leftJoin"]>) => {
-		const [identifier] = params;
-		const [table] = identifier.toString().split(" ");
-		return wrapQueryBuilder(_leftJoin(...params), [...tables, table]);
-	};
-
-	const _rightJoin = qb.rightJoin.bind(qb);
-	const rightJoin = (...params: Parameters<T["rightJoin"]>) => {
-		const [identifier] = params;
-		const [table] = identifier.toString().split(" ");
-		return wrapQueryBuilder(_rightJoin(...params), [...tables, table]);
-	};
-
-	const _innerJoin = qb.innerJoin.bind(qb);
-	const innerJoin = (...params: Parameters<T["innerJoin"]>) => {
-		const [identifier] = params;
-		const [table] = identifier.toString().split(" ");
-		return wrapQueryBuilder(_innerJoin(...params), [...tables, table]);
-	};
-
-	const getTables = () => [...new Set(tables)];
-
-	return Object.assign(qb, { leftJoin, rightJoin, innerJoin, fullJoin, getTables });
 };
 
-type WrappedQueryBuilder<T extends SelectQueryBuilder<any, any, any>> = T & {
-	getTables: () => string[];
+/**
+ * Note: the following code was shamelssly copied from the crstore library:
+ * https://github.com/Azarattum/CRStore
+ */
+type Selectable<T> = {
+	execute(): Promise<T>;
+	compile(): CompiledQuery;
+	toOperationNode(): SelectQueryNode;
 };
 
-export const wrapKysely = <T extends Kysely<any>>(kysely: T): T => {
-	const _selectFrom = kysely.selectFrom.bind(kysely);
-	const selectFrom = (...params: Parameters<T["selectFrom"]>) => {
-		const [identifier] = params;
-		const [table] = identifier.toString().split(" ");
-		const qb = _selectFrom(...params);
-		return wrapQueryBuilder(qb, [table]);
-	};
+/**
+ * Note: the following code was shamelssly copied from the crstore library:
+ * https://github.com/Azarattum/CRStore
+ */
+type Node = SelectQueryNode | TableNode | ColumnNode | ReferenceNode | RawNode | AggregateFunctionNode | AliasNode | SelectAllNode;
 
-	return Object.assign(kysely, { selectFrom });
-};
-
-export const createReactive =
-	<K extends Kysely<any>>(db: K) =>
-	<QB extends ReturnType<K["selectFrom"]>>(qb: (db: Kysely<any>) => QB) => {
-		const _qb = qb(wrapKysely(db));
-		const tables = (_qb as WrappedQueryBuilder<QB>).getTables();
-		console.log(tables);
-	};
+/**
+ * Parses a query node and retrieves the affected tables - we're using this to determine which updates should
+ * trigger a re-query (for reactive subscriptions)
+ *
+ * Note: the following code was shamelssly copied from the crstore library:
+ * https://github.com/Azarattum/CRStore
+ * @param target
+ * @returns
+ */
+function affectedTables(target: Node): string[] {
+	// if (typeof target === "string") {
+	// 	return [...new Set(decode(target).map((x) => x.table))];
+	// }
+	if (target.kind === "TableNode") {
+		return [target.table.identifier.name];
+	}
+	if (target.kind === "ReferenceNode" && target.table) {
+		return [target.table.table.identifier.name];
+	}
+	if (target.kind === "AliasNode") {
+		return affectedTables(target.node as Node);
+	}
+	if (target.kind === "SelectQueryNode") {
+		const tables = (
+			[
+				...(target.from?.froms || []),
+				...(target.joins?.map((x) => x.table) || []),
+				...(target.selections?.map((x) => x.selection) || []),
+				...(target.with?.expressions.map((x) => x.expression) || [])
+			] as Node[]
+		).flatMap(affectedTables);
+		return [...new Set(tables)];
+	}
+	return [];
+}
