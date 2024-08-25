c7d7f3df0259157df1b0944e59a8b70e2939dd11
ikusteu <ivankust57@gmail.com>
2024-08-25 12:10:53 +0200

    WIP: sqlocal db version 2.1 impl

---
+++ pkg/db/src/implementations/version-2.1/history.ts
@@ -0,0 +1,67 @@
+import { map } from "rxjs";
+
+import { VolumeStock, wrapIter } from "@librocco/shared";
+
+import { HistoryInterface, NoteType, PastTransaction } from "@/types";
+import { InventoryDatabaseInterface } from "./types";
+
+import {} from "@/helpers";
+
+class History implements HistoryInterface {
+	#db: InventoryDatabaseInterface;
+
+	constructor(db: InventoryDatabaseInterface) {
+		this.#db = db;
+	}
+
+	stream() {
+		return this.#db
+			._stream((db) =>
+				db
+					.selectFrom("notes as n")
+					.innerJoin("bookTransactions as t", "n.id", "t.noteId")
+					.where("n.committed", "==", 1)
+					.select([
+						"t.isbn",
+						"t.warehouseId",
+						"t.quantity",
+						"n.id as noteId",
+						"n.displayName as noteDisplayName",
+						"n.noteType",
+						"n.createdAt",
+						"n.updatedAt",
+						"n.committedAt"
+					])
+					.orderBy("n.committedAt", "asc")
+			)
+			.pipe(map(newPastTransactionsMap));
+	}
+}
+
+const newPastTransactionsMap = (
+	rows: Array<
+		VolumeStock<"book"> & {
+			noteId: string;
+			noteType: string;
+			noteDisplayName: string;
+			createdAt: string;
+			updatedAt?: string | null;
+			committedAt?: string | null;
+		}
+	>
+) => {
+	const internal = rows.map(({ createdAt, updatedAt, committedAt, noteType, ...row }) => ({
+		...row,
+		noteType: noteType as NoteType,
+		// In practice this should always be 'committedAt', but we're doing this to keep TS happy,
+		// and for backwards compatibility
+		date: (committedAt ?? updatedAt ?? createdAt).slice(0, 10)
+	}));
+	return {
+		by(key: keyof PastTransaction) {
+			return wrapIter(internal)._groupIntoMap((entry) => [entry[key] as string, entry]);
+		}
+	};
+};
+
+export const createHistoryInterface = (db: InventoryDatabaseInterface): HistoryInterface => new History(db);
