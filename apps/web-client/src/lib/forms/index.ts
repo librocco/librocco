885d1dc08f3f4cc6dff7c203f4c3ec95805913ab
ikusteu <ivankust57@gmail.com>
2024-09-25 15:55:03 +0200

    db management ui: add db create flow:
    * create DatabaseCreateForm
    * create and select the new db after confirming the dialog
    * update active db comparison (create a function to add `.sqlite3` extension if file name doesn't contain it)

--- apps/web-client/src/lib/forms/index.ts
+++ apps/web-client/src/lib/forms/index.ts
@@ -3,5 +3,6 @@ export { default as SettingsForm, type SettingsFormOptions } from "./SettingsFor
 export { default as WarehouseForm } from "./WarehouseForm.svelte";
 export { default as ScannerForm } from "./ScannerForm.svelte";
 export { default as DatabaseDeleteForm } from "./DatabaseDeleteForm.svelte";
+export { default as DatabaseCreateForm } from "./DatabaseCreateForm.svelte";
 
 export * from "./schemas";
