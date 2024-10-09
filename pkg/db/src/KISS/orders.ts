30cfcd9659ec5c023b10ae2bd315f68c1de3e3ce
Silvio Tomatis <silviot@gmail.com>
2024-10-09 10:47:41 +0200

    fix: Replace SQLite3 import with sqlite3InitModule

--- pkg/db/src/KISS/orders.ts
+++ pkg/db/src/KISS/orders.ts
@@ -1,14 +1,5 @@
-import { SQLite3 } from '@sqlite.org/sqlite-wasm'
+import sqlite3InitModule from "@sqlite.org/sqlite-wasm";
 
-let dbCache: { [key: string]: SQLite3.Database } = {}
+let dbCache: { [key: string]: SQLite3.Database } = {};
 
-export async function getDB(dbname: string): Promise<SQLite3.Database> {
-  if (dbCache[dbname]) {
-    return dbCache[dbname]
-  }
-
-  const sqlite3 = await SQLite3.load()
-  const db = new sqlite3.oo1.DB({ filename: `opfs:${dbname}`, flags: 'c' })
-  dbCache[dbname] = db
-  return db
-}
+export async function getDB(dbname: string): Promise<any> {}
