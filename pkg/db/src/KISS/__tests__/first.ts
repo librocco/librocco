69f7e600e17384e4d3b5d5cbed29a92ffa5952dc
Silvio Tomatis <silviot@gmail.com>
2024-10-09 12:28:07 +0200

    feat: add customer create and update tests

--- pkg/db/src/KISS/__tests__/first.ts
+++ pkg/db/src/KISS/__tests__/first.ts
@@ -1,11 +1,33 @@
-import { describe, it, expect } from "vitest";
-import { getDB, initializeDB, getAllCustomers, execAsync } from "../orders";
+import { describe, it, expect, beforeEach } from "vitest";
+import { getDB, initializeDB, getAllCustomers, upsertCustomer } from "../orders";
 
-describe("KISS test suite", () => {
+describe("Customer order tests", () => {
+	let db;
+	// Each test run will use a different db
+	// birthday paradox chance of collision for 1k runs is 0.5%)
+	let randomTestRunId;
+	beforeEach(async () => {
+		randomTestRunId = Math.floor(Math.random() * 100000000);
+		db = await getDB("testdb" + randomTestRunId);
+		await initializeDB(db);
+	});
 	it("should allow initializing a database", async () => {
-		const db = await getDB("testdb");
-		await expect(execAsync(db, "SELECT id, fullname, email, deposit FROM customer;")).rejects.toThrow();
+		const db = await getDB("init-db-test" + randomTestRunId);
+		await expect(getAllCustomers(db)).rejects.toThrow();
 		initializeDB(db);
 		expect((await getAllCustomers(db)).length).toBe(0);
+	});
+	it("can create and update a customer", async () => {
+		await expect(upsertCustomer(db, { fullname: "John Doe" })).rejects.toThrow("Customer must have an id");
+		upsertCustomer(db, { fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 });
+		upsertCustomer(db, { fullname: "Jane Doe", id: 2 });
+		let customers = await getAllCustomers(db);
+		expect(customers.length).toBe(2);
+		expect(customers[0].fullname).toBe("John Doe");
+		expect(customers[0].email).toBe("john@example.com");
+		upsertCustomer(db, { fullname: "Jane Doe", id: 2, email: "jane@example.com" });
+		expect(customers.length).toBe(2);
+		customers = await getAllCustomers(db);
+		expect(customers[1].email).toBe("jane@example.com");
 	});
 });
