e26715d503061b78f157b3e624166240c1d5a01b
Chris De Sousa <cdelasoul@gmail.com>
2024-10-11 16:17:10 -0700

    Chore: format

--- apps/web-client/.storybook/main.ts
+++ apps/web-client/.storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from "@storybook/sveltekit";
 const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
 	addons: ["@storybook/addon-essentials", "@storybook/addon-svelte-csf", "@storybook/addon-a11y"],
-	framework: "@storybook/sveltekit",
+	framework: "@storybook/sveltekit"
 };
 
 export default config;
