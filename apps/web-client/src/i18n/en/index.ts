8a4c9fe4ead4ed258d66ef9eda43d52d3124a453
Chris De Sousa <cdelasoul@gmail.com>
2024-10-11 17:27:31 -0700

    Chore: format & typecheck

--- apps/web-client/src/i18n/en/index.ts
+++ apps/web-client/src/i18n/en/index.ts
@@ -1,13 +1,14 @@
-import type { BaseTranslation } from '../i18n-types.js'
+// TODO: it'd be nice to typecheck the dict e.g `const en = {} satisfies BaseTranslation` but it's not working right now
+// import type { BaseTranslation } from '../i18n-types.js'
 
 const nav = {
-	"search": "Search stock",
-	"inventory": "Manage inventory",
-	"outbound": "Outbound",
-	"inbound": "Inbound",
-	"settings": "Settings",
-	"history": "History"
-}
+	search: "Search stock",
+	inventory: "Manage inventory",
+	outbound: "Outbound",
+	inbound: "Inbound",
+	settings: "Settings",
+	history: "History"
+};
 
 const search = {
 	title: "Search",
@@ -15,11 +16,11 @@ const search = {
 		title: "Search for stock",
 		description: "Get started by searching by title, author, ISBN"
 	}
-}
+};
 
 const en = {
 	nav,
 	search
-} satisfies BaseTranslation
+};
 
-export default en
+export default en;
