5b723a874839da2e4fbf50bb1cb1012e2cf9f779
ikusteu <ivankust57@gmail.com>
2024-10-21 20:53:32 +0200

    add ws-client to web-client

---
+++ apps/web-client/src/lib/db/orders/__tests__/worker.ts
@@ -0,0 +1,11 @@
+import { type Config, defaultConfig } from "@vlcn.io/ws-client";
+import { start } from "@vlcn.io/ws-client/worker.js";
+// Interface to WASM sqlite
+import { createDbProvider } from "@vlcn.io/ws-browserdb";
+
+export const config: Config = {
+	dbProvider: createDbProvider(),
+	transportProvider: defaultConfig.transportProvider
+};
+
+start(config);
