ec4dce9f269e6f4933389b9cbe30900879cbe5a7
Silvio Tomatis <silviot@gmail.com>
2024-10-24 15:55:54 +0200

    New getPossibleSupplerOrderInfos providing a summary of possible supplier orders

--- apps/web-client/src/lib/db/orders/__tests__/supplier-order.test.ts
+++ apps/web-client/src/lib/db/orders/__tests__/supplier-order.test.ts
@@ -4,7 +4,7 @@ import { type DB } from "../types";
 
 import { getRandomDb } from "./lib";
 
-import { upsertSupplier, associatePublisher, getPossibleSupplerOrders } from "../suppliers";
+import { upsertSupplier, associatePublisher, getPossibleSupplerOrderLines, getPossibleSupplerOrderInfos } from "../suppliers";
 import { upsertCustomer, addBooksToCustomer } from "../customers";
 import { upsertBook } from "../books";
 
@@ -13,9 +13,9 @@ describe("Suppliers order creation", () => {
 	beforeEach(async () => {
 		db = await getRandomDb();
 		// Add three books
-		await upsertBook(db, { isbn: "1", publisher: "MathsAndPhysicsPub", title: "Physics" });
-		await upsertBook(db, { isbn: "2", publisher: "ChemPub", title: "Chemistry" });
-		await upsertBook(db, { isbn: "3", publisher: "PhantasyPub", title: "The Hobbit" });
+		await upsertBook(db, { isbn: "1", publisher: "MathsAndPhysicsPub", title: "Physics", price: 7 });
+		await upsertBook(db, { isbn: "2", publisher: "ChemPub", title: "Chemistry", price: 13 });
+		await upsertBook(db, { isbn: "3", publisher: "PhantasyPub", title: "The Hobbit", price: 5 });
 
 		// Two customers order some books
 		await upsertCustomer(db, { fullname: "John Doe", id: 1 });
@@ -38,15 +38,19 @@ describe("Suppliers order creation", () => {
 	});
 
 	it("creates a supplier order from a client order", async () => {
-		expect(await getPossibleSupplerOrders(db)).toStrictEqual([
+		expect(await getPossibleSupplerOrderLines(db)).toStrictEqual([
 			{ supplier_id: 1, isbn: "1", quantity: 1 },
 			{ supplier_id: 1, isbn: "2", quantity: 1 },
 			{ supplier_id: 2, isbn: "3", quantity: 2 }
 		]);
+		expect(await getPossibleSupplerOrderInfos(db)).toStrictEqual([
+			{ supplier_name: "Science Books LTD", supplier_id: 1, total_book_number: 2, total_book_price: 20 },
+			{ supplier_name: "Phantasy Books LTD", supplier_id: 2, total_book_number: 2, total_book_price: 10 }
+		]);
 		// If we change the supplier for ChemPub to Phantasy Books LTD
 		// the supplier order will reflect that
 		await associatePublisher(db, 2, "ChemPub");
-		expect(await getPossibleSupplerOrders(db)).toStrictEqual([
+		expect(await getPossibleSupplerOrderLines(db)).toStrictEqual([
 			{ supplier_id: 1, isbn: "1", quantity: 1 },
 			{ supplier_id: 2, isbn: "2", quantity: 1 }, // This is now from supplier 2
 			{ supplier_id: 2, isbn: "3", quantity: 2 }
