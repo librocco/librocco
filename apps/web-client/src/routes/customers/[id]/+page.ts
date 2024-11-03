bba7f0d625ed42d55d0fefb4551244e7fda36ecd
fadwa <fadwa.mahmoud@gmail.com>
2024-11-03 13:31:40 +0200

    Display book data and edit quantity

--- apps/web-client/src/routes/customers/[id]/+page.ts
+++ apps/web-client/src/routes/customers/[id]/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from "./$types";
 import { currentCustomer } from "$lib/stores/orders";
 import { getCustomerBooks, getCustomerDetails } from "$lib/db/orders/customers";
 import type { Customer } from "$lib/db/orders/types";
+import type { BookEntry } from "@librocco/db";
 
 export const load: PageLoad = async ({ parent, params }) => {
 	const { ordersDb } = await parent();
@@ -11,14 +12,32 @@ export const load: PageLoad = async ({ parent, params }) => {
 		return {};
 	}
 
-	/** @TODO in customer orders page, we need to get the latest/biggest customer id
-	 and increment it */
 	const customerBooks = await getCustomerBooks(ordersDb, Number(params.id));
+
 	const customerDetails = await getCustomerDetails(ordersDb, Number(params.id));
 
+	const isbns = customerBooks.map((book) => book.isbn);
+
+	const bookData = (await ordersDb.execO(`SELECT * FROM book WHERE isbn IN (${isbns.join(", ")})`)) as BookEntry[];
+
+	const bookDataMap = new Map<string, BookEntry>();
+	bookData.forEach((book) => {
+		bookDataMap.set(book.isbn, book);
+	});
+	const mergedBooks = customerBooks.map((customerBook) => {
+		const additionalData = bookDataMap.get(customerBook.isbn) || ({} as BookEntry);
+		return {
+			...customerBook,
+			...additionalData
+		};
+	});
+
+	// merge book data with customerBooks
 	currentCustomer.update((prev) =>
-		prev && prev.customerDetails.id === customerDetails[0].id ? prev : { customerBooks, customerDetails: customerDetails[0] }
+		prev && prev.customerDetails.id === customerDetails[0].id
+			? { ...prev, customerBooks: mergedBooks }
+			: { customerBooks: mergedBooks, customerDetails: customerDetails[0] }
 	);
 
-	return { customerBooks, customerDetails: customerDetails[0] || ({} as Customer) };
+	return { customerBooks: mergedBooks, customerDetails: customerDetails[0] || ({} as Customer) };
 };
