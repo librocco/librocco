16494914dae1b8168efca442a0f1ad9b7f773439
Chris De Sousa <cdelasoul@gmail.com>
2024-11-06 14:41:09 +0900

    Feat: build customer order meta form with new form patterns
    - add base FormFieldProxy & Check/TextControl components for building
    accessible controls
    - add CustomerOrderMetaForm & story using these controls
    - adjut vitest setup to mock all sveltekit app stores & extend matchers

---
+++ apps/web-client/src/lib/forms/controls/CheckControl.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { getFormFieldCtx, createFormControl } from "./form-field/";
+	import { nanoid } from "nanoid";
+
+	export let id = nanoid();
+	export let wrapperClass: string;
+
+	const formField = getFormFieldCtx();
+
+	const { labelAttrs, controlAttrs } = createFormControl(id, formField);
+</script>
+
+<label {...labelAttrs} class={wrapperClass}>
+	<slot controlAttrs={$controlAttrs} />
+
+	<slot name="label" />
+</label>
