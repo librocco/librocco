3395a740268bfacd63ee8829fe423584fe187c7d
ikusteu <ivankust57@gmail.com>
2024-11-08 18:17:39 +0100

    refactor: remove usage of stores from /cusotmers/[id] page: * remove store usage from the page * load the data in the page load and pass as page data * refactor TextEditable bind updates (crazy TEMP functionality) * remove orders stores (not necessary atm) * remove a bunch of unused stuff in customers/[id] page file

--- apps/web-client/src/routes/customers/[id]/+page.ts
+++ apps/web-client/src/routes/customers/[id]/+page.ts
@@ -1,5 +1,4 @@
 import type { PageLoad } from "./$types";
-import { currentCustomer } from "$lib/stores/orders";
 import { getCustomerBooks, getCustomerDetails } from "$lib/db/orders/customers";
 import type { Customer } from "$lib/db/orders/types";
 import type { BookEntry } from "@librocco/db";
@@ -12,14 +11,12 @@ export const load: PageLoad = async ({ parent, params }) => {
 		return {};
 	}
 
-	const customerBooks = await getCustomerBooks(ordersDb, Number(params.id));
-
 	const customerDetails = await getCustomerDetails(ordersDb, Number(params.id));
 
+	// TODO: make this a single query
+	const customerBooks = await getCustomerBooks(ordersDb, Number(params.id));
 	const isbns = customerBooks.map((book) => book.isbn);
-
 	const bookData = (await ordersDb.execO(`SELECT * FROM book WHERE isbn IN (${isbns.join(", ")})`)) as BookEntry[];
-
 	const bookDataMap = new Map<string, BookEntry>();
 	bookData.forEach((book) => {
 		bookDataMap.set(book.isbn, book);
@@ -31,13 +28,6 @@ export const load: PageLoad = async ({ parent, params }) => {
 			...additionalData
 		};
 	});
-
-	// merge book data with customerBooks
-	currentCustomer.update((prev) =>
-		prev && prev.customerDetails.id === customerDetails[0].id
-			? { ...prev, customerBooks: mergedBooks }
-			: { customerBooks: mergedBooks, customerDetails: customerDetails[0] }
-	);
 
 	return { customerBooks: mergedBooks, customerDetails: customerDetails[0] || ({} as Customer) };
 };
