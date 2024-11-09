a967c807d8a6a6709fa2e5c3de1b9054e7ae0025
ikusteu <ivankust57@gmail.com>
2024-11-09 11:59:49 +0100

    make the customers/* pages reactive by invalidating load functions on relevant changes

--- apps/web-client/src/routes/customers/+layout.ts
+++ apps/web-client/src/routes/customers/+layout.ts
@@ -1,7 +1,11 @@
+import type { LayoutLoad } from "../$types";
+
 import { getInitializedDB } from "$lib/db/orders";
 import { getAllCustomers } from "$lib/db/orders/customers";
 
-export const load = async () => {
+export const load: LayoutLoad = async ({ depends }) => {
+	depends("customer:data");
+
 	const ordersDb = await getInitializedDB("librocco-current-db");
 
 	const allCustomers = await getAllCustomers(ordersDb);
