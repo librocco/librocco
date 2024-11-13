55e08793f37f1b40ebad3fb2727c13ef5a77a2ab
Chris De Sousa <cdelasoul@gmail.com>
2024-11-13 13:12:35 +0900

    Touch up list view style

--- apps/web-client/src/routes/orders/c/+page.svelte
+++ apps/web-client/src/routes/orders/c/+page.svelte
@@ -25,7 +25,7 @@
 		<input type="checkbox" value="forest" class="theme-controller toggle" />
 	</header>
 
-	<div class="flex h-full flex-col gap-y-6 px-4">
+	<div class="mx-auto flex h-full max-w-5xl flex-col gap-y-10 px-4">
 		<div class="flex items-center justify-between">
 			<h1 class="prose text-2xl font-bold">Customer Orders</h1>
 			<a href="/orders/c/new" class="btn-primary btn gap-2">
@@ -34,49 +34,44 @@
 			</a>
 		</div>
 
-		<div class="flex gap-2">
-			<button
-				class="btn-outline btn-sm btn {selectedStatus === 'in_progress' ? 'btn-primary' : ''}"
-				on:click={() => (selectedStatus = "in_progress")}
-			>
-				In Progress
-			</button>
-			<button
-				class="btn-outline btn-sm btn {selectedStatus === 'completed' ? 'btn-primary' : ''}"
-				on:click={() => (selectedStatus = "completed")}
-			>
-				Completed
-			</button>
-		</div>
-
-		<div class="overflow-x-auto">
+		<div class="flex flex-col gap-y-6 overflow-x-auto">
+			<div class="flex gap-2">
+				<button
+					class="btn-outline btn-sm btn {selectedStatus === 'in_progress' ? 'btn-primary' : ''}"
+					on:click={() => (selectedStatus = "in_progress")}
+				>
+					In Progress
+				</button>
+				<button
+					class="btn-outline btn-sm btn {selectedStatus === 'completed' ? 'btn-primary' : ''}"
+					on:click={() => (selectedStatus = "completed")}
+				>
+					Completed
+				</button>
+			</div>
 			<table class="table-lg table">
 				<thead>
 					<tr>
-						<th>Customer</th>
-						<th>Email</th>
-						<th>Status</th>
-						<th>Action</th>
+						<th scope="col">Customer</th>
+						<th scope="col">Order Id</th>
+						<th scope="col"> <span class="sr-only"> Link to update </span></th>
 					</tr>
 				</thead>
 				<tbody>
-					{#each filteredOrders as order}
-						<tr class="hover">
-							<td class="flex items-center gap-3">
-								<UserCircle size={24} />
-								{order.fullname}
-							</td>
-							<td class="flex items-center gap-3">
-								<Mail size={20} />
-								{order.email}
+					{#each filteredOrders as { fullname, email, id }}
+						<tr class="hover focus-within:bg-base-200">
+							<td>
+								<dl class="flex flex-col gap-y-1">
+									<dt class="sr-only">Customer details</dt>
+									<dd>{fullname}</dd>
+									<dd class="text-sm">{email}</dd>
+								</dl>
 							</td>
 							<td>
-								<div class="badge {order.status === 'completed' ? 'badge-success' : 'badge-warning'} gap-2">
-									{order.status === "completed" ? "Completed" : "In Progress"}
-								</div>
+								<span class="italic">{id}</span>
 							</td>
 							<td>
-								<a href="/orders/c/{order.id}" class="btn-outline btn-sm btn">Update</a>
+								<a href="/orders/c/{id}" class="btn-outline btn-sm btn">Update</a>
 							</td>
 						</tr>
 					{/each}
