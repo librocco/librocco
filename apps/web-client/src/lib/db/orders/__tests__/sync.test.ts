dfec80243ec254f7a68bbe7f00dcfe259216dcdb
ikusteu <ivankust57@gmail.com>
2024-11-14 12:00:10 +0100

    fix: sync test: * reinstate the sync.test.ts to test sync between two local DBs (got lost along the way) * add a 'waitFor' helper within sync test - uses 'testUtils.waitFor' under the hood, but adds 50ms pause between retries

--- apps/web-client/src/lib/db/orders/__tests__/sync.test.ts
+++ apps/web-client/src/lib/db/orders/__tests__/sync.test.ts
@@ -1,5 +1,7 @@
-import { describe, it, expect, beforeAll } from "vitest";
+import { describe, it, expect, beforeAll, beforeEach, afterEach } from "vitest";
 import { WorkerInterface } from "@vlcn.io/ws-client";
+
+import type { DB } from "../types";
 
 import { getInitializedDB } from "../db";
 
@@ -13,58 +15,97 @@ const url = "ws://localhost:3000/sync";
 
 let worker: WorkerInterface;
 
+// A helper to wait 50ms before assertion retries
+// NOTE: Also waits 50ms before the first try, but we can live with that
+const pause = 50;
+const waitFor = (cb: () => Promise<any>) =>
+	testUtils.waitFor(async () => {
+		await new Promise<void>((res) => setTimeout(res, pause));
+		return cb();
+	});
+
 describe("Remote db setup", () => {
 	// Worker is set up in async manner
 	beforeAll(async () => {
 		const _worker = new SyncWorker();
-
-		// Wait for worker setup
-		await new Promise<void>((r) => {
-			_worker.onmessage = (event) => {
-				if (event.data === "ready") {
-					console.log("worker ready");
-					r();
-				}
-			};
-		});
-
 		worker = new WorkerInterface(_worker);
-
-		_worker.onmessage = (event) => {
-			console.log(event.data);
-		};
 	});
 
-	it("upserts customer(s)", async () => {
+	let room: string;
+	let dbid1: string;
+	let dbid2: string;
+	let db1: DB;
+	let db2: DB;
+
+	beforeEach(async () => {
+		// Get dbid - unique for each run
 		const randomTestRunId = Math.floor(Math.random() * 100000000);
-		const dbName = `test-${randomTestRunId}`;
+		// Room is the common identifier for synced db server side: all dbs syncing with a particular room id sync with the same db
+		room = randomTestRunId.toString();
 
-		// Insert the customer
-		await remote.upsertCustomer(dbName, { fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 });
-		expect(await remote.getAllCustomers(dbName)).toEqual([{ fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 }]);
+		// Create two distinct instances of db (both to sync via the remote one)
+		dbid1 = `${room}-1`;
+		dbid2 = `${room}-2`;
 
-		// Try the update
-		await remote.upsertCustomer(dbName, { fullname: "John Doe Updated", id: 1, email: "john@example.com", deposit: 13.2 });
-		expect(await remote.getAllCustomers(dbName)).toEqual([
-			{ fullname: "John Doe Updated", id: 1, email: "john@example.com", deposit: 13.2 }
-		]);
+		db1 = await getInitializedDB(dbid1).then(({ db }) => db);
+		db2 = await getInitializedDB(dbid2).then(({ db }) => db);
+
+		// Start the sync
+		worker.startSync(dbid1, { room, url });
+		worker.startSync(dbid2, { room, url });
+
+		// Ping the backend to initialise the DB before tests are run
+		await remote.getAllCustomers(room);
 	});
 
-	it("syncs with the remote db: server - client", async () => {
-		const randomTestRunId = Math.floor(Math.random() * 100000000);
-		const dbid = randomTestRunId.toString();
+	afterEach(() => {
+		worker.stopSync(dbid1);
+		worker.stopSync(dbid2);
+	});
 
-		await remote.upsertCustomer(dbid, { fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 });
-
-		const localDB = await getInitializedDB(dbid).then(({ db }) => db);
-
-		worker.startSync(dbid, { room: dbid, url });
-
-		await testUtils.waitFor(async () =>
-			expect(await local.getAllCustomers(localDB)).toEqual([{ fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 }])
+	it("keeps sync between client and server", async () => {
+		// Insert + sync db1 -> db2
+		await local.upsertCustomer(db1, { fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 });
+		await waitFor(async () =>
+			expect(await local.getAllCustomers(db2)).toEqual([{ fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 }])
 		);
 
-		// After test
-		worker.stopSync(dbid);
+		// Insert + sync db2 -> db1
+		await local.upsertCustomer(db2, { fullname: "Jane Doe", id: 2, email: "jane@example.com", deposit: 13.2 });
+		await waitFor(async () =>
+			expect(await local.getAllCustomers(db1)).toEqual([
+				{ fullname: "John Doe", id: 1, email: "john@example.com", deposit: 13.2 },
+				{ fullname: "Jane Doe", id: 2, email: "jane@example.com", deposit: 13.2 }
+			])
+		);
+
+		// Update + sync db1 -> db2
+		await local.upsertCustomer(db1, { fullname: "John Doe the II", id: 1, email: "john@example.com", deposit: 13.2 });
+		await waitFor(async () =>
+			expect(await local.getAllCustomers(db2)).toEqual([
+				{ fullname: "John Doe the II", id: 1, email: "john@example.com", deposit: 13.2 },
+				{ fullname: "Jane Doe", id: 2, email: "jane@example.com", deposit: 13.2 }
+			])
+		);
+
+		// Update + sync db2 -> db1
+		await local.upsertCustomer(db2, { fullname: "Jane Doe", id: 2, email: "jane@gmail.com", deposit: 13.2 });
+		await waitFor(async () =>
+			expect(await local.getAllCustomers(db1)).toEqual([
+				{ fullname: "John Doe the II", id: 1, email: "john@example.com", deposit: 13.2 },
+				{ fullname: "Jane Doe", id: 2, email: "jane@gmail.com", deposit: 13.2 }
+			])
+		);
+
+		// Test reading from the remote db directly (remember, the remote db is identified by room id).
+		//
+		// NOTE: Writing to remote db directly is not reactive: I don't understand the intricacies, but the writes (sync) through
+		// web socket are communicated back to all subscribers, but not direct writes (I would assume it has something to do with FS locks or similar constructs)
+		await waitFor(async () =>
+			expect(await remote.getAllCustomers(room)).toEqual([
+				{ fullname: "John Doe the II", id: 1, email: "john@example.com", deposit: 13.2 },
+				{ fullname: "Jane Doe", id: 2, email: "jane@gmail.com", deposit: 13.2 }
+			])
+		);
 	});
 });
