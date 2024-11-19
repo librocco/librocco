3d733aa6e4c99f91da24ab343ce6c934f721badd
Chris De Sousa <cdelasoul@gmail.com>
2024-11-19 15:33:29 +0900

    Fix: small screen content wrapping

--- apps/web-client/src/lib/components/supplier-orders/OrderedTable.svelte
+++ apps/web-client/src/lib/components/supplier-orders/OrderedTable.svelte
@@ -33,70 +33,70 @@
 </script>
 
 <div class="overflow-x-auto">
-<table class="table-pin-rows table-lg table whitespace-nowrap">
-	<thead>
-		<tr>
-			<th scope="col" class="w-16">
-				<span class="sr-only">Select</span>
-			</th>
-			<th scope="col">Supplier</th>
-			<th scope="col">Books</th>
-			<th scope="col">Placed</th>
-			<th scope="col"><span class="sr-only">Actions</span></th>
-		</tr>
-	</thead>
-	<tbody>
-		{#if hasSelectedOrders}
-			<tr aria-live="polite" aria-atomic="true" class="bg-base-200">
-				<td role="cell" />
-				<th role="columnheader" scope="row">
-					<span class="sr-only">Selected orders summary: </span>
-					{selectedOrders.length} orders selected
+	<table class="table-pin-rows table-lg table whitespace-nowrap">
+		<thead>
+			<tr>
+				<th scope="col" class="w-16">
+					<span class="sr-only">Select</span>
 				</th>
-				<td role="cell" />
-				<td role="cell" />
-				<td role="cell" class="text-right">
-					<button
-						class="btn-primary btn-sm btn gap-x-2"
-						on:click={handleBulkReconcile}
-						aria-label="Reconcile {selectedOrders.length} selected orders"
-					>
-						<ListTodo aria-hidden focusable="false" size={20} />
-
-						Reconcile Selected
-					</button>
-				</td>
+				<th scope="col">Supplier</th>
+				<th scope="col">Books</th>
+				<th scope="col">Placed</th>
+				<th scope="col"><span class="sr-only">Actions</span></th>
 			</tr>
-		{/if}
-		{#each orders as { supplier_name, supplier_id, total_book_number, created }}
-			<tr class="hover focus-within:bg-base-200">
-				<td>
-					<input
-						type="checkbox"
-						class="checkbox"
-						checked={selectedOrders.includes(supplier_id)}
-						on:change={() => toggleOrderSelection(supplier_id)}
-					/>
-				</td>
-				<td>{supplier_name}</td>
-				<td>{total_book_number}</td>
-				<td>
-					<span class="badge-primary badge-outline badge">
-						{created.toLocaleDateString()}
-					</span>
-				</td>
-				<td class="text-right">
-					{#if !hasSelectedOrders}
-						<button class="btn-primary btn-sm btn gap-x-2.5" on:click={() => handleReconcile(supplier_id)}>
+		</thead>
+		<tbody>
+			{#if hasSelectedOrders}
+				<tr aria-live="polite" aria-atomic="true" class="bg-base-200">
+					<td role="cell" />
+					<th role="columnheader" scope="row">
+						<span class="sr-only">Selected orders summary: </span>
+						{selectedOrders.length} orders selected
+					</th>
+					<td role="cell" />
+					<td role="cell" />
+					<td role="cell" class="text-right">
+						<button
+							class="btn-primary btn-sm btn flex-nowrap gap-x-2"
+							on:click={handleBulkReconcile}
+							aria-label="Reconcile {selectedOrders.length} selected orders"
+						>
 							<ListTodo aria-hidden focusable="false" size={20} />
-							Reconcile
+
+							Reconcile Selected
 						</button>
-					{/if}
-				</td>
-			</tr>
-		{/each}
-	</tbody>
-</table>
+					</td>
+				</tr>
+			{/if}
+			{#each orders as { supplier_name, supplier_id, total_book_number, created }}
+				<tr class="hover focus-within:bg-base-200">
+					<td>
+						<input
+							type="checkbox"
+							class="checkbox"
+							checked={selectedOrders.includes(supplier_id)}
+							on:change={() => toggleOrderSelection(supplier_id)}
+						/>
+					</td>
+					<td>{supplier_name}</td>
+					<td>{total_book_number}</td>
+					<td>
+						<span class="badge-primary badge-outline badge">
+							{created.toLocaleDateString()}
+						</span>
+					</td>
+					<td class="text-right">
+						{#if !hasSelectedOrders}
+							<button class="btn-primary btn-sm btn flex-nowrap gap-x-2.5" on:click={() => handleReconcile(supplier_id)}>
+								<ListTodo aria-hidden focusable="false" size={20} />
+								Reconcile
+							</button>
+						{/if}
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </div>
 
 <style>
