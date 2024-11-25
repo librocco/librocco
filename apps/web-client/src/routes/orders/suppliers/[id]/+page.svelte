7c4874ddd8b5a1ed8024cb77cc77c67bc2b5a905
fadwa <fadwa.mahmoud@gmail.com>
2024-11-25 15:43:49 +0200

    Replace page component in new routes with layout

--- apps/web-client/src/routes/orders/suppliers/[id]/+page.svelte
+++ apps/web-client/src/routes/orders/suppliers/[id]/+page.svelte
@@ -90,126 +90,119 @@
 		const numToSelect = Math.floor(books.length * portion);
 		selectedBooks = books.slice(0, numToSelect).map((b) => b.isbn);
 	}
-	let initialized = true;
 </script>
 
-<Page view={"orders/suppliers/reconcile"} loaded={initialized}>
-	<svelte:fragment slot="topbar">
-		<header class="navbar mb-4 bg-neutral">
-			<input type="checkbox" value="forest" class="theme-controller toggle" />
-		</header>
-	</svelte:fragment>
+<header class="navbar mb-4 bg-neutral">
+	<input type="checkbox" value="forest" class="theme-controller toggle" />
+</header>
 
-	<svelte:fragment slot="main">
-		<main class="h-screen">
-			<div class="flex h-full flex-col gap-y-10 px-4 max-md:overflow-y-auto md:flex-row md:divide-x">
-				<div class="min-w-fit md:basis-96 md:overflow-y-auto">
-					<div class="card">
-						<div class="card-body gap-y-2 p-0">
-							<div class="sticky top-0 flex gap-2 bg-base-100 pb-3 md:flex-col">
-								<h1 class="prose card-title">{supplier.name}</h1>
+<main class="h-screen">
+	<div class="flex h-full flex-col gap-y-10 px-4 max-md:overflow-y-auto md:flex-row md:divide-x">
+		<div class="min-w-fit md:basis-96 md:overflow-y-auto">
+			<div class="card">
+				<div class="card-body gap-y-2 p-0">
+					<div class="sticky top-0 flex gap-2 bg-base-100 pb-3 md:flex-col">
+						<h1 class="prose card-title">{supplier.name}</h1>
 
-								<div class="flex flex-row items-center justify-between gap-y-2 md:flex-col md:items-start">
-									<h2 class="prose">#{supplier.id}</h2>
-								</div>
-							</div>
-							<dl class="flex flex-col">
-								<div class="stats md:stats-vertical">
-									<div class="stat md:px-1">
-										<dt class="stat-title">Total books</dt>
-										<dd class="stat-value text-2xl">{totalBooks}</dd>
-									</div>
-									<div class="stat md:px-1">
-										<dt class="stat-title">Total value</dt>
-										<dd class="stat-value text-2xl">€{totalValue}</dd>
-									</div>
-									<div class="stat md:px-1">
-										<dt class="stat-title">Last updated</dt>
-										<dd class="stat-value text-2xl">
-											<time dateTime={supplier.lastUpdated.toISOString()}>{supplier.lastUpdated.toLocaleDateString()}</time>
-										</dd>
-									</div>
-								</div>
-							</dl>
+						<div class="flex flex-row items-center justify-between gap-y-2 md:flex-col md:items-start">
+							<h2 class="prose">#{supplier.id}</h2>
 						</div>
 					</div>
-				</div>
-
-				<div class="relative mb-20 flex h-full w-full flex-col gap-y-6 md:px-4">
-					<div class="prose flex w-full max-w-full flex-col gap-y-3">
-						<h3 class="max-md:divider-start max-md:divider">Books</h3>
-						<div class="flex flex-wrap items-center gap-4">
-							<div class="flex flex-wrap gap-2">
-								<button class="btn-outline btn-sm btn border-dotted" on:click={() => selectPortion(0.25)}>Select 1/4</button>
-								<button class="btn-outline btn-sm btn border-dashed" on:click={() => selectPortion(0.5)}>Select 1/2</button>
-								<button class="btn-outline btn-sm btn" on:click={() => selectPortion(0.75)}>Select 3/4</button>
+					<dl class="flex flex-col">
+						<div class="stats md:stats-vertical">
+							<div class="stat md:px-1">
+								<dt class="stat-title">Total books</dt>
+								<dd class="stat-value text-2xl">{totalBooks}</dd>
+							</div>
+							<div class="stat md:px-1">
+								<dt class="stat-title">Total value</dt>
+								<dd class="stat-value text-2xl">€{totalValue}</dd>
+							</div>
+							<div class="stat md:px-1">
+								<dt class="stat-title">Last updated</dt>
+								<dd class="stat-value text-2xl">
+									<time dateTime={supplier.lastUpdated.toISOString()}>{supplier.lastUpdated.toLocaleDateString()}</time>
+								</dd>
 							</div>
 						</div>
-					</div>
-
-					<div class="relative h-full overflow-x-auto">
-						<table class="table-pin-rows table pb-20">
-							<thead>
-								<tr>
-									<th class="w-16">
-										<span class="sr-only">Select</span>
-									</th>
-									<th>ISBN</th>
-									<th>Title</th>
-									<th>Authors</th>
-									<th>Price</th>
-								</tr>
-							</thead>
-							<tbody>
-								{#each books as { isbn, title, authors, price }}
-									<tr>
-										<td>
-											<input
-												type="checkbox"
-												class="checkbox"
-												checked={selectedBooks.includes(isbn)}
-												on:change={() => {
-													if (selectedBooks.includes(isbn)) {
-														selectedBooks = selectedBooks.filter((id) => id !== isbn);
-													} else {
-														selectedBooks = [...selectedBooks, isbn];
-													}
-												}}
-											/>
-										</td>
-										<th>{isbn}</th>
-										<td>{title}</td>
-										<td>{authors}</td>
-										<td>€{price}</td>
-									</tr>
-								{/each}
-							</tbody>
-						</table>
-					</div>
-					{#if canPlaceOrder}
-						<div class="card fixed bottom-4 left-0 z-10 flex w-screen flex-row bg-transparent md:absolute md:bottom-24 md:mx-2 md:w-full">
-							<div class="mx-2 flex w-full flex-row justify-between bg-base-300 px-4 py-2 shadow-lg">
-								<dl class="stats flex">
-									<div class="stat flex shrink flex-row place-items-center py-2 max-md:px-4">
-										<div class="stat-title">Selected books:</div>
-										<div class="stat-value text-lg">
-											{selectedBooks.length}
-										</div>
-									</div>
-									<div class="stat flex place-items-center py-2 max-md:px-4">
-										<div class="stat-title sr-only">Total</div>
-										<div class="stat-value text-lg">€{totalAmount.toFixed(2)}</div>
-									</div>
-								</dl>
-								<button class="btn-primary btn" on:click={handlePlaceOrder}>
-									Place Order
-									<Truck aria-hidden size={20} class="hidden md:block" />
-								</button>
-							</div>
-						</div>
-					{/if}
+					</dl>
 				</div>
 			</div>
-		</main>
-	</svelte:fragment>
-</Page>
+		</div>
+
+		<div class="relative mb-20 flex h-full w-full flex-col gap-y-6 md:px-4">
+			<div class="prose flex w-full max-w-full flex-col gap-y-3">
+				<h3 class="max-md:divider-start max-md:divider">Books</h3>
+				<div class="flex flex-wrap items-center gap-4">
+					<div class="flex flex-wrap gap-2">
+						<button class="btn-outline btn-sm btn border-dotted" on:click={() => selectPortion(0.25)}>Select 1/4</button>
+						<button class="btn-outline btn-sm btn border-dashed" on:click={() => selectPortion(0.5)}>Select 1/2</button>
+						<button class="btn-outline btn-sm btn" on:click={() => selectPortion(0.75)}>Select 3/4</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="relative h-full overflow-x-auto">
+				<table class="table-pin-rows table pb-20">
+					<thead>
+						<tr>
+							<th class="w-16">
+								<span class="sr-only">Select</span>
+							</th>
+							<th>ISBN</th>
+							<th>Title</th>
+							<th>Authors</th>
+							<th>Price</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each books as { isbn, title, authors, price }}
+							<tr>
+								<td>
+									<input
+										type="checkbox"
+										class="checkbox"
+										checked={selectedBooks.includes(isbn)}
+										on:change={() => {
+											if (selectedBooks.includes(isbn)) {
+												selectedBooks = selectedBooks.filter((id) => id !== isbn);
+											} else {
+												selectedBooks = [...selectedBooks, isbn];
+											}
+										}}
+									/>
+								</td>
+								<th>{isbn}</th>
+								<td>{title}</td>
+								<td>{authors}</td>
+								<td>€{price}</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+			{#if canPlaceOrder}
+				<div class="card fixed bottom-4 left-0 z-10 flex w-screen flex-row bg-transparent md:absolute md:bottom-24 md:mx-2 md:w-full">
+					<div class="mx-2 flex w-full flex-row justify-between bg-base-300 px-4 py-2 shadow-lg">
+						<dl class="stats flex">
+							<div class="stat flex shrink flex-row place-items-center py-2 max-md:px-4">
+								<div class="stat-title">Selected books:</div>
+								<div class="stat-value text-lg">
+									{selectedBooks.length}
+								</div>
+							</div>
+							<div class="stat flex place-items-center py-2 max-md:px-4">
+								<div class="stat-title sr-only">Total</div>
+								<div class="stat-value text-lg">€{totalAmount.toFixed(2)}</div>
+							</div>
+						</dl>
+						<button class="btn-primary btn" on:click={handlePlaceOrder}>
+							Place Order
+							<Truck aria-hidden size={20} class="hidden md:block" />
+						</button>
+					</div>
+				</div>
+			{/if}
+		</div>
+	</div>
+</main>
