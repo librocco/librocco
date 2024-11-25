d3d6acaecd5cd62152def2a8677af1a40dc6dc79
fadwa <fadwa.mahmoud@gmail.com>
2024-11-25 15:43:49 +0200

    Replace page component in new routes with layout

---
+++ apps/web-client/src/routes/orders/+layout.svelte
@@ -0,0 +1,219 @@
+<script lang="ts">
+	import {
+		BookCopy,
+		Library,
+		PackageMinus,
+		Search,
+		Settings,
+		PersonStanding,
+		AlignLeft as Menu,
+		ArrowLeftToLine,
+		QrCode,
+		Book,
+		Truck
+	} from "lucide-svelte";
+	import { fade, fly } from "svelte/transition";
+	import { createDialog, melt } from "@melt-ui/svelte";
+
+	import { testId, type WebClientView } from "@librocco/shared";
+
+	import { LL } from "$i18n/i18n-svelte";
+
+	import { goto } from "$lib/utils/navigation";
+	import { TooltipWrapper } from "$lib/components";
+	import { appPath } from "$lib/paths";
+	import { getDB } from "$lib/db";
+
+	import { page } from "$app/stores";
+
+	interface Link {
+		label: string;
+		href: string;
+		icon: any;
+		// This is used purely for testing purposes
+		linkto?: WebClientView;
+	}
+
+	$: ({ nav: tNav } = $LL);
+
+	let links: Link[];
+	$: links = [
+		{
+			label: tNav.search(),
+			href: appPath("stock"),
+			icon: Search,
+			linkto: "stock"
+		},
+		{
+			label: tNav.inventory(),
+			href: appPath("inventory"),
+			icon: Library,
+			linkto: "inventory"
+		},
+		{
+			label: tNav.outbound(),
+			href: appPath("outbound"),
+			icon: PackageMinus,
+			linkto: "outbound"
+		},
+		{
+			label: tNav.settings(),
+			href: appPath("settings"),
+			icon: Settings,
+			linkto: "settings"
+		},
+		{
+			label: tNav.history(),
+			href: appPath("history/date"),
+			icon: Book,
+			linkto: "history/date"
+		},
+		{
+			label: "Customers",
+			href: appPath("customers"),
+			icon: PersonStanding,
+			linkto: "orders/customers"
+		},
+		{
+			label: tNav.supplier_orders(),
+			href: appPath("supplier_orders"),
+			icon: Truck,
+			linkto: "orders/suppliers"
+		}
+	];
+
+	export let view: WebClientView;
+	export let loaded: boolean;
+
+	const {
+		elements: { trigger, overlay, content, close: closeMobileMenu, portalled },
+		states: { open: mobilMenuOpen }
+	} = createDialog({
+		forceVisible: true
+	});
+
+	/**
+	 * Handle create note is an `on:click` handler used to create a new outbound note
+	 * _(and navigate to the newly created note page)_.
+	 */
+	const handleCreateNote = async () => {
+		const note = await getDB().db?.warehouse().note().create();
+		await goto(appPath("outbound", note.id));
+	};
+</script>
+
+<div id={testId("page-container")} data-view={view} data-loaded={loaded} class="flex h-screen w-screen overflow-hidden">
+	<!-- Sidenav -->
+	<div class="flex">
+		<div class="hidden h-screen bg-gray-800 sm:inline-block">
+			<div class="block px-6 py-4">
+				<BookCopy color="white" strokeWidth={2} size={36} />
+			</div>
+
+			<nav class="px-3" aria-label="Main navigation">
+				<ul class="flex flex-col items-center gap-y-3">
+					{#each links as { label, icon, href, linkto }}
+						<TooltipWrapper
+							options={{
+								positioning: {
+									placement: "right"
+								},
+								openDelay: 0,
+								closeDelay: 0,
+								closeOnPointerDown: true,
+								forceVisible: true
+							}}
+							let:trigger
+						>
+							<li {...trigger} use:trigger.action>
+								<a
+									data-linkto={linkto}
+									{href}
+									class="inline-block rounded-sm p-4 text-gray-400 {$page.url.pathname.startsWith(href)
+										? 'bg-gray-900'
+										: 'hover:bg-gray-700'}"
+								>
+									<svelte:component this={icon} size={24} />
+								</a>
+							</li>
+
+							<p slot="tooltip-content" class="px-4 py-1 text-white">{label}</p>
+						</TooltipWrapper>
+					{/each}
+				</ul>
+			</nav>
+		</div>
+	</div>
+	<!-- Sidenav end -->
+
+	<!-- Main content -->
+	<div id="content" data-loaded={loaded} class="relative flex h-screen w-full flex-col overflow-hidden bg-gray-50">
+		<!-- Main section -->
+		<main class="relative flex h-full w-full flex-col justify-between overflow-hidden bg-white">
+			<div class="relative h-full w-full overflow-y-auto">
+				<slot />
+			</div>
+
+			<div class="flex h-8 items-center justify-end border-t bg-gray-100 px-4">
+				<slot name="footer" />
+			</div>
+		</main>
+		<!-- Main section end -->
+	</div>
+</div>
+
+<!-- Mobile menu overlay -->
+<div use:melt={$portalled}>
+	{#if $mobilMenuOpen}
+		<div use:melt={$overlay} class="fixed inset-0 z-50 bg-black/50" transition:fade|global={{ duration: 150 }}>
+			<div
+				use:melt={$content}
+				class="fixed left-0 top-0 bottom-0 z-50 h-full w-2/3 min-w-[256px] overflow-y-auto bg-gray-900"
+				transition:fly|global={{
+					x: -350,
+					duration: 300,
+					opacity: 1
+				}}
+			>
+				<div class="my-5 flex items-center px-4 text-white">
+					<BookCopy class="mr-[5px]" size={36} />
+					<p class="text-2xl font-extrabold leading-8 tracking-tight text-white">Libraio</p>
+					<button use:melt={$closeMobileMenu} class="ml-auto block">
+						<ArrowLeftToLine size={24} />
+					</button>
+				</div>
+
+				<!-- Nav -->
+				<nav class="px-2">
+					<ul class="mb-20">
+						{#each links as { label, icon, href }}
+							<li>
+								<a
+									{href}
+									class="mb-1 flex items-center rounded-md p-2 pr-3 {$page.url.pathname.startsWith(href)
+										? 'bg-gray-200'
+										: 'hover:bg-gray-700'}"
+								>
+									<svelte:component this={icon} class="mr-3 text-gray-400" size={24} />
+									<span class="text-sm font-medium leading-5 text-gray-600">{label}</span>
+								</a>
+							</li>
+						{/each}
+					</ul>
+
+					<button on:click={handleCreateNote} class="flex gap-3 p-2 pr-3">
+						<div class="flex h-10 w-10 items-center justify-center rounded-md bg-gray-50">
+							<QrCode size={24} class="text-black" />
+						</div>
+						<div>
+							<p class="text-sm font-medium leading-5 text-gray-50">Checkout</p>
+							<p class="text-xs font-normal leading-4 text-gray-400">Create a new outbound note</p>
+						</div>
+					</button>
+				</nav>
+				<!-- Nav end -->
+			</div>
+		</div>
+	{/if}
+</div>
+<!-- Mobile menu overlay end -->
