2ec97a15cbf49533185d76ad2d7a6d94e03f6254
Silvio Tomatis <silviot@gmail.com>
2024-12-13 12:39:08 +0100

    Fix DbCtx/DB confusion

--- apps/web-client/src/routes/orders/customers/[id]/+page.ts
+++ apps/web-client/src/routes/orders/customers/[id]/+page.ts
@@ -7,19 +7,21 @@ export const load: PageLoad = async ({ parent, params, depends }) => {
 	depends("customer:data");
 	depends("customer:books");
 
-	const { ordersDb } = await parent();
+	const {
+		ordersDb: { db }
+	} = await parent();
 
 	// If db is not returned (we're not in the browser environment, no need for additional loading)
-	if (!ordersDb) {
+	if (!db) {
 		return {};
 	}
 
-	const customerDetails = await getCustomerDetails(ordersDb, Number(params.id));
+	const customerDetails = await getCustomerDetails(db, Number(params.id));
 
 	// TODO: make this a single query
-	const customerOrderLines = await getCustomerBooks(ordersDb, Number(params.id));
+	const customerOrderLines = await getCustomerBooks(db, Number(params.id));
 	const isbns = customerOrderLines.map((book) => book.isbn);
-	const bookData = (await ordersDb.execO(`SELECT * FROM book WHERE isbn IN (${isbns.join(", ")})`)) as BookEntry[];
+	const bookData = (await db.execO(`SELECT * FROM book WHERE isbn IN (${isbns.join(", ")})`)) as BookEntry[];
 	const bookDataMap = new Map<string, BookEntry>();
 	bookData.forEach((book) => {
 		bookDataMap.set(book.isbn, book);
