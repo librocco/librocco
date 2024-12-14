cfcd4f501335cdaeddcb89d80fb4b29d754ad371
fadwa <fadwa.mahmoud@gmail.com>
2024-12-14 20:27:32 +0200

    Add jsdocs for supplier functions

--- apps/web-client/src/routes/orders/suppliers/+layout.ts
+++ apps/web-client/src/routes/orders/suppliers/+layout.ts
@@ -1,13 +1,13 @@
 import { getInitializedDB } from "$lib/db/orders";
 
-import { getPlacedSupplierOrders, getPossibleSupplierOrderInfos } from "$lib/db/orders/suppliers";
+import { getPlacedSupplierOrders, getPossibleSupplierOrders } from "$lib/db/orders/suppliers";
 import type { LayoutLoad } from "./$types";
 
 export const load: LayoutLoad = async ({ depends }) => {
 	depends("suppliers:data");
 
 	const ordersDb = await getInitializedDB("librocco-current-db");
-	const possibleOrdersInfo = await getPossibleSupplierOrderInfos(ordersDb);
+	const possibleOrdersInfo = await getPossibleSupplierOrders(ordersDb);
 	const placedOrders = await getPlacedSupplierOrders(ordersDb);
 
 	return { placedOrders, possibleOrders: possibleOrdersInfo, ordersDb };
