6ea7f4bf7b7a3071a914171f2e5d3c733f8f5e48
Chris De Sousa <cdelasoul@gmail.com>
2024-12-15 16:35:44 +0900

    Fix: remove unused 'get all supplier order lines' handler
    - we only need to get supplier order lines for a single supplier at a time =>
    remove similar but unused handler that gets all lines aggregated for all suppliers
    - rename `getPossibleOrderLinesForSupplier` and remove GROUP BY supplier_id
    - Update relevant tests

--- apps/web-client/src/lib/db/orders/__tests__/supplier-order.test.ts
+++ apps/web-client/src/lib/db/orders/__tests__/supplier-order.test.ts
@@ -7,43 +7,54 @@ import { getRandomDb, createCustomerOrders } from "./lib";
 import {
 	associatePublisher,
 	getPlacedSupplierOrders,
-	getPossibleSupplerOrderLines,
+	getPossibleSupplierOrderLines,
 	getPossibleSupplierOrders,
 	createSupplierOrder
 } from "../suppliers";
 
-describe("Suppliers order creation", () => {
+describe("Supplier order handlers should", () => {
 	let db: DB;
 	beforeEach(async () => {
 		db = await getRandomDb();
 		await createCustomerOrders(db);
 	});
 
-	it("sees possible supplier orders from client orders", async () => {
-		expect(await getPossibleSupplerOrderLines(db)).toStrictEqual([
-			{ supplier_id: 1, isbn: "1", quantity: 1, supplier_name: "Science Books LTD" },
-			{ supplier_id: 1, isbn: "2", quantity: 1, supplier_name: "Science Books LTD" },
-			{ supplier_id: 2, isbn: "3", quantity: 2, supplier_name: "Phantasy Books LTD" }
-		]);
+	it("list all pending supplier orders from unplaced customer order lines", async () => {
+		// Customer order lines can be aggregated into these suppliers.
+		// The aggregation includes the total number of books and total price
 		expect(await getPossibleSupplierOrders(db)).toStrictEqual([
 			{ supplier_name: "Phantasy Books LTD", supplier_id: 2, total_book_number: 2, total_book_price: 10 },
 			{ supplier_name: "Science Books LTD", supplier_id: 1, total_book_number: 2, total_book_price: 20 }
 		]);
+	});
+
+	it("Retrieves possible order lines for a specific supplier from unplaced customer orders", async () => {
+		// Supplier 1 should have the following lines
+		expect(await getPossibleSupplierOrderLines(db, 1)).toStrictEqual([
+			{ supplier_id: 1, isbn: "1", quantity: 1, supplier_name: "Science Books LTD" },
+			{ supplier_id: 1, isbn: "2", quantity: 1, supplier_name: "Science Books LTD" }
+		]);
+		// Supplier 2 lines should have the following lines
+		expect(await getPossibleSupplierOrderLines(db, 2)).toStrictEqual([
+			{ supplier_id: 2, isbn: "3", quantity: 2, supplier_name: "Phantasy Books LTD" }
+		]);
+
 		// If we change the supplier for ChemPub to Phantasy Books LTD
 		// the supplier order will reflect that
 		await associatePublisher(db, 2, "ChemPub");
-		expect(await getPossibleSupplerOrderLines(db)).toStrictEqual([
-			{ supplier_id: 1, isbn: "1", quantity: 1, supplier_name: "Science Books LTD" },
+		expect(await getPossibleSupplierOrderLines(db, 2)).toStrictEqual([
 			{ supplier_id: 2, isbn: "2", quantity: 1, supplier_name: "Phantasy Books LTD" }, // This is now from supplier 2
 			{ supplier_id: 2, isbn: "3", quantity: 2, supplier_name: "Phantasy Books LTD" }
 		]);
 	});
 
 	it("creates two new supplier orders", async () => {
-		const possibleOrderLines = await getPossibleSupplerOrderLines(db);
+		const possibleOrderLines = await getPossibleSupplierOrderLines(db, 1);
+
 		const newOrders = await createSupplierOrder(db, possibleOrderLines);
-		expect(newOrders.length).toStrictEqual(2);
-		const newPossibleOrderLines = await getPossibleSupplerOrderLines(db);
+		expect(newOrders.length).toStrictEqual(1);
+
+		const newPossibleOrderLines = await getPossibleSupplierOrderLines(db, 1);
 		expect(newPossibleOrderLines.length).toStrictEqual(0);
 	});
 
