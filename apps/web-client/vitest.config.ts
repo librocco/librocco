33abecd8fc1e7490bcaf02fac2e94ab0523b822c
ikusteu <ivankust57@gmail.com>
2024-12-16 19:31:34 +0100

    fix test glob pattern

--- apps/web-client/vitest.config.ts
+++ apps/web-client/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		include: ["src/lib/db/orders/__tests__/**/*.{test,spec}.{js,ts}"],
+		include: ["src/lib/db/cr-sqlite/__tests__/**/*.{test,spec}.{js,ts}"],
 		browser: {
 			enabled: true,
 			provider: "playwright",
