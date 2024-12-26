779994567ef436d4137e44ae33370ea96bf17a2e
fadwa <fadwa.mahmoud@gmail.com>
2024-12-26 19:01:15 +0200

    Add reconcile route to svelte config

--- apps/web-client/svelte.config.js
+++ apps/web-client/svelte.config.js
@@ -36,7 +36,7 @@ const config = {
 				"/orders/customers/",
 				"/orders/customers/1/",
 				"/orders/suppliers/",
-				"/orders/suppliers/reconcile/",
+				"/orders/suppliers/reconcile/1/",
 				"/orders/suppliers/1/",
 				"/history/date/1/",
 				"/history/isbn/",
