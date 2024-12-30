a03cfecf75778e1d89dae24a0eb1f1f3a0ae4074
ikusteu <ivankust57@gmail.com>
2024-12-30 10:10:50 +0100

    inventory db refactor: history: warehouse view:
    * refactor the warehouse list view to use cr-sqlite handlers + load funciton + invalidate
    * refactor the warehouse date range view to use cr-sqlite handlers + load funciton + invalidate
    * TODO: some e2e tests are failing due to DB timestamp issues
    * TODO: some e2e tests are failing due to navigation issues

--- apps/web-client/src/routes/history/warehouse/[warehouseId]/[from]/[to]/+page.ts
+++ apps/web-client/src/routes/history/warehouse/[warehouseId]/[from]/[to]/+page.ts
@@ -1,5 +1,24 @@
+import { browser } from "$app/environment";
+import { getPastTransactions } from "$lib/db/cr-sqlite/history";
+import { getWarehouseById } from "$lib/db/cr-sqlite/warehouse";
+
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({ parent }) => {
-	return await parent();
+export const load: PageLoad = async ({ parent, params }) => {
+	const { to, from } = params;
+	const warehouseId = Number(params.warehouseId);
+
+	const { dbCtx } = await parent();
+
+	if (!browser) {
+		return { displayName: "N/A", transactions: [] };
+	}
+
+	const startDate = new Date(from);
+	const endDate = new Date(to);
+
+	const { displayName } = await getWarehouseById(dbCtx.db, warehouseId);
+	const transactions = await getPastTransactions(dbCtx.db, { warehouseId, startDate, endDate });
+
+	return { displayName, transactions };
 };
