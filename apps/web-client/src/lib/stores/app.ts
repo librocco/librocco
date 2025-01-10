36de5c02dacb370b1a691eb6c624c6552229726d
fadwa <fadwa.mahmoud@gmail.com>
2025-01-10 12:28:43 +0200

    Fix settingSchema initial load

--- apps/web-client/src/lib/stores/app.ts
+++ apps/web-client/src/lib/stores/app.ts
@@ -1,7 +1,9 @@
 import { persisted } from "svelte-local-storage-store";
 
-import { LOCAL_STORAGE_APP_SETTINGS } from "$lib/constants";
+import { LOCAL_STORAGE_APP_SETTINGS, LOCAL_STORAGE_SETTINGS } from "$lib/constants";
 
 const autoPrintLabelsInner = persisted(LOCAL_STORAGE_APP_SETTINGS, false);
 const toggleAutoprintLabels = () => autoPrintLabelsInner.update((v) => !v);
 export const autoPrintLabels = Object.assign(autoPrintLabelsInner, { toggle: toggleAutoprintLabels });
+
+export const settingsStore = persisted(LOCAL_STORAGE_SETTINGS, {});
