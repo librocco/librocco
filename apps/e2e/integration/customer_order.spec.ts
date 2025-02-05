63cc704d04ab69f8d84f8c35c2578b5b933323de
fadwa <fadwa.mahmoud@gmail.com>
2025-02-05 13:09:31 +0200

    Separate fixtures

--- apps/e2e/integration/customer_order.spec.ts
+++ apps/e2e/integration/customer_order.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 import { baseURL } from "./constants";
-import { getDbHandle } from "@/helpers";
-import { addBooksToCustomer, upsertCustomer } from "@/helpers/cr-sqlite";
 
-test.beforeEach(async ({ page }) => {
-	await page.goto(`${baseURL}orders/customers/`);
-});
+import { testOrders } from "@/helpers/fixtures";
+import { getDbHandle } from "@/helpers";
+import { addBooksToCustomer } from "@/helpers/cr-sqlite";
+
 test("should create a new customer order", async ({ page }) => {
+	await page.goto(`${baseURL}orders/customers/`);
 	await page.getByRole("button", { name: "New Order" }).first().click();
 
 	const dialog = page.getByRole("dialog");
@@ -18,30 +18,10 @@ test("should create a new customer order", async ({ page }) => {
 
 	await page.getByRole("button", { name: "Create" }).click();
 
+	await page.waitForURL(`${baseURL}orders/customers/**`);
 	// Verify new customer appears in list
 	await expect(page.getByText("New Customer")).toBeVisible();
 	await expect(page.getByText("new@example.com")).toBeVisible();
-});
-
-type CustomerTestFixture = {
-	customer: { id: number; fullname: string; email: string; displayId: string };
-};
-
-const testOrders = test.extend<CustomerTestFixture>({
-	customer: async ({ page }, use) => {
-		await page.goto(baseURL);
-
-		const customer = { id: 1, fullname: "John Doe", email: "john@gmail.com", displayId: "1" };
-
-		const dbHandle = await getDbHandle(page);
-
-		// dbHandler
-		await dbHandle.evaluate(upsertCustomer, customer);
-		await dbHandle.evaluate(addBooksToCustomer, { customerId: 1, bookIsbns: ["1234"] });
-		await dbHandle.evaluate(addBooksToCustomer, { customerId: 1, bookIsbns: ["1234", "4321"] });
-
-		await use(customer);
-	}
 });
 
 testOrders("should show list of In Progress orders", async ({ page, customer }) => {
@@ -51,7 +31,11 @@ testOrders("should show list of In Progress orders", async ({ page, customer }) 
 	await expect(page.getByText(customer.fullname)).toBeVisible();
 	await expect(page.getByText(customer.email)).toBeVisible();
 });
-testOrders("should show a customer order with the correct details", async ({ page, customer }) => {
+testOrders("should allow navigation to a specific order", async ({ page, customer, books }) => {
+	const dbHandle = await getDbHandle(page);
+
+	await dbHandle.evaluate(addBooksToCustomer, { customerId: 1, bookIsbns: [books[0].isbn] });
+	await dbHandle.evaluate(addBooksToCustomer, { customerId: 1, bookIsbns: [books[0].isbn, books[1].isbn] });
 	await page.goto(`${baseURL}orders/customers/`);
 	const updateButton = page.getByRole("link", { name: "Update" }).first();
 	await updateButton.click();
@@ -60,43 +44,63 @@ testOrders("should show a customer order with the correct details", async ({ pag
 	await expect(page.getByText(customer.fullname)).toBeVisible();
 	await expect(page.getByText(customer.email)).toBeVisible();
 
-	await expect(page.getByText("1234").first()).toBeVisible();
-	await expect(page.getByText("1234").nth(1)).toBeVisible();
+	await expect(page.getByText(books[0].isbn).first()).toBeVisible();
+	await expect(page.getByText(books[0].title).first()).toBeVisible();
+	await expect(page.getByText(books[0].authors).first()).toBeVisible();
+	await expect(page.getByText(`${books[0].price}`).first()).toBeVisible();
+	await expect(page.getByText(books[0].isbn).nth(1)).toBeVisible();
+	await expect(page.getByText(books[0].isbn).nth(1)).toBeVisible();
+	await expect(page.getByText(books[0].title).nth(1)).toBeVisible();
+	await expect(page.getByText(books[0].authors).nth(1)).toBeVisible();
+	await expect(page.getByText(`${books[0].price}`).nth(1)).toBeVisible();
 
-	await expect(page.getByText("4321")).toBeVisible();
+	await expect(page.getByText(books[1].isbn)).toBeVisible();
 });
 testOrders("should update a customer details", async ({ page, customer }) => {
 	await page.goto(`${baseURL}orders/customers/1/`);
 
-	page.getByText(customer.fullname);
+	const newCustomer = { fullname: "New Customer", email: "new@gmail.com", deposit: "10" };
+	await expect(page.getByText(customer.fullname)).toBeVisible();
+	await expect(page.getByText(customer.email)).toBeVisible();
+
 	await page.getByLabel("Edit customer order name, email or deposit").click();
 	const dialog = page.getByRole("dialog");
 	await expect(dialog).toBeVisible();
-	await page.getByLabel("Name").nth(2).fill("New Customer");
-	await page.getByLabel("Email").nth(2).fill("new@gmail.com");
 
-	await page.getByLabel("Deposit").nth(2).fill("10");
+	await page.getByLabel("Name").nth(2).fill(newCustomer.fullname);
+	await page.getByLabel("Email").nth(2).fill(newCustomer.email);
+	await page.getByLabel("Deposit").nth(2).fill(newCustomer.deposit);
 
 	await page.locator('button[type="submit"]').click();
 
 	await expect(page.getByLabel("Edit customer order name, email or deposit").nth(1)).toBeHidden();
 
-	await expect(page.getByText("New Customer")).toBeVisible();
-	await expect(page.getByText("new@gmail.com")).toBeVisible();
+	await expect(page.getByText(newCustomer.fullname)).toBeVisible();
+	await expect(page.getByText(newCustomer.email)).toBeVisible();
+	await expect(page.getByText(newCustomer.deposit)).toBeVisible();
 });
 
-testOrders("should add books to a customer order", async ({ page, customer }) => {
-	await page.goto(`${baseURL}orders/customers/`);
+testOrders("should add books to a customer order", async ({ page, customer, books }) => {
+	await page.goto(`${baseURL}orders/customers/1`);
 
-	await page.getByRole("link", { name: "Update" }).first().click();
-
-	await page.waitForURL(`${baseURL}orders/customers/1/`);
 	await expect(page.getByText(customer.fullname)).toBeVisible();
 	await expect(page.getByText(customer.email)).toBeVisible();
 
 	const isbnField = page.getByRole("textbox");
+	isbnField.fill(books[0].isbn);
+	isbnField.press("Enter");
+
+	await expect(page.getByText(books[0].isbn)).toBeVisible();
+	await expect(page.getByText(books[0].title)).toBeVisible();
+	await expect(page.getByText(books[0].authors)).toBeVisible();
+	await expect(page.getByText("Draft")).toBeVisible();
+
 	isbnField.fill("5678");
 	isbnField.press("Enter");
 
 	await expect(page.getByText("5678")).toBeVisible();
+	await expect(page.getByText("N/A").first()).toBeVisible();
+	await expect(page.getByText("N/A").nth(1)).toBeVisible();
+	await expect(page.getByText("0", { exact: true })).toBeVisible();
+	await expect(page.getByText("Draft").nth(1)).toBeVisible();
 });
