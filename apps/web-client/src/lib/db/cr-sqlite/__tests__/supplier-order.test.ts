28defa0dba2e2538950e92a4a1baf6539e5d9567
Chris De Sousa <cdelasoul@gmail.com>
2025-02-05 20:00:21 +0800

    Fix: re-write getPossibleSupplierOrders tests
    - use more explicit test setup (even if it results in duplicate code) so that
    its esaier to reference test data in assertions
    - extend test cases

--- apps/web-client/src/lib/db/cr-sqlite/__tests__/supplier-order.test.ts
+++ apps/web-client/src/lib/db/cr-sqlite/__tests__/supplier-order.test.ts
@@ -5,6 +5,7 @@ import { type DB } from "../types";
 import { getRandomDb, createCustomerOrders } from "./lib";
 
 import {
+	upsertSupplier,
 	associatePublisher,
 	getPlacedSupplierOrders,
 	getPossibleSupplierOrderLines,
@@ -12,23 +13,156 @@ import {
 	createSupplierOrder,
 	getPlacedSupplierOrderLines
 } from "../suppliers";
-import { addBooksToCustomer } from "../customers";
+import { addBooksToCustomer, upsertCustomer } from "../customers";
+import { upsertBook } from "../books";
+
+describe("Possible (new) supplier orders:", () => {
+	const customer1 = { fullname: "John Doe", id: 1, displayId: "100" };
+	const customer2 = { fullname: "Harry Styles", id: 2, displayId: "100" };
+
+	const book1 = { isbn: "1", publisher: "MathsAndPhysicsPub", title: "Physics", price: 7 };
+	const book2 = { isbn: "2", publisher: "ChemPub", title: "Chemistry", price: 13 };
+	const books = [book1, book2];
+
+	const supplier1 = { id: 1, name: "Alphabet Books LTD" };
+	const supplier2 = { id: 2, name: "Xanax Books LTD" };
+
+	let db: DB;
+
+	beforeEach(async () => {
+		db = await getRandomDb();
+
+		// Setup the entities that we're going to need each time, but don't associate them...
+		// leave this to the tests for different scenarios
+		await upsertCustomer(db, customer1);
+		await upsertCustomer(db, customer2);
+		await upsertBook(db, book1);
+		await upsertBook(db, book2);
+		await upsertSupplier(db, supplier1);
+		await upsertSupplier(db, supplier2);
+	});
+
+	describe("getPossibleSupplierOrders should", () => {
+		it("aggregate a supplier order from multiple client orders", async () => {
+			const { id: supplierId, name: supplierName } = supplier1;
+
+			// Associate book1 and book2 with the same supplier
+			await associatePublisher(db, supplierId, book1.publisher);
+			await associatePublisher(db, supplierId, book2.publisher);
+
+			// Add book1 and book2 to different customer orders
+			addBooksToCustomer(db, customer1.id, [book1.isbn]);
+			addBooksToCustomer(db, customer2.id, [book2.isbn]);
+
+			// There should be one possible supplier order...
+			const result = await getPossibleSupplierOrders(db);
+			expect(result.length).toBe(1);
+
+			const [supplierOrder] = result;
+			expect(supplierOrder.supplier_id).toBe(supplierId);
+			expect(supplierOrder.supplier_name).toBe(supplierName);
+			// It should include both books from the two customer orders
+			expect(supplierOrder.total_book_number).toBe(books.length);
+			expect(supplierOrder.total_book_price).toBe(books.reduce((acc, { price }) => acc + price, 0));
+		});
+
+		it("group supplier orders and order them ascending by supplier name", async () => {
+			// Associate book1 and book2 with the different suppliers
+			await associatePublisher(db, supplier1.id, book1.publisher);
+			await associatePublisher(db, supplier2.id, book2.publisher);
+
+			// Add books to a customer order
+			addBooksToCustomer(db, customer1.id, [book1.isbn, book2.isbn]);
+
+			// There should be two possible supplier order...
+			const result = await getPossibleSupplierOrders(db);
+			expect(result.length).toBe(2);
+
+			const [supplierOrder1, supplierOrder2] = result;
+			// They should be ordered: Supplier1 name = "Alphabet ...", Suplier2 name = "Xanax ..."
+			expect(supplierOrder1.supplier_name).toBe(supplier1.name);
+			expect(supplierOrder2.supplier_name).toBe(supplier2.name);
+		});
+
+		it("should return an empty list when there are no customer order lines for that supplier", async () => {
+			// Associate the books with suppliers...
+			await associatePublisher(db, supplier1.id, book1.publisher);
+			await associatePublisher(db, supplier2.id, book2.publisher);
+
+			// But don't add any books to a customer order
+			const result = await getPossibleSupplierOrders(db);
+			expect(result).toEqual([]);
+		});
+
+		it("should only aggregate customer order lines that have not been placed", async () => {
+			// Associate the books with a suppliers
+			const { id: supplierId } = supplier1;
+			await associatePublisher(db, supplierId, book1.publisher);
+			await associatePublisher(db, supplierId, book2.publisher);
+
+			// Add two books
+			// book1 will not have any status timestamps
+			addBooksToCustomer(db, customer1.id, [book1.isbn]);
+			// book2 will have a placed timestamp
+			await db.exec(
+				`INSERT INTO customer_order_lines (customer_id, isbn, placed)
+				VALUES ($customerId, $isbn, $placed)`,
+				[customer1.id, book2.isbn, new Date().getTime()]
+			);
+
+			// Only book1 should be included
+			const [newSupplierOrder] = await getPossibleSupplierOrders(db);
+			expect(newSupplierOrder.total_book_number).toBe(1);
+			expect(newSupplierOrder.total_book_price).toBe(book1.price);
+		});
+
+		it("should handle missing book prices", async () => {
+			// Associate book1 with supplier1
+			await associatePublisher(db, supplier1.id, book1.publisher);
+
+			// Associate an isbn without book data to both suppliers
+			const rogueBook = { isbn: "666", publisher: "Devil business" };
+			await upsertBook(db, rogueBook);
+
+			await associatePublisher(db, supplier1.id, rogueBook.publisher);
+			await associatePublisher(db, supplier2.id, rogueBook.publisher);
+
+			// Add all the books to the customer order
+			addBooksToCustomer(db, customer1.id, [book1.isbn, rogueBook.isbn]);
+
+			const [newSupplierOrder1, newSupplierOrder2] = await getPossibleSupplierOrders(db);
+			// Supplier1 order should = book1.price + 0
+			// TODO: is this desirable?
+			expect(newSupplierOrder1.supplier_name).toBe(supplier1.name);
+			expect(newSupplierOrder1.total_book_price).toBe(book1.price);
+
+			// Supplier2 order should be 0
+			expect(newSupplierOrder2.total_book_price).toBe(0);
+		});
+
+		it("aggregate books that are not associated with a supplier into a 'General' order", async () => {
+			// Do not associatePublisher
+
+			// Add book1 and book2 to different customer orders
+			addBooksToCustomer(db, customer1.id, [book1.isbn]);
+			addBooksToCustomer(db, customer2.id, [book2.isbn]);
+
+			// There should be one possible supplier order...
+			const result = await getPossibleSupplierOrders(db);
+			expect(result.length).toBe(1);
+
+			const [supplierOrder] = result;
+			expect(supplierOrder.supplier_id).toBe(null);
+			expect(supplierOrder.supplier_name).toBe("General");
+		});
+	});
+});
 
 describe("Supplier order handlers should", () => {
 	let db: DB;
 	beforeEach(async () => {
 		db = await getRandomDb();
 		await createCustomerOrders(db);
-	});
-
-	it("list all pending supplier orders from unplaced customer order lines", async () => {
-		// Customer order lines can be aggregated into these suppliers.
-		// The aggregation includes the total number of books and total price
-		expect(await getPossibleSupplierOrders(db)).toStrictEqual([
-			{ supplier_name: "General", supplier_id: null, total_book_number: 2, total_book_price: 100_000_060 },
-			{ supplier_name: "Phantasy Books LTD", supplier_id: 2, total_book_number: 2, total_book_price: 10 },
-			{ supplier_name: "Science Books LTD", supplier_id: 1, total_book_number: 2, total_book_price: 20 }
-		]);
 	});
 
 	// TODO: export and match to test data instead of all of this duplication
