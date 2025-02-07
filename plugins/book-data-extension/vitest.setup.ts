3557ab1b151d75bb18c6402b71c02bb3751e96a1
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2025-02-07 12:24:10 +0100

    Fix book-data-extension tests by enabling ESM support

--- plugins/book-data-extension/vitest.setup.ts
+++ plugins/book-data-extension/vitest.setup.ts
@@ -1,13 +1,6 @@
 import { expect } from "vitest";
-import matchers, { type TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 
-// Required to appease TS
-// Merges standard matchers declaration with js-dom extensions
-declare global {
-	namespace Vi {
-		interface JestAssertion<T = any> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> {}
-	}
-}
+const matchers = await import("@testing-library/jest-dom/matchers").then(m => m.default || m);
 
 // Add custom jest matchers
 expect.extend(matchers);
