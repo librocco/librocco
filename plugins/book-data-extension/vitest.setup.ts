2b66df6fd9375ea850fe6df77f2d91bf00f7bab4
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2025-02-10 08:55:29 +0100

    Fix formatting

--- plugins/book-data-extension/vitest.setup.ts
+++ plugins/book-data-extension/vitest.setup.ts
@@ -1,6 +1,6 @@
 import { expect } from "vitest";
 
-const matchers = await import("@testing-library/jest-dom/matchers").then(m => m.default || m);
+const matchers = await import("@testing-library/jest-dom/matchers").then((m) => m.default || m);
 
 // Add custom jest matchers
 expect.extend(matchers);
