e06a4fdc07d28b1fbc8be64f24b521bf6a6243a5
ikusteu <ivankust57@gmail.com>
2025-03-20 20:32:23 +0100

    test: add a test for observed error (null price in past notes if no book data)

--- apps/web-client/src/lib/db/cr-sqlite/__tests__/history.test.ts
+++ apps/web-client/src/lib/db/cr-sqlite/__tests__/history.test.ts
@@ -69,6 +69,34 @@ describe("getPastNotes", () => {
 			})
 		]);
 	});
+
+	it("falls back to 0 for price calculation if no book data available", async () => {
+		const db = await getRandomDb();
+
+		// Set up warehouses
+		await upsertWarehouse(db, { id: 1, displayName: "Warehouse 1" });
+
+		// Create and commit notes
+		await createInboundNote(db, 1, 1);
+		await addVolumesToNote(db, 1, { isbn: "1111111111", quantity: 5, warehouseId: 1 });
+		await commitNote(db, 1);
+
+		// NOTE: explicit SQL update -- I know, dirty, but making it easier to make assertions
+		await db.exec("UPDATE note SET committed_at = strftime('%s', '2024-01-02T10:00:00') * 1000 WHERE id = 1");
+
+		const [note] = await getPastNotes(db, "2024-01-02");
+
+		expect(note).toEqual(
+			expect.objectContaining({
+				id: 1,
+				noteType: "inbound",
+				totalBooks: 5,
+				warehouseName: "Warehouse 1",
+				totalCoverPrice: 0,
+				totalDiscountedPrice: 0
+			})
+		);
+	});
 });
 
 describe("getPastTransactions", async () => {
