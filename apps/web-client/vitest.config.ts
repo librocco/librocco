6f49cb6e9a2f1b5ca602042335b611c71d64785a
ikusteu <ivankust57@gmail.com>
2025-03-24 19:12:38 +0100

    db sync: create and test the sync interface

--- apps/web-client/vitest.config.ts
+++ apps/web-client/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		include: ["src/lib/db/cr-sqlite/__tests__/**/*.{test,spec}.{js,ts}"],
+		include: ["src/lib/db/cr-sqlite/__tests__/**/*.{test,spec}.{js,ts}", "src/lib/__tests__/**/*.{test,spec}.{js,ts}"],
 		browser: {
 			enabled: true,
 			provider: "playwright",
