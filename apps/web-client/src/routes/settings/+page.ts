3826b0a425e398879682dd76dc2d364d28e7421a
ikusteu <ivankust57@gmail.com>
2025-03-26 11:07:36 +0100

    db sync: create and wire in the SyncSettingsForm:
    * create SyncSettingsForm (UI component)
    * create a syncSettingsSchema
    * replace 'dbName' and 'dbNamePersisted' with single (persisted) 'dbid' store
    * create a syncConfig store (wrapping dbid and url persisted stores)
    * namespece the 'sync.ts' under the 'db' folder
    * wire in the SynsSettingsForm in the settings page
    * update root layout sync with the full control from the sync config (set in settings form)
    * remove PUBLIC_WITH_SYNC and PUBLIC_WS_URL env veriables -- fully configurable in the sync settings form

--- apps/web-client/src/routes/settings/+page.ts
+++ apps/web-client/src/routes/settings/+page.ts
@@ -5,10 +5,16 @@ import { get } from "svelte/store";
 import type { PageLoad } from "./$types";
 
 import { deviceSettingsStore } from "$lib/stores/app";
-import { deviceSettingsSchema } from "$lib/forms/schemas";
+import { deviceSettingsSchema, syncSettingsSchema } from "$lib/forms/schemas";
+
+import { syncConfig } from "$lib/db";
+
 export const load: PageLoad = async () => {
 	const deviceSettingsData = get(deviceSettingsStore);
 	const deviceSettingsForm = await superValidate(deviceSettingsData, zod(deviceSettingsSchema));
 
-	return { deviceSettingsForm };
+	const syncSettingsData = get(syncConfig);
+	const syncSettingsForm = await superValidate(syncSettingsData, zod(syncSettingsSchema));
+
+	return { deviceSettingsForm, syncSettingsForm };
 };
