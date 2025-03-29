afb1c526a32b21b237a23afe9dd4fd3e81fefec0
ikusteu <ivankust57@gmail.com>
2025-03-29 13:16:05 +0100

    fix: remove div nested under table (in supplier's publisher list)

--- apps/web-client/src/routes/orders/suppliers/[id]/+page.svelte
+++ apps/web-client/src/routes/orders/suppliers/[id]/+page.svelte
@@ -160,27 +160,24 @@
 			<div class="prose flex w-full max-w-full flex-row gap-x-8 md:px-4">
 				<div class="w-full">
 					<h2 class="text-lg">Assigned publishers</h2>
-					<div class="w-full rounded border border-gray-200">
-						<table class="!my-0 flex-col items-stretch overflow-hidden">
-							<thead>
+					<div class="relative max-h-[208px] w-full overflow-y-auto rounded border border-gray-200">
+						<table class="!my-0 flex-col items-stretch overflow-y-auto">
+							<thead class="sticky top-0 left-0 right-0 bg-white shadow">
 								<tr>
 									<th scope="col" class="px-2 py-2">Publisher name</th>
 								</tr>
 							</thead>
 							<tbody>
-								<div class="max-h-[164px] w-full overflow-y-auto">
-									{#each assignedPublishers as publisher}
-										<tr class="hover flex w-full justify-between focus-within:bg-base-200">
-											<td class="px-2">{publisher}</td>
-											<td class="px-2 text-end"
-												><button
-													on:click={handleUnassignPublisher(publisher)}
-													class="btn-primary btn-xs btn flex-nowrap gap-x-2.5 rounded-lg">Remove publisher</button
-												></td
-											>
-										</tr>
-									{/each}
-								</div>
+								{#each assignedPublishers as publisher}
+									<tr class="hover flex w-full justify-between focus-within:bg-base-200">
+										<td class="px-2">{publisher}</td>
+										<td class="px-2 text-end"
+											><button on:click={handleUnassignPublisher(publisher)} class="btn-primary btn-xs btn flex-nowrap gap-x-2.5 rounded-lg"
+												>Remove publisher</button
+											></td
+										>
+									</tr>
+								{/each}
 							</tbody>
 						</table>
 					</div>
@@ -188,26 +185,24 @@
 
 				<div class="w-full">
 					<h2 class="text-lg">Unassigned publishers</h2>
-					<div class="w-full rounded border border-gray-200">
-						<table class="!my-0 flex-col items-stretch overflow-hidden">
-							<thead>
+					<div class="relative max-h-[208px] w-full overflow-y-auto rounded border border-gray-200">
+						<table class="!my-0 flex-col items-stretch overflow-y-auto">
+							<thead class="sticky top-0 left-0 right-0 bg-white shadow">
 								<tr>
 									<th scope="col" class="px-2 py-2">Publisher name</th>
 								</tr>
 							</thead>
 							<tbody>
-								<div class="max-h-[164px] overflow-y-auto">
-									{#each unassignedPublishers as publisher}
-										<tr class="hover flex w-full justify-between focus-within:bg-base-200">
-											<td class="px-2">{publisher}</td>
-											<td class="px-2 text-end"
-												><button on:click={handleAssignPublisher(publisher)} class="btn-primary btn-xs btn flex-nowrap gap-x-2.5 rounded-lg"
-													>Add to supplier</button
-												></td
-											>
-										</tr>
-									{/each}
-								</div>
+								{#each unassignedPublishers as publisher}
+									<tr class="hover flex w-full justify-between focus-within:bg-base-200">
+										<td class="px-2">{publisher}</td>
+										<td class="px-2 text-end"
+											><button on:click={handleAssignPublisher(publisher)} class="btn-primary btn-xs btn flex-nowrap gap-x-2.5 rounded-lg"
+												>Add to supplier</button
+											></td
+										>
+									</tr>
+								{/each}
 							</tbody>
 						</table>
 					</div>
