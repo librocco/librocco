af17b2259d3c73ffcae1c3d64d6e1d2335e39c49
fadwa <fadwa.mahmoud@gmail.com>
2025-04-03 23:09:58 +0200

    Adjust labels

--- apps/web-client/src/routes/history/isbn/[isbn]/+page.svelte
+++ apps/web-client/src/routes/history/isbn/[isbn]/+page.svelte
@@ -50,7 +50,7 @@
 	$: transactions = data.transactions;
 	$: stock = data.stock;
 
-	$: t = $LL.history_page.isbn_tab.isbnId;
+	$: t = $LL.history_page.isbn_tab.isbn_id;
 
 	const createMetaString = ({ authors, year, publisher }: Pick<BookData, "authors" | "year" | "publisher">) =>
 		[authors, year, publisher].filter(Boolean).join(", ");
@@ -105,7 +105,7 @@
 			<!-- 'entity-list-container' class is used for styling, as well as for e2e test selector(s). If changing, expect the e2e to break - update accordingly -->
 			<div class={testId("entity-list-container")} data-view={entityListView("outbound-list")} data-loaded={true}>
 				<div class="border-b border-gray-300">
-					<h2 class="border-b border-gray-300 px-4 py-4 pt-8 text-xl font-semibold">{t.stock()}</h2>
+					<h2 class="border-b border-gray-300 px-4 py-4 pt-8 text-xl font-semibold">{t.titles.stock()}</h2>
 
 					<div data-testid={testId("history-stock-report")} class="divide grid grid-cols-4 gap-x-24 gap-y-4 p-4">
 						{#each stock as s}
@@ -127,12 +127,12 @@
 
 				{#if !transactions?.length}
 					<!-- Start entity list placeholder -->
-					<PlaceholderBox title={t.placeholderBox.title()} description={t.placeholderBox.description()} class="center-absolute" />
+					<PlaceholderBox title={t.placeholder_box.title()} description={t.placeholder_box.description()} class="center-absolute" />
 					<!-- End entity list placeholder -->
 				{:else}
 					<div class="sticky top-0">
 						<h2 class="border-b border-gray-300 bg-white px-4 py-4 pt-8 text-xl font-semibold">
-							{$LL.history_page.isbn_tab.transactions()}
+							{$LL.history_page.isbn_tab.titles.transactions()}
 						</h2>
 					</div>
 					<ul id="history-table" class="grid w-full grid-cols-12 divide-y">
