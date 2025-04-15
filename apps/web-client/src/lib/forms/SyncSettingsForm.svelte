6754cebab561f7ce52347e596cfb26ee0c4e47b3
Chris De Sousa <cdelasoul@gmail.com>
2025-04-16 00:20:31 +0100

    Fix: additional touches to shitory page & forms to apply new styles

--- apps/web-client/src/lib/forms/SyncSettingsForm.svelte
+++ apps/web-client/src/lib/forms/SyncSettingsForm.svelte
@@ -18,7 +18,12 @@
 	$: hasChanges = $tainted && !compare($formStore, data.data);
 </script>
 
-<form class="divide-y-secondary flex flex-col gap-y-10 px-4 py-4" use:enhance method="POST" aria-label="Edit remote database connection config">
+<form
+	class="divide-y-secondary flex flex-col gap-y-10 px-4 py-4"
+	use:enhance
+	method="POST"
+	aria-label="Edit remote database connection config"
+>
 	<div class="flex flex-col justify-between gap-6 lg:flex-row-reverse">
 		<div class="flex grow flex-col flex-wrap gap-y-4 lg:flex-row">
 			<label class="form-control basis-full">
@@ -39,11 +44,11 @@
 
 	<div class="flex items-center justify-between gap-x-2">
 		<div class="flex items-center gap-x-2">
-			<span class="w-10 text-center {$active ? '' : 'font-bold text-red-500'}">OFF</span>
+			<span class="w-10 text-center {$active ? '' : 'text-error font-bold'}">OFF</span>
 			<input type="checkbox" bind:checked={$active} class="toggle" />
-			<span class="w-10 text-center {$active ? 'font-bold text-green-500' : ''}">ON</span>
+			<span class="w-10 text-center {$active ? 'text-success font-bold' : ''}">ON</span>
 		</div>
 
-		<button type="submit" class="btn btn-primary disabled:bg-gray-400" disabled={!hasChanges}>Save and Reload</button>
+		<button type="submit" class="btn btn-primary disabled:btn-disabled" disabled={!hasChanges}>Save and Reload</button>
 	</div>
 </form>
