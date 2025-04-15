3c5d82218be2101b8faae02e79eee03cddd8915f
Chris De Sousa <cdelasoul@gmail.com>
2025-04-15 18:16:25 +0100

    Fix: styles on stock page & associated components

--- apps/web-client/src/lib/components/Placeholders/PlaceholderBox.svelte
+++ apps/web-client/src/lib/components/Placeholders/PlaceholderBox.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
-	let className = "";
-	export { className as class };
-
 	export let title = "";
 	export let description = "";
-
-	const iconProps = {
-		size: 36,
-		class: "text-white"
-	};
 </script>
 
-<div class="text-center {className}">
-	{#if $$slots.icon}
-		<div class="mx-auto mb-3 h-14 w-14 rounded-md bg-teal-500 p-2.5 text-center"><slot {iconProps} name="icon" /></div>
-	{/if}
-	<p class="text-xl font-semibold leading-7 text-gray-900">{title}</p>
-	<p class="mb-3 text-base font-normal leading-7 text-gray-600">{description}</p>
-	<slot />
+<div class="card card-bordered border-base-content">
+	<div class="card-body">
+		<div class="card-title flex items-center gap-x-4">
+			<slot name="icon" />
+			<p class="card-title">{title}</p>
+		</div>
+		<p>{description}</p>
+		<div class="card-actions">
+			<slot name="actions" />
+		</div>
+	</div>
 </div>
