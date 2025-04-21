5581411061cf14a03dc744980d944c247136e9c2
Chris De Sousa <cdelasoul@gmail.com>
2025-04-21 13:33:58 +0100

    Chore: format

--- apps/web-client/src/routes/outbound/[id]/+page.svelte
+++ apps/web-client/src/routes/outbound/[id]/+page.svelte
@@ -407,7 +407,7 @@
 
 					<div class="w-fit">
 						{#if updatedAt}
-							<span class="badge-primary badge-outline badge badge-md">
+							<span class="badge-outline badge-primary badge badge-md">
 								{tOutbound.stats.last_updated()}: {generateUpdatedAtString(updatedAt)}
 							</span>
 						{/if}
@@ -429,7 +429,7 @@
 						</select>
 					</div>
 					<button
-						class="btn-primary btn-sm btn xs:block hidden"
+						class="btn-primary btn-sm btn hidden xs:block"
 						use:melt={$confirmDialogTrigger}
 						on:m-click={() => {
 							dialogContent = {
@@ -464,7 +464,7 @@
 									type: "commit"
 								};
 							}}
-							class="text-base-content data-[highlighted]:bg-base-300 xs:hidden flex w-full items-center gap-2 px-4 py-3 text-sm font-normal leading-5"
+							class="flex w-full items-center gap-2 px-4 py-3 text-sm font-normal leading-5 text-base-content data-[highlighted]:bg-base-300 xs:hidden"
 						>
 							<FileCheck class="text-base-content/70" size={20} /><span class="text-base-content">Commit</span>
 						</div>
@@ -472,7 +472,7 @@
 							{...item}
 							use:item.action
 							on:m-click={handlePrintReceipt}
-							class="text-base-content data-[highlighted]:bg-base-300 flex w-full items-center gap-2 px-4 py-3 text-sm font-normal leading-5"
+							class="flex w-full items-center gap-2 px-4 py-3 text-sm font-normal leading-5 text-base-content data-[highlighted]:bg-base-300"
 						>
 							<Printer class="text-base-content/70" size={20} /><span class="text-base-content">Print</span>
 						</div>
@@ -480,7 +480,7 @@
 							{...item}
 							use:item.action
 							use:melt={$confirmDialogTrigger}
-							class="bg-error data-[highlighted]:bg-error/80 flex w-full items-center gap-2 px-4 py-3 text-sm font-normal leading-5"
+							class="flex w-full items-center gap-2 bg-error px-4 py-3 text-sm font-normal leading-5 data-[highlighted]:bg-error/80"
 							on:m-click={() => {
 								dialogContent = {
 									onConfirm: handleDeleteSelf,
@@ -690,8 +690,8 @@
 		<div use:melt={$editBookDialogOverlay} class="fixed inset-0 z-50 bg-black/50" transition:fade|global={{ duration: 150 }}></div>
 		<div
 			use:melt={$editBookDialogContent}
-			class="divide-y-secondary bg-base-200 fixed right-0 top-0 z-50 flex h-full w-full max-w-xl flex-col gap-y-4 divide-y
-			overflow-y-auto shadow-lg focus:outline-none"
+			class="divide-y-secondary fixed right-0 top-0 z-50 flex h-full w-full max-w-xl flex-col gap-y-4 divide-y overflow-y-auto
+			bg-base-200 shadow-lg focus:outline-none"
 			in:fly|global={{
 				x: 350,
 				duration: 300,
@@ -702,7 +702,7 @@
 				duration: 100
 			}}
 		>
-			<div class="bg-base-200 flex w-full flex-row justify-between p-6">
+			<div class="flex w-full flex-row justify-between bg-base-200 p-6">
 				<div>
 					<h2 use:melt={$editBookDialogTitle} class="text-lg font-medium">{tCommon.edit_book_dialog.title()}</h2>
 					<p use:melt={$editBookDialogDescription} class="leading-normal">
@@ -751,8 +751,8 @@
 		<div use:melt={$customItemDialogOverlay} class="fixed inset-0 z-50 bg-black/50" transition:fade|global={{ duration: 150 }}></div>
 		<div
 			use:melt={$customItemDialogContent}
-			class="divide-y-secondary bg-base-200 fixed right-0 top-0 z-50 flex h-full w-full max-w-xl flex-col gap-y-4 divide-y
-			overflow-y-auto shadow-lg focus:outline-none"
+			class="divide-y-secondary fixed right-0 top-0 z-50 flex h-full w-full max-w-xl flex-col gap-y-4 divide-y overflow-y-auto
+			bg-base-200 shadow-lg focus:outline-none"
 			in:fly|global={{
 				x: 350,
 				duration: 300,
@@ -763,7 +763,7 @@
 				duration: 100
 			}}
 		>
-			<div class="bg-base-200 flex w-full flex-row justify-between p-6">
+			<div class="flex w-full flex-row justify-between bg-base-200 p-6">
 				<div>
 					<h2 use:melt={$customItemDialogTitle} class="text-lg font-medium">
 						{customItemFormData ? tCommon.edit_custom_item_dialog.title() : tCommon.create_custom_item_dialog.title()}
