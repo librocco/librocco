f3b758fcddb9fe50f1693850858e9a469fe7a0a4
fadwa <fadwa.mahmoud@gmail.com>
2025-04-22 12:26:25 +0200

    Navigation to  outbound fix

--- apps/e2e/integration/constants.ts
+++ apps/e2e/integration/constants.ts
@@ -1,1 +1,1 @@
-export const baseURL = process.env.CI ? "http://localhost:4173/preview/" : "http://localhost:4173/preview/";
+export const baseURL = process.env.CI ? "http://localhost:4173/preview/" : "http://localhost:5173/preview/";
