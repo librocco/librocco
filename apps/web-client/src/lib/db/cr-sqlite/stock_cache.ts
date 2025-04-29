dc71ba15894149620088ecbdf002f8dd238e4ef2
ikusteu <ivankust57@gmail.com>
2025-04-29 22:09:47 +0200

    create a stock cache functionality: * internal store keeps the current query (a Promise) * two stores: stockByWarehouse and warehouseTotals are derived from it (both Promises) * expose the 'invalidate' function * implement enable/disable functionality - allowing for skipping of requerying if not needed by the current view

---
+++ apps/web-client/src/lib/db/cr-sqlite/stock_cache.ts
@@ -0,0 +1,70 @@
+import { writable, get, derived } from "svelte/store";
+
+import type { DB, GetStockResponseItem } from "$lib/db/cr-sqlite/types";
+
+import { getStock } from "$lib/db/cr-sqlite/stock";
+import { reduce, wrapIter } from "@librocco/shared";
+
+const dbStore = writable<DB | null>(null);
+const valid = writable(false);
+
+/**
+ * Executes the stock query and sets the cache as valid (upon resolution)
+ */
+const execQuery = async (db: DB) => {
+	const stock = await getStock(db);
+	valid.set(true);
+	return stock;
+};
+
+/** An internal store keeping the full stock query as a promise */
+const query = writable<Promise<GetStockResponseItem[]>>(new Promise(() => {}));
+
+/**
+ * A store derived from cached stock query:
+ * - it contains a promise which resolves to a Map { warehouseId => Iterable<GetStockResponseItem> }
+ * - having a promise allows us to use Svelte's async await block
+ * - being a store, it automatically updates when the cache is invalidated
+ */
+export const stockByWarehouse = derived(query, ($query) =>
+	$query.then((stock) => wrapIter(stock)._groupIntoMap((item) => [item.warehouseId, item]))
+);
+
+/**
+ * A store derived from cached stock query:
+ * - it contains a promise which resolves to a Map { warehouseId => number }
+ * - having a promise allows us to use Svelte's async await block
+ * - being a store, it automatically updates when the cache is invalidated
+ */
+export const warehouseTotals = derived(stockByWarehouse, ($stockByWarehouse) =>
+	// Wait for the stock by warehouse (effectively stock query)
+	$stockByWarehouse.then(
+		(stock) =>
+			// Reduce the quantities of items for each warehouse and create a Map { warehouseId => totalQuantiy }
+			new Map(wrapIter(stock).map(([warehouseId, items]) => [warehouseId, reduce(items, (acc, { quantity }) => acc + quantity, 0)]))
+	)
+);
+
+export const enable = (db: DB) => {
+	// Set the DB -- effectively enabling the cache
+	dbStore.set(db);
+
+	// If cache invalidated while not active, rerun the query
+	if (!get(valid)) {
+		query.set(execQuery(db));
+	}
+};
+
+export const disable = () => dbStore.set(null);
+
+export const invalidate = () => {
+	// Invalidate the cache
+	valid.set(false);
+
+	const db = get(dbStore);
+
+	// If currently active, rerun the query
+	if (db) {
+		query.set(execQuery(db));
+	}
+};
