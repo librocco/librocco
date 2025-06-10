86caada018cf823bd8c9d01df407c2fc66e5a5a5
fadwa <fadwa.mahmoud@gmail.com>
2025-06-11 02:06:20 +0300

    Run prettier

--- apps/web-client/src/routes/orders/suppliers/reconcile/[id]/+page.svelte
+++ apps/web-client/src/routes/orders/suppliers/reconcile/[id]/+page.svelte
@@ -151,14 +151,14 @@
 						<div class="flex flex-row items-center justify-between gap-y-2 md:flex-col md:items-start">
 							<h2 class="prose">#{data?.reconciliationOrder.id}</h2>
 
-							<span class="badge-outline badge-accent badge badge-md gap-x-2 py-2.5">
+							<span class="badge-accent badge-outline badge badge-md gap-x-2 py-2.5">
 								<span class="sr-only">{t.stats.created()}</span>
 								<ClockArrowUp size={16} aria-hidden />
 								<time dateTime={new Date(data?.reconciliationOrder.created).toISOString()}
 									>{new Date(data?.reconciliationOrder.created).toLocaleString()}</time
 								>
 							</span>
-							<span class="badge-outline badge-accent badge badge-md gap-x-2 py-2.5">
+							<span class="badge-accent badge-outline badge badge-md gap-x-2 py-2.5">
 								<span class="sr-only">{t.stats.last_updated()}</span>
 								<ClockArrowUp size={16} aria-hidden />
 								<time dateTime={new Date(data?.reconciliationOrder.updatedAt).toISOString()}
@@ -166,7 +166,7 @@
 								>
 							</span>
 							{#if data?.reconciliationOrder.finalized}
-								<span class="badge-outline badge-accent badge badge-md gap-x-2 py-2.5">
+								<span class="badge-accent badge-outline badge badge-md gap-x-2 py-2.5">
 									<span class="sr-only">Finalized At</span>
 									<ClockArrowUp size={16} aria-hidden />
 									<time dateTime={new Date(data?.reconciliationOrder.updatedAt).toISOString()}
@@ -182,7 +182,7 @@
 							<dt class="mt-0">{t.stats.includes_supplier_orders()}:</dt>
 							<div class="flex flex-wrap gap-x-4 md:flex-col">
 								{#each placedOrders as [order_id, supplier_name]}
-									<dd class="badge-outline badge-accent badge badge-md gap-x-2">
+									<dd class="badge-accent badge-outline badge badge-md gap-x-2">
 										#{order_id}
 										<span class="text-sm font-light">({supplier_name})</span>
 									</dd>
