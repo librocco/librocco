065ba3aaddc8655767f3ac5007871d8f4efb4899
ikusteu <ivankust57@gmail.com>
2025-06-15 16:23:54 +0200

    opfs test: create local DB flow using opfs:
    * create /debug/opfs - a page to play around with the opfs vfs SQLite
    * create a small DB wrapper, exposing the minimal (`exec`, `execA`, `execO`) functionality similar to crsql-wasm

---
+++ apps/web-client/src/routes/debug/opfs/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { initDB } from "./db";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		const wasmUrlBase = `http://${window.location.host}/preview/`;
+		const wasmUrl = new URL("wa-sqlite-async.wasm", wasmUrlBase).href;
+
+		window["initDB"] = (dbid: string, kind: string) => initDB(dbid, kind, () => wasmUrl);
+	});
+</script>
+
+<h1 class="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 text-2xl">Use the console</h1>
