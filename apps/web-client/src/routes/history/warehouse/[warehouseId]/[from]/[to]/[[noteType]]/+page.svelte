e83066737e394faf1d54c715ecce71ab187d911d
fadwa <fadwa.mahmoud@gmail.com>
2025-06-17 01:28:21 +0300

    History by note and warehouse printable list

--- apps/web-client/src/routes/history/warehouse/[warehouseId]/[from]/[to]/[[noteType]]/+page.svelte
+++ apps/web-client/src/routes/history/warehouse/[warehouseId]/[from]/[to]/[[noteType]]/+page.svelte
@@ -78,6 +78,10 @@
 	};
 	// #endregion date picker
 
+	const handlePrint = () => {
+		window.print();
+	};
+
 	// #region dropdown
 	const options = [
 		{
@@ -124,13 +128,15 @@
 
 <HistoryPage view="history/date" {db} {plugins}>
 	<div slot="main" class="h-full w-full">
-		<div class="flex w-full flex-wrap justify-between gap-y-4 xl:flex-nowrap">
+		<div id="container" class="flex w-full flex-wrap justify-between gap-y-4 xl:flex-nowrap">
 			<h1 class="order-1 whitespace-nowrap text-2xl font-bold leading-7 text-base-content">
 				{displayName || ""}
 				{t.heading.history()}
 			</h1>
 
 			<button on:click={handleExportCsv} class="btn-primary btn order-2 whitespace-nowrap xl:order-3">{t.heading.export_csv()}</button>
+
+			<button on:click={handlePrint} class="btn-primary btn order-2 whitespace-nowrap xl:order-3">Print Table</button>
 
 			<div class="order-3 w-full items-center gap-3 md:flex xl:order-2 xl:justify-center">
 				<p>{t.heading.from()}:</p>
@@ -225,3 +231,11 @@
 		<!-- End entity list contaier -->
 	</div>
 </HistoryPage>
+
+<style>
+	@media print {
+		#container {
+			display: none;
+		}
+	}
+</style>
