63265dcfd15b14e8e9ad2ee1c95102867a5fb73f
ikusteu <ivankust57@gmail.com>
2025-06-17 14:59:17 +0200

    update the 'build-wasm' script to provide CRSQLITE_COMMIT_SHA (a dummy value required by cr-sqlite build process)

--- 3rd-party/crsqlite-wasm/scripts/build-wasm.sh
+++ 3rd-party/crsqlite-wasm/scripts/build-wasm.sh
@@ -2,6 +2,12 @@
 
 mkdir -p dist
 
+# This env variable is required by the crsql build process
+# (as it's, and I quote, "only meant to be built as a submodule of @vlcn.io/js")
+#
+# We're providing a dummy value here (this has nothing to do with the underlaying build, but here we are)
+export CRSQLITE_COMMIT_SHA="$(git rev-parse HEAD)"
+
 cd ../emsdk
 ./emsdk install 3.1.45
 ./emsdk activate 3.1.45
