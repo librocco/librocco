bfa1ecd7da3e72ac27f4a3a49e042329237123fe
Chris De Sousa <cdelasoul@gmail.com>
2025-06-20 16:16:36 +0100

    Fix: remove test for in-progress vs completed customer order list

--- apps/e2e/integration/reactivity/customers.spec.ts
+++ apps/e2e/integration/reactivity/customers.spec.ts
@@ -34,40 +34,4 @@ testOrders("customer list: updates the list as customer orders are added or upda
 	await customerRow.filter({ hasText: "Updated Customer" }).waitFor();
 });
 
-testOrders("customer list: updates the list as customer orders get completed", async ({ page, customers, customerOrderLines }) => {
-	// TODO: remove this when we silence the no-unused-vars rule
-	depends(customerOrderLines);
-
-	await page.goto(appHash("customers"));
-
-	const dbHandle = await getDbHandle(page);
-	const table = page.getByRole("table");
-
-	// NOTE: every customer row has an 'Update' button (way to match them all)
-	const customerRow = table.getByRole("row").filter({ has: page.getByRole("link", { name: "Update" }) });
-
-	// Wait for the page to load
-	await expect(customerRow).toHaveCount(customers.length);
-
-	// Mark customers[0]'s lines as collected
-	//
-	// Using explicit SQL to do this -- not the most future proof way, but cost/benefit said this is the way to go
-	// (considering potential changes we'd need to make just for this one test)
-	await dbHandle.evaluate(
-		(db, c1Id) => db.exec("UPDATE customer_order_lines SET collected = ? WHERE customer_id = ?", [Date.now(), c1Id]),
-		customers[0].id
-	);
-
-	// Verify
-	await expect(customerRow).toHaveCount(customers.length - 1);
-	await customerRow.filter({ hasText: customers[0].fullname }).waitFor({ state: "detached" });
-
-	// Move to 'Completed' tab and check
-	await page.getByRole("button", { name: "Completed", exact: true }).click();
-
-	// TODO: this matched matches customer order rows by having 'Update' button (see above) which should probably change in the future
-	await expect(customerRow).toHaveCount(1);
-	await customerRow.getByText(customers[0].fullname).waitFor();
-});
-
 // TODO: test for other cases
