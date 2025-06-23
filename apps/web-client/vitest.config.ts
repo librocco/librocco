7cc16dd7904590a6d6476f76a7669b48a4b5fa10
ikusteu <ivankust57@gmail.com>
2025-06-23 19:25:44 +0200

    update vlcn package resolution:
    * if in dev mode (dev server) or testing locally -- alias the vlcn related imports to submoduled paths
    * if building for production (that includes build:e2e), or testing in CI, use the (installed) built artefacts (no aliasing)

--- apps/web-client/vitest.config.ts
+++ apps/web-client/vitest.config.ts
@@ -1,6 +1,39 @@
 import path from "path";
 import { defineConfig } from "vitest/config";
 
+/**
+ * A helper used to provide aliases for vlcn.io packages in dev mode:
+ * - if in local test mode, we're aliasing vlcn.io imports to submoduled packages (under '3rd-party/js')
+ * - if in CI, no aliases are provided: installed dependencies built from tarballs ('3rd-party/artefacts') are used
+ */
+function alias_vlcn_dev() {
+	console.log("\n");
+	if (process.env.CI === "true") {
+		console.log("testing in CI: using vlcn.io packages installed from '3rd-party/artefacts'");
+		console.log("\n");
+		return {};
+	}
+
+	const aliases = {
+		"@vlcn.io/crsqlite": path.resolve(__dirname, "../../3rd-party/js/deps/cr-sqlite/core/nodejs-helper.js"),
+		"@vlcn.io/crsqlite-wasm": path.resolve(__dirname, "../../3rd-party/js/packages/crsqlite-wasm/dist"),
+		"@vlcn.io/rx-tbl": path.resolve(__dirname, "../../3rd-party/js/packages/rx-tbl/dist"),
+		"@vlcn.io/ws-browserdb": path.resolve(__dirname, "../../3rd-party/js/packages/ws-browserdb/dist"),
+		"@vlcn.io/ws-client/worker.js": path.resolve(__dirname, "../../3rd-party/js/packages/ws-client/dist/worker/worker.js"),
+		"@vlcn.io/ws-client": path.resolve(__dirname, "../../3rd-party/js/packages/ws-client/dist"),
+		"@vlcn.io/ws-common": path.resolve(__dirname, "../../3rd-party/js/packages/ws-common/dist"),
+		"@vlcn.io/ws-server": path.resolve(__dirname, "../../3rd-party/js/packages/ws-server/dist")
+	};
+
+	console.log("local test mode: aliasing vlcn.io packages to respective submodules:");
+	for (const [pkg, path] of Object.entries(aliases)) {
+		console.log(`  ${pkg} -> ${path}`);
+	}
+	console.log("\n");
+
+	return aliases;
+}
+
 export default defineConfig({
 	test: {
 		include: ["src/lib/db/cr-sqlite/__tests__/**/*.{test,spec}.{js,ts}", "src/lib/db/__tests__/**/*.{test,spec}.{js,ts}"],
@@ -10,7 +43,8 @@ export default defineConfig({
 			name: "chromium"
 		},
 		alias: {
-			$lib: path.resolve(__dirname, "src/lib")
+			$lib: path.resolve(__dirname, "src/lib"),
+			...alias_vlcn_dev()
 		}
 	},
 	server: {
