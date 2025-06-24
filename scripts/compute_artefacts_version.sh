c30a5f7af99f9cbc72f612e6f4fc8abdabd75b3f
Silvio Tomatis <silviot@gmail.com>
2025-06-24 21:50:48 +0200

    Revert commit to js submodule

--- scripts/compute_artefacts_version.sh
+++ scripts/compute_artefacts_version.sh
@@ -2,8 +2,10 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd $SCRIPT_DIR/..
-echo built crsqlite-wasm hash: $(git log -1 --format=%H 3rd-party/artefacts/vlcn.io-crsqlite-wasm-0.16.0.tgz)
+echo Note that this script will happily misreport on a dirty repo:
+echo "don't commit after running on a dirty repo"
 echo js repo hash: $(git -C 3rd-party/js log -1 --format=%H)
+echo typed-sql repo hash: $(git -C 3rd-party/typed-sql log -1 --format=%H)
 
-echo built crsqlite-wasm hash: $(git log -1 --format=%H 3rd-party/artefacts/vlcn.io-crsqlite-wasm-0.16.0.tgz) > 3rd-party/artefacts/version.txt
-echo js repo hash: $(git -C 3rd-party/js log -1 --format=%H) >> 3rd-party/artefacts/version.txt
+echo js repo hash: $(git -C 3rd-party/js log -1 --format=%H) > 3rd-party/artefacts/version.txt
+echo typed-sql repo hash: $(git -C 3rd-party/typed-sql log -1 --format=%H) >> 3rd-party/artefacts/version.txt
