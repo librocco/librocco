ec13d972168d56a95605f118dac42eede7ca1992
Chris De Sousa <cdelasoul@gmail.com>
2025-07-02 10:49:36 +0100

    Fix: other tests affected by customer order view strings updates

--- apps/e2e/integration/reactivity/customers.spec.ts
+++ apps/e2e/integration/reactivity/customers.spec.ts
@@ -11,23 +11,26 @@ testOrders("customer list: updates the list as customer orders are added or upda
 	const dbHandle = await getDbHandle(page);
 	const table = page.getByRole("table");
 
-	// NOTE: every customer row has an 'Update' button (way to match them all)
-	const customerRow = table.getByRole("row").filter({ has: page.getByRole("link", { name: "Update" }) });
+	// Get every row in the table: customer rows + head
+	const baseRowCount = customers.length + 1;
+
+	const customerRow = table.getByRole("row");
 
 	// Wait for the page to load
-	await expect(customerRow).toHaveCount(customers.length);
+	await expect(customerRow).toHaveCount(baseRowCount);
 
 	// Add one more customer order
 	const newCustomer = { id: 4, displayId: "4", fullname: "New Customer", email: "cus@email.com" };
 	await dbHandle.evaluate(upsertCustomer, newCustomer);
 
 	// Check for new customer
-	await expect(customerRow).toHaveCount(customers.length + 1);
+	await expect(customerRow).toHaveCount(baseRowCount + 1);
 
 	// Validate the row
 	const newCustomerRow = customerRow.filter({ hasText: newCustomer.fullname });
 	await newCustomerRow.getByText(newCustomer.email).waitFor();
-	await newCustomerRow.getByText(newCustomer.displayId).waitFor();
+	// Ids are prefixed with `#<id>`
+	await newCustomerRow.getByText(`#${newCustomer.displayId}`).waitFor();
 
 	// Update the row
 	await dbHandle.evaluate(upsertCustomer, { ...newCustomer, fullname: "Updated Customer" });
