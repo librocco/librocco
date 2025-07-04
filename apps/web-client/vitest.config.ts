ae6cab8c5d0f7a19bd2b8ed4b4e266b7a29cc057
Silvio Tomatis <silviot@gmail.com>
2025-07-04 17:38:50 +0200

    Load translations from weblate

--- apps/web-client/vitest.config.ts
+++ apps/web-client/vitest.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		include: ["src/lib/db/cr-sqlite/__tests__/**/*.{test,spec}.{js,ts}", "src/lib/db/__tests__/**/*.{test,spec}.{js,ts}"],
+		include: [
+			"src/lib/db/cr-sqlite/__tests__/**/*.{test,spec}.{js,ts}",
+			"src/lib/db/__tests__/**/*.{test,spec}.{js,ts}",
+			"src/lib/**/*.spec.ts"
+		],
 		browser: {
 			enabled: true,
 			provider: "playwright",
