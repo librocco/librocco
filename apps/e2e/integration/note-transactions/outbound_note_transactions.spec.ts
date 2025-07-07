9db1336990ab2f2e66b1ebf3930ea113d9069343
fadwa <fadwa.mahmoud@gmail.com>
2025-07-08 00:22:41 +0300

    Lint

--- apps/e2e/integration/note-transactions/outbound_note_transactions.spec.ts
+++ apps/e2e/integration/note-transactions/outbound_note_transactions.spec.ts
@@ -498,7 +498,9 @@ test("should check validity of the transactions and commit the note on 'commit' 
 	await entries.row(0).field("warehouseName").click();
 	await page.getByText("Force Withdrawal").waitFor();
 
-	await page.getByTestId("dropdown-menu").getByRole("button", { name: "Force Withdrawal" }).click();
+	const forceWithdrawalDropdown = page.getByTestId("dropdown-menu");
+	await forceWithdrawalDropdown.waitFor();
+	await forceWithdrawalDropdown.getByRole("button", { name: "Force Withdrawal" }).click();
 	const forceWithdrawalDialog = page.getByRole("dialog");
 	await forceWithdrawalDialog.locator("#warehouse-force-withdrawal").selectOption({ label: "Warehouse 2" });
 	await forceWithdrawalDialog.getByRole("button", { name: "Confirm" }).click();
