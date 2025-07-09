08a3c80477498ef99dfb1c2b19c5bc5a1d7ab4d4
Chris De Sousa <cdelasoul@gmail.com>
2025-07-09 15:33:51 +0100

    Touchup supplier list view  style and strings
    - Remove SupplierOrder table and setup directly on the view
    - Remove unused Delete button

--- apps/web-client/src/lib/components/Tables/OrderTables/index.ts
+++ apps/web-client/src/lib/components/Tables/OrderTables/index.ts
@@ -1,4 +1,3 @@
 export { default as CustomerOrderTable } from "./CustomerOrderTable.svelte";
 export { default as SupplierOrderTable } from "./SupplierOrderTable.svelte";
-export { default as SupplierTable } from "./SupplierTable.svelte";
 export { default as OrderLineTable } from "./OrderLineTable.svelte";
