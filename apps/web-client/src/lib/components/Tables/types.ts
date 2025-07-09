cf62005cc5ff9319d5d78b104365bc944a18de9a
Chris De Sousa <cdelasoul@gmail.com>
2025-07-09 21:15:34 +0100

    Chore: remove unused OrderTable components

--- apps/web-client/src/lib/components/Tables/types.ts
+++ apps/web-client/src/lib/components/Tables/types.ts
@@ -1,19 +1,6 @@
 import type { NoteCustomItem, NoteEntriesItem } from "$lib/db/cr-sqlite/types";
 import type { VolumeStockKind } from "@librocco/shared";
 
-/**
- * TODO: Temmp interface to be replaced with DB Customer/Supplier Order interface
- */
-export interface OrderData {
-	name: string;
-	email: string;
-	id: string;
-	pending: boolean;
-	lastUpdated: string;
-	actionLink: string;
-}
-
-// TODO: add availableWarehouses
 type InventoryTableDataBook = NoteEntriesItem & {
 	__kind?: "book";
 	availableWarehouses?: Map<number, { displayName: string; quantity: number }>;
@@ -25,16 +12,3 @@ type InventoryTableDataCustomItem = NoteCustomItem & {
 export type InventoryTableData<K extends VolumeStockKind = VolumeStockKind> = K extends "book"
 	? InventoryTableDataBook
 	: InventoryTableDataCustomItem;
-
-// TODO probably no longer necessary
-export type BookCoreRowData = {
-	isbn: string;
-	title: string;
-	price: number;
-	quantity: number;
-	warehouseId: number;
-	warehouseName: string;
-	warehouseDiscount: number;
-	authors?: string;
-	year?: string;
-};
