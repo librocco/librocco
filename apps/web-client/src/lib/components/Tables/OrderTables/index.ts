752eeec4d737a3179564bb1ccf3e4968c97d3b91
Chris De Sousa <cdelasoul@gmail.com>
2025-07-09 21:15:34 +0100

    Chore: remove unused OrderTable components

--- apps/web-client/src/lib/components/Tables/OrderTables/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CustomerOrderTable } from "./CustomerOrderTable.svelte";
-export { default as SupplierOrderTable } from "./SupplierOrderTable.svelte";
-export { default as OrderLineTable } from "./OrderLineTable.svelte";
