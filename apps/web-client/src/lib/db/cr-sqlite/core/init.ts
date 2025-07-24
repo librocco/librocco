c59fc3cd7089d64fbc7b2a146810cd189aa258c2
ikusteu <ivankust57@gmail.com>
2025-07-24 21:27:56 +0200

    vfs selection initial setup:
    * support idb-batch-atomic and opfs-any-context for now
    * the initialisation currently uses a baked-in constant (DEFAULT_VFS)

--- apps/web-client/src/lib/db/cr-sqlite/core/init.ts
+++ apps/web-client/src/lib/db/cr-sqlite/core/init.ts
@@ -2,12 +2,15 @@ import initWasm from "@vlcn.io/crsqlite-wasm";
 import wasmUrl from "@vlcn.io/crsqlite-wasm/crsqlite.wasm?url";
 
 import type { DBAsync } from "./types";
+
+import { DEFAULT_VFS } from "$lib/constants";
+
 import { createVFSFactory } from "./vfs";
 
 export async function getCrsqliteDB(dbname: string): Promise<DBAsync> {
 	const sqlite = await initWasm({
 		locateWasm: () => wasmUrl,
-		vfsFactory: createVFSFactory()
+		vfsFactory: createVFSFactory(DEFAULT_VFS)
 	});
 	return sqlite.open(dbname);
 }
--- apps/web-client/src/lib/db/cr-sqlite/core/vfs.ts
+++ apps/web-client/src/lib/db/cr-sqlite/core/vfs.ts
@@ -1,6 +1,23 @@
-export function createVFSFactory(): (module: SQLiteAPI) => Promise<SQLiteVFS> {
-	return async (module: SQLiteAPI) => {
-		const { IDBBatchAtomicVFS: VFS } = await import("@vlcn.io/wa-sqlite/src/examples/IDBBatchAtomicVFS.js");
-		return VFS.create("idb-batch-atomic", module) as SQLiteVFS;
-	};
+const idbBatchAtomic = async (module: SQLiteAPI): Promise<SQLiteVFS> => {
+	const { IDBBatchAtomicVFS } = await import("@vlcn.io/wa-sqlite/src/examples/IDBBatchAtomicVFS.js");
+	const vfs = IDBBatchAtomicVFS.create("idb-batch-atomic", module);
+	return vfs;
+};
+
+const opfsAnyContext = async (module: SQLiteAPI): Promise<SQLiteVFS> => {
+	const { OPFSAnyContextVFS } = await import("@vlcn.io/wa-sqlite/src/examples/OPFSAnyContextVFS.js");
+	const vfs = await OPFSAnyContextVFS.create("opfs-any-context", module);
+	return vfs;
+};
+
+const vfsLookup = {
+	"idb-batch-atomic": idbBatchAtomic,
+	"opfs-any-context": opfsAnyContext
+};
+
+export function createVFSFactory(vfs: keyof typeof vfsLookup): (module: SQLiteAPI) => Promise<SQLiteVFS> {
+	if (!(vfs in vfsLookup)) {
+		throw new Error("unknown vfs: " + vfs);
+	}
+	return vfsLookup[vfs];
 }
