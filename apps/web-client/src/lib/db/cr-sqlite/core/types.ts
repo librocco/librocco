e194bb6248e75bd4d0c01e611a9a0a1211b4374d
ikusteu <ivankust57@gmail.com>
2025-07-29 23:02:39 +0200

    implement worker-db functionality:
    * implement the 'getWorkerDB' functionality (with wrapped worker DB interface on client side)
    * implement the db worker (with wrapped DB safe for Comlink usage)

--- apps/web-client/src/lib/db/cr-sqlite/core/types.ts
+++ apps/web-client/src/lib/db/cr-sqlite/core/types.ts
@@ -1,1 +1,7 @@
+import type { DBAsync } from "@vlcn.io/xplat-api";
+export type { DBAsync, TXAsync, StmtAsync, TMutex, UpdateType } from "@vlcn.io/xplat-api";
+
 export type { VFSWhitelist } from "./vfs";
+
+export type TXCallback = Parameters<DBAsync["tx"]>[0];
+export type OnUpdateCallback = Parameters<DBAsync["onUpdate"]>[0];
