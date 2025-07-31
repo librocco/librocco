062febe9c8e2c868f4ace70691ee5ce1659f93ef
ikusteu <ivankust57@gmail.com>
2025-07-31 21:08:16 +0200

    add another hop to dynamic import of VFS module

--- apps/web-client/src/lib/db/cr-sqlite/core/init.ts
+++ apps/web-client/src/lib/db/cr-sqlite/core/init.ts
@@ -1,9 +1,9 @@
 import initWasm, { type DB } from "@vlcn.io/crsqlite-wasm";
 import wasmUrl from "@vlcn.io/crsqlite-wasm/crsqlite.wasm?url";
 
-import { createVFSFactory } from "./vfs";
 
 export async function getCrsqliteDB(dbname: string): Promise<DB> {
+	const { createVFSFactory } = await import("./vfs");
 	const sqlite = await initWasm({
 		locateWasm: () => wasmUrl,
 		vfsFactory: createVFSFactory()
--- apps/web-client/src/lib/db/cr-sqlite/core/vfs.ts
+++ apps/web-client/src/lib/db/cr-sqlite/core/vfs.ts
@@ -1,7 +1,7 @@
 export function createVFSFactory(): (module: SQLiteAPI) => Promise<SQLiteVFS> {
 	return async (module: SQLiteAPI) => {
 		const { IDBBatchAtomicVFS: VFS } = await import("@vlcn.io/wa-sqlite/src/examples/IDBBatchAtomicVFS.js")
-		return VFS.create(vfs, module) as SQLiteVFS;
+		return VFS.create("idb-batch-atomic", module) as SQLiteVFS;
 	};
 }
 
