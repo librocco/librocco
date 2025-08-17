1ec349569d9355e2756d3f3bbd2b996f40dd5370
fadwa <fadwa.mahmoud@gmail.com>
2025-08-18 01:17:53 +0300

    Lint

--- apps/e2e/integration/reconciliation_order.spec.ts
+++ apps/e2e/integration/reconciliation_order.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "@playwright/test";
 
-import { appHash, baseURL } from "@/constants";
+import { appHash } from "@/constants";
 import { depends, testOrders } from "@/helpers/fixtures";
 
 testOrders("create: single order: on row button click", async ({ page, supplierOrders }) => {
