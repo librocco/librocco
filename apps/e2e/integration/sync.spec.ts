65cb7db1ec716ee965197edf9045280a4dc3947c
ikusteu <ivankust57@gmail.com>
2025-08-29 20:05:06 +0200

    add sync e2e tests:
    * add 'getRemoteDbHandle' function to e2e package (for interaction with remote -- sync server -- db in the same way as with the local one)
    * add 'getAllWarehouses' (currently not used for tests due to db.tx usage -- not implemented for remote db -- as e.g., upsertWarehouse needs it)
    * add 'getCustomerOrderList' (currently used for sync test -- not ideal as we can't test for deletion)
    * ad 'testBase' add additional setup: initialise the DB with random name (to avoid conflicts in sync tests...not much use for the rest)

---
+++ apps/e2e/integration/sync.spec.ts
@@ -0,0 +1,72 @@
+import { expect } from "@playwright/test";
+
+import { appHash, baseURL, remoteDbURL, syncUrl } from "@/constants";
+
+import { testOrders } from "@/helpers/fixtures";
+import { getDbHandle } from "@/helpers";
+
+import { getCustomerOrderList, getRemoteDbHandle, upsertCustomer } from "@/helpers/cr-sqlite";
+
+// NOTE: using customer list for sync test...we could also test for other cases, but if sync is working here (and reactivity is there -- different tests)
+// the sync should work for other cases all the same
+testOrders("should delete the warehouse on delete button click (after confirming the prompt)", async ({ page, customers }) => {
+	// Start the sync
+	await page.evaluate(
+		([syncUrl]) => {
+			// NOTE: the surrounding double quotes need to be part of the string as the svelte-persisted reads (and stores) the
+			// values as JSON objects (and the string including quotes is a valid JSON value)
+			window.localStorage.setItem("librocco-sync-url", `"${syncUrl}"`);
+			window.localStorage.setItem("librocco-sync-active", "true");
+		},
+		[syncUrl]
+	);
+	await page.reload();
+	await page.goto(baseURL);
+
+	await page.goto(appHash("customers"));
+
+	const table = page.getByRole("table");
+
+	// Get every row in the table: customer rows + head
+	const baseRowCount = customers.length + 1;
+
+	const customerRow = table.getByRole("row");
+
+	// Wait for the page to load
+	await expect(customerRow).toHaveCount(baseRowCount);
+
+	const dbHandle = await getDbHandle(page);
+	const remoteDbHandle = await getRemoteDbHandle(page, remoteDbURL);
+
+	// Create
+	//
+	// UPDATE A
+	await dbHandle.evaluate(upsertCustomer, { id: 4, displayId: "4", fullname: "Customer 4", email: "cus4@email.com" });
+	// UPDATE B
+	await remoteDbHandle.evaluate(upsertCustomer, { id: 5, displayId: "5", fullname: "Customer 5", email: "cus5@email.com" });
+	// Wait for UPDATE B
+	await customerRow.filter({ hasText: "Customer 4" }).getByText("cus4@email.com").waitFor();
+	await customerRow.filter({ hasText: "Customer 5" }).getByText("cus5@email.com").waitFor();
+	// Check for UPDATE A in remote
+	let remoteCustomers = await remoteDbHandle.evaluate(getCustomerOrderList);
+	expect(remoteCustomers[3].id).toEqual(4);
+	expect(remoteCustomers[3].fullname).toEqual("Customer 4");
+	expect(remoteCustomers[4].id).toEqual(5);
+	expect(remoteCustomers[4].fullname).toEqual("Customer 5");
+
+	// Update
+	//
+	// UPDATE A
+	await dbHandle.evaluate(upsertCustomer, { id: 5, displayId: "5", fullname: "Customer 5 - updated locally" });
+	// UPDATE B
+	await remoteDbHandle.evaluate(upsertCustomer, { id: 4, displayId: "4", fullname: "Customer 4 - updated remotely" });
+	// Wait for UPDATE B
+	await customerRow.filter({ hasText: "Customer 4 - updated remotely" }).getByText("cus4@email.com").waitFor();
+	await customerRow.filter({ hasText: "Customer 5 - updated locally" }).getByText("cus5@email.com").waitFor();
+	// Check for UPDATE A in remote
+	remoteCustomers = await remoteDbHandle.evaluate(getCustomerOrderList);
+	expect(remoteCustomers[3].id).toEqual(4);
+	expect(remoteCustomers[3].fullname).toEqual("Customer 4 - updated remotely");
+	expect(remoteCustomers[4].id).toEqual(5);
+	expect(remoteCustomers[4].fullname).toEqual("Customer 5 - updated locally");
+});
