b3140d53e76e434dd9282e189b607ac869f58879
ikusteu <ivankust57@gmail.com>
2025-10-14 14:41:12 +0200

    setup error debug flow:
    * create 'clientError' (global) store -- holding errors encountered client-side
    * add 'svelte:window on:error' handler (in root +layout.svelte) - to catch client side (post load) errors
    * when client error caught: (1) set it to 'clientError' store, (2) navigate (imperatively) to +error.svelte page
    * in '+error.svelte': check if error came from the client ('clientError' store) and use it, if not, use the 'page.error' (load time error)
    * in '+error.svelte': display only the error "header" (first line(s), filtering out stack trace within 'error.message')
    * add buttons, in '/debug' page, that trigger: load error, page render error, runtime error (for testing of different err handling)

--- apps/web-client/src/routes/+error.svelte
+++ apps/web-client/src/routes/+error.svelte
@@ -1,13 +1,42 @@
 <!-- src/routes/+error.svelte -->
-<script>
-	export let error;
-	export let status;
+<script lang="ts">
+	import { get } from "svelte/store";
+	import { page } from "$app/state";
+
+	import { clientError } from "$lib/stores/errors";
+
+	export let source;
+	export let message;
+
+	if (get(clientError)) {
+		source = "client side error";
+		const err = get(clientError);
+		message = err.message;
+
+		// Clear the error after displaying it
+		clientError.set(null);
+	} else if (page.error) {
+		source = "load time error";
+		message = page.error.message;
+	}
+
+	function formatErrMsg(msg: string) {
+		// return msg;
+		return msg
+			.split("\n")
+			.filter(Boolean)
+			.filter((l) => !l.trim().startsWith("at "))
+			.filter((l) => !l.trim().startsWith("in "));
+	}
 </script>
 
-<div style="padding: 2rem; text-align: center;">
-	<h1>⚠️ Oops! Something went wrong.</h1>
-	<p><strong>Status:</strong> {status}</p>
-	<p><strong>Message:</strong> {error?.message}</p>
+<div class="relative h-full w-full overflow-y-auto overflow-x-hidden">
+	<div class="absolute left-1/2 top-1/2 w-[80%] -translate-x-1/2 -translate-y-1/2 p-8 text-center">
+		<h1>⚠️ Oops! Something went wrong.</h1>
 
-	<button on:click={() => location.reload()}>🔄 Reload</button>
+		<div class="w-full text-left">
+			<p class="my-8"><strong>Source:</strong> {source}</p>
+			<p class="mt-8"><strong>Message:</strong> {formatErrMsg(message)}</p>
+		</div>
+	</div>
 </div>
