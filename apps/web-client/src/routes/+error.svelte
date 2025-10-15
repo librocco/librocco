677e13b0bcc6a523f697491681143bccdc02b321
ikusteu <ivankust57@gmail.com>
2025-10-15 20:47:06 +0200

    refactor error boundary (and debug) logic:
    * create ErrorView component: displaying error message in the same way for both 'load' and 'runtime' variants
    * render ErrorView: from +error.svelte for load errors, from root +layout.svelte for runtime errors
    * ditch render error (this may be TODO)
    * don't render the sidebar when showing error page

--- apps/web-client/src/routes/+error.svelte
+++ apps/web-client/src/routes/+error.svelte
@@ -1,42 +1,8 @@
 <!-- src/routes/+error.svelte -->
 <script lang="ts">
-	import { get } from "svelte/store";
-	import { page } from "$app/state";
+	import { page } from "$app/stores";
 
-	import { clientError } from "$lib/stores/errors";
-
-	export let source;
-	export let message;
-
-	if (get(clientError)) {
-		source = "client side error";
-		const err = get(clientError);
-		message = err.message;
-
-		// Clear the error after displaying it
-		clientError.set(null);
-	} else if (page.error) {
-		source = "load time error";
-		message = page.error.message;
-	}
-
-	function formatErrMsg(msg: string) {
-		// return msg;
-		return msg
-			.split("\n")
-			.filter(Boolean)
-			.filter((l) => !l.trim().startsWith("at "))
-			.filter((l) => !l.trim().startsWith("in "));
-	}
+	import ErrorView from "$lib/components/ErrorView.svelte";
 </script>
 
-<div class="relative h-full w-full overflow-y-auto overflow-x-hidden">
-	<div class="absolute left-1/2 top-1/2 w-[80%] -translate-x-1/2 -translate-y-1/2 p-8 text-center">
-		<h1>⚠️ Oops! Something went wrong.</h1>
-
-		<div class="w-full text-left">
-			<p class="my-8"><strong>Source:</strong> {source}</p>
-			<p class="mt-8"><strong>Message:</strong> {formatErrMsg(message)}</p>
-		</div>
-	</div>
-</div>
+<ErrorView source="load" message={$page.error.message} />
