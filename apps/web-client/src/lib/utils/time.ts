5fe26564878efc51ad1a77782298729eb329dd88
Silvio Tomatis <silviot@gmail.com>
2025-11-18 13:06:53 +0100

    Retire generateUpdatedAtString and use formatters instead

--- apps/web-client/src/lib/utils/time.ts
+++
@@ -1,7 +0,0 @@
-import { timeOnly as formatTimeOnly, dateTime as formatDateTime } from "@librocco/shared/i18n-formatters";
-
-export const generateUpdatedAtString = (updatedAt?: Date | string, mode?: "time-only") => {
-	if (!updatedAt) return "";
-
-	return mode === "time-only" ? formatTimeOnly(updatedAt) : formatDateTime(updatedAt);
-};
