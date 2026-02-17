391c9a88f4a986c6637a451c666077da63ea98f3
ikusteu <ivankust57@gmail.com>
2026-02-17 20:03:50 +0100

    fix storybook: * add a global load function to set the locale - fix i18n not displaying * update SupplierCard stories to include the underdelivery policy

--- apps/web-client/.storybook/preview.ts
+++ apps/web-client/.storybook/preview.ts
@@ -1,1 +1,23 @@
 import "../src/lib/main.css";
+
+import { setLocale } from "@librocco/shared/i18n-svelte";
+import { loadLocaleAsync } from "@librocco/shared/i18n-util.async";
+import { DEFAULT_LOCALE } from "$lib/constants";
+
+// Initialize i18n globally for all stories
+// TODO: Consider updating to use decorators or a more sophisticated i18n provider pattern
+// to handle locale changes and provide better control over translation loading
+let i18nInitialized = false;
+
+export default {
+	loaders: [
+		async () => {
+			if (!i18nInitialized) {
+				await loadLocaleAsync(DEFAULT_LOCALE);
+				setLocale(DEFAULT_LOCALE);
+				i18nInitialized = true;
+			}
+			return {};
+		}
+	]
+};
