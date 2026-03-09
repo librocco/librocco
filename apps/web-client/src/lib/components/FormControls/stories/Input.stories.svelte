b16f8968e5c2799e3241362ecd0389242ca77a21
Silvio Tomatis <silviot@gmail.com>
2026-03-09 17:13:23 +0100

    style(web-client): format input story

--- apps/web-client/src/lib/components/FormControls/stories/Input.stories.svelte
+++ apps/web-client/src/lib/components/FormControls/stories/Input.stories.svelte
@@ -14,21 +14,13 @@
 </script>
 
 <Story name="Default">
-	<Input
-		name="default-placeholder"
-		label="Default with placeholder"
-		placeholder={"placeholder"}
-	>
+	<Input name="default-placeholder" label="Default with placeholder" placeholder={"placeholder"}>
 		<p slot="helpText">And some help text</p>
 	</Input>
 </Story>
 
 <Story name="Start Adornment">
-	<Input
-		name="start-adornment"
-		placeholder={"placeholderemail@gmail.com"}
-		label="Start Adornment"
-	>
+	<Input name="start-adornment" placeholder={"placeholderemail@gmail.com"} label="Start Adornment">
 		<span slot="start-adornment">
 			<Mail />
 		</span>
@@ -36,11 +28,7 @@
 </Story>
 
 <Story name="Start & End Adonrnments">
-	<Input
-		name="start-end-adornment"
-		placeholder={"placeholderemail@gmail.com"}
-		label="Start & End Adornments"
-	>
+	<Input name="start-end-adornment" placeholder={"placeholderemail@gmail.com"} label="Start & End Adornments">
 		<span slot="start-adornment">$</span>
 		<span slot="end-adornment">USD</span>
 	</Input>
