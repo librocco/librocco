cf8ae64ad5dae2857026b32f68e517655e446aff
Silvio Tomatis <silviot@gmail.com>
2026-03-10 20:10:26 +0100

    style(web-client): format SupplierCard

--- apps/web-client/src/lib/components-new/SupplierCard/SupplierCard.svelte
+++ apps/web-client/src/lib/components-new/SupplierCard/SupplierCard.svelte
@@ -14,7 +14,6 @@
 
 	const dispatch = createEventDispatcher();
 
-
 	function handleEdit() {
 		dispatch("edit");
 	}
@@ -66,7 +65,11 @@
 			<dt class="sr-only">{$LL.suppliers_page.card.underdelivery_policy()}</dt>
 			<dd class="flex flex-1 items-start gap-3 text-sm">
 				<History aria-hidden="true" class="text-muted-foreground mt-0.5 h-4 w-4 flex-shrink-0" />
-				<span class="flex-1 justify-start">{underdeliveryPolicy === 1 ? $LL.reconcile_page.step2.underdelivery.options.reorder() : $LL.reconcile_page.step2.underdelivery.options.keep_open()}</span>
+				<span class="flex-1 justify-start"
+					>{underdeliveryPolicy === 1
+						? $LL.reconcile_page.step2.underdelivery.options.reorder()
+						: $LL.reconcile_page.step2.underdelivery.options.keep_open()}</span
+				>
 			</dd>
 		</div>
 	</dl>
